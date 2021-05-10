859d5ef121b998f853fd0353c81030c4a1dde87c
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2021-05-10 18:26:16 +0200

    Capture items order for "zero/one-many" association #10 - front manual done

--- test/ng/projects/test/src/lib/bclass-sorting/bclass-sorting.component.ts
+++ test/ng/projects/test/src/lib/bclass-sorting/bclass-sorting.component.ts
@@ -9,25 +9,13 @@ import { Router, RouterState } from '@angular/router';
 import { BclassDB } from '../bclass-db'
 import { BclassService } from '../bclass.service'
 
-import { FrontRepoService, FrontRepo } from '../front-repo.service'
+import { FrontRepoService, FrontRepo, NullInt64 } from '../front-repo.service'
 @Component({
   selector: 'lib-bclass-sorting',
   templateUrl: './bclass-sorting.component.html',
   styleUrls: ['./bclass-sorting.component.css']
 })
-export class BclassSortingComponent implements OnInit  {
-
-  movies = [
-    'Episode I - The Phantom Menace',
-    'Episode II - Attack of the Clones',
-    'Episode III - Revenge of the Sith',
-    'Episode IV - A New Hope',
-    'Episode V - The Empire Strikes Back',
-    'Episode VI - Return of the Jedi',
-    'Episode VII - The Force Awakens',
-    'Episode VIII - The Last Jedi',
-    'Episode IX – The Rise of Skywalker'
-  ];
+export class BclassSortingComponent implements OnInit {
 
   frontRepo: FrontRepo
 
@@ -57,25 +45,60 @@ export class BclassSortingComponent implements OnInit  {
     this.frontRepoService.pull().subscribe(
       frontRepo => {
         this.frontRepo = frontRepo
-        console.log("front repo pull returned")
 
-        if (this.dialogData != undefined) {
-          this.frontRepo.Bclasss_array.forEach(
-            bclass => {
-              let ID = this.dialogData.ID
-              let revPointer = bclass[this.dialogData.ReversePointer]
-              if (revPointer.Int64 == ID) {
-                this.associatedBclasss.push(bclass)
-              }
+        let index = 0
+        for (let bclass of this.frontRepo.Bclasss_array) {
+          let ID = this.dialogData.ID
+          let revPointer = bclass[this.dialogData.ReversePointer]
+          if (revPointer.Int64 == ID) {
+            if (bclass.Aclass_AnarrayofbDBID_Order == undefined) {
+              bclass.Aclass_AnarrayofbDBID_Order = new NullInt64
+              bclass.Aclass_AnarrayofbDBID_Order.Valid = true
+              bclass.Aclass_AnarrayofbDBID_Order.Int64 = index++
             }
-          )
+            this.associatedBclasss.push(bclass)
+          }
         }
+
+        // sort associated bclass according to order
+        this.associatedBclasss.sort((t1, t2) => {
+          if (t1.Aclass_AnarrayofbDBID_Order.Int64 > t2.Aclass_AnarrayofbDBID_Order.Int64) {
+            return 1;
+          }
+          if (t1.Aclass_AnarrayofbDBID_Order.Int64 < t2.Aclass_AnarrayofbDBID_Order.Int64) {
+            return -1;
+          }
+          return 0;
+        });
+
+        console.log("front repo pull returned")
       }
     )
   }
 
   drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.associatedBclasss, event.previousIndex, event.currentIndex);
+
+    // set the order of Bclass instances
+    let index = 0
+    for (let bclass of this.associatedBclasss) {
+      bclass.Aclass_AnarrayofbDBID_Order.Int64 = index++
+    }
+    console.log("after drop")
   }
 
+  save() {
+
+    this.associatedBclasss.forEach(
+      bclass => {
+        this.bclassService.updateBclass(bclass)
+          .subscribe(bclass => {
+            this.bclassService.BclassServiceChanged.next("update")
+            console.log("bclass saved")
+          });
+      }
+    )
+
+    this.dialogRef.close('Sorting of Aclass.Anarrayofb done');
+  }
 }
