498e4aa21a4e56319c1193a3dd85a67e06219d3f
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2021-05-10 17:44:02 +0200

    Capture items order for "zero/one-many" association Fixes #10 - manual version for visual ordering

--- test/ng/projects/test/src/lib/bclass-sorting/bclass-sorting.component.ts
+++ test/ng/projects/test/src/lib/bclass-sorting/bclass-sorting.component.ts
@@ -1,12 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+import { Component, OnInit, Inject, Optional } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog'
+import { DialogData } from '../front-repo.service'
+import { SelectionModel } from '@angular/cdk/collections';
+
+import { Router, RouterState } from '@angular/router';
+import { BclassDB } from '../bclass-db'
+import { BclassService } from '../bclass.service'
+
+import { FrontRepoService, FrontRepo } from '../front-repo.service'
 @Component({
   selector: 'lib-bclass-sorting',
   templateUrl: './bclass-sorting.component.html',
   styleUrls: ['./bclass-sorting.component.css']
 })
-export class BclassSortingComponent {
+export class BclassSortingComponent implements OnInit  {
 
   movies = [
     'Episode I - The Phantom Menace',
@@ -20,10 +29,53 @@ export class BclassSortingComponent {
     'Episode IX – The Rise of Skywalker'
   ];
 
-  constructor() { }
+  frontRepo: FrontRepo
+
+  // array of Bclass instances that are in the association
+  associatedBclasss = new Array<BclassDB>();
+
+  constructor(
+    private bclassService: BclassService,
+    private frontRepoService: FrontRepoService,
+
+    // not null if the component is called as a selection component of bclass instances
+    public dialogRef: MatDialogRef<BclassSortingComponent>,
+    @Optional() @Inject(MAT_DIALOG_DATA) public dialogData: DialogData,
+
+    private router: Router,
+  ) {
+    this.router.routeReuseStrategy.shouldReuseRoute = function () {
+      return false;
+    };
+  }
+
+  ngOnInit(): void {
+    this.getBclasss()
+  }
+
+  getBclasss(): void {
+    this.frontRepoService.pull().subscribe(
+      frontRepo => {
+        this.frontRepo = frontRepo
+        console.log("front repo pull returned")
+
+        if (this.dialogData != undefined) {
+          this.frontRepo.Bclasss_array.forEach(
+            bclass => {
+              let ID = this.dialogData.ID
+              let revPointer = bclass[this.dialogData.ReversePointer]
+              if (revPointer.Int64 == ID) {
+                this.associatedBclasss.push(bclass)
+              }
+            }
+          )
+        }
+      }
+    )
+  }
 
   drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+    moveItemInArray(this.associatedBclasss, event.previousIndex, event.currentIndex);
   }
 
 }
