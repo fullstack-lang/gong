241034a451a2a55802b629a22cfc3197e186c7a2
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2021-05-11 11:14:58 +0200

    Capture items order for "zero/one-many" association #10 - add sorting component to map of component

--- test/ng/projects/test/src/lib/map-components.ts
+++ test/ng/projects/test/src/lib/map-components.ts
@@ -1,18 +1,14 @@
 // insertion point sub template for components imports 
-import { AclasssTableComponent } from './aclasss-table/aclasss-table.component'
-import { BclasssTableComponent } from './bclasss-table/bclasss-table.component'
-import { BclassSortingComponent } from './bclass-sorting/bclass-sorting.component'
+  import { AclasssTableComponent } from './aclasss-table/aclasss-table.component'
+  import { AclassSortingComponent } from './aclass-sorting/aclass-sorting.component'
+  import { BclasssTableComponent } from './bclasss-table/bclasss-table.component'
+  import { BclassSortingComponent } from './bclass-sorting/bclass-sorting.component'
 
 // insertion point sub template for map of components per struct 
-export const MapOfAclasssComponents: Map<string, any> = new Map([["AclasssTableComponent", AclasssTableComponent],])
-export const MapOfBclasssComponents: Map<string, any> = new Map([
-  ["BclasssTableComponent", BclasssTableComponent],
-])
-
-export const MapOfBclassSortingComponents: Map<string, any> = new Map([
-  ["BclassSortingComponent", BclassSortingComponent],
-])
-
+  export const MapOfAclasssComponents: Map<string, any> = new Map([["AclasssTableComponent", AclasssTableComponent],])
+  export const MapOfAclassSortingComponents: Map<string, any> = new Map([["AclassSortingComponent", AclassSortingComponent],])
+  export const MapOfBclasssComponents: Map<string, any> = new Map([["BclasssTableComponent", BclasssTableComponent],])
+  export const MapOfBclassSortingComponents: Map<string, any> = new Map([["BclassSortingComponent", BclassSortingComponent],])
 
 // map of all ng components of the stacks
 export const MapOfComponents: Map<string, any> =
@@ -24,11 +20,12 @@ export const MapOfComponents: Map<string, any> =
     ]
   )
 
-  // map of all ng sorting components of the stacks
+// map of all ng components of the stacks
 export const MapOfSortingComponents: Map<string, any> =
-new Map(
-  [
-    // insertion point sub template for map of components 
-    ["Bclass", MapOfBclassSortingComponents],
-  ]
-)
+  new Map(
+    [
+    // insertion point sub template for map of sorting components 
+      ["Aclass", MapOfAclassSortingComponents],
+      ["Bclass", MapOfBclassSortingComponents],
+    ]
+  )
