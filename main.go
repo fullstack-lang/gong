2bb891a3a6d716fb33bf1115840628eef8bd50f8
peugeot <thomaspeugeot@yahoo.fr>
2021-05-31 06:15:19 +0200

    new gongc

--- main.go
+++ main.go
@@ -44,11 +44,7 @@ func main() {
 	// mandatory, otherwise, bizarre errors occurs
 	db.DB().SetMaxOpenConns(1)
 
-	// Provide db variable to controllers
-	r.Use(func(c *gin.Context) {
-		c.Set("db", db) // a gin Context can have a map of variable that is set up at runtime
-		c.Next()
-	})
+	orm.BackRepo.Init(db)
 
 	controllers.RegisterControllers(r)
 
