f806ad3a5b346efaff7482dbdae99b53a1c78e06
peugeot <thomaspeugeot@yahoo.fr>
2021-06-01 09:52:36 +0200

    raw geenration

--- test/go/orm/AclassDB.go
+++ test/go/orm/AclassDB.go
@@ -15,6 +15,8 @@ import (
 
 	"github.com/jinzhu/gorm"
 
+	"github.com/tealeg/xlsx/v3"
+
 	"github.com/fullstack-lang/gong/test/go/models"
 )
 
@@ -622,6 +624,33 @@ func (backRepoAclass *BackRepoAclassStruct) Backup(dirPath string) {
 	err = ioutil.WriteFile(filename, file, 0644)
 	if err != nil {
 		log.Panic("Cannot write the json Aclass file", err.Error())
+	}
+}
+
+// Backup generates a json file from a slice of all AclassDB instances in the backrepo
+func (backRepoAclass *BackRepoAclassStruct) BackupXL(file *xlsx.File) {
+
+	// organize the map into an array with increasing IDs, in order to have repoductible
+	// backup file
+	forBackup := make([]*AclassDB, 0)
+	for _, aclassDB := range *backRepoAclass.Map_AclassDBID_AclassDB {
+		forBackup = append(forBackup, aclassDB)
+	}
+
+	sort.Slice(forBackup[:], func(i, j int) bool {
+		return forBackup[i].ID < forBackup[j].ID
+	})
+
+	sh, err := file.AddSheet("Aclass")
+	if err != nil {
+		log.Panic("Cannot add XL file", err.Error())
+	}
+	_ = sh
+
+	for _, aclassDB := range forBackup {
+
+		row := sh.AddRow()
+		row.WriteStruct(aclassDB, -1)
 	}
 }
 
