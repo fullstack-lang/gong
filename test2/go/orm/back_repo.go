8f75c5a0170ed28b1c63045b875652ff427867b0
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2021-06-10 15:37:04 +0200

    Port to gorm.io Fixes #34 - setup of test example

--- test2/go/orm/back_repo.go
+++ test2/go/orm/back_repo.go
@@ -2,11 +2,18 @@
 package orm
 
 import (
+	"bufio"
+	"bytes"
+	"io/ioutil"
+	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/jinzhu/gorm"
 
 	"github.com/fullstack-lang/gong/test2/go/models"
+
+	"github.com/tealeg/xlsx/v3"
 )
 
 // BackRepoStruct supports callback functions
@@ -71,6 +78,28 @@ func (backRepo *BackRepoStruct) Backup(stage *models.StageStruct, dirPath string
 	backRepo.BackRepoAclass.Backup(dirPath)
 }
 
+// Backup in XL the BackRepoStruct
+func (backRepo *BackRepoStruct) BackupXL(stage *models.StageStruct, dirPath string) {
+	os.Mkdir(dirPath, os.ModePerm)
+
+	// open an existing file
+	file := xlsx.NewFile()
+
+	// insertion point for per struct backup
+	backRepo.BackRepoAclass.BackupXL(file)
+
+	var b bytes.Buffer
+	writer := bufio.NewWriter(&b)
+	file.Write(writer)
+	theBytes := b.Bytes()
+
+	filename := filepath.Join(dirPath, "bckp.xlsx")
+	err := ioutil.WriteFile(filename, theBytes, 0644)
+	if err != nil {
+		log.Panic("Cannot write the XL file", err.Error())
+	}
+}
+
 // Restore the database into the back repo
 func (backRepo *BackRepoStruct) Restore(stage *models.StageStruct, dirPath string) {
 	models.Stage.Commit()
@@ -94,3 +123,6 @@ func (backRepo *BackRepoStruct) Restore(stage *models.StageStruct, dirPath strin
 	models.Stage.Checkout()
 }
 
+// Restore the database into the back repo
+func (backRepo *BackRepoStruct) RestoreXL(stage *models.StageStruct, dirPath string) {
+}
--- test2/ng/projects/test2/src/lib/app-routing.module.ts
+++ test2/ng/projects/test2/src/lib/app-routing.module.ts
@@ -6,6 +6,14 @@ import { AclasssTableComponent } from './aclasss-table/aclasss-table.component'
 import { AclassDetailComponent } from './aclass-detail/aclass-detail.component'
 import { AclassPresentationComponent } from './aclass-presentation/aclass-presentation.component'
 
+export enum Paths {
+	ACLASS_ALL = 'aclasss',
+	ACLASS_ADDER = 'aclass-adder',
+	ACLASS_ADDER_ID_ASSOCIATION = 'aclass-adder/:id/:association',
+	ACLASS_PRESENTATION_ID = 'aclass-presentation/:id',
+	ACLASS_PRESENTATION_SPECIAL = 'aclass-presentation-special/:id',
+	
+}
 
 const routes: Routes = [ // insertion point for routes declarations
 	{ path: 'aclasss', component: AclasssTableComponent, outlet: 'table' },
