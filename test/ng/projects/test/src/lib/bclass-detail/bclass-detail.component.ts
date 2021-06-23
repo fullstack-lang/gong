b68931b62257f38728ea01afae2b16798921ea11
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2021-06-23 17:35:42 +0200

    A more sexy/simpler detail editor #42- generated

--- test/ng/projects/test/src/lib/bclass-detail/bclass-detail.component.ts
+++ test/ng/projects/test/src/lib/bclass-detail/bclass-detail.component.ts
@@ -32,6 +32,11 @@ export class BclassDetailComponent implements OnInit {
 	// front repo
 	frontRepo: FrontRepo
 
+	// this stores the information related to string fields
+	// if false, the field is inputed with an <input ...> form 
+	// if true, it is inputed with a <textarea ...> </textarea>
+	mapFields_displayAsTextArea = new Map<string, boolean>()
+
 	constructor(
 		private bclassService: BclassService,
 		private frontRepoService: FrontRepoService,
@@ -82,9 +87,9 @@ export class BclassDetailComponent implements OnInit {
 
 		// some fields needs to be translated into serializable forms
 		// pointers fields, after the translation, are nulled in order to perform serialization
-		
+
 		// insertion point for translation/nullation of each field
-		
+
 		// save from the front pointer space to the non pointer space for serialization
 		if (association == undefined) {
 			// insertion point for translation/nullation of each pointers
@@ -198,7 +203,24 @@ export class BclassDetailComponent implements OnInit {
 
 	fillUpNameIfEmpty(event) {
 		if (this.bclass.Name == undefined) {
-			this.bclass.Name = event.value.Name		
+			this.bclass.Name = event.value.Name
+		}
+	}
+
+	toggleTextArea(fieldName: string) {
+		if (this.mapFields_displayAsTextArea.has(fieldName)) {
+			let displayAsTextArea = this.mapFields_displayAsTextArea.get(fieldName)
+			this.mapFields_displayAsTextArea.set(fieldName, !displayAsTextArea)
+		} else {
+			this.mapFields_displayAsTextArea.set(fieldName, true)
+		}
+	}
+
+	isATextArea(fieldName: string): boolean {
+		if (this.mapFields_displayAsTextArea.has(fieldName)) {
+			return this.mapFields_displayAsTextArea.get(fieldName)
+		} else {
+			return false
 		}
 	}
 }
