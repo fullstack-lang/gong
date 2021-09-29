c20b41662cf834e577b3523aa901d8f012722f6a
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2021-09-29 15:55:16 +0200

    In service origin should follow document Fixes #68

--- go/models/ng_file_push_from_front_nb_ts.go
+++ go/models/ng_file_push_from_front_nb_ts.go
@@ -8,9 +8,10 @@ import (
 	"strings"
 )
 
-const NgPushFromFrontTemplateTS = `import { Injectable } from '@angular/core';
+const NgPushFromFrontTemplateTS = `import { Injectable, Component, Inject } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { DOCUMENT, Location } from '@angular/common'
 
 /*
  * Behavior subject
@@ -28,11 +29,22 @@ export class PushFromFrontNbService {
         headers: new HttpHeaders({ 'Content-Type': 'application/json' })
     };
 
+    private commitNbUrl: string
     constructor(
-        private http: HttpClient
-    ) { }
+        private http: HttpClient,
+        private location: Location,
+        @Inject(DOCUMENT) private document: Document
+    ) {
+        // path to the service share the same origin with the path to the document
+        // get the origin in the URL to the document
+        let origin = this.document.location.origin
 
-    private commitNbUrl = 'http://{{addr}}/{{PkgPathRoot}}/pushfromfrontnb';
+        // if debugging with ng, replace 4200 with 8080
+        origin = origin.replace("4200", "8080")
+
+        // compute path to the service
+        this.commitNbUrl = origin + '/api/{{PkgPathRoot}}/pushfromfrontnb';
+    }
 
     // observable of the commit nb getter
     public getPushFromFrontNb(): Observable<number> {
