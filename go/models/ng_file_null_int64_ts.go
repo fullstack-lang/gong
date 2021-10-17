3598e63f6e764086b2d506095ff01402b864a7fb
peugeot <thomaspeugeot@yahoo.fr>
2021-10-17 11:57:59 +0200

    correction of Valid = true

--- go/models/ng_file_null_int64_ts.go
+++ go/models/ng_file_null_int64_ts.go
@@ -10,7 +10,7 @@ import (
 const NgNullInt64TemplateTS = `// define the type of nullable Int64 in order to support back pointers IDs
 export class NullInt64 {
     Int64: number = 0
-    Valid: boolean = true // exception to the golang convention that boolean default value is false
+    Valid: boolean = false
 }
 `
 
