9239b47ed3ef02f1d8bc3ad459df0747d66a6d2e
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2021-10-26 11:32:57 +0200

    robustness on windows

--- go/gongc/main.go
+++ go/gongc/main.go
@@ -75,6 +75,10 @@ func main() {
 				// compute name of package
 				abs, _ := filepath.Abs(filepath.Join(*pkgPath, "../.."))
 				log.Println("Abs is " + abs)
+
+				// to slash to have standardized separators between unix and windows
+				abs = filepath.ToSlash(abs)
+
 				dirs := strings.Split(abs, "/")
 				pkgName := dirs[len(dirs)-1]
 				log.Println("PkgName is " + pkgName)
