5ff3f50a600db6d5c8ce961acd845e000b9da7f2
peugeot <thomaspeugeot@yahoo.fr>
2021-11-01 18:39:09 +0100

    new embed

--- ng/src.go
+++
@@ -1,8 +0,0 @@
-package ng
-
-import "embed"
-
-// Src is the export of the angular source code. This allows for go vendoring to import ng sources
-// and align go source and ng source of the gong stack
-//go:embed src
-var Src embed.FS
