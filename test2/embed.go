558dfb2ff87481729254be99b7acee0e4bc14cd5
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2021-11-09 14:43:12 +0100

    new gongc

---
+++ test2/embed.go
@@ -0,0 +1,8 @@
+package test2
+
+import "embed"
+
+// NgDistNg is the export of angular distribution. This allows
+// embedding of the pages in the web server
+//go:embed ng/dist/ng
+var NgDistNg embed.FS
