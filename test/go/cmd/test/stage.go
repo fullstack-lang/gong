a42fbd3fc968ae8cd43d7c8a60adba9cac57cc20
peugeot <thomaspeugeot@yahoo.fr>
2022-02-07 08:35:48 +0100

    Generates a Marshall function that generates an Unmarshall function Fixes #94 - first steps in gong

--- test/go/cmd/test/stage.go
+++ test/go/cmd/test/stage.go
@@ -3,3 +3,35 @@ package main
 import "github.com/fullstack-lang/gong/test/go/models"
 
 var Stage models.StageStruct
+
+
+
+func Unmarshall(stage *models.StageStruct) {
+
+	// map of identifiers
+	var __Astruct__000000_A1 *models.Astruct
+	var __Astruct__000001_a2 *models.Astruct
+
+	var __Bstruct__000002_B1 *models.Bstruct
+	var __Bstruct__000003_B2 *models.Bstruct
+	var __Bstruct__000004_B3_with_a_very_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_name *models.Bstruct
+
+	// initializers of values
+
+	//Init Astruct A1
+	__Astruct__000000_A1.Name = "A1"
+	__Astruct__000000_A1.Aenum = "ENUM_VAL2"
+
+	//Init Astruct a2
+	__Astruct__000001_a2.Name = "a2"
+	__Astruct__000001_a2.Aenum = ""
+
+	__Bstruct__000002_B1.Name = "B1"
+	__Bstruct__000003_B2.Name = "B2"
+	__Bstruct__000004_B3_with_a_very_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_name.Name = "B3 with a very long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long name"
+
+	// initializers of pointers{{PointersInitializers}}
+
+	return
+}
+
