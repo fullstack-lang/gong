7803ed993ba39fa79c153dcdad619badcf3cea61
peugeot <thomaspeugeot@yahoo.fr>
2022-02-10 01:41:09 +0100

    Generates a Marshall function that generates an Unmarshall function #94 - generates stage.go by default

---
+++ go/models/package_main_stage.go
@@ -0,0 +1,16 @@
+package models
+
+const PackageStage = `package main
+
+import (
+	"time"
+
+	"github.com/fullstack-lang/gong/test/go/models"
+)
+
+var __Dummy_time_variable time.Time
+
+func Unmarshall(stage *models.StageStruct) {
+
+}
+`
---
+++ test2/go/cmd/test2/stage.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"time"
+
+	"github.com/fullstack-lang/gong/test/go/models"
+)
+
+var __Dummy_time_variable time.Time
+
+func Unmarshall(stage *models.StageStruct) {
+
+}
