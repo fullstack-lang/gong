9849b7e0bced0f1f971ccdaff08177ed3192a5fd
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2022-02-11 10:08:39 +0100

    Custom name for stage.go Fixes #96 - cleanup

--- test/go/cmd/test/stage.go
+++ test/go/cmd/test/stage.go
@@ -13,27 +13,27 @@ func Unmarshall(stage *models.StageStruct) {
 	// map of identifiers
 
 	// Declarations of staged instances of Astruct
-	__Astruct__000000_A1 := (&models.Astruct{ Name : "A1"}).Stage()
-	__Astruct__000001_A2 := (&models.Astruct{ Name : "A2"}).Stage()
-	__Astruct__000002_A3 := (&models.Astruct{ Name : "A3"}).Stage()
+	__Astruct__000000_A1 := (&models.Astruct{Name : "A1"}).Stage()
+	__Astruct__000001_A2 := (&models.Astruct{Name : "A2"}).Stage()
+	__Astruct__000002_A3 := (&models.Astruct{Name : "A3"}).Stage()
 
 	// Declarations of staged instances of AstructBstruct2Use
-	__AstructBstruct2Use__000000_ := (&models.AstructBstruct2Use{ Name : ""}).Stage()
-	__AstructBstruct2Use__000001_ := (&models.AstructBstruct2Use{ Name : ""}).Stage()
+	__AstructBstruct2Use__000000_ := (&models.AstructBstruct2Use{Name : ""}).Stage()
+	__AstructBstruct2Use__000001_ := (&models.AstructBstruct2Use{Name : ""}).Stage()
 
 	// Declarations of staged instances of AstructBstructUse
 
 	// Declarations of staged instances of Bstruct
-	__Bstruct__000000_B1 := (&models.Bstruct{ Name : "B1"}).Stage()
-	__Bstruct__000001_B2 := (&models.Bstruct{ Name : "B2"}).Stage()
-	__Bstruct__000002_B3 := (&models.Bstruct{ Name : "B3"}).Stage()
+	__Bstruct__000000_B1 := (&models.Bstruct{Name : "B1"}).Stage()
+	__Bstruct__000001_B2 := (&models.Bstruct{Name : "B2"}).Stage()
+	__Bstruct__000002_B3 := (&models.Bstruct{Name : "B3"}).Stage()
 
 	// Declarations of staged instances of Dstruct
-	__Dstruct__000000_D1 := (&models.Dstruct{ Name : "D1"}).Stage()
+	__Dstruct__000000_D1 := (&models.Dstruct{Name : "D1"}).Stage()
 
 	// initializers of values
 
-	// Init Astruct values A1
+	// Astruct A1 values setup
 	__Astruct__000000_A1.Name = "A1"
 	__Astruct__000000_A1.Date, _ = time.Parse("2006-01-02 15:04:05.999999999 -0700 MST", "2022-11-14 03:08:21 +0000 UTC")
 	__Astruct__000000_A1.Booleanfield = false
@@ -47,7 +47,7 @@ func Unmarshall(stage *models.StageStruct) {
 	__Astruct__000000_A1.Anotherbooleanfield = false
 	__Astruct__000000_A1.Duration1 = 79653000000000
 
-	// Init Astruct values A2
+	// Astruct A2 values setup
 	__Astruct__000001_A2.Name = "A2"
 	__Astruct__000001_A2.Date, _ = time.Parse("2006-01-02 15:04:05.999999999 -0700 MST", "2022-01-18 01:09:21 +0000 UTC")
 	__Astruct__000001_A2.Booleanfield = false
@@ -61,7 +61,7 @@ func Unmarshall(stage *models.StageStruct) {
 	__Astruct__000001_A2.Anotherbooleanfield = false
 	__Astruct__000001_A2.Duration1 = 0
 
-	// Init Astruct values A3
+	// Astruct A3 values setup
 	__Astruct__000002_A3.Name = "A3"
 	__Astruct__000002_A3.Date, _ = time.Parse("2006-01-02 15:04:05.999999999 -0700 MST", "2022-02-10 01:06:11.446 +0000 UTC")
 	__Astruct__000002_A3.Booleanfield = true
@@ -75,28 +75,28 @@ func Unmarshall(stage *models.StageStruct) {
 	__Astruct__000002_A3.Anotherbooleanfield = false
 	__Astruct__000002_A3.Duration1 = 0
 
-	// Init AstructBstruct2Use values 
+	// AstructBstruct2Use  values setup
 	__AstructBstruct2Use__000000_.Name = ""
 
-	// Init AstructBstruct2Use values 
+	// AstructBstruct2Use  values setup
 	__AstructBstruct2Use__000001_.Name = ""
 
-	// Init Bstruct values B1
+	// Bstruct B1 values setup
 	__Bstruct__000000_B1.Name = "B1"
 	__Bstruct__000000_B1.Floatfield = 0.000000
 	__Bstruct__000000_B1.Intfield = 0
 
-	// Init Bstruct values B2
+	// Bstruct B2 values setup
 	__Bstruct__000001_B2.Name = "B2"
 	__Bstruct__000001_B2.Floatfield = 0.000000
 	__Bstruct__000001_B2.Intfield = 0
 
-	// Init Bstruct values B3
+	// Bstruct B3 values setup
 	__Bstruct__000002_B3.Name = "B3"
 	__Bstruct__000002_B3.Floatfield = 0.000000
 	__Bstruct__000002_B3.Intfield = 0
 
-	// Init Dstruct values D1
+	// Dstruct D1 values setup
 	__Dstruct__000000_D1.Name = "D1"
 
 	// initializers of pointers
@@ -107,8 +107,8 @@ func Unmarshall(stage *models.StageStruct) {
 	__Astruct__000000_A1.Anotherarrayofb = append(__Astruct__000000_A1.Anotherarrayofb, __Bstruct__000001_B2)
 	__Astruct__000000_A1.Anotherarrayofb = append(__Astruct__000000_A1.Anotherarrayofb, __Bstruct__000002_B3)
 	__Astruct__000000_A1.Anarrayofa = append(__Astruct__000000_A1.Anarrayofa, __Astruct__000000_A1)
-	__Astruct__000000_A1.Anarrayofb2Use = append(__Astruct__000000_A1.Anarrayofb2Use, __AstructBstruct2Use__000000_)
-	__Astruct__000001_A2.Anarrayofb2Use = append(__Astruct__000001_A2.Anarrayofb2Use, __AstructBstruct2Use__000001_)
+	__Astruct__000000_A1.Anarrayofb2Use = append(__Astruct__000000_A1.Anarrayofb2Use, __AstructBstruct2Use__000001_)
+	__Astruct__000001_A2.Anarrayofb2Use = append(__Astruct__000001_A2.Anarrayofb2Use, __AstructBstruct2Use__000000_)
 	__AstructBstruct2Use__000000_.Bstrcut2 = __Bstruct__000000_B1
 	__AstructBstruct2Use__000001_.Bstrcut2 = __Bstruct__000000_B1
 }
