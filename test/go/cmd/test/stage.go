bca5fa767638c35562535e9d6c4805f7f577f7d2
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2022-02-11 14:04:14 +0100

    Custom name for stage.go #96 - cleanup before level +2

--- test/go/cmd/test/stage.go
+++ test/go/cmd/test/stage.go
@@ -112,3 +112,5 @@ func Unmarshall(stage *models.StageStruct) {
 	__AstructBstruct2Use__000000_.Bstrcut2 = __Bstruct__000000_B1
 	__AstructBstruct2Use__000001_.Bstrcut2 = __Bstruct__000000_B1
 }
+
+
