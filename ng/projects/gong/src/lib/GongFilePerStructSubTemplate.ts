ef2bf8ebcee42e13b6009e493b21fcd44e4ec29b
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2022-02-17 10:58:15 +0100

    changed gitignore in ordre to have the GongFilePerStructSubTemplate.ts included

---
+++ ng/projects/gong/src/lib/GongFilePerStructSubTemplate.ts
@@ -0,0 +1,28 @@
+// generated from ng_file_enum.ts.go
+export enum GongFilePerStructSubTemplate {
+	// insertion point	
+	GongFileFieldSubTmplSetBasicFieldBool = 0,
+	GongFileFieldSubTmplSetBasicFieldFloat64 = 2,
+	GongFileFieldSubTmplSetBasicFieldInt = 1,
+	GongFileFieldSubTmplSetBasicFieldString = 3,
+	GongFileFieldSubTmplSetBasicFieldStringEnum = 5,
+	GongFileFieldSubTmplSetPointerField = 6,
+	GongFileFieldSubTmplSetSliceOfPointersField = 7,
+	GongFileFieldSubTmplSetTimeField = 4,
+}
+
+export interface GongFilePerStructSubTemplateSelect {
+	value: string;
+	viewValue: string;
+}
+
+export const GongFilePerStructSubTemplateList: GongFilePerStructSubTemplateSelect[] = [ // insertion point	
+	{ value: 'GongFileFieldSubTmplSetBasicFieldBool', viewValue: '0' },
+	{ value: 'GongFileFieldSubTmplSetBasicFieldFloat64', viewValue: '2' },
+	{ value: 'GongFileFieldSubTmplSetBasicFieldInt', viewValue: '1' },
+	{ value: 'GongFileFieldSubTmplSetBasicFieldString', viewValue: '3' },
+	{ value: 'GongFileFieldSubTmplSetBasicFieldStringEnum', viewValue: '5' },
+	{ value: 'GongFileFieldSubTmplSetPointerField', viewValue: '6' },
+	{ value: 'GongFileFieldSubTmplSetSliceOfPointersField', viewValue: '7' },
+	{ value: 'GongFileFieldSubTmplSetTimeField', viewValue: '4' },
+];
