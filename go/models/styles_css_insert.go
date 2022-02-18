38df6f97f7054ce6d7196e9f0743d73379fa590c
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2022-02-18 17:35:04 +0100

    Have the default main application use gongdoc #104 - styles

---
+++ go/models/styles_css_insert.go
@@ -0,0 +1,8 @@
+package models
+
+const StylesCssInsert = `
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+@import '~material-design-icons/iconfont/material-icons.css';
+@import '~typeface-roboto/index.css';
+`
