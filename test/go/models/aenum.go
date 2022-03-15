81d1cfc51e47f602c3234afccbb4605c1001ea48
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2022-03-15 11:04:48 +0100

    In detail, convert displayValue to value in detail component Fixes #121 - manual modifs

--- test/go/models/aenum.go
+++ test/go/models/aenum.go
@@ -12,3 +12,29 @@ const (
 	ENUM_VAL1 AEnumType = "ENUM_VAL1_NOT_THE_SAME"
 	ENUM_VAL2 AEnumType = "ENUM_VAL2"
 )
+
+func (aEnum AEnumType) ToString() (res string) {
+
+	// migration of former implementation of enum
+	switch aEnum {
+	case ENUM_VAL1:
+		res = "ENUM_VAL1_NOT_THE_SAME"
+	case ENUM_VAL2:
+		res = "ENUM_VAL2"
+	}
+	return
+}
+
+func (aEnum *AEnumType) FromString(input string) {
+
+	switch input {
+	case "ENUM_VAL1_NOT_THE_SAME":
+		*aEnum = ENUM_VAL1
+	case "ENUM_VAL2":
+		*aEnum = ENUM_VAL2
+
+	// migration of former implementation of enum
+	case "ENUM_VAL1":
+		*aEnum = ENUM_VAL1
+	}
+}
