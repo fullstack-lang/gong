899fae4c35408ffd210ae38efb8c35308c974088
peugeot <thomaspeugeot@yahoo.fr>
2022-05-01 10:30:04 +0200

    Use classshape select to route the table Fixes #151 - in the test application, inject the gongdoc.GongdocCommand instance and pool over it

--- test/ng/src/app/app.component.ts
+++ test/ng/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { Observable, combineLatest, timer } from 'rxjs'
 
+import * as gongdoc from 'gongdoc'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,15 +17,38 @@ export class AppComponent implements OnInit {
 
   views: string[] = [this.default, this.diagrams, this.meta];
 
+  // variable that enables pooling of selected gongstruct
   obsTimer: Observable<number> = timer(1000, 1000)
+  gongdocCommandSingloton: gongdoc.GongdocCommandDB = new gongdoc.GongdocCommandDB
+  lastSelectionDate: string = ''
+
+  constructor(private gongdocCommandService: gongdoc.GongdocCommandService) {
+
+  }
 
   ngOnInit(): void {
 
+    // pool the gongdoc command and check wether a gongstruct has been selected
     this.obsTimer.subscribe(
       currTime => {
-        console.log(currTime)
+
+        // fetch the command singloton
+        this.gongdocCommandService.getGongdocCommands().subscribe(
+          gongdocCommands => {
+            for (let gongdocCommand_ of gongdocCommands) {
+              this.gongdocCommandSingloton = gongdocCommand_
+            }
+
+            // check the type of command
+            if (this.gongdocCommandSingloton.Command == gongdoc.GongdocCommandType.DIAGRAM_GONGSTRUCT_SELECT) {
+              if (this.lastSelectionDate != this.gongdocCommandSingloton.Date) {
+                console.log("New user selection of gongstruct " + this.gongdocCommandSingloton.StructName)
+                this.lastSelectionDate = this.gongdocCommandSingloton.Date
+              }
+            }
+          }
+        )
       }
     )
-
   }
 }
