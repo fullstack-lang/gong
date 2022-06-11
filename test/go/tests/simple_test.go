d5a9dc80dea5400e8391b62c306ce753b30b67a9
peugeot <thomaspeugeot@yahoo.fr>
2022-06-11 11:46:51 +0200

    Generate refactorable navigation maps between staged instances that go backward association links #160 - manual for direct assocations

--- test/go/tests/simple_test.go
+++ test/go/tests/simple_test.go
@@ -71,7 +71,7 @@ func TestStageCallBack(t *testing.T) {
 	log.Println(models.GetAssociationName[models.Astruct]().Anarrayofb[0].Name)
 	log.Println(models.GetAssociationName[models.Astruct]().Anarrayofa[0].Name)
 
-	reverseMapAstruct_AssocationTob := models.GetReverseMap[models.Astruct, models.Bstruct](models.GetAssociationName[models.Astruct]().Associationtob.Name)
+	reverseMapAstruct_AssocationTob := models.GetDirectAssociationReverseMap[models.Astruct, models.Bstruct](models.GetAssociationName[models.Astruct]().Associationtob.Name)
 	for _, astruct := range reverseMapAstruct_AssocationTob[bclass1] {
 		log.Println("astruct ", astruct.Name)
 	}
