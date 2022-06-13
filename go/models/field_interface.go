16ca306a77f3652bb7c6f591b68d3d6bc3b5b131
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2022-06-13 16:13:51 +0200

    Generate Field fails for Embedded struct Fixes #162

--- go/models/field_interface.go
+++ go/models/field_interface.go
@@ -3,4 +3,9 @@ package models
 type FieldInterface interface {
 	GetName() string
 	GetIndex() int // get the field index with its belonging gongstruct
+
+	// GetCompositeStructName provides, if it is from a composite struct, the name of the composite struct
+	//  where the field is present.
+	// It is empty otherwise
+	GetCompositeStructName() string
 }
