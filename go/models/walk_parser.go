4b3935ad3a7702438d21b989563d01659fe40360
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2022-06-20 16:02:22 +0200

    Use parser.ParseDir instead of packages.Load #172 - First pass

---
+++ go/models/walk_parser.go
@@ -0,0 +1,142 @@
+package models
+
+import (
+	"go/ast"
+	"log"
+	"strings"
+)
+
+func WalkParser(parserPkgs map[string]*ast.Package, modelPkg *ModelPkg) {
+
+	var pkg *ast.Package
+	var ok bool
+	if pkg, ok = parserPkgs["models"]; !ok {
+		log.Fatal("No package models")
+	}
+
+	modelPkg.GongEnums = make(map[string]*GongEnum)
+	modelPkg.GongStructs = make(map[string]*GongStruct)
+
+	if len(pkg.Files) == 0 {
+		log.Fatal("No go file to parse")
+	}
+
+	// first pass : get "type" definition for enum & struct
+	//
+	// search all files
+	for filePath, file := range pkg.Files {
+
+		var fileName string
+		if strings.Contains(filePath, "\\") {
+			fileNames := strings.Split(filePath, "\\")
+			fileName = fileNames[len(fileNames)-1]
+
+			// package name is three steps behind
+			modelPkg.Name = fileNames[len(fileNames)-4]
+			modelPkg.PkgPath =
+				fileNames[len(fileNames)-7] + "/" +
+					fileNames[len(fileNames)-6] + "/" +
+					fileNames[len(fileNames)-5] + "/" +
+					fileNames[len(fileNames)-4] + "/" +
+					fileNames[len(fileNames)-3] + "/" +
+					fileNames[len(fileNames)-2]
+		}
+
+		if fileName == "gong.go" {
+			continue
+		}
+
+		// for exploration
+		// if fileName != "astruct.go" {
+		// 	continue
+		// }
+
+		log.Println("Parsing file ", fileName)
+
+		// for structName, scope := range file.Scope.Objects {
+
+		// 	log.Println("Parsing structName ", structName)
+		// 	_ = scope
+		// }
+
+		for _, decl := range file.Decls {
+			_ = decl
+
+			switch genDecl := decl.(type) {
+			case *ast.GenDecl:
+				// log.Println("Length of Spec", len(genDecl.Specs))
+
+				for _, spec := range genDecl.Specs {
+
+					switch typeSpec := spec.(type) {
+					case *ast.TypeSpec:
+
+						// TypeSpec is for declaration with the "type" keyword
+						// in gong, it can be either a GongStruct or a GongEnum
+
+						// we are only interested in exported symbols
+						if strings.Title(typeSpec.Name.Name) != typeSpec.Name.Name {
+							continue
+						}
+
+						log.Println("Type spec name is ", typeSpec.Name.Name)
+
+						// If it is a GongEnum, the typeSpec Type is a int or a string
+						switch _type := typeSpec.Type.(type) {
+						case *ast.Ident:
+							log.Println("Type spec type is ", _type.Name)
+
+							var enumType GongEnumType
+							switch _type.Name {
+							case "int":
+								enumType = Int
+							case "string":
+								enumType = String
+							default:
+								log.Fatal("Invalid definition of a Gongstruct ", typeSpec.Name.Name, " type ", _type.Name)
+							}
+
+							modelEnum := &GongEnum{
+								Name: typeSpec.Name.Name,
+								Type: enumType,
+							}
+							modelPkg.GongEnums[modelPkg.PkgPath+"."+typeSpec.Name.Name] = modelEnum
+
+							// If it is a GongStruct, the ast is a StructType
+						case *ast.StructType:
+							log.Println("Ng of fields is ", len(_type.Fields.List))
+
+							// fetch the name of the Gongstruct by identifying if there is a field with name "Name"
+							var isGongStruct bool
+							for _, field := range _type.Fields.List {
+								if len(field.Names) > 0 && field.Names[0].Name == "Name" {
+									isGongStruct = true
+								}
+							}
+
+							gongstruct := GongStruct{Name: typeSpec.Name.Name}
+							if isGongStruct {
+								modelPkg.GongStructs[modelPkg.PkgPath+"."+typeSpec.Name.Name] = &gongstruct
+							}
+						default:
+						}
+					default:
+					}
+
+				}
+
+			default:
+				continue
+			}
+		}
+
+		// for _, decl := range file.Decls {
+
+		// 	switch decl2 := decl.(type) {
+		// 		case *D
+		// 	}
+		// }
+
+	}
+
+}
