15f4f5a852a2bd44dd8985eb830af1d875424685
peugeot <thomaspeugeot@yahoo.fr>
2022-07-02 06:43:35 +0200

    push gong note to stage

--- test/go/models/note.go
+++ test/go/models/note.go
@@ -1,8 +1,7 @@
 package models
 
-// GONGDOC(note on the organization between line and points): first line
-// this is an example of a note that
-// could be displayed on a diagram.
+// GONGDOC(note on the organization between line and points):
+// github.com/fullstack-lang/test/go/models is a model that showcases gong possibilites
 //
 // Astruct is associated with Bstruct via 2 kinds of association
 // - a N-0..1 association (directly)
