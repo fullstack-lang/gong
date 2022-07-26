3b6e4ead6c5e5d95a1b71b7c26535ebc301b8ed8
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2022-07-26 15:36:56 +0200

    [refeactorable] Have a generated oject function that return the name of a field #181 - Generate field

--- go/models/model_file_gong_coder.go
+++ go/models/model_file_gong_coder.go
@@ -2,7 +2,6 @@ package models
 
 import (
 	"fmt"
-	"go/types"
 	"log"
 	"os"
 	"path/filepath"
@@ -27,7 +26,7 @@ func GongfieldCoder[Type Gongstruct]() Type {
 }
 
 type Gongfield interface {
-	string | bool | int | float64 | time.Time | time.Duration
+	string | bool | int | float64 | time.Time | time.Duration{{` + string(rune(ModelGongCoderGenericGongstructTypes)) + `}}
 }
 
 func GongfieldName[Type Gongstruct, FieldType Gongfield](field FieldType) string {
@@ -39,9 +38,6 @@ func GongfieldName[Type Gongstruct, FieldType Gongfield](field FieldType) string
 }
 `
 
-var ModelGongCoderStructSubTemplateCode map[ModelGongCoderStructInsertionId]string = // new line
-map[ModelGongCoderStructInsertionId]string{}
-
 //
 // insertion points are places where the code is
 // generated per gong struct
@@ -49,8 +45,14 @@ map[ModelGongCoderStructInsertionId]string{}
 type ModelGongCoderStructInsertionId int
 
 const (
-	ModelGongCoder ModelGongCoderStructInsertionId = iota
+	ModelGongCoderGenericGongstructTypes ModelGongCoderStructInsertionId = iota
+	ModelGongCoderStructInsertionsNb
 )
+
+var ModelGongCoderStructSubTemplateCode map[ModelGongCoderStructInsertionId]string = // new line
+map[ModelGongCoderStructInsertionId]string{
+	ModelGongCoderGenericGongstructTypes: ` | *{{Structname}} | []*{{Structname}}`,
+}
 
 func CodeGeneratorModelGongCoder(
 	mdlPkg *ModelPkg,
@@ -81,84 +83,18 @@ func CodeGeneratorModelGongCoder(
 		}
 
 		for subStructTemplate := range ModelGongCoderStructSubTemplateCode {
-
-			// replace {{ValuesInitialization}}
-			valInitCode := ""
-			pointerInitCode := ""
-			fieldNames := `
-		res = []string{`
-			fieldStringValues := ``
-			fieldReverseAssociationMapCreationCode := ``
-			fieldReversePointerAssociationMapCode := ``
-			fieldReverseSliceOfPointersAssociationMapCode := ``
-			associationFieldInitialization := ``
-
-			for idx, field := range gongStruct.Fields {
-
-				switch field := field.(type) {
-				case *GongBasicField:
-
-					switch field.basicKind {
-					case types.String:
-					case types.Bool:
-					case types.Float64:
-					case types.Int, types.Int64:
-					default:
-					}
-				case *GongTimeField:
-				case *PointerToGongStructField:
-				case *SliceOfPointerToGongStructField:
-				default:
-				}
-
-				if idx > 0 {
-					fieldNames += ", "
-				}
-				fieldNames += Replace1(
-					GongFileFieldFieldSubTemplateCode[GongFileFieldSubTmplStringFieldName],
-					"{{FieldName}}", field.GetName())
-			}
-
-			valInitCode = Replace2(valInitCode,
-				"{{structname}}", strings.ToLower(gongStruct.Name),
-				"{{Structname}}", gongStruct.Name)
-
-			pointerInitCode = Replace2(pointerInitCode,
-				"{{structname}}", strings.ToLower(gongStruct.Name),
-				"{{Structname}}", gongStruct.Name)
-
-			fieldStringValues = Replace2(fieldStringValues,
-				"{{structname}}", strings.ToLower(gongStruct.Name),
-				"{{Structname}}", gongStruct.Name)
-
-			fieldReverseAssociationMapCreationCode = Replace2(fieldReverseAssociationMapCreationCode,
-				"{{structname}}", strings.ToLower(gongStruct.Name),
-				"{{Structname}}", gongStruct.Name)
-
-			fieldReversePointerAssociationMapCode = Replace2(fieldReversePointerAssociationMapCode,
-				"{{structname}}", strings.ToLower(gongStruct.Name),
-				"{{Structname}}", gongStruct.Name)
-
-			fieldReverseSliceOfPointersAssociationMapCode = Replace2(fieldReverseSliceOfPointersAssociationMapCode,
-				"{{structname}}", strings.ToLower(gongStruct.Name),
-				"{{Structname}}", gongStruct.Name)
-
-			fieldNames += `}`
-			generatedCodeFromSubTemplate := Replace10(ModelGongCoderStructSubTemplateCode[subStructTemplate],
+			generatedCodeFromSubTemplate := Replace2(ModelGongCoderStructSubTemplateCode[subStructTemplate],
 				"{{structname}}", strings.ToLower(gongStruct.Name),
 				"{{Structname}}", gongStruct.Name,
-				"{{ValuesInitialization}}", valInitCode,
-				"{{PointersInitialization}}", pointerInitCode,
-				"{{ListOfFieldsName}}", fieldNames,
-				"{{StringValueOfFields}}", fieldStringValues,
-				"{{ReverseAssociationMapFunctions}}", fieldReverseAssociationMapCreationCode,
-				"{{fieldReversePointerAssociationMapCode}}", fieldReversePointerAssociationMapCode,
-				"{{fieldReverseSliceOfPointersAssociationMapCode}}", fieldReverseSliceOfPointersAssociationMapCode,
-				"{{associationFieldInitialization}}", associationFieldInitialization,
 			)
 
 			subStructCodes[subStructTemplate] += generatedCodeFromSubTemplate
 		}
+	}
+
+	for insertionPerStructId := ModelGongCoderStructInsertionId(0); insertionPerStructId < ModelGongCoderStructInsertionsNb; insertionPerStructId++ {
+		toReplace := "{{" + string(rune(insertionPerStructId)) + "}}"
+		codeGO = strings.ReplaceAll(codeGO, toReplace, subStructCodes[insertionPerStructId])
 	}
 
 	codeGO = Replace4(codeGO,
