f21554b69cba9565c9de256f2fedb383e0b79dfe
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2022-07-27 17:46:15 +0200

    [refeactorable] Have a generated oject function that return the name of a field #181 - test2 gong

---
+++ ng/projects/gong/src/lib/ModelGongCoderStructInsertionId.ts
@@ -0,0 +1,20 @@
+// generated from ng_file_enum.ts.go
+export enum ModelGongCoderStructInsertionId {
+	// insertion point	
+	ModelGongCoderGenericGongstructTypes = 0,
+	ModelGongCoderGenericGongstructCoder = 1,
+	ModelGongCoderGenericGongstructNamerString = 2,
+	ModelGongCoderStructInsertionsNb = 3,
+}
+
+export interface ModelGongCoderStructInsertionIdSelect {
+	value: number;
+	viewValue: string;
+}
+
+export const ModelGongCoderStructInsertionIdList: ModelGongCoderStructInsertionIdSelect[] = [ // insertion point	
+	{ value: ModelGongCoderStructInsertionId.ModelGongCoderGenericGongstructTypes, viewValue: "ModelGongCoderGenericGongstructTypes" },
+	{ value: ModelGongCoderStructInsertionId.ModelGongCoderGenericGongstructCoder, viewValue: "ModelGongCoderGenericGongstructCoder" },
+	{ value: ModelGongCoderStructInsertionId.ModelGongCoderGenericGongstructNamerString, viewValue: "ModelGongCoderGenericGongstructNamerString" },
+	{ value: ModelGongCoderStructInsertionId.ModelGongCoderStructInsertionsNb, viewValue: "ModelGongCoderStructInsertionsNb" },
+];
---
+++ test2/go/models/gong_coder.go
@@ -0,0 +1,67 @@
+package models
+
+import "time"
+
+// GongfieldCoder return an instance of Type where each field 
+// encodes the index of the field
+//
+// This allows for refactorable field names
+// 
+func GongfieldCoder[Type Gongstruct]() Type {
+	var t Type
+
+	switch any(t).(type) {
+	// insertion point for cases
+	case Astruct:
+		fieldCoder := Astruct{}
+		// insertion point for field dependant code
+		fieldCoder.Name = "0"
+		return (any)(fieldCoder).(Type)
+	default:
+		return t
+	}
+}
+
+type Gongfield interface {
+	string | bool | int | float64 | time.Time | time.Duration | *Astruct | []*Astruct
+}
+
+// GongfieldName provides the name of the field by passing the instance of the coder to
+// the fonction.
+//
+// This allows for refactorable field name
+//
+// fieldCoder := models.GongfieldCoder[models.Astruct]()
+// log.Println( models.GongfieldName[*models.Astruct](fieldCoder.Name))
+// log.Println( models.GongfieldName[*models.Astruct](fieldCoder.Booleanfield))
+// log.Println( models.GongfieldName[*models.Astruct](fieldCoder.Intfield))
+// log.Println( models.GongfieldName[*models.Astruct](fieldCoder.Floatfield))
+// 
+// limitations:
+// 1. cannot encode boolean fields
+// 2. for associations (pointer to gongstruct or slice of pointer to gongstruct, uses GetAssociationName)
+func GongfieldName[Type PointerToGongstruct, FieldType Gongfield](field FieldType) string {
+	var t Type
+
+	switch any(t).(type) {
+	// insertion point for cases
+	case *Astruct:
+		switch field := any(field).(type) {
+		case string:
+			// insertion point for field dependant name
+			if field == "0" {
+				return "Name"
+			}
+		case int, int64:
+			// insertion point for field dependant name
+		case float64:
+			// insertion point for field dependant name
+		case time.Time:
+			// insertion point for field dependant name
+		}
+	default:
+		return ""
+	}
+	_ = field
+	return ""
+}
