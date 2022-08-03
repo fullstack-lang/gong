94a7381dc00eb07745f82b6d478c7fd2d6419dc1
peugeot <thomaspeugeot@yahoo.fr>
2022-08-03 09:30:00 +0200

    [refeactorable] Have a generated oject function that return the name of a field Fixes #181 - new gonc

--- ng/projects/gong/src/lib/ModelGongCoderFieldInsertionId.ts
+++ ng/projects/gong/src/lib/ModelGongCoderFieldInsertionId.ts
@@ -5,11 +5,13 @@ export enum ModelGongCoderFieldInsertionId {
 	ModelGongCoderFieldCodeInt = 1,
 	ModelGongCoderFieldCodeFloat64 = 2,
 	ModelGongCoderFieldCodeDate = 3,
-	ModelGongCoderFieldNameString = 4,
-	ModelGongCoderFieldNameInt = 5,
-	ModelGongCoderFieldNameFloat64 = 6,
-	ModelGongCoderFieldNameDate = 7,
-	ModelGongCoderFieldInsertionsNb = 8,
+	ModelGongCoderFieldCodeBoolean = 4,
+	ModelGongCoderFieldNameString = 5,
+	ModelGongCoderFieldNameInt = 6,
+	ModelGongCoderFieldNameFloat64 = 7,
+	ModelGongCoderFieldNameDate = 8,
+	ModelGongCoderFieldNameBoolean = 9,
+	ModelGongCoderFieldInsertionsNb = 10,
 }
 
 export interface ModelGongCoderFieldInsertionIdSelect {
@@ -22,9 +24,11 @@ export const ModelGongCoderFieldInsertionIdList: ModelGongCoderFieldInsertionIdS
 	{ value: ModelGongCoderFieldInsertionId.ModelGongCoderFieldCodeInt, viewValue: "ModelGongCoderFieldCodeInt" },
 	{ value: ModelGongCoderFieldInsertionId.ModelGongCoderFieldCodeFloat64, viewValue: "ModelGongCoderFieldCodeFloat64" },
 	{ value: ModelGongCoderFieldInsertionId.ModelGongCoderFieldCodeDate, viewValue: "ModelGongCoderFieldCodeDate" },
+	{ value: ModelGongCoderFieldInsertionId.ModelGongCoderFieldCodeBoolean, viewValue: "ModelGongCoderFieldCodeBoolean" },
 	{ value: ModelGongCoderFieldInsertionId.ModelGongCoderFieldNameString, viewValue: "ModelGongCoderFieldNameString" },
 	{ value: ModelGongCoderFieldInsertionId.ModelGongCoderFieldNameInt, viewValue: "ModelGongCoderFieldNameInt" },
 	{ value: ModelGongCoderFieldInsertionId.ModelGongCoderFieldNameFloat64, viewValue: "ModelGongCoderFieldNameFloat64" },
 	{ value: ModelGongCoderFieldInsertionId.ModelGongCoderFieldNameDate, viewValue: "ModelGongCoderFieldNameDate" },
+	{ value: ModelGongCoderFieldInsertionId.ModelGongCoderFieldNameBoolean, viewValue: "ModelGongCoderFieldNameBoolean" },
 	{ value: ModelGongCoderFieldInsertionId.ModelGongCoderFieldInsertionsNb, viewValue: "ModelGongCoderFieldInsertionsNb" },
 ];
