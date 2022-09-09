d0da9d45e478ad52ffbcf14ef77462b856408e8d
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2022-09-09 17:39:37 +0200

    "skipFlutter" (default false) generate flutter project directory if absent Fixes #190 - generated default main & removed test dir

---
+++ go/flutter/generate_default_main_file.go
@@ -0,0 +1,58 @@
+package flutter
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/fullstack-lang/gong/go/models"
+)
+
+const main_dart_template = `import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    const textWidget = Text(
+      'Hello, How are you?',
+      textAlign: TextAlign.center,
+      overflow: TextOverflow.ellipsis,
+      style: TextStyle(fontWeight: FontWeight.bold),
+      textDirection: TextDirection.ltr,
+    );
+
+    return textWidget;
+  }
+}
+`
+
+func GenerateDefaultMainFile(
+	mdlPkg *models.ModelPkg,
+	mainFilePath string) {
+	{
+		codeGo := main_dart_template
+
+		codeGo = models.Replace4(codeGo,
+			"{{PkgName}}", mdlPkg.Name,
+			"{{TitlePkgName}}", strings.Title(mdlPkg.Name),
+			"{{pkgname}}", strings.ToLower(mdlPkg.Name),
+			"{{PkgPathRoot}}", strings.ReplaceAll(mdlPkg.PkgPath, "/models", ""))
+		codeGo = strings.ReplaceAll(codeGo, "{{PkgPathAboveRoot}}",
+			strings.ReplaceAll(mdlPkg.PkgPath, "/go/models", ""))
+
+		file, err := os.Create(mainFilePath)
+		if err != nil {
+			log.Panic(err)
+		}
+		defer file.Close()
+		fmt.Fprint(file, codeGo)
+	}
+}
