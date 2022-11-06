8510706013d6144700945ded5341303ab0ccaa87
peugeot <thomaspeugeot@yahoo.fr>
2022-11-06 18:41:48 +0100

    create the fullstack package Fixes #213 - new gongc

--- test2/go/fullstack/init.go
+++ test2/go/fullstack/init.go
@@ -1,16 +1,16 @@
 package fullstack
 
 import (
-	// gong stack for model analysis
+	// test2 stack for model analysis
 
-	gong_controllers "github.com/fullstack-lang/gong/go/controllers"
-	gong_orm "github.com/fullstack-lang/gong/go/orm"
+	test2_controllers "github.com/fullstack-lang/gong/test2/go/controllers"
+	test2_orm "github.com/fullstack-lang/gong/test2/go/orm"
 	"github.com/gin-gonic/gin"
 
 	// this will import the angular front end source code directory (versionned with git) in the vendor directory
 	// this path will be included in the "tsconfig.json" front end compilation paths
 	// to include this stack front end code
-	_ "github.com/fullstack-lang/gong/ng"
+	_ "github.com/fullstack-lang/gong/test2/ng"
 )
 
 func Init(r *gin.Engine, filenames ...string) {
@@ -19,9 +19,9 @@ func Init(r *gin.Engine, filenames ...string) {
 		filenames = append(filenames, ":memory:")
 	}
 
-	db_inMemory := gong_orm.SetupModels(false, filenames[0])
+	db_inMemory := test2_orm.SetupModels(false, filenames[0])
 
-	// since gongsim is a multi threaded application. It is important to set up
+	// since test2sim is a multi threaded application. It is important to set up
 	// only one open connexion at a time
 	dbDB_inMemory, err := db_inMemory.DB()
 	if err != nil {
@@ -30,5 +30,5 @@ func Init(r *gin.Engine, filenames ...string) {
 	// it is mandatory to allow parallel access, otherwise, bizarre errors occurs
 	dbDB_inMemory.SetMaxOpenConns(1)
 
-	gong_controllers.RegisterControllers(r)
+	test2_controllers.RegisterControllers(r)
 }
