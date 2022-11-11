60bbbb6e66638f66da71367afec7c972f1c3266d
peugeot <thomaspeugeot@yahoo.fr>
2022-11-11 19:12:19 +0100

    Have a generated API for serializing to a tabular interface #169

--- test/go/tests/parse_tests/input/stage.go
+++ test/go/tests/parse_tests/input/stage.go
@@ -19,15 +19,14 @@ func stageInjection() {
 
 	// Declarations of staged instances of Astruct
 	__Astruct__000000_A1 := (&models.Astruct{Name: "A1"}).Stage()
-	_ = __Astruct__000000_A1
-	//	__Astruct__000000_A1.Name = `A1`
-	//	__Astruct__000000_A1.Date, _ = time.Parse("2006-01-02 15:04:05.999999999 -0700 MST", "2022-11-14 03:08:21 +0000 +0000")
-	//	__Astruct__000000_A1.Booleanfield = false
-	//	__Astruct__000000_A1.Aenum = models.ENUM_VAL1
-	//	__Astruct__000000_A1.Aenum_2 = models.ENUM_VAL2
-	//	__Astruct__000000_A1.Benum = models.BENUM_VAL2
-	//	__Astruct__000000_A1.CEnum = models.CENUM_VAL2
-	//	__Astruct__000000_A1.CName = `CName1
+	__Astruct__000000_A1.Name = `A1`
+	__Astruct__000000_A1.Date, _ = time.Parse("2006-01-02 15:04:05.999999999 -0700 MST", "2022-11-14 03:08:21 +0000 +0000")
+	__Astruct__000000_A1.Booleanfield = false
+	__Astruct__000000_A1.Aenum = models.ENUM_VAL1
+	__Astruct__000000_A1.Aenum_2 = models.ENUM_VAL2
+	__Astruct__000000_A1.Benum = models.BENUM_VAL2
+	__Astruct__000000_A1.CEnum = models.CENUM_VAL2
+	__Astruct__000000_A1.CName = `CName1
 	//
 	// Second Line`
 }
