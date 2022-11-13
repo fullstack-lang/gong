635112a41a38891d53fc45fa39b7048f6b35bbbc
peugeot <thomaspeugeot@yahoo.fr>
2022-11-13 12:42:18 +0100

    DEvelop a parser of generated stage go objects #206 - before Lhs

--- go/golang/gong_ast_generator.go
+++ go/golang/gong_ast_generator.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"sort"
 	"strings"
 
 	"github.com/fullstack-lang/gong/go/models"
@@ -12,9 +13,82 @@ import (
 	"golang.org/x/text/language"
 )
 
+// insertion points are places where the code is
+// generated per gong struct
+type ModelGongAstStructInsertionId int
+
+const (
+	ModelGongAstGenericMaps ModelGongAstStructInsertionId = iota
+	ModelGongAstGenericLhs
+	ModelGongAstStructInsertionsNb
+)
+
+var ModelGongAstStructSubTemplateCode map[ModelGongAstStructInsertionId]string = // new line
+map[ModelGongAstStructInsertionId]string{
+	ModelGongAstGenericMaps: `
+var __gong__map_{{Structname}} = make(map[string]*{{Structname}})`,
+	ModelGongAstGenericLhs: `
+									case "{{Structname}}":
+										instance{{Structname}} := (&{{Structname}}{Name: instanceName}).Stage()
+										instance = any(instance{{Structname}})
+										__gong__map_{{Structname}}[identifier] = instance{{Structname}}`,
+}
+
 func GongAstGenerator(modelPkg *models.ModelPkg, pkgPath string) {
 
 	codeGO := GongAstTemplate
+
+	subStructCodes := make(map[ModelGongAstStructInsertionId]string)
+	for subStructTemplate := range ModelGongAstStructSubTemplateCode {
+		subStructCodes[subStructTemplate] = ""
+	}
+
+	// sort gong structs per name (for reproductibility)
+	gongStructs := []*models.GongStruct{}
+	for _, _struct := range modelPkg.GongStructs {
+		gongStructs = append(gongStructs, _struct)
+	}
+	sort.Slice(gongStructs[:], func(i, j int) bool {
+		return gongStructs[i].Name < gongStructs[j].Name
+	})
+
+	for _, gongStruct := range gongStructs {
+
+		if !gongStruct.HasNameField() {
+			continue
+		}
+
+		for subStructTemplate := range ModelGongAstStructSubTemplateCode {
+			fieldCode := ""
+			fieldNameString := ""
+			fieldNameInt := ""
+			fieldNameBool := ""
+			fieldNameFloat64 := ""
+			fieldNameDate := ""
+
+			generatedCodeFromSubTemplate := models.Replace8(ModelGongAstStructSubTemplateCode[subStructTemplate],
+				"{{structname}}", strings.ToLower(gongStruct.Name),
+				"{{Structname}}", gongStruct.Name,
+				"{{FieldCode}}", fieldCode,
+				"{{FieldNameString}}", fieldNameString,
+				"{{FieldNameInt}}", fieldNameInt,
+				"{{FieldNameFloat64}}", fieldNameFloat64,
+				"{{FieldNameDate}}", fieldNameDate,
+				"{{FieldNameBoolean}}", fieldNameBool,
+			)
+
+			subStructCodes[subStructTemplate] += generatedCodeFromSubTemplate
+		}
+	}
+
+	for insertionPerStructId := ModelGongAstStructInsertionId(0); insertionPerStructId < ModelGongAstStructInsertionsNb; insertionPerStructId++ {
+		toReplace := "{{" + string(rune(insertionPerStructId)) + "}}"
+		isPresent := strings.Contains(codeGO, toReplace)
+		if !isPresent {
+			log.Fatal("insertion point " + toReplace + " not found in template")
+		}
+		codeGO = strings.ReplaceAll(codeGO, toReplace, subStructCodes[insertionPerStructId])
+	}
 
 	caserEnglish := cases.Title(language.English)
 	codeGO = models.Replace4(codeGO,
