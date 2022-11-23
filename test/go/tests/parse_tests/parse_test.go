8bb568ca6612d663804f65523c00cedbda9715ec
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2022-11-23 17:03:06 +0100

    Before automatic generation

--- test/go/tests/parse_tests/parse_test.go
+++ test/go/tests/parse_tests/parse_test.go
@@ -2,15 +2,9 @@ package parsetests_test
 
 import (
 	"fmt"
-	"go/ast"
-	"go/parser"
-	"go/token"
 	"log"
 	"os"
-	"path/filepath"
-	"strings"
 	"testing"
-	"time"
 
 	"github.com/fullstack-lang/gong/test/go/fullstack"
 	"github.com/fullstack-lang/gong/test/go/models"
@@ -23,94 +17,7 @@ func TestParseTest(t *testing.T) {
 	r := gin.Default()
 	fullstack.Init(r)
 
-	fileOfInterest, err := filepath.Abs("./input/stage.go")
-	if err != nil {
-		log.Panic("Path does not exist %s ;" + fileOfInterest)
-	}
-
-	fset := token.NewFileSet()
-	startParser := time.Now()
-	inFile, errParser := parser.ParseFile(fset, fileOfInterest, nil, parser.ParseComments)
-	log.Printf("Parser took %s", time.Since(startParser))
-
-	if errParser != nil {
-		log.Panic("Unable to parser ", errParser.Error())
-	}
-
-	// astCoordinate := "File "
-	// log.Println(// astCoordinate)
-	for _, decl := range inFile.Decls {
-		switch decl := decl.(type) {
-		case *ast.FuncDecl:
-			funcDecl := decl
-			// astCoordinate := // astCoordinate + "\tFunction " + funcDecl.Name.Name
-			if name := funcDecl.Name; name != nil {
-				isOfInterest := strings.Contains(funcDecl.Name.Name, "Injection")
-				if !isOfInterest {
-					continue
-				}
-				// log.Println(// astCoordinate)
-			}
-			if doc := funcDecl.Doc; doc != nil {
-				// astCoordinate := // astCoordinate + "\tDoc"
-				for _, comment := range doc.List {
-					_ = comment
-					// astCoordinate := // astCoordinate + "\tComment: " + comment.Text
-					// log.Println(// astCoordinate)
-				}
-			}
-			if body := funcDecl.Body; body != nil {
-				// astCoordinate := // astCoordinate + "\tBody: "
-				for _, stmt := range body.List {
-					switch stmt := stmt.(type) {
-					case *ast.ExprStmt:
-						exprStmt := stmt
-						// astCoordinate := // astCoordinate + "\tExprStmt: "
-						switch expr := exprStmt.X.(type) {
-						case *ast.CallExpr:
-							// astCoordinate := // astCoordinate + "\tCallExpr: "
-							callExpr := expr
-							switch fun := callExpr.Fun.(type) {
-							case *ast.Ident:
-								ident := fun
-								_ = ident
-								// astCoordinate := // astCoordinate + "\tIdent: " + ident.Name
-								// log.Println(// astCoordinate)
-							}
-						}
-					case *ast.AssignStmt:
-						astCoordinate := "\tAssignStmt: "
-						// log.Println(// astCoordinate)
-						assignStmt := stmt
-						instance, id, gongstruct, fieldName := models.UnmarshallGongstructStaging(assignStmt, astCoordinate)
-						_ = instance
-						_ = id
-						_ = gongstruct
-						_ = fieldName
-					}
-				}
-			}
-		case *ast.GenDecl:
-			genDecl := decl
-			// log.Println("\tAST GenDecl: ")
-			if doc := genDecl.Doc; doc != nil {
-				for _, comment := range doc.List {
-					_ = comment
-					// log.Println("\tAST Comment: ", comment.Text)
-				}
-			}
-			for _, spec := range genDecl.Specs {
-				switch spec := spec.(type) {
-				case *ast.ImportSpec:
-					importSpec := spec
-					if path := importSpec.Path; path != nil {
-						// log.Println("\t\tAST Path: ", path.Value)
-					}
-				}
-			}
-		}
-
-	}
+	models.ParseAstFile("./input/stage.go")
 
 	// serialize into another file
 	out_file, err := os.Create(fmt.Sprintf("./out/%s.go", "out"))
