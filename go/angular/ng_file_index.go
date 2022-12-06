b2725f11a6cdbbd7d09ef00d09a75e38c9983015
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2022-12-06 17:54:14 +0100

    In the front, replace packages "typeface-open-sans" and "typeface-roboto" with @frontsource/roboto Fixes #226 - manual

--- go/angular/ng_file_index.go
+++ go/angular/ng_file_index.go
@@ -1,4 +1,6 @@
-<!doctype html>
+package angular
+
+const NgFileIndex = `<!doctype html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
@@ -6,8 +8,10 @@
   <base href="/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
+  <!-- This overides the default generation by ng add @angular/material (which import online fonts) in order to use offline fonts -->
 </head>
 <body>
   <app-root></app-root>
 </body>
 </html>
+`
