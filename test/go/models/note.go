5fe60316d39bb7171162d429764b60875941bee2
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2023-01-03 18:25:25 +0100

    Add Doc Link to gongnote Fixes #236 - Modifiy example

--- test/go/models/note.go
+++ test/go/models/note.go
@@ -1,9 +1,9 @@
 package models
 
-// GONGDOC(note on the organization between line and points):
+// GONGDOC([NoteOnOrganisation]):
 // github.com/fullstack-lang/test/go/models is a model that showcases gong possibilites
 //
-// Astruct is associated with Bstruct via 2 kinds of association
+// [Astruct] is associated with [Bstruct] via 2 kinds of association
 // - a N-0..1 association (directly)
-// - a N-M association via AstructBstructUse
-const NoteOnOrganisation = "Note on organisation of Astruct and Bstruct"
+// - a N-M association via [Astruct.AstructBstructUse]
+const NoteOnOrganisation = ""
