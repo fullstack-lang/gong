534280fd9cfcd4c34cad73122ef3cbc8155bf66f
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2023-01-06 15:17:25 +0100

    well formatted generated code for embed.FS

--- go/golang/go_file_projects.go
+++ go/golang/go_file_projects.go
@@ -6,6 +6,7 @@ import "embed"
 
 // Projects is the export of the angular source code. This allows for go vendoring to import ng sources
 // and align go source and ng source of the gong stack
+//
 //go:embed all:*
 var Projects embed.FS
 `
