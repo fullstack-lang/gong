5f023549b58882c479f41fe15ce9e8677afad0bd
peugeot <thomaspeugeot@yahoo.fr>
2023-01-12 03:27:40 +0100

    Add a `//gong:ident <meta_package>.<Meta Struct>]` comment in gongstruct field assignment #238 - moved enum in a separate file

--- go/golang/model_file_gong.go
+++ go/golang/model_file_gong.go
@@ -48,60 +48,6 @@ const (
 	ModelGongStructInsertionsNb
 )
 
-// insertion code for all enums
-type ModelGongEnumInsertionId int
-
-const (
-	// iota + 40 is to separate the insertion code of gongstruct from insertion code of gongenum
-	ModelGongEnumUtilityFunctions ModelGongEnumInsertionId = iota + 40
-	ModelGongEnumInsertionsNb
-)
-
-var ModelGongEnumSubTemplateCode map[ModelGongEnumInsertionId]string = // new line
-map[ModelGongEnumInsertionId]string{
-	ModelGongEnumUtilityFunctions: `
-// Utility function for {{EnumName}}
-// if enum values are string, it is stored with the value
-// if enum values are int, they are stored with the code of the value
-func ({{enumName}} {{EnumName}}) To{{Type}}() (res {{type}}) {
-
-	// migration of former implementation of enum
-	switch {{enumName}} {
-	// insertion code per enum code{{ToStringPerCodeCode}}
-	}
-	return
-}
-
-func ({{enumName}} *{{EnumName}}) From{{Type}}(input {{type}}) (err error) {
-
-	switch input {
-	// insertion code per enum code{{FromStringPerCodeCode}}
-	default:
-		return errUnkownEnum
-	}
-	return
-}
-
-func ({{enumName}} *{{EnumName}}) FromCodeString(input string) (err error) {
-
-	switch input {
-	// insertion code per enum code{{FromCodeStringPerCodeCode}}
-	default:
-		return errUnkownEnum
-	}
-	return
-}
-
-func ({{enumName}} *{{EnumName}}) ToCodeString() (res string) {
-
-	switch *{{enumName}} {
-	// insertion code per enum code{{ToCodeStringPerCodeCode}}
-	}
-	return
-}
-`,
-}
-
 var ModelGongStructSubTemplateCode map[ModelGongStructInsertionId]string = // new line
 map[ModelGongStructInsertionId]string{
 	ModelGongStructInsertionCommitCheckout: `
@@ -365,7 +311,6 @@ const (
 	GongFileFieldSubTmplAssociationNameSliceOfPointersField
 	GongFileFieldSubTmplAssociationNameEnclosingCompositePointerField
 	GongFileFieldSubTmplAssociationNameCompositePointerField
-	GongFileFieldSubTmplAssociationNameCompositeSliceOfPointersField
 
 	GongFileFieldSubTmplPointerFieldAssociationMapFunction
 	GongFileFieldSubTmplSliceOfPointersFieldAssociationMapFunction
@@ -434,12 +379,6 @@ map[GongFilePerStructSubTemplateId]string{
 				//
 				{{FieldName}}: &{{AssocStructName}}{Name: "{{FieldName}}"},`,
 
-	GongFileFieldSubTmplAssociationNameCompositeSliceOfPointersField: `
-			// field is initialized with an instance of {{AssocStructName}} ({{AssocCompositeStructName}} as it is a composite) with the name of the field
-			{{AssocCompositeStructName}}: {{AssocCompositeStructName}}{
-				{{FieldName}}: []*{{AssocStructName}}{{Name: "{{FieldName}}"}},
-			},`,
-
 	GongFileFieldSubTmplPointerFieldAssociationMapFunction: `
 func (stageStruct *StageStruct) CreateReverseMap_{{Structname}}_{{FieldName}}() (res map[*{{AssocStructName}}][]*{{Structname}}) {
 	res = make(map[*{{AssocStructName}}][]*{{Structname}})
@@ -503,33 +442,6 @@ func (stageStruct *StageStruct) CreateReverseMap_{{Structname}}_{{FieldName}}() 
 	return
 }
 `,
-}
-
-// gongenum value template
-type GongModelEnumValueSubTemplateId int
-
-const (
-	GongModelEnumValueFromString GongModelEnumValueSubTemplateId = iota
-	GongModelEnumValueFromCodeString
-	GongModelEnumValueToString
-	GongModelEnumValueToCodeString
-)
-
-var GongModelEnumValueSubTemplateCode map[GongModelEnumValueSubTemplateId]string = // declaration of the sub templates
-map[GongModelEnumValueSubTemplateId]string{
-
-	GongModelEnumValueFromString: `
-	case {{GongEnumValue}}:
-		*{{enumName}} = {{GongEnumCode}}`,
-	GongModelEnumValueFromCodeString: `
-	case "{{GongEnumCode}}":
-		*{{enumName}} = {{GongEnumCode}}`,
-	GongModelEnumValueToString: `
-	case {{GongEnumCode}}:
-		res = {{GongEnumValue}}`,
-	GongModelEnumValueToCodeString: `
-	case {{GongEnumCode}}:
-		res = "{{GongEnumCode}}"`,
 }
 
 func CodeGeneratorModelGong(
@@ -716,73 +628,6 @@ func CodeGeneratorModelGong(
 	for insertionPerStructId := ModelGongStructInsertionId(0); insertionPerStructId < ModelGongStructInsertionsNb; insertionPerStructId++ {
 		toReplace := "{{" + string(rune(insertionPerStructId)) + "}}"
 		codeGO = strings.ReplaceAll(codeGO, toReplace, subStructCodes[insertionPerStructId])
-	}
-
-	subEnumCodes := make(map[ModelGongEnumInsertionId]string)
-	for subEnumTemplate := range ModelGongEnumSubTemplateCode {
-		subEnumCodes[subEnumTemplate] = ""
-	}
-
-	// sort gong enums per name (for reproductibility)
-	gongEnums := []*models.GongEnum{}
-	for _, _enum := range mdlPkg.GongEnums {
-		gongEnums = append(gongEnums, _enum)
-	}
-	sort.Slice(gongEnums[:], func(i, j int) bool {
-		return gongEnums[i].Name < gongEnums[j].Name
-	})
-
-	for _, gongEnum := range gongEnums {
-
-		for subEnumTemplate := range ModelGongEnumSubTemplateCode {
-
-			codeFromStringPerGongValue := ""
-			codeFromCodeStringPerGongValue := ""
-			codeToStringPerGongValue := ""
-			codeToCodeStringPerGongValue := ""
-
-			for _, enumValue := range gongEnum.GongEnumValues {
-				codeFromStringPerGongValue += models.Replace2(GongModelEnumValueSubTemplateCode[GongModelEnumValueFromString],
-					"{{GongEnumValue}}", enumValue.Value,
-					"{{GongEnumCode}}", enumValue.Name)
-				codeFromCodeStringPerGongValue += models.Replace1(GongModelEnumValueSubTemplateCode[GongModelEnumValueFromCodeString],
-					"{{GongEnumCode}}", enumValue.Name)
-				codeToStringPerGongValue += models.Replace2(GongModelEnumValueSubTemplateCode[GongModelEnumValueToString],
-					"{{GongEnumValue}}", enumValue.Value,
-					"{{GongEnumCode}}", enumValue.Name)
-
-				codeToCodeStringPerGongValue += models.Replace2(GongModelEnumValueSubTemplateCode[GongModelEnumValueToCodeString],
-					"{{GongEnumValue}}", enumValue.Value,
-					"{{GongEnumCode}}", enumValue.Name)
-			}
-
-			generatedCodeFromSubTemplate := models.Replace4(ModelGongEnumSubTemplateCode[subEnumTemplate],
-				"{{ToStringPerCodeCode}}", codeToStringPerGongValue,
-				"{{FromStringPerCodeCode}}", codeFromStringPerGongValue,
-				"{{FromCodeStringPerCodeCode}}", codeFromCodeStringPerGongValue,
-				"{{ToCodeStringPerCodeCode}}", codeToCodeStringPerGongValue)
-
-			var typeOfEnumAsString string
-			if gongEnum.Type == models.String {
-				typeOfEnumAsString = "String"
-			} else {
-				typeOfEnumAsString = "Int"
-			}
-
-			generatedCodeFromSubTemplate = models.Replace4(generatedCodeFromSubTemplate,
-				"{{enumName}}", strings.ToLower(gongEnum.Name),
-				"{{EnumName}}", gongEnum.Name,
-				"{{Type}}", typeOfEnumAsString,
-				"{{type}}", strings.ToLower(typeOfEnumAsString))
-
-			subEnumCodes[subEnumTemplate] += generatedCodeFromSubTemplate
-		}
-	}
-
-	// substitutes {{<<insertionPerEnumId points>>}} stuff with generated code
-	for insertionPerEnumId := ModelGongEnumUtilityFunctions; insertionPerEnumId < ModelGongEnumInsertionsNb; insertionPerEnumId++ {
-		toReplace := "{{" + string(rune(insertionPerEnumId)) + "}}"
-		codeGO = strings.ReplaceAll(codeGO, toReplace, subEnumCodes[insertionPerEnumId])
 	}
 
 	caserEnglish := cases.Title(language.English)
--- test/go/models/gong.go
+++ test/go/models/gong.go
@@ -1189,14 +1189,6 @@ func GetAssociationName[Type Gongstruct]() *Type {
 			Anarrayofb2Use: []*AstructBstruct2Use{{Name: "Anarrayofb2Use"}},
 			// field is initialized with an instance of Astruct with the name of the field
 			AnAstruct: &Astruct{Name: "AnAstruct"},
-			// field is initialized with Estruct as it is a composite
-			Estruct: Estruct{
-				// per field init
-				//
-				Dstruct3: &Dstruct{Name: "Dstruct3"},
-				//
-				Dstruct4: &Dstruct{Name: "Dstruct4"},
-			},
 			// field is initialized with Cstruct as it is a composite
 			Cstruct: Cstruct{
 				// per field init
@@ -1208,6 +1200,14 @@ func GetAssociationName[Type Gongstruct]() *Type {
 				Dstruct: &Dstruct{Name: "Dstruct"},
 				//
 				Dstruct2: &Dstruct{Name: "Dstruct2"},
+			},
+			// field is initialized with Estruct as it is a composite
+			Estruct: Estruct{
+				// per field init
+				//
+				Dstruct3: &Dstruct{Name: "Dstruct3"},
+				//
+				Dstruct4: &Dstruct{Name: "Dstruct4"},
 			},
 		}).(*Type)
 	case AstructBstruct2Use:
@@ -1741,174 +1741,4 @@ func GetFieldStringValue[Type Gongstruct](instance Type, fieldName string) (res 
 	}
 	return
 }
-
-// insertion point of enum utility functions
-// Utility function for AEnumType
-// if enum values are string, it is stored with the value
-// if enum values are int, they are stored with the code of the value
-func (aenumtype AEnumType) ToString() (res string) {
-
-	// migration of former implementation of enum
-	switch aenumtype {
-	// insertion code per enum code
-	case ENUM_VAL1:
-		res = "ENUM_VAL1_NOT_THE_SAME"
-	case ENUM_VAL2:
-		res = "ENUM_VAL2"
-	}
-	return
-}
-
-func (aenumtype *AEnumType) FromString(input string) (err error) {
-
-	switch input {
-	// insertion code per enum code
-	case "ENUM_VAL1_NOT_THE_SAME":
-		*aenumtype = ENUM_VAL1
-	case "ENUM_VAL2":
-		*aenumtype = ENUM_VAL2
-	default:
-		return errUnkownEnum
-	}
-	return
-}
-
-func (aenumtype *AEnumType) FromCodeString(input string) (err error) {
-
-	switch input {
-	// insertion code per enum code
-	case "ENUM_VAL1":
-		*aenumtype = ENUM_VAL1
-	case "ENUM_VAL2":
-		*aenumtype = ENUM_VAL2
-	default:
-		return errUnkownEnum
-	}
-	return
-}
-
-func (aenumtype *AEnumType) ToCodeString() (res string) {
-
-	switch *aenumtype {
-	// insertion code per enum code
-	case ENUM_VAL1:
-		res = "ENUM_VAL1"
-	case ENUM_VAL2:
-		res = "ENUM_VAL2"
-	}
-	return
-}
-
-// Utility function for BEnumType
-// if enum values are string, it is stored with the value
-// if enum values are int, they are stored with the code of the value
-func (benumtype BEnumType) ToString() (res string) {
-
-	// migration of former implementation of enum
-	switch benumtype {
-	// insertion code per enum code
-	case BENUM_VAL1:
-		res = "BENUM_VAL1_NOT_THE_SAME"
-	case BENUM_VAL2:
-		res = "BENUM_VAL2"
-	}
-	return
-}
-
-func (benumtype *BEnumType) FromString(input string) (err error) {
-
-	switch input {
-	// insertion code per enum code
-	case "BENUM_VAL1_NOT_THE_SAME":
-		*benumtype = BENUM_VAL1
-	case "BENUM_VAL2":
-		*benumtype = BENUM_VAL2
-	default:
-		return errUnkownEnum
-	}
-	return
-}
-
-func (benumtype *BEnumType) FromCodeString(input string) (err error) {
-
-	switch input {
-	// insertion code per enum code
-	case "BENUM_VAL1":
-		*benumtype = BENUM_VAL1
-	case "BENUM_VAL2":
-		*benumtype = BENUM_VAL2
-	default:
-		return errUnkownEnum
-	}
-	return
-}
-
-func (benumtype *BEnumType) ToCodeString() (res string) {
-
-	switch *benumtype {
-	// insertion code per enum code
-	case BENUM_VAL1:
-		res = "BENUM_VAL1"
-	case BENUM_VAL2:
-		res = "BENUM_VAL2"
-	}
-	return
-}
-
-// Utility function for CEnumTypeInt
-// if enum values are string, it is stored with the value
-// if enum values are int, they are stored with the code of the value
-func (cenumtypeint CEnumTypeInt) ToInt() (res int) {
-
-	// migration of former implementation of enum
-	switch cenumtypeint {
-	// insertion code per enum code
-	case CENUM_VAL1:
-		res = 0
-	case CENUM_VAL2:
-		res = 1
-	}
-	return
-}
-
-func (cenumtypeint *CEnumTypeInt) FromInt(input int) (err error) {
-
-	switch input {
-	// insertion code per enum code
-	case 0:
-		*cenumtypeint = CENUM_VAL1
-	case 1:
-		*cenumtypeint = CENUM_VAL2
-	default:
-		return errUnkownEnum
-	}
-	return
-}
-
-func (cenumtypeint *CEnumTypeInt) FromCodeString(input string) (err error) {
-
-	switch input {
-	// insertion code per enum code
-	case "CENUM_VAL1":
-		*cenumtypeint = CENUM_VAL1
-	case "CENUM_VAL2":
-		*cenumtypeint = CENUM_VAL2
-	default:
-		return errUnkownEnum
-	}
-	return
-}
-
-func (cenumtypeint *CEnumTypeInt) ToCodeString() (res string) {
-
-	switch *cenumtypeint {
-	// insertion code per enum code
-	case CENUM_VAL1:
-		res = "CENUM_VAL1"
-	case CENUM_VAL2:
-		res = "CENUM_VAL2"
-	}
-	return
-}
-
 // Last line of the template
