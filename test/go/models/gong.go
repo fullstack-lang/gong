583be08e0d31c398e633cbcdce95048b2eca2ee5
peugeot <thomaspeugeot@yahoo.fr>
2023-01-23 01:30:43 +0100

    cosmetic changes

--- test/go/models/gong.go
+++ test/go/models/gong.go
@@ -86,12 +86,12 @@ type StageStruct struct { // insertion point for definition of arrays registerin
 	// store meta package import
 	MetaPackageImportPath  string
 	MetaPackageImportAlias string
-	Map_DocLink_Renaming   map[string]__GONG__Identifier
+	Map_DocLink_Renaming   map[string]GONG__Identifier
 }
 
-type __GONG__Identifier struct {
+type GONG__Identifier struct {
 	ident string
-	__GONG__ExpressionType
+	GONG__ExpressionType
 }
 
 type OnInitCommitInterface interface {
