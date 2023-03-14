4bc578935e09d360105c66b94315033e1fe3e059
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2023-03-14 18:13:09 +0100

    Creates a fullstack.ServeStaticFiles that serves static files Fixes #308 - first try

--- test/go/cmd/test/main.go
+++ test/go/cmd/test/main.go
@@ -1,25 +1,20 @@
 package main
 
 import (
-	"embed"
 	"flag"
 	"fmt"
-	"io/fs"
 	"log"
-	"net/http"
 	"os"
 	"strings"
 
 	"github.com/gin-contrib/cors"
-	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 
+	"github.com/fullstack-lang/gong/test"
 	"github.com/fullstack-lang/gong/test/go/fullstack"
 	"github.com/fullstack-lang/gong/test/go/models"
 
 	gongdoc_load "github.com/fullstack-lang/gongdoc/go/load"
-
-	test "github.com/fullstack-lang/gong/test"
 )
 
 var (
@@ -146,34 +141,8 @@ func main() {
 		*embeddedDiagrams,
 		&stage.Map_GongStructName_InstancesNb)
 
-	// insertion point for serving the static file
-	// provide the static route for the angular pages
-	r.Use(static.Serve("/", EmbedFolder(test.NgDistNg, "ng/dist/ng")))
-	r.NoRoute(func(c *gin.Context) {
-		fmt.Println(c.Request.URL.Path, "doesn't exists, redirect on /")
-		c.Redirect(http.StatusMovedPermanently, "/")
-		c.Abort()
-	})
+	fullstack.ServeStaticFiles(r)
 
 	log.Printf("Server ready serve on localhost:8080")
 	r.Run()
-}
-
-type embedFileSystem struct {
-	http.FileSystem
-}
-
-func (e embedFileSystem) Exists(prefix string, path string) bool {
-	_, err := e.Open(path)
-	return err == nil
-}
-
-func EmbedFolder(fsEmbed embed.FS, targetPath string) static.ServeFileSystem {
-	fsys, err := fs.Sub(fsEmbed, targetPath)
-	if err != nil {
-		panic(err)
-	}
-	return embedFileSystem{
-		FileSystem: http.FS(fsys),
-	}
 }
