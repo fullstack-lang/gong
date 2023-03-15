a365a7495553ff919e4f0ff6403babd7df86a7ce
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2023-03-15 15:05:12 +0100

    Generates routes at sidebar init with stack path #307 - progres

--- test/ng/projects/test/src/lib/app-routing.module.ts
+++ test/ng/projects/test/src/lib/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [ // insertion point for routes declarations
-
-];
+const routes: Routes = [];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
