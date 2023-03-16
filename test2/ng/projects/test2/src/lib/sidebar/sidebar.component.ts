2a601d19dcff91d0debe6656ae8f6b88f62fa669
peugeot <thomaspeugeot@yahoo.fr>
2023-03-16 02:36:53 +0100

    Generates routes at sidebar init with stack path #307 on test2

--- test2/ng/projects/test2/src/lib/sidebar/sidebar.component.ts
+++ test2/ng/projects/test2/src/lib/sidebar/sidebar.component.ts
@@ -12,6 +12,8 @@ import { GongstructSelectionService } from '../gongstruct-selection.service'
 
 // insertion point for per struct import code
 
+import { RouteService } from '../route-service';
+
 /**
  * Types of a GongNode / GongFlatNode
  */
@@ -155,6 +157,8 @@ export class SidebarComponent implements OnInit {
     private gongstructSelectionService: GongstructSelectionService,
 
     // insertion point for per struct service declaration
+
+    private routeService: RouteService,
   ) { }
 
   ngOnDestroy() {
@@ -165,6 +169,10 @@ export class SidebarComponent implements OnInit {
   ngOnInit(): void {
 
     console.log("Sidebar init: " + this.GONG__StackPath)
+
+    // add the routes that will used by this side panel component and
+    // by the component that are called from this component
+    this.routeService.addDataPanelRoutes(this.GONG__StackPath)
 
     this.subscription = this.gongstructSelectionService.gongtructSelected$.subscribe(
       gongstructName => {
@@ -234,9 +242,11 @@ export class SidebarComponent implements OnInit {
    * @param path for the outlet selection
    */
   setTableRouterOutlet(path: string) {
+    let outletName = this.routeService.getTableOutlet(this.GONG__StackPath)
+    let fullPath = this.routeService.getPathRoot() + "-" + path
     this.router.navigate([{
       outlets: {
-        github_com_fullstack_lang_gong_test2_go_table: ["github_com_fullstack_lang_gong_test2_go-" + path]
+        outletName: [fullPath]
       }
     }]);
   }
@@ -248,34 +258,41 @@ export class SidebarComponent implements OnInit {
   setTableRouterOutletFromTree(path: string, type: GongNodeType, structName: string, id: number) {
 
     if (type == GongNodeType.STRUCT) {
-      this.router.navigate([{
-        outlets: {
-          github_com_fullstack_lang_gong_test2_go_table: ["github_com_fullstack_lang_gong_test2_go-" + path.toLowerCase(), this.GONG__StackPath]
-        }
-      }]);
+      let outletName = this.routeService.getTableOutlet(this.GONG__StackPath)
+      let fullPath = this.routeService.getPathRoot() + "-" + path.toLowerCase()
+      let outletConf: any = {}
+      outletConf[outletName] = [fullPath, this.GONG__StackPath]
+
+      this.router.navigate([{ outlets: outletConf }])
     }
 
     if (type == GongNodeType.INSTANCE) {
-      this.router.navigate([{
-        outlets: {
-          github_com_fullstack_lang_gong_test2_go_editor: ["github_com_fullstack_lang_gong_test2_go-" + structName.toLowerCase() + "-detail", id]
-        }
-      }]);
+      let outletName = this.routeService.getEditorOutlet(this.GONG__StackPath)
+      let fullPath = this.routeService.getPathRoot() + "-" + structName.toLowerCase() + "-detail"
+
+      let outletConf: any = {}
+      outletConf[outletName] = [fullPath, id, this.GONG__StackPath]
+
+      this.router.navigate([{ outlets: outletConf }])
     }
   }
 
   setEditorRouterOutlet(path: string) {
-    this.router.navigate([{
-      outlets: {
-        github_com_fullstack_lang_gong_test2_go_editor: ["github_com_fullstack_lang_gong_test2_go-" + path.toLowerCase(), this.GONG__StackPath]
-      }
-    }]);
+    let outletName = this.routeService.getEditorOutlet(this.GONG__StackPath)
+    let fullPath = this.routeService.getPathRoot() + "-" + path.toLowerCase()
+    
+    let outletConf : any = {}
+    outletConf[outletName] = [fullPath, this.GONG__StackPath]
+    
+    this.router.navigate([ { outlets: outletConf } ]);
   }
 
   setEditorSpecialRouterOutlet(node: GongFlatNode) {
+    let outletName = this.routeService.getEditorOutlet(this.GONG__StackPath)
+    let fullPath = this.routeService.getPathRoot() + "-" + node.associatedStructName.toLowerCase() + "-adder"
     this.router.navigate([{
       outlets: {
-        github_com_fullstack_lang_gong_test2_go_editor: ["github_com_fullstack_lang_gong_test2_go-" + node.associatedStructName.toLowerCase() + "-adder", node.id, node.structName, node.associationField, this.GONG__StackPath]
+        outletName: [fullPath, node.id, node.structName, node.associationField, this.GONG__StackPath]
       }
     }]);
   }
