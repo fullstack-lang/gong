3e78f037aefd514501280d5837b817d0ff854907
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2023-03-17 16:55:51 +0100

    put serve static file in a separate package than fullstack #311 - generation

--- go/golang/fullstack_new_stack_instance.go
+++ go/golang/fullstack_new_stack_instance.go
@@ -8,6 +8,11 @@ import (
 	"{{PkgPathRoot}}/orm"
 
 	"github.com/gin-gonic/gin"
+
+	// this will import the angular front end source code directory (versionned with git) in the vendor directory
+	// this path will be included in the "tsconfig.json" front end compilation paths
+	// to include this stack front end code
+	_ "{{PkgPathAboveRoot}}/ng/projects"
 )
 
 // NewStackInstance creates a new stack instance from the Stack Model
