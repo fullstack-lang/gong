7dfee3a6654c1321aa79d3a3ad0af2a4892c131f
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2023-03-17 17:26:32 +0100

    new gongc

--- go/fullstack/new_stack_instance.go
+++ go/fullstack/new_stack_instance.go
@@ -1,19 +1,10 @@
 package fullstack
 
 import (
-	"embed"
-	"fmt"
-	"io/fs"
-	"net/http"
-	"os"
-
-	"github.com/fullstack-lang/gong"
 	"github.com/fullstack-lang/gong/go/controllers"
 	"github.com/fullstack-lang/gong/go/models"
 	"github.com/fullstack-lang/gong/go/orm"
 
-	"github.com/gin-contrib/cors"
-	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 
 	// this will import the angular front end source code directory (versionned with git) in the vendor directory
@@ -55,46 +46,4 @@ func NewStackInstance(
 	controllers.Register(r)
 
 	return
-}
-
-
-func ServeStaticFiles(logGINFlag bool) (r *gin.Engine) {
-
-	// setup controlers
-	if !logGINFlag {
-		myfile, _ := os.Create("/tmp/server.log")
-		gin.DefaultWriter = myfile
-	}
-	r = gin.Default()
-	r.Use(cors.Default())
-
-	// insertion point for serving the static file
-	// provide the static route for the angular pages
-	r.Use(static.Serve("/", EmbedFolder(gong.NgDistNg, "ng/dist/ng")))
-	r.NoRoute(func(c *gin.Context) {
-		fmt.Println(c.Request.URL.Path, "doesn't exists, redirect on /")
-		c.Redirect(http.StatusMovedPermanently, "/")
-		c.Abort()
-	})
-
-	return
-}
-
-type embedFileSystem struct {
-	http.FileSystem
-}
-
-func (e embedFileSystem) Exists(prefix string, path string) bool {
-	_, err := e.Open(path)
-	return err == nil
-}
-
-func EmbedFolder(fsEmbed embed.FS, targetPath string) static.ServeFileSystem {
-	fsys, err := fs.Sub(fsEmbed, targetPath)
-	if err != nil {
-		panic(err)
-	}
-	return embedFileSystem{
-		FileSystem: http.FS(fsys),
-	}
 }
