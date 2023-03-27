7449b43901fcb55977fe1ebab103537d6b95e8c9
peugeot <thomaspeugeot@yahoo.fr>
2023-03-27 05:23:11 +0200

    updated default conf

---
+++ test/go/diagrams/NewDiagram_1.go
@@ -0,0 +1,83 @@
+package diagrams
+
+import (
+	"time"
+
+	"github.com/fullstack-lang/gongdoc/go/models"
+
+	// injection point for ident package import declaration
+	ref_models "github.com/fullstack-lang/gong/test/go/models"
+)
+
+// generated in order to avoid error in the package import
+// if there are no elements in the stage to marshall
+var ___dummy__Stage_NewDiagram_1 models.StageStruct
+var ___dummy__Time_NewDiagram_1 time.Time
+
+// Injection point for meta package dummy declaration
+var ___dummy__ref_models_NewDiagram_1 ref_models.StageStruct
+
+// currently, DocLink renaming is not enabled in gopls
+// the following map are devised to overcome this limitation
+// those maps and the processing code will be eleminated when
+// DocLink renaming will be enabled in gopls
+// [Corresponding Issue](https://github.com/golang/go/issues/57559)
+//
+// When parsed, those maps will help with the renaming process
+var map_DocLink_Identifier_NewDiagram_1 map[string]any = map[string]any{
+	// injection point for docLink to identifiers
+}
+
+// init might be handy if one want to have the data embedded in the binary
+// but it has to properly reference the Injection gateway in the main package
+// func init() {
+// 	_ = __Dummy_time_variable
+// 	InjectionGateway["NewDiagram_1"] = NewDiagram_1Injection
+// }
+
+// NewDiagram_1Injection will stage objects of database "NewDiagram_1"
+func NewDiagram_1Injection(stage *models.StageStruct) {
+
+	// Declaration of instances to stage
+
+	// Declarations of staged instances of Classdiagram
+	__Classdiagram__000000_NewDiagram_1 := (&models.Classdiagram{Name: `NewDiagram_1`}).Stage(stage)
+
+	// Declarations of staged instances of DiagramPackage
+
+	// Declarations of staged instances of Field
+
+	// Declarations of staged instances of GongEnumShape
+
+	// Declarations of staged instances of GongEnumValueEntry
+
+	// Declarations of staged instances of GongStructShape
+
+	// Declarations of staged instances of Link
+
+	// Declarations of staged instances of Node
+
+	// Declarations of staged instances of NoteShape
+
+	// Declarations of staged instances of NoteShapeLink
+
+	// Declarations of staged instances of Position
+
+	// Declarations of staged instances of Tree
+
+	// Declarations of staged instances of UmlState
+
+	// Declarations of staged instances of Umlsc
+
+	// Declarations of staged instances of Vertice
+
+	// Setup of values
+
+	// Classdiagram values setup
+	__Classdiagram__000000_NewDiagram_1.Name = `NewDiagram_1`
+	__Classdiagram__000000_NewDiagram_1.IsInDrawMode = false
+
+	// Setup of pointers
+}
+
+
--- vendor/github.com/fullstack-lang/gongdoc/ng/projects/gongdocdiagrams/src/lib/pkgelt-docs/pkgelt-docs.component.ts
+++ vendor/github.com/fullstack-lang/gongdoc/ng/projects/gongdocdiagrams/src/lib/pkgelt-docs/pkgelt-docs.component.ts
@@ -16,27 +16,6 @@ export class PkgeltDocsComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-
-    // the class diagram editor is setup with a component that has no data
-    // this component will fetch the new commits from the back
-    this.setEditorRouterOutlet()
     console.log("PkgeltDocsComponent->GONG__StackPath : ", this.GONG__StackPath)
   }
-
-  // set editor outlet
-  setEditorRouterOutlet() {
-
-    console.log("pkgElt setEditorRouterOutlet with GONG__StackPath", this.GONG__StackPath)
-
-    this.router.navigate([{
-      outlets: {
-        diagrameditor: ["classdiagram-editor", this.GONG__StackPath]
-      }
-    }]).catch(
-      reason => {
-        console.log(reason)
-      }
-    );
-  }
-
 }
