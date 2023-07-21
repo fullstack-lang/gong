122d11643579184b22fc4934c89a3c09fa260a0d
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2023-07-21 16:59:40 +0200

    Add a Data2 navigation component that uses gongtree and gongrouter #356 - manual over

--- test/go/data/button_impl_gongstruct.go
+++ test/go/data/button_impl_gongstruct.go
@@ -2,23 +2,33 @@ package data
 
 import (
 	"log"
+	"strings"
 
+	gong_models "github.com/fullstack-lang/gong/go/models"
+	gongrouter_models "github.com/fullstack-lang/gongrouter/go/models"
 	gongtree_buttons "github.com/fullstack-lang/gongtree/go/buttons"
 	gongtree_models "github.com/fullstack-lang/gongtree/go/models"
 )
 
 type ButtonImplGongstruct struct {
-
-	// type of button
-	Icon gongtree_buttons.ButtonType
+	gongStruct      *gong_models.GongStruct
+	Icon            gongtree_buttons.ButtonType
+	gongrouterStage *gongrouter_models.StageStruct
+	editorRouter    *gongrouter_models.Outlet
 }
 
 func NewButtonImplGongstruct(
+	gongStruct *gong_models.GongStruct,
 	icon gongtree_buttons.ButtonType,
+	gongrouterStage *gongrouter_models.StageStruct,
+	editorRouter *gongrouter_models.Outlet,
 ) (buttonImplGongstruct *ButtonImplGongstruct) {
 
 	buttonImplGongstruct = new(ButtonImplGongstruct)
 	buttonImplGongstruct.Icon = icon
+	buttonImplGongstruct.gongStruct = gongStruct
+	buttonImplGongstruct.editorRouter = editorRouter
+	buttonImplGongstruct.gongrouterStage = gongrouterStage
 
 	return
 }
@@ -28,4 +38,10 @@ func (buttonImpl *ButtonImplGongstruct) ButtonUpdated(
 	stageButton, front *gongtree_models.Button) {
 
 	log.Println("ButtonImplGongstruct: ButtonUpdated")
+
+	buttonImpl.editorRouter.Path =
+		"github_com_fullstack_lang_gong_test_go-" +
+			strings.ToLower(buttonImpl.gongStruct.Name) + "-adder"
+
+	buttonImpl.gongrouterStage.Commit()
 }
