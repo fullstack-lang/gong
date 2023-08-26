8c8f2e7a826ab9379ad5aadc3dab6265614a52b8
peugeot <thomaspeugeot@yahoo.fr>
2023-08-26 22:38:44 +0200

    Add material/form to data/table #366 - on the path to solving he problem

--- test/go/data/form_callback.go
+++ test/go/data/form_callback.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"log"
+	"time"
 
 	"github.com/fullstack-lang/gong/test/go/models"
 	table "github.com/fullstack-lang/gongtable/go/models"
@@ -28,11 +29,27 @@ func (astructFormCallback *AstructFormCallback) BeforeCommit(formStage *table.St
 	// get the formGroup
 	formGroup := formStage.FormGroups_mapString[table.FormGroupDefaultName.ToString()]
 
-	// get Name field
-	{
-		formDiv := formGroup.FormDivs[0]
-		astruct.Name = formDiv.FormFields[0].FormFieldString.Value
+	for _, formDiv := range formGroup.FormDivs {
+		switch formDiv.Name {
+		case "Name":
+			astruct.Name = formDiv.FormFields[0].FormFieldString.Value
+		case "Date":
+			date := formDiv.FormFields[0].FormFieldDate.Value
+
+			time := formDiv.FormFields[1].FormFieldTime.Value
+			astruct.Date = addTimeComponents(date, time)
+		}
+
 	}
 
 	astructFormCallback.stageOfInterest.Commit()
+}
+
+func addTimeComponents(x, y time.Time) time.Time {
+	h, m, s := y.Clock()
+	x = x.Add(time.Duration(h) * time.Hour)
+	x = x.Add(time.Duration(m) * time.Minute)
+	x = x.Add(time.Duration(s) * time.Second)
+	x = x.Add(time.Duration(y.Nanosecond()) * time.Nanosecond)
+	return x
 }
