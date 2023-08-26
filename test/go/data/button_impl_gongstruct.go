558237ad16245d93c9224cfbce3a47758319a386
peugeot <thomaspeugeot@yahoo.fr>
2023-08-26 21:43:50 +0200

    Add material/form to data/table #366 - manual for form creation

--- test/go/data/button_impl_gongstruct.go
+++ test/go/data/button_impl_gongstruct.go
@@ -5,23 +5,27 @@ import (
 	"log"
 
 	gong_models "github.com/fullstack-lang/gong/go/models"
+	table "github.com/fullstack-lang/gongtable/go/models"
 	gongtree_buttons "github.com/fullstack-lang/gongtree/go/buttons"
 	gongtree_models "github.com/fullstack-lang/gongtree/go/models"
 )
 
 type ButtonImplGongstruct struct {
-	gongStruct      *gong_models.GongStruct
-	Icon            gongtree_buttons.ButtonType
+	gongStruct *gong_models.GongStruct
+	Icon       gongtree_buttons.ButtonType
+	formStage  *table.StageStruct
 }
 
 func NewButtonImplGongstruct(
 	gongStruct *gong_models.GongStruct,
 	icon gongtree_buttons.ButtonType,
+	formStage *table.StageStruct,
 ) (buttonImplGongstruct *ButtonImplGongstruct) {
 
 	buttonImplGongstruct = new(ButtonImplGongstruct)
 	buttonImplGongstruct.Icon = icon
 	buttonImplGongstruct.gongStruct = gongStruct
+	buttonImplGongstruct.formStage = formStage
 
 	return
 }
@@ -31,4 +35,32 @@ func (buttonImpl *ButtonImplGongstruct) ButtonUpdated(
 	stageButton, front *gongtree_models.Button) {
 
 	log.Println("ButtonImplGongstruct: ButtonUpdated")
+
+	formStage := buttonImpl.formStage
+	formStage.Reset()
+	formStage.Commit()
+
+	formGroup := (&table.FormGroup{
+		Name: table.FormGroupDefaultName.ToString(),
+	}).Stage(formStage)
+
+	switch buttonImpl.gongStruct.Name {
+	case "Astruct":
+		formDiv := (&table.FormDiv{
+			Name: "Name",
+		}).Stage(formStage)
+		formGroup.FormDivs = append(formGroup.FormDivs, formDiv)
+		formField := (&table.FormField{
+			Name:        "Name",
+			Label:       "Name",
+			Placeholder: "Astruct",
+		}).Stage(formStage)
+		formDiv.FormFields = append(formDiv.FormFields, formField)
+
+		formFieldString := (&table.FormFieldString{
+			Name: "Name",
+		}).Stage(formStage)
+		formField.FormFieldString = formFieldString
+	}
+	formStage.Commit()
 }
