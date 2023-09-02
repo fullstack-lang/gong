9f6a5fc7bbda371fbc7cb6338b1236cf6d5509c9
peugeot <thomaspeugeot@yahoo.fr>
2023-09-02 11:39:22 +0200

    Add material/form to data/table #366 - associations 0..1

---
+++ test/go/data/form_div_select_to_field.go
@@ -0,0 +1,19 @@
+package data
+
+import (
+	"github.com/fullstack-lang/gong/test/go/models"
+	form "github.com/fullstack-lang/gongtable/go/models"
+)
+
+func FormDivSelectFieldToField[TF models.PointerToGongstruct, TFinfered models.Gongstruct](field *TF, stageOfInterest *models.StageStruct, formDiv *form.FormDiv) {
+
+	if formDiv.FormFields[0].FormFieldSelect.Value == nil {
+		*field = nil
+	} else {
+		for _instance := range *models.GetGongstructInstancesSet[TFinfered](stageOfInterest) {
+			if any(_instance).(TF).GetName() == formDiv.FormFields[0].FormFieldSelect.Value.GetName() {
+				*field = any(_instance).(TF)
+			}
+		}
+	}
+}
