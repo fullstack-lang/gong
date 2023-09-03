5c3770ea95494a9c8a9609172f0fab6e75934608
peugeot <thomaspeugeot@yahoo.fr>
2023-09-03 19:35:29 +0200

    OK for association

---
+++ test/go/data/association_slice_to_form.go
@@ -0,0 +1,120 @@
+// generated code - do not edit
+package data
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/fullstack-lang/gong/test/go/models"
+	gongtable_fullstack "github.com/fullstack-lang/gongtable/go/fullstack"
+	form "github.com/fullstack-lang/gongtable/go/models"
+	gongtable_models "github.com/fullstack-lang/gongtable/go/models"
+)
+
+// AssociationSliceToForm add a form div with 2 buttons
+// - one for selection
+// - one for sorting
+func AssociationSliceToForm[FieldType models.PointerToGongstruct](
+	fieldName string, field []FieldType,
+	stageOfInterest *models.StageStruct, formStage *form.StageStruct, formGroup *form.FormGroup,
+	r *gin.Engine,
+) {
+
+	formDiv := (&form.FormDiv{
+		Name: fieldName,
+	}).Stage(formStage)
+	formGroup.FormDivs = append(formGroup.FormDivs, formDiv)
+
+	formEditAssocButton := (&form.FormEditAssocButton{
+		Name:  fieldName,
+		Label: fieldName,
+	}).Stage(formStage)
+	formDiv.FormEditAssocButton = formEditAssocButton
+	onAssocEditon := NewOnAssocEditon(r, formStage, field, stageOfInterest)
+	formEditAssocButton.OnAssocEditon = onAssocEditon
+
+	formSortAssocButton := (&form.FormSortAssocButton{
+		Name:  fieldName,
+		Label: fieldName,
+	}).Stage(formStage)
+	formDiv.FormSortAssocButton = formSortAssocButton
+	onSortingEditon := NewOnSortingEditon(r, formStage)
+	formSortAssocButton.OnSortEdition = onSortingEditon
+
+}
+
+func NewOnAssocEditon[FieldType models.PointerToGongstruct](
+	r *gin.Engine,
+	formStage *form.StageStruct,
+	field []FieldType,
+	stageOfInterest *models.StageStruct,
+) (onAssocEdition *OnAssocEditon[FieldType]) {
+
+	onAssocEdition = new(OnAssocEditon[FieldType])
+
+	onAssocEdition.r = r
+	onAssocEdition.formStage = formStage
+	onAssocEdition.field = field
+	onAssocEdition.stageOfInterest = stageOfInterest
+
+	return
+}
+
+type OnAssocEditon[FieldType models.PointerToGongstruct] struct {
+	r               *gin.Engine
+	formStage       *form.StageStruct
+	field           []FieldType
+	stageOfInterest *models.StageStruct
+}
+
+func (onAssocEditon *OnAssocEditon[FieldType]) OnButtonPressed() {
+
+	tableStackName := onAssocEditon.formStage.GetPath() + string(form.StackNamePostFixForTableForAssociation)
+
+	// tableStackName supposed to be "test-form-table"
+	tableStageForSelection, _ := gongtable_fullstack.NewStackInstance(onAssocEditon.r, tableStackName)
+
+	instanceSet := *models.GetGongstructInstancesSetFromPointerType[FieldType](onAssocEditon.stageOfInterest)
+
+	table := new(gongtable_models.Table).Stage(tableStageForSelection)
+	table.Name = string(form.TableSelectExtraName)
+	table.HasColumnSorting = true
+	table.HasFiltering = true
+	table.HasPaginator = false
+	table.HasCheckableRows = true
+	table.HasSaveButton = true
+
+	filterdInstanceSet := make(map[FieldType]any, 0)
+	for _, instance := range onAssocEditon.field {
+		filterdInstanceSet[instance] = nil
+	}
+
+	for _, fieldName := range models.GetFieldsFromPointer[FieldType]() {
+		column := new(gongtable_models.DisplayedColumn).Stage(tableStageForSelection)
+		column.Name = fieldName
+		table.DisplayedColumns = append(table.DisplayedColumns, column)
+	}
+	for instance := range instanceSet {
+		row := new(gongtable_models.Row).Stage(tableStageForSelection)
+		row.Name = instance.GetName()
+		table.Rows = append(table.Rows, row)
+
+		if _, ok := filterdInstanceSet[instance]; ok {
+			row.IsChecked = true
+		}
+
+		for _, fieldName := range models.GetFieldsFromPointer[FieldType]() {
+			cell := new(gongtable_models.Cell).Stage(tableStageForSelection)
+			cell.Name = fmt.Sprintf("Row %s - Column %s", instance.GetName(), fieldName)
+
+			cellString := new(gongtable_models.CellString).Stage(tableStageForSelection)
+			cellString.Name = models.GetFieldStringValueFromPointer(instance, fieldName)
+			cellString.Value = cellString.Name
+			cell.CellString = cellString
+
+			row.Cells = append(row.Cells, cell)
+		}
+	}
+	tableStageForSelection.Commit()
+}
