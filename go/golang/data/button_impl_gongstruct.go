c42f47f2bb8f04bac6f40d2224220ff12b8d5bf0
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2023-09-05 17:40:17 +0200

    before going multi code generator

--- go/golang/data/button_impl_gongstruct.go
+++ go/golang/data/button_impl_gongstruct.go
@@ -6,24 +6,45 @@ package data
 import (
 	"log"
 
+	"github.com/gin-gonic/gin"
+
 	gong_models "github.com/fullstack-lang/gong/go/models"
+	form "github.com/fullstack-lang/gongtable/go/models"
 	gongtree_buttons "github.com/fullstack-lang/gongtree/go/buttons"
 	gongtree_models "github.com/fullstack-lang/gongtree/go/models"
+
+	"{{PkgPathRoot}}/models"
+	"{{PkgPathRoot}}/orm"
 )
 
 type ButtonImplGongstruct struct {
-	gongStruct      *gong_models.GongStruct
-	Icon            gongtree_buttons.ButtonType
+	gongStruct         *gong_models.GongStruct
+	Icon               gongtree_buttons.ButtonType
+	tableStage         *form.StageStruct
+	formStage          *form.StageStruct
+	stageOfInterest    *models.StageStruct
+	r                  *gin.Engine
+	backRepoOfInterest *orm.BackRepoStruct
 }
 
 func NewButtonImplGongstruct(
 	gongStruct *gong_models.GongStruct,
 	icon gongtree_buttons.ButtonType,
+	tableStage *form.StageStruct,
+	formStage *form.StageStruct,
+	stageOfInterest *models.StageStruct,
+	r *gin.Engine,
+	backRepoOfInterest *orm.BackRepoStruct,
 ) (buttonImplGongstruct *ButtonImplGongstruct) {
 
 	buttonImplGongstruct = new(ButtonImplGongstruct)
 	buttonImplGongstruct.Icon = icon
 	buttonImplGongstruct.gongStruct = gongStruct
+	buttonImplGongstruct.tableStage = tableStage
+	buttonImplGongstruct.formStage = formStage
+	buttonImplGongstruct.stageOfInterest = stageOfInterest
+	buttonImplGongstruct.r = r
+	buttonImplGongstruct.backRepoOfInterest = backRepoOfInterest
 
 	return
 }
@@ -33,5 +54,59 @@ func (buttonImpl *ButtonImplGongstruct) ButtonUpdated(
 	stageButton, front *gongtree_models.Button) {
 
 	log.Println("ButtonImplGongstruct: ButtonUpdated")
+
+	formStage := buttonImpl.formStage
+	formStage.Reset()
+	formStage.Commit()
+
+	switch buttonImpl.gongStruct.Name {
+	// insertion point{{` + string(rune(ButtonImplPerGongstructCallToForm)) + `}}
+	}
+	formStage.Commit()
 }
+
+func FillUpForm[T models.Gongstruct](
+	instance *T,
+	stageOfInterest *models.StageStruct,
+	formStage *form.StageStruct,
+	formGroup *form.FormGroup,
+	r *gin.Engine,
+) {
+
+	switch instanceWithInferedType := any(instance).(type) {
+	// insertion point{{` + string(rune(ButtonImplPerGongstructCallToFormGenerator)) + `}}
+	default:
+		_ = instanceWithInferedType
+	}
+}
+
 `
+
+type ButtonImplGongstructInsertionId int
+
+const (
+	ButtonImplPerGongstructCallToForm ButtonImplGongstructInsertionId = iota
+	ButtonImplPerGongstructCallToFormGenerator
+)
+
+var ButtonImplGongstructSubTemplateCode map[string]string = // new line
+map[string]string{
+	string(rune(ButtonImplPerGongstructCallToForm)): `
+	case "{{Structname}}":
+		formGroup := (&form.FormGroup{
+			Name: form.FormGroupDefaultName.ToString(),
+			OnSave: New{{Structname}}FormCallback(
+				buttonImpl.stageOfInterest,
+				buttonImpl.tableStage,
+				formStage,
+				nil,
+				buttonImpl.r,
+				buttonImpl.backRepoOfInterest,
+			),
+		}).Stage(formStage)
+		{{structname}} := new(models.{{Structname}})
+		FillUpForm({{structname}}, buttonImpl.stageOfInterest, formStage, formGroup, buttonImpl.r)`,
+	string(rune(ButtonImplPerGongstructCallToFormGenerator)): `
+	case *models.{{Structname}}:
+		`,
+}
