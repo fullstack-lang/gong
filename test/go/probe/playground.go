135a60bf575c6853069e85ff3ca2ecf75ebd7483
peugeot <thomaspeugeot@yahoo.fr>
2023-09-09 09:51:46 +0200

    assoc slices

---
+++ test/go/probe/playground.go
@@ -0,0 +1,18 @@
+package probe
+
+import (
+	"github.com/gin-gonic/gin"
+
+	form "github.com/fullstack-lang/gongtable/go/models"
+
+	"github.com/fullstack-lang/gong/test/go/models"
+	"github.com/fullstack-lang/gong/test/go/orm"
+)
+
+type Playground struct {
+	r                  *gin.Engine
+	stageOfInterest    *models.StageStruct
+	backRepoOfInterest *orm.BackRepoStruct
+	formStage          *form.StageStruct
+	tableStage         *form.StageStruct
+}
