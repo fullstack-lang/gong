225d64946a663e800580ebdbe1a0b9936c399999
peugeot <thomaspeugeot@yahoo.fr>
2023-09-09 16:24:32 +0200

    easy and not so easy stuff

--- test/go/probe/form_callback.go
+++ test/go/probe/form_callback.go
@@ -16,16 +16,16 @@ const __dummmy__time = time.Nanosecond
 func NewAstructFormCallback(
 	astruct *models.Astruct,
 	playground *Playground,
-
 ) (astructFormCallback *AstructFormCallback) {
 	astructFormCallback = new(AstructFormCallback)
-	astructFormCallback.astruct = astruct
 	astructFormCallback.playground = playground
+	astructFormCallback.astruct = astruct
 	return
 }
 
 type AstructFormCallback struct {
-	astruct    *models.Astruct
+	astruct *models.Astruct
+
 	playground *Playground
 }
 
@@ -69,7 +69,40 @@ func (astructFormCallback *AstructFormCallback) OnSave() {
 			FormDivBasicFieldToField(&(astruct_.CName), formDiv)
 		case "CFloatfield":
 			FormDivBasicFieldToField(&(astruct_.CFloatfield), formDiv)
-
+		case "Bstruct":
+			FormDivSelectFieldToField(&(astruct_.Bstruct), astructFormCallback.playground.stageOfInterest, formDiv)
+		case "Bstruct2":
+			FormDivSelectFieldToField(&(astruct_.Bstruct2), astructFormCallback.playground.stageOfInterest, formDiv)
+		case "Dstruct":
+			FormDivSelectFieldToField(&(astruct_.Dstruct), astructFormCallback.playground.stageOfInterest, formDiv)
+		case "Dstruct2":
+			FormDivSelectFieldToField(&(astruct_.Dstruct2), astructFormCallback.playground.stageOfInterest, formDiv)
+		case "Dstruct3":
+			FormDivSelectFieldToField(&(astruct_.Dstruct3), astructFormCallback.playground.stageOfInterest, formDiv)
+		case "Dstruct4":
+			FormDivSelectFieldToField(&(astruct_.Dstruct4), astructFormCallback.playground.stageOfInterest, formDiv)
+		case "Floatfield":
+			FormDivBasicFieldToField(&(astruct_.Floatfield), formDiv)
+		case "Intfield":
+			FormDivBasicFieldToField(&(astruct_.Intfield), formDiv)
+		case "Anotherbooleanfield":
+			FormDivBasicFieldToField(&(astruct_.Anotherbooleanfield), formDiv)
+		case "Duration1":
+			FormDivBasicFieldToField(&(astruct_.Duration1), formDiv)
+		case "AnAstruct":
+			FormDivSelectFieldToField(&(astruct_.AnAstruct), astructFormCallback.playground.stageOfInterest, formDiv)
+		case "StructRef":
+			FormDivBasicFieldToField(&(astruct_.StructRef), formDiv)
+		case "FieldRef":
+			FormDivBasicFieldToField(&(astruct_.FieldRef), formDiv)
+		case "EnumIntRef":
+			FormDivBasicFieldToField(&(astruct_.EnumIntRef), formDiv)
+		case "EnumStringRef":
+			FormDivBasicFieldToField(&(astruct_.EnumStringRef), formDiv)
+		case "EnumValue":
+			FormDivBasicFieldToField(&(astruct_.EnumValue), formDiv)
+		case "ConstIdentifierValue":
+			FormDivBasicFieldToField(&(astruct_.ConstIdentifierValue), formDiv)
 		}
 	}
 
@@ -78,4 +111,202 @@ func (astructFormCallback *AstructFormCallback) OnSave() {
 		astructFormCallback.playground,
 	)
 	astructFormCallback.playground.tableStage.Commit()
+}
+func NewAstructBstruct2UseFormCallback(
+	astructbstruct2use *models.AstructBstruct2Use,
+	playground *Playground,
+) (astructbstruct2useFormCallback *AstructBstruct2UseFormCallback) {
+	astructbstruct2useFormCallback = new(AstructBstruct2UseFormCallback)
+	astructbstruct2useFormCallback.playground = playground
+	astructbstruct2useFormCallback.astructbstruct2use = astructbstruct2use
+	return
+}
+
+type AstructBstruct2UseFormCallback struct {
+	astructbstruct2use *models.AstructBstruct2Use
+
+	playground *Playground
+}
+
+func (astructbstruct2useFormCallback *AstructBstruct2UseFormCallback) OnSave() {
+
+	log.Println("AstructBstruct2UseFormCallback, OnSave")
+
+	// checkout formStage to have the form group on the stage synchronized with the
+	// back repo (and front repo)
+	astructbstruct2useFormCallback.playground.formStage.Checkout()
+
+	if astructbstruct2useFormCallback.astructbstruct2use == nil {
+		astructbstruct2useFormCallback.astructbstruct2use = new(models.AstructBstruct2Use).Stage(astructbstruct2useFormCallback.playground.stageOfInterest)
+	}
+	astructbstruct2use_ := astructbstruct2useFormCallback.astructbstruct2use
+	_ = astructbstruct2use_
+
+	// get the formGroup
+	formGroup := astructbstruct2useFormCallback.playground.formStage.FormGroups_mapString[table.FormGroupDefaultName.ToString()]
+
+	for _, formDiv := range formGroup.FormDivs {
+		switch formDiv.Name {
+		// insertion point per field
+		case "Name":
+			FormDivBasicFieldToField(&(astructbstruct2use_.Name), formDiv)
+		case "Bstrcut2":
+			FormDivSelectFieldToField(&(astructbstruct2use_.Bstrcut2), astructbstruct2useFormCallback.playground.stageOfInterest, formDiv)
+		}
+	}
+
+	astructbstruct2useFormCallback.playground.stageOfInterest.Commit()
+	fillUpTable[models.AstructBstruct2Use](
+		astructbstruct2useFormCallback.playground,
+	)
+	astructbstruct2useFormCallback.playground.tableStage.Commit()
+}
+func NewAstructBstructUseFormCallback(
+	astructbstructuse *models.AstructBstructUse,
+	playground *Playground,
+) (astructbstructuseFormCallback *AstructBstructUseFormCallback) {
+	astructbstructuseFormCallback = new(AstructBstructUseFormCallback)
+	astructbstructuseFormCallback.playground = playground
+	astructbstructuseFormCallback.astructbstructuse = astructbstructuse
+	return
+}
+
+type AstructBstructUseFormCallback struct {
+	astructbstructuse *models.AstructBstructUse
+
+	playground *Playground
+}
+
+func (astructbstructuseFormCallback *AstructBstructUseFormCallback) OnSave() {
+
+	log.Println("AstructBstructUseFormCallback, OnSave")
+
+	// checkout formStage to have the form group on the stage synchronized with the
+	// back repo (and front repo)
+	astructbstructuseFormCallback.playground.formStage.Checkout()
+
+	if astructbstructuseFormCallback.astructbstructuse == nil {
+		astructbstructuseFormCallback.astructbstructuse = new(models.AstructBstructUse).Stage(astructbstructuseFormCallback.playground.stageOfInterest)
+	}
+	astructbstructuse_ := astructbstructuseFormCallback.astructbstructuse
+	_ = astructbstructuse_
+
+	// get the formGroup
+	formGroup := astructbstructuseFormCallback.playground.formStage.FormGroups_mapString[table.FormGroupDefaultName.ToString()]
+
+	for _, formDiv := range formGroup.FormDivs {
+		switch formDiv.Name {
+		// insertion point per field
+		case "Name":
+			FormDivBasicFieldToField(&(astructbstructuse_.Name), formDiv)
+		case "Bstruct2":
+			FormDivSelectFieldToField(&(astructbstructuse_.Bstruct2), astructbstructuseFormCallback.playground.stageOfInterest, formDiv)
+		}
+	}
+
+	astructbstructuseFormCallback.playground.stageOfInterest.Commit()
+	fillUpTable[models.AstructBstructUse](
+		astructbstructuseFormCallback.playground,
+	)
+	astructbstructuseFormCallback.playground.tableStage.Commit()
+}
+func NewBstructFormCallback(
+	bstruct *models.Bstruct,
+	playground *Playground,
+) (bstructFormCallback *BstructFormCallback) {
+	bstructFormCallback = new(BstructFormCallback)
+	bstructFormCallback.playground = playground
+	bstructFormCallback.bstruct = bstruct
+	return
+}
+
+type BstructFormCallback struct {
+	bstruct *models.Bstruct
+
+	playground *Playground
+}
+
+func (bstructFormCallback *BstructFormCallback) OnSave() {
+
+	log.Println("BstructFormCallback, OnSave")
+
+	// checkout formStage to have the form group on the stage synchronized with the
+	// back repo (and front repo)
+	bstructFormCallback.playground.formStage.Checkout()
+
+	if bstructFormCallback.bstruct == nil {
+		bstructFormCallback.bstruct = new(models.Bstruct).Stage(bstructFormCallback.playground.stageOfInterest)
+	}
+	bstruct_ := bstructFormCallback.bstruct
+	_ = bstruct_
+
+	// get the formGroup
+	formGroup := bstructFormCallback.playground.formStage.FormGroups_mapString[table.FormGroupDefaultName.ToString()]
+
+	for _, formDiv := range formGroup.FormDivs {
+		switch formDiv.Name {
+		// insertion point per field
+		case "Name":
+			FormDivBasicFieldToField(&(bstruct_.Name), formDiv)
+		case "Floatfield":
+			FormDivBasicFieldToField(&(bstruct_.Floatfield), formDiv)
+		case "Floatfield2":
+			FormDivBasicFieldToField(&(bstruct_.Floatfield2), formDiv)
+		case "Intfield":
+			FormDivBasicFieldToField(&(bstruct_.Intfield), formDiv)
+		}
+	}
+
+	bstructFormCallback.playground.stageOfInterest.Commit()
+	fillUpTable[models.Bstruct](
+		bstructFormCallback.playground,
+	)
+	bstructFormCallback.playground.tableStage.Commit()
+}
+func NewDstructFormCallback(
+	dstruct *models.Dstruct,
+	playground *Playground,
+) (dstructFormCallback *DstructFormCallback) {
+	dstructFormCallback = new(DstructFormCallback)
+	dstructFormCallback.playground = playground
+	dstructFormCallback.dstruct = dstruct
+	return
+}
+
+type DstructFormCallback struct {
+	dstruct *models.Dstruct
+
+	playground *Playground
+}
+
+func (dstructFormCallback *DstructFormCallback) OnSave() {
+
+	log.Println("DstructFormCallback, OnSave")
+
+	// checkout formStage to have the form group on the stage synchronized with the
+	// back repo (and front repo)
+	dstructFormCallback.playground.formStage.Checkout()
+
+	if dstructFormCallback.dstruct == nil {
+		dstructFormCallback.dstruct = new(models.Dstruct).Stage(dstructFormCallback.playground.stageOfInterest)
+	}
+	dstruct_ := dstructFormCallback.dstruct
+	_ = dstruct_
+
+	// get the formGroup
+	formGroup := dstructFormCallback.playground.formStage.FormGroups_mapString[table.FormGroupDefaultName.ToString()]
+
+	for _, formDiv := range formGroup.FormDivs {
+		switch formDiv.Name {
+		// insertion point per field
+		case "Name":
+			FormDivBasicFieldToField(&(dstruct_.Name), formDiv)
+		}
+	}
+
+	dstructFormCallback.playground.stageOfInterest.Commit()
+	fillUpTable[models.Dstruct](
+		dstructFormCallback.playground,
+	)
+	dstructFormCallback.playground.tableStage.Commit()
 }
