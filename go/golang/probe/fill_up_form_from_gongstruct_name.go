5edc8a73d9273af212829569db5f7dfb2fc8f67b
peugeot <thomaspeugeot@yahoo.fr>
2023-09-16 13:42:46 +0200

    Add folder for instances in the probe sidebar #402 - fill_up_form_from_gongstruct_name

---
+++ go/golang/probe/fill_up_form_from_gongstruct_name.go
@@ -0,0 +1,57 @@
+package probe
+
+const FillUpFormFromGongstructNameTemplate = `// generated code - do not edit
+package probe
+
+import (
+	form "github.com/fullstack-lang/gongtable/go/models"
+
+	"{{PkgPathRoot}}/models"
+)
+
+func FillUpFormFromGongstructName(
+	playground *Playground,
+	gongstructName string,
+	isNewInstance bool,
+) {
+	formStage := playground.formStage
+	formStage.Reset()
+	formStage.Commit()
+
+	var prefix string
+
+	if isNewInstance {
+		prefix = "New"
+	} else {
+		prefix = "Update"
+	}
+
+	switch gongstructName {
+	// insertion point{{` + string(rune(FillUpFormFromGongstructNameSwitchCase)) + `}}
+	}
+	formStage.Commit()
+}
+`
+
+type FillUpFormFromGongstructNameInsertionId int
+
+const (
+	FillUpFormFromGongstructNameSwitchCase FillUpFormFromGongstructNameInsertionId = iota
+	FillUpFormFromGongstructNameInsertionNb
+)
+
+var FillUpFormFromGongstructNameSubTemplateCode map[string]string = // new line
+map[string]string{
+	string(rune(ButtonImplPerGongstructCallToForm)): `
+	case "{{Structname}}":
+		formGroup := (&form.FormGroup{
+			Name:  form.FormGroupDefaultName.ToString(),
+			Label: prefix + " {{Structname}} Form",
+			OnSave: New{{Structname}}FormCallback(
+				nil,
+				playground,
+			),
+		}).Stage(formStage)
+		{{structname}} := new(models.{{Structname}})
+		FillUpForm({{structname}}, formGroup, playground)`,
+}
