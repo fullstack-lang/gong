5069f7846db7256b47d52a27f2fc347cab694ae8
peugeot <thomaspeugeot@yahoo.fr>
2023-09-16 06:45:30 +0200

    Add delete button impl to probe table #398 - generation

--- go/golang/probe/playground.go
+++ go/golang/probe/playground.go
@@ -6,7 +6,9 @@ package probe
 import (
 	"github.com/gin-gonic/gin"
 
+	gong "github.com/fullstack-lang/gong/go/models"
 	form "github.com/fullstack-lang/gongtable/go/models"
+	tree "github.com/fullstack-lang/gongtree/go/models"
 
 	"{{PkgPathRoot}}/models"
 	"{{PkgPathRoot}}/orm"
@@ -16,7 +18,30 @@ type Playground struct {
 	r                  *gin.Engine
 	stageOfInterest    *models.StageStruct
 	backRepoOfInterest *orm.BackRepoStruct
+	gongStage          *gong.StageStruct
+	treeStage          *tree.StageStruct
 	formStage          *form.StageStruct
 	tableStage         *form.StageStruct
+}
+
+func NewPlayground(
+	r *gin.Engine,
+	stageOfInterest *models.StageStruct,
+	backRepoOfInterest *orm.BackRepoStruct,
+	gongStage *gong.StageStruct,
+	treeStage *tree.StageStruct,
+	formStage *form.StageStruct,
+	tableStage *form.StageStruct,
+) (playground *Playground) {
+	playground = new(Playground)
+	playground.r = r
+	playground.stageOfInterest = stageOfInterest
+	playground.backRepoOfInterest = backRepoOfInterest
+	playground.gongStage = gongStage
+	playground.treeStage = treeStage
+	playground.formStage = formStage
+	playground.tableStage = tableStage
+
+	return
 }
 `
