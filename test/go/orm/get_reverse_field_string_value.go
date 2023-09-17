c98c2a79d3f3a37e2c5e00750b78dfa3c2a2df95
peugeot <thomaspeugeot@yahoo.fr>
2023-09-18 00:17:34 +0200

    Probe, Reverse Association Slices #375 - updated manual

--- test/go/orm/get_reverse_field_string_value.go
+++ test/go/orm/get_reverse_field_string_value.go
@@ -2,7 +2,7 @@ package orm
 
 import "github.com/fullstack-lang/gong/test/go/models"
 
-func GetReverseFieldStringValue[T models.Gongstruct](
+func GetReverseFieldOwnerName[T models.Gongstruct](
 	stage *models.StageStruct,
 	backRepo *BackRepoStruct,
 	instance *T,
@@ -15,11 +15,15 @@ func GetReverseFieldStringValue[T models.Gongstruct](
 		tmp := GetInstanceDBFromInstance[models.Bstruct, BstructDB](
 			stage, backRepo, inst,
 		)
-		if tmp.Astruct_AnarrayofbDBID.Int64 != 0 {
-			id := uint(tmp.Astruct_AnarrayofbDBID.Int64)
-			reservePointerTarget := backRepo.BackRepoAstruct.Map_AstructDBID_AstructPtr[id]
-			res = reservePointerTarget.Name
+		switch reverseFieldName {
+		case "Anarrayofb":
+			if tmp.Astruct_AnarrayofbDBID.Int64 != 0 {
+				id := uint(tmp.Astruct_AnarrayofbDBID.Int64)
+				reservePointerTarget := backRepo.BackRepoAstruct.Map_AstructDBID_AstructPtr[id]
+				res = reservePointerTarget.Name
+			}
 		}
+
 	default:
 		_ = inst
 	}
