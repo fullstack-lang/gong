b39bb9d108ea441660116a3bce48801c4a8ceded
peugeot <thomaspeugeot@yahoo.fr>
2023-09-24 21:05:21 +0200

    carve out the datamodel lib in front Fixes #415 - new deps

--- vendor/github.com/fullstack-lang/gongtable/ng/projects/gongtabledatamodel/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of gongtabledatamodel
- */
-
-export * from './lib/gongtabledatamodel.service';
-export * from './lib/gongtabledatamodel.component';
-export * from './lib/gongtabledatamodel.module';
--- vendor/github.com/fullstack-lang/gongtree/ng/projects/gongtreedatamodel/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of gongtreedatamodel
- */
-
-export * from './lib/gongtreedatamodel.service';
-export * from './lib/gongtreedatamodel.component';
-export * from './lib/gongtreedatamodel.module';
