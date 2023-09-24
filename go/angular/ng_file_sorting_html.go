395baf0b93049505a4e8c0dee992d9cbbed0d04a
peugeot <thomaspeugeot@yahoo.fr>
2023-09-24 20:05:19 +0200

    carve out the datamodel lib in front Fixes #415 - manual + auto

--- go/angular/ng_file_sorting_html.go
+++
@@ -1,8 +0,0 @@
-package angular
-
-const NgSortingTemplateHTML = `<div cdkDropList class="example-list" (cdkDropListDropped)="drop($event)">
-<div class="example-box" *ngFor="let {{structname}} of associated{{Structname}}s" cdkDrag>{{{{structname}}.Name}}</div>
-</div>
-<button class="table__save" color="primary" mat-raised-button (click)="save()">
-Save
-</button>`
