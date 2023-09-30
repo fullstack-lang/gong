f62b07b215c17894d1f4bc0d8f8e08bbc9c1e0da
peugeot <thomaspeugeot@yahoo.fr>
2023-09-30 12:58:06 +0200

    Remove GetDefaultStage Fixes #425 - manual

--- go/models/gong.go
+++ go/models/gong.go
@@ -4,7 +4,6 @@ package models
 import (
 	"errors"
 	"fmt"
-	"sync"
 	"time"
 )
 
@@ -224,15 +223,6 @@ type BackRepoInterface interface {
 }
 
 var _stage *StageStruct
-
-var once sync.Once
-
-func GetDefaultStage() *StageStruct {
-	once.Do(func() {
-		_stage = NewStage("")
-	})
-	return _stage
-}
 
 func NewStage(path string) (stage *StageStruct) {
 
