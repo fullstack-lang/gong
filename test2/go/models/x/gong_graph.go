921c1146a7a9fe9c1642b21cde3308d841ea2709
peugeot <thomaspeugeot@yahoo.fr>
2023-10-01 04:08:37 +0200

    experimental stuff

---
+++ test2/go/models/x/gong_graph.go
@@ -0,0 +1,87 @@
+// generated code - do not edit
+package x
+
+func IsStaged[Type Gongstruct](stage *StageStruct, instance *Type) (ok bool) {
+
+	switch target := any(instance).(type) {
+	// insertion point for stage
+	case *A:
+		ok = stage.IsStagedA(target)
+
+	default:
+		_ = target
+	}
+	return
+}
+
+// insertion point for stage per struct
+func (stage *StageStruct) IsStagedA(a *A) (ok bool) {
+
+	_, ok = stage.As[a]
+
+	return
+}
+
+// StageBranch stages instance and apply StageBranch on all gongstruct instances that are
+// referenced by pointers or slices of pointers of the insance
+//
+// the algorithm stops along the course of graph if a vertex is already staged
+func StageBranch[Type Gongstruct](stage *StageStruct, instance *Type) {
+
+	switch target := any(instance).(type) {
+	// insertion point for stage branch
+	case *A:
+		stage.StageBranchA(target)
+
+	default:
+		_ = target
+	}
+}
+
+// insertion point for stage branch per struct
+func (stage *StageStruct) StageBranchA(a *A) {
+
+	// check if instance is already staged
+	if IsStaged(stage, a) {
+		return
+	}
+
+	a.Stage(stage)
+
+	//insertion point for the staging of instances referenced by pointers
+
+	//insertion point for the staging of instances referenced by slice of pointers
+
+}
+
+// UnstageBranch stages instance and apply UnstageBranch on all gongstruct instances that are
+// referenced by pointers or slices of pointers of the insance
+//
+// the algorithm stops along the course of graph if a vertex is already staged
+func UnstageBranch[Type Gongstruct](stage *StageStruct, instance *Type) {
+
+	switch target := any(instance).(type) {
+	// insertion point for unstage branch
+	case *A:
+		stage.UnstageBranchA(target)
+
+	default:
+		_ = target
+	}
+}
+
+// insertion point for unstage branch per struct
+func (stage *StageStruct) UnstageBranchA(a *A) {
+
+	// check if instance is already staged
+	if !IsStaged(stage, a) {
+		return
+	}
+
+	a.Unstage(stage)
+
+	//insertion point for the staging of instances referenced by pointers
+
+	//insertion point for the staging of instances referenced by slice of pointers
+
+}
