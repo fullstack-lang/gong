d355fbf8ecb7de65224c75a2ceee378d605d7e5b
peugeot <thomaspeugeot@yahoo.fr>
2023-10-03 01:41:49 +0200

    progress on test2

--- test2/go/cmd/test2/main.go
+++ test2/go/cmd/test2/main.go
@@ -12,6 +12,9 @@ import (
 	test2_models "github.com/fullstack-lang/gong/test2/go/models"
 	test2_orm "github.com/fullstack-lang/gong/test2/go/orm"
 	test2_probe "github.com/fullstack-lang/gong/test2/go/probe"
+
+	test2_fullstack_x "github.com/fullstack-lang/gong/test2/go/fullstack/x"
+
 	test2_static "github.com/fullstack-lang/gong/test2/go/static"
 )
 
@@ -23,6 +26,8 @@ var (
 
 	diagrams         = flag.Bool("diagrams", true, "parse/analysis go/models and go/diagrams")
 	embeddedDiagrams = flag.Bool("embeddedDiagrams", false, "parse/analysis go/models and go/embeddedDiagrams")
+
+	testPackageHierarchy = flag.Bool("testPackageHierarchy", false, "performs some tests related to package hierarchy")
 
 	port = flag.Int("port", 8080, "port server")
 )
@@ -58,44 +63,54 @@ func main() {
 	// setup the static file server and get the controller
 	r := test2_static.ServeStaticFiles(*logGINFlag)
 
-	// setup stack
-	var stage *test2_models.StageStruct
-	var backRepo *test2_orm.BackRepoStruct
-
-	if *marshallOnCommit != "" {
-		// persistence in a SQLite file on disk in memory
-		stage, backRepo = test2_fullstack.NewStackInstance(r, "test2")
-	} else {
-		// persistence in a SQLite file on disk
-		stage, backRepo = test2_fullstack.NewStackInstance(r, "test2", "./test2.db")
+	if *testPackageHierarchy {
+		stage, backRepo := test2_fullstack.NewStackInstance(r, "test2")
+		_ = stage
+		_ = backRepo
+		stage_x, backRepo_x := test2_fullstack_x.NewStackInstance(r, "test2")
+		_ = stage_x
+		_ = backRepo_x
 	}
 
-	if *unmarshallFromCode != "" {
-		stage.Checkout()
-		stage.Reset()
-		stage.Commit()
-		err := test2_models.ParseAstFile(stage, *unmarshallFromCode)
+	if !*testPackageHierarchy {
+		// setup stack
+		var stage *test2_models.StageStruct
+		var backRepo *test2_orm.BackRepoStruct
 
-		// if the application is run with -unmarshallFromCode=xxx.go -marshallOnCommit
-		// xxx.go might be absent the first time. However, this shall not be a show stopper.
-		if err != nil {
-			log.Println("no file to read " + err.Error())
+		if *marshallOnCommit != "" {
+			// persistence in a SQLite file on disk in memory
+			stage, backRepo = test2_fullstack.NewStackInstance(r, "test2")
+		} else {
+			// persistence in a SQLite file on disk
+			stage, backRepo = test2_fullstack.NewStackInstance(r, "test2", "./test2.db")
 		}
 
-		stage.Commit()
-	} else {
-		// in case the database is used, checkout the content to the stage
-		stage.Checkout()
-	}
+		if *unmarshallFromCode != "" {
+			stage.Checkout()
+			stage.Reset()
+			stage.Commit()
+			err := test2_models.ParseAstFile(stage, *unmarshallFromCode)
 
-	// hook automatic marshall to go code at every commit
-	if *marshallOnCommit != "" {
-		hook := new(BeforeCommitImplementation)
-		stage.OnInitCommitCallback = hook
-	}
+			// if the application is run with -unmarshallFromCode=xxx.go -marshallOnCommit
+			// xxx.go might be absent the first time. However, this shall not be a show stopper.
+			if err != nil {
+				log.Println("no file to read " + err.Error())
+			}
 
-	test2_probe.NewProbe(r, test2_go.GoModelsDir, test2_go.GoDiagramsDir, 
-		*embeddedDiagrams,"test2", stage, backRepo)
+			stage.Commit()
+		} else {
+			// in case the database is used, checkout the content to the stage
+			stage.Checkout()
+		}
+
+		// hook automatic marshall to go code at every commit
+		if *marshallOnCommit != "" {
+			hook := new(BeforeCommitImplementation)
+			stage.OnInitCommitCallback = hook
+		}
+		test2_probe.NewProbe(r, test2_go.GoModelsDir, test2_go.GoDiagramsDir,
+			*embeddedDiagrams, "test2", stage, backRepo)
+	}
 
 	log.Printf("Server ready serve on localhost:" + strconv.Itoa(*port))
 	err := r.Run(":" + strconv.Itoa(*port))
