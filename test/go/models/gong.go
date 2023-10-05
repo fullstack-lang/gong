acc80603c6a9d8cdd8c2887479dc7571c7a08819
peugeot <thomaspeugeot@yahoo.fr>
2023-10-05 04:56:51 +0200

    duration diplay and neg

--- test/go/models/gong.go
+++ test/go/models/gong.go
@@ -1635,7 +1635,7 @@ func GetFieldStringValue[Type Gongstruct](instance Type, fieldName string) (res 
 		case "Anotherbooleanfield":
 			res = fmt.Sprintf("%t", inferedInstance.Anotherbooleanfield)
 		case "Duration1":
-			res = fmt.Sprintf("%d", inferedInstance.Duration1)
+			res = fmt.Sprintf("%s", inferedInstance.Duration1)
 		case "Anarrayofa":
 			for idx, __instance__ := range inferedInstance.Anarrayofa {
 				if idx > 0 {
