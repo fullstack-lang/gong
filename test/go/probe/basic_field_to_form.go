394809188bdcb8237cbdd9b2957db564c22f0116
peugeot <thomaspeugeot@yahoo.fr>
2023-10-05 05:32:11 +0200

    probe : Duration display is not correct in table and can be not good in form Fixes #429 - manual

--- test/go/probe/basic_field_to_form.go
+++ test/go/probe/basic_field_to_form.go
@@ -2,6 +2,7 @@
 package probe
 
 import (
+	"math"
 	"time"
 
 	form "github.com/fullstack-lang/gongtable/go/models"
@@ -27,8 +28,8 @@ func BasicFieldtoForm[T models.PointerToGongstruct, TF models.GongtructBasicFiel
 		formDiv.FormFields = append(formDiv.FormFields, formField)
 
 		formFieldString := (&form.FormFieldString{
-			Name:  		"string",
-			Value: 		fieldWithInterferedType,
+			Name:       "string",
+			Value:      fieldWithInterferedType,
 			IsTextArea: isTextArea,
 		}).Stage(formStage)
 		formField.FormFieldString = formFieldString
@@ -116,6 +117,14 @@ func BasicFieldtoForm[T models.PointerToGongstruct, TF models.GongtructBasicFiel
 		formGroup.FormDivs = append(formGroup.FormDivs, formDiv)
 
 		{
+			checkBox := (&form.CheckBox{
+				Name:  "negative",
+				Value: fieldWithInterferedType < 0,
+			}).Stage(formStage)
+			formDiv.CheckBoxs = append(formDiv.CheckBoxs, checkBox)
+		}
+
+		{
 			formFieldHours := (&form.FormField{
 				Name:  "Hours",
 				Label: "Hours",
@@ -127,12 +136,12 @@ func BasicFieldtoForm[T models.PointerToGongstruct, TF models.GongtructBasicFiel
 
 			formFieldIntHours := (&form.FormFieldInt{
 				Name:  "Hours",
-				Value: int(fieldWithInterferedType.Hours()) % 24,
+				Value: int(math.Abs(fieldWithInterferedType.Hours())) % 24,
 			}).Stage(formStage)
 			formFieldIntHours.HasMaxValidator = true
 			formFieldIntHours.MaxValue = 23
 			formFieldIntHours.HasMinValidator = true
-			formFieldIntHours.MinValue = 0
+			formFieldIntHours.MinValue = -24
 			formFieldHours.FormFieldInt = formFieldIntHours
 		}
 
@@ -147,7 +156,7 @@ func BasicFieldtoForm[T models.PointerToGongstruct, TF models.GongtructBasicFiel
 
 			formFieldIntMinutes := (&form.FormFieldInt{
 				Name:  "Minutes",
-				Value: int(fieldWithInterferedType.Minutes()) % 60,
+				Value: int(math.Abs(fieldWithInterferedType.Minutes())) % 60,
 			}).Stage(formStage)
 			formFieldIntMinutes.HasMaxValidator = true
 			formFieldIntMinutes.MaxValue = 59
@@ -166,7 +175,7 @@ func BasicFieldtoForm[T models.PointerToGongstruct, TF models.GongtructBasicFiel
 
 			formFieldIntSeconds := (&form.FormFieldInt{
 				Name:  "Seconds",
-				Value: int(fieldWithInterferedType.Seconds()) % 60,
+				Value: int(math.Abs(fieldWithInterferedType.Seconds())) % 60,
 			}).Stage(formStage)
 			formFieldIntSeconds.HasMaxValidator = true
 			formFieldIntSeconds.MaxValue = 59
