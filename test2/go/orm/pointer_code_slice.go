85f690006403820477ba0bdf920d58b7b5403ba3
peugeot <thomaspeugeot@yahoo.fr>
2023-10-14 11:51:10 +0200

    Working version

--- test2/go/orm/pointer_code_slice.go
+++ test2/go/orm/pointer_code_slice.go
@@ -3,15 +3,22 @@ package orm
 import (
 	"database/sql/driver"
 	"encoding/json"
+	"fmt"
 )
 
-type PointerCodeSlice []int
+type StringSlice []int
 
-func (ss *PointerCodeSlice) Scan(value interface{}) error {
-	return json.Unmarshal([]byte(value.(string)), ss)
+// Value makes StringSlice implement the driver.Valuer interface.
+func (s StringSlice) Value() (driver.Value, error) {
+	return json.Marshal(s)
 }
 
-func (ss PointerCodeSlice) Value() (driver.Value, error) {
-	b, err := json.Marshal(ss)
-	return string(b), err
+// Scan makes StringSlice implement the sql.Scanner interface.
+func (s *StringSlice) Scan(value interface{}) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return error(fmt.Errorf("Failed to unmarshal StringSlice value: %v", value))
+	}
+
+	return json.Unmarshal(bytes, s)
 }
