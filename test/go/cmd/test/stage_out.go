f3667321a6f368a7337a5551cbc1e4a3bc76996d
peugeot <thomaspeugeot@yahoo.fr>
2023-10-21 11:35:29 +0200

    new gongc

--- test/go/cmd/test/stage_out.go
+++ test/go/cmd/test/stage_out.go
@@ -83,7 +83,7 @@ Second Line`
 	__Astruct__000000_A1.Floatfield = 0.000000
 	__Astruct__000000_A1.Intfield = 3
 	__Astruct__000000_A1.Anotherbooleanfield = false
-	__Astruct__000000_A1.Duration1 = 79653000000000
+	__Astruct__000000_A1.Duration1 = -79653000000000
 
 	// comment added to overcome the problem with the comment map association
 
@@ -126,7 +126,7 @@ Second Line`
 	__Astruct__000001_A2.Floatfield = 0.000000
 	__Astruct__000001_A2.Intfield = 0
 	__Astruct__000001_A2.Anotherbooleanfield = false
-	__Astruct__000001_A2.Duration1 = -2773200000000000
+	__Astruct__000001_A2.Duration1 = 796530000000000000
 
 	// comment added to overcome the problem with the comment map association
 
@@ -233,11 +233,15 @@ Second Line`
 	// Setup of pointers
 	__Astruct__000000_A1.Associationtob = __Bstruct__000001_B2
 	__Astruct__000000_A1.Anarrayofb = append(__Astruct__000000_A1.Anarrayofb, __Bstruct__000002_B3)
-	__Astruct__000000_A1.Anarrayofb = append(__Astruct__000000_A1.Anarrayofb, __Bstruct__000000_B1)
+	__Astruct__000000_A1.Anotherassociationtob_2 = __Bstruct__000001_B2
 	__Astruct__000000_A1.Anarrayofa = append(__Astruct__000000_A1.Anarrayofa, __Astruct__000000_A1)
 	__Astruct__000000_A1.Anotherarrayofb = append(__Astruct__000000_A1.Anotherarrayofb, __Bstruct__000001_B2)
 	__Astruct__000000_A1.Anotherarrayofb = append(__Astruct__000000_A1.Anotherarrayofb, __Bstruct__000002_B3)
-	__Astruct__000000_A1.Anarrayofb2Use = append(__Astruct__000000_A1.Anarrayofb2Use, __AstructBstruct2Use__000001_)
+	__Astruct__000000_A1.Anarrayofb2Use = append(__Astruct__000000_A1.Anarrayofb2Use, __AstructBstruct2Use__000000_)
+	__Astruct__000001_A2.Associationtob = __Bstruct__000000_B1
+	__Astruct__000001_A2.Anarrayofb = append(__Astruct__000001_A2.Anarrayofb, __Bstruct__000000_B1)
+	__Astruct__000001_A2.Anarrayofb = append(__Astruct__000001_A2.Anarrayofb, __Bstruct__000001_B2)
+	__Astruct__000001_A2.Anarrayofa = append(__Astruct__000001_A2.Anarrayofa, __Astruct__000002_A3)
 	__AstructBstruct2Use__000000_.Bstrcut2 = __Bstruct__000000_B1
 	__AstructBstruct2Use__000001_.Bstrcut2 = __Bstruct__000000_B1
 	__Dstruct__000000_D1.Anarrayofb = append(__Dstruct__000000_D1.Anarrayofb, __Bstruct__000001_B2)
