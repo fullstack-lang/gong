48e444674651339d25f159ce9130f5a9f4381235
peugeot <thomaspeugeot@yahoo.fr>
2023-10-21 16:22:42 +0200

    In the POST, UPDATE front operations, encode slices of pointers and decode at the back (even if targets are not yet synchronized). #438 - manual

--- test2/ng/projects/test2/src/lib/a-db.ts
+++ test2/ng/projects/test2/src/lib/a-db.ts
@@ -1,6 +1,5 @@
 // insertion point for imports
 import { BDB } from './b-db'
-import { FrontRepo } from './front-repo.service'
 
 // usefull for managing pointer ID values that can be nullable
 import { NullInt64 } from './null-int64'
@@ -23,28 +22,6 @@ export class ADB {
 	Bs: Array<BDB> = []
 
 	APointersEncoding: APointersEncoding = new APointersEncoding
-
-	redeemPointers(frontRepo: FrontRepo) {
-		// insertion point sub sub template for ONE-/ZERO-ONE associations pointers redeeming
-		// insertion point for pointer field B redeeming
-		{
-			let _b = frontRepo.Bs.get(this.APointersEncoding.BID.Int64)
-			if (_b) {
-				this.B = _b
-			}
-		}
-
-		// insertion point for redeeming ONE-MANY associations
-		this.Bs = new Array<BDB>()
-		for (let _bId of this.APointersEncoding.Bs) {
-			let _b = frontRepo.Bs.get(_bId)
-			this.Bs.push(_b!)
-		}
-	}
-
-	toto() {
-		console.log("toto")
-	}
 }
 
 export class APointersEncoding {
