4636410c8449d7c22e38e86a63697c0958beaea8
peugeot <thomaspeugeot@yahoo.fr>
2023-10-23 04:53:22 +0200

    new gongc

--- test/ng/projects/test/src/lib/bstruct-db.ts
+++ test/ng/projects/test/src/lib/bstruct-db.ts
@@ -1,6 +1,4 @@
 // insertion point for imports
-import { AstructDB } from './astruct-db'
-import { DstructDB } from './dstruct-db'
 
 // usefull for managing pointer ID values that can be nullable
 import { NullInt64 } from './null-int64'
@@ -26,19 +24,4 @@ export class BstructDB {
 
 export class BstructPointersEncoding {
 	// insertion point for pointers and slices of pointers encoding fields
-	// reverse pointers encoding (to be removed)
-	Astruct_AnarrayofbDBID: NullInt64 = new NullInt64
-	Astruct_AnarrayofbDBID_Index: NullInt64  = new NullInt64 // store the index of the bstruct instance in Astruct.Anarrayofb
-	Astruct_Anarrayofb_reverse?: AstructDB 
-
-	// reverse pointers encoding (to be removed)
-	Astruct_AnotherarrayofbDBID: NullInt64 = new NullInt64
-	Astruct_AnotherarrayofbDBID_Index: NullInt64  = new NullInt64 // store the index of the bstruct instance in Astruct.Anotherarrayofb
-	Astruct_Anotherarrayofb_reverse?: AstructDB 
-
-	// reverse pointers encoding (to be removed)
-	Dstruct_AnarrayofbDBID: NullInt64 = new NullInt64
-	Dstruct_AnarrayofbDBID_Index: NullInt64  = new NullInt64 // store the index of the bstruct instance in Dstruct.Anarrayofb
-	Dstruct_Anarrayofb_reverse?: DstructDB 
-
 }
