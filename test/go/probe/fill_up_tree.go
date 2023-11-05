bdc6489292676e6e18b99eacd10fea5d0f8a3680
peugeot <thomaspeugeot@yahoo.fr>
2023-11-05 11:01:27 +0100

    Add Refresh Button to Probe Fixes #448 - manual

--- test/go/probe/fill_up_tree.go
+++ test/go/probe/fill_up_tree.go
@@ -55,12 +55,11 @@ func fillUpTree(
 
 		nodeGongstruct := (&tree.Node{Name: name}).Stage(probe.treeStage)
 
-
 		nodeGongstruct.IsExpanded = false
 		if _, ok := expandedNodesSet[strings.Fields(name)[0]]; ok {
 			nodeGongstruct.IsExpanded = true
 		}
-		
+
 		switch gongStruct.Name {
 		// insertion point
 		case "Astruct":
@@ -122,7 +121,7 @@ func fillUpTree(
 				nodeInstance.Impl = NewInstanceNodeCallback(_fstruct, "Fstruct", probe)
 
 				nodeGongstruct.Children = append(nodeGongstruct.Children, nodeInstance)
-			}	
+			}
 		}
 
 		nodeGongstruct.IsNodeClickable = true
@@ -141,13 +140,23 @@ func fillUpTree(
 
 		sidebar.RootNodes = append(sidebar.RootNodes, nodeGongstruct)
 	}
+
+	// Add a refresh button
+	nodeRefreshButton := (&tree.Node{Name: ""}).Stage(probe.treeStage)
+	sidebar.RootNodes = append(sidebar.RootNodes, nodeRefreshButton)
+	refreshButton := (&tree.Button{
+		Name: "RefreshButton" + " " + string(gongtree_buttons.BUTTON_refresh),
+		Icon: string(gongtree_buttons.BUTTON_refresh)}).Stage(probe.treeStage)
+	nodeRefreshButton.Buttons = append(nodeRefreshButton.Buttons, refreshButton)
+	refreshButton.Impl = NewButtonImplRefresh(probe)
+
 	probe.treeStage.Commit()
 }
 
 type InstanceNodeCallback[T models.Gongstruct] struct {
 	Instance       *T
 	gongstructName string
-	probe     *Probe
+	probe          *Probe
 }
 
 func NewInstanceNodeCallback[T models.Gongstruct](
