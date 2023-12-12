871c013a00a707b01ca8832b64440be631e98e8e
peugeot <thomaspeugeot@yahoo.fr>
2023-12-12 23:30:45 +0100

    suppress button for probe

--- test/go/probe/fill_up_form_from_gongstruct_name.go
+++ test/go/probe/fill_up_form_from_gongstruct_name.go
@@ -19,9 +19,9 @@ func FillUpFormFromGongstructName(
 	var prefix string
 
 	if isNewInstance {
-		prefix = "New"
+		prefix = ""
 	} else {
-		prefix = "Update"
+		prefix = ""
 	}
 
 	switch gongstructName {
@@ -29,68 +29,74 @@ func FillUpFormFromGongstructName(
 	case "Astruct":
 		formGroup := (&form.FormGroup{
 			Name:  form.FormGroupDefaultName.ToString(),
-			Label: prefix + " Astruct Form",
+			Label: prefix + "Astruct Form",
 			OnSave: __gong__New__AstructFormCallback(
 				nil,
 				probe,
 			),
 		}).Stage(formStage)
 		astruct := new(models.Astruct)
+		formGroup.HasSuppressButton = !isNewInstance
 		FillUpForm(astruct, formGroup, probe)
 	case "AstructBstruct2Use":
 		formGroup := (&form.FormGroup{
 			Name:  form.FormGroupDefaultName.ToString(),
-			Label: prefix + " AstructBstruct2Use Form",
+			Label: prefix + "AstructBstruct2Use Form",
 			OnSave: __gong__New__AstructBstruct2UseFormCallback(
 				nil,
 				probe,
 			),
 		}).Stage(formStage)
 		astructbstruct2use := new(models.AstructBstruct2Use)
+		formGroup.HasSuppressButton = !isNewInstance
 		FillUpForm(astructbstruct2use, formGroup, probe)
 	case "AstructBstructUse":
 		formGroup := (&form.FormGroup{
 			Name:  form.FormGroupDefaultName.ToString(),
-			Label: prefix + " AstructBstructUse Form",
+			Label: prefix + "AstructBstructUse Form",
 			OnSave: __gong__New__AstructBstructUseFormCallback(
 				nil,
 				probe,
 			),
 		}).Stage(formStage)
 		astructbstructuse := new(models.AstructBstructUse)
+		formGroup.HasSuppressButton = !isNewInstance
 		FillUpForm(astructbstructuse, formGroup, probe)
 	case "Bstruct":
 		formGroup := (&form.FormGroup{
 			Name:  form.FormGroupDefaultName.ToString(),
-			Label: prefix + " Bstruct Form",
+			Label: prefix + "Bstruct Form",
 			OnSave: __gong__New__BstructFormCallback(
 				nil,
 				probe,
 			),
 		}).Stage(formStage)
 		bstruct := new(models.Bstruct)
+		formGroup.HasSuppressButton = !isNewInstance
 		FillUpForm(bstruct, formGroup, probe)
 	case "Dstruct":
 		formGroup := (&form.FormGroup{
 			Name:  form.FormGroupDefaultName.ToString(),
-			Label: prefix + " Dstruct Form",
+			Label: prefix + "Dstruct Form",
 			OnSave: __gong__New__DstructFormCallback(
 				nil,
 				probe,
 			),
 		}).Stage(formStage)
 		dstruct := new(models.Dstruct)
+		formGroup.HasSuppressButton = !isNewInstance
 		FillUpForm(dstruct, formGroup, probe)
 	case "Fstruct":
 		formGroup := (&form.FormGroup{
 			Name:  form.FormGroupDefaultName.ToString(),
-			Label: prefix + " Fstruct Form",
+			Label: prefix + "Fstruct Form",
 			OnSave: __gong__New__FstructFormCallback(
 				nil,
 				probe,
 			),
 		}).Stage(formStage)
 		fstruct := new(models.Fstruct)
+		formGroup.HasSuppressButton = !isNewInstance
 		FillUpForm(fstruct, formGroup, probe)
 	}
 	formStage.Commit()
