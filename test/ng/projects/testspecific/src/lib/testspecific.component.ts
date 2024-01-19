7c522c63a935f4264b80a721eec4c75ba2349cfc
peugeot <thomaspeugeot@yahoo.fr>
2024-01-19 19:58:07 +0100

    manage CreatedAt / DeletedAt

--- test/ng/projects/testspecific/src/lib/testspecific.component.ts
+++ test/ng/projects/testspecific/src/lib/testspecific.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { AstructDB, BstructDB, FrontRepo } from 'test';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable, timer } from 'rxjs';
+
+import * as test from 'test'
+
 
 @Component({
   selector: 'lib-testspecific',
@@ -11,19 +14,54 @@ import { AstructDB, BstructDB, FrontRepo } from 'test';
   styles: [
   ]
 })
-export class TestspecificComponent {
+export class TestspecificComponent implements OnInit {
 
-  frontRepo = new FrontRepo
+  @Input() GONG__StackPath: string = ""
 
-  constructor() {
+  frontRepo = new test.FrontRepo
 
-    let frontRepo = new (FrontRepo)
+  // the component is refreshed when modification are performed in the back repo 
+  // the checkCommiNbFromBagetCommitNbFromBackTimer polls the commit number of the back repo
+  // if the commit number has increased, it pulls the front repo and redraw the diagram
+  checkCommiNbFromBagetCommitNbFromBackTimer: Observable<number> = timer(500, 500);
+  lastCommitNbFromBack = -1
+  lastPushFromFrontNb = -1
+  currTime: number = 0
 
-    frontRepo.Astructs_array.push(new (AstructDB))
-    frontRepo.Astructs_array.push(new (AstructDB))
+  constructor(
+    public frontRepoService: test.FrontRepoService,
+    private astructService: test.AstructService,
+    private commitNbFromBackService: test.CommitNbFromBackService,
 
-    let array = frontRepo.getArray(AstructDB.GONGSTRUCT_NAME)
-    console.log("", array.length)
+  ) {
+
   }
+  ngOnInit(): void {
 
+    console.log("Material component->ngOnInit : GONG__StackPath, " + this.GONG__StackPath)
+
+
+    // see above for the explanation
+    this.commitNbFromBackService.getCommitNbFromBack(500, this.GONG__StackPath).subscribe(
+      commiNbFromBagetCommitNbFromBack => {
+        if (this.lastCommitNbFromBack < commiNbFromBagetCommitNbFromBack) {
+
+          console.log("last commit nb " + this.lastCommitNbFromBack + " new: " + commiNbFromBagetCommitNbFromBack)
+          this.lastCommitNbFromBack = commiNbFromBagetCommitNbFromBack
+
+          this.frontRepoService.pull(this.GONG__StackPath).subscribe(
+            frontRepo => {
+              this.frontRepo = frontRepo
+              let astructs = this.frontRepo.getFrontArray<test.Astruct>(test.Astruct.GONGSTRUCT_NAME)
+              console.log("Nb of Astruct is ", astructs.length)
+
+              let astruct = astructs[0]
+
+              this.astructService.updateFront(astruct, this.GONG__StackPath).subscribe()
+            }
+          )
+        }
+      }
+    )
+  }
 }
