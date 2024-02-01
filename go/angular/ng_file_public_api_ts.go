e5bba1f52169e026d0bb0630ea1ab9d2ae5b98c9
peugeot <thomaspeugeot@yahoo.fr>
2024-02-01 08:18:15 +0100

    Send back repo content via web socket #468 - public api

--- go/angular/ng_file_public_api_ts.go
+++ go/angular/ng_file_public_api_ts.go
@@ -22,6 +22,8 @@ export * from './lib/front-repo.service'
 export * from './lib/null-int64'
 export * from './lib/commitnbfromback.service'
 export * from './lib/push_from_front_nb.service'
+export * from './lib/back-repo-data'
+export * from './lib/web-socket-service'
 
 {{` + string(rune(NgPublicApiInsertionStructComponentsExportDeclaration)) + `}}
 {{` + string(rune(NgPublicApiInsertionEnumsExportDeclaration)) + `}}
