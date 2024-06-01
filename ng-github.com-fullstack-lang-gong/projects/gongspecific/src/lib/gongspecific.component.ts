65b2e20895c02eb49fab9551585a9abcbca94a76
peugeot <thomaspeugeot@yahoo.fr>
2024-06-01 08:57:42 +0200

    Port to ng17 second Fixes #486

---
+++ ng-github.com-fullstack-lang-gong/projects/gongspecific/src/lib/gongspecific.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-gongspecific',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      gongspecific works!
+    </p>
+  `,
+  styles: ``
+})
+export class GongspecificComponent {
+
+}
