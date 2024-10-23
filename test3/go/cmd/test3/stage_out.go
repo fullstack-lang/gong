0e9b9e57dec462c179b5244b9478871f814da65c
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2024-10-23 19:51:37 +0200

    Develop a DB lite version within a lite stack #517 - OK for Save

--- test3/go/cmd/test3/stage_out.go
+++ test3/go/cmd/test3/stage_out.go
@@ -24,29 +24,23 @@ func _(stage *models.StageStruct) {
 
 	// Declaration of instances to stage
 
-	__A__000000_f := (&models.A{}).Stage(stage)
-	__A__000001_f := (&models.A{}).Stage(stage)
-	__A__000002_g := (&models.A{}).Stage(stage)
-	__A__000003_q := (&models.A{}).Stage(stage)
-	__A__000004_q := (&models.A{}).Stage(stage)
-	__A__000005_q := (&models.A{}).Stage(stage)
-	__A__000006_q := (&models.A{}).Stage(stage)
+	__A__000000_ := (&models.A{}).Stage(stage)
+	__A__000001_ := (&models.A{}).Stage(stage)
+	__A__000002_a := (&models.A{}).Stage(stage)
+	__A__000003_b := (&models.A{}).Stage(stage)
+	__A__000004_c := (&models.A{}).Stage(stage)
 
 	// Setup of values
 
-	__A__000000_f.Name = `f`
+	__A__000000_.Name = ``
 
-	__A__000001_f.Name = `f`
+	__A__000001_.Name = ``
 
-	__A__000002_g.Name = `g`
+	__A__000002_a.Name = `a`
 
-	__A__000003_q.Name = `q`
+	__A__000003_b.Name = `b`
 
-	__A__000004_q.Name = `q`
-
-	__A__000005_q.Name = `q`
-
-	__A__000006_q.Name = `q`
+	__A__000004_c.Name = `c`
 
 	// Setup of pointers
 }
