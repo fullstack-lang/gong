20b0374e682f9497ab34e774b606066e7b1dda2c
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2024-10-23 19:30:02 +0200

    Develop a DB lite version within a lite stack #517 - changed strange save call

--- test3/go/orm/back_repo.go
+++ test3/go/orm/back_repo.go
@@ -39,15 +39,14 @@ func NewBackRepo(stage *models.StageStruct, filename string) (backRepo *BackRepo
 
 	var db db.DBInterface
 
-	if true {
+	if false {
 		db = NewDBLite()
 	} else {
 		db = dbgorm.NewDBWrapper(filename, "github_com_fullstack_lang_gong_test3_go",
-		&ADB{},
-		&BDB{},
+			&ADB{},
+			&BDB{},
 		)
 	}
-
 
 	backRepo = new(BackRepoStruct)
 
