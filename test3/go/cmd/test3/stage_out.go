453737547b3ac3b2a3236e487a00f6bf1fd3f5da
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2024-10-23 19:40:56 +0200

    WIP with big issue

--- test3/go/cmd/test3/stage_out.go
+++ test3/go/cmd/test3/stage_out.go
@@ -24,11 +24,29 @@ func _(stage *models.StageStruct) {
 
 	// Declaration of instances to stage
 
-	__A__000000_eee := (&models.A{}).Stage(stage)
+	__A__000000_f := (&models.A{}).Stage(stage)
+	__A__000001_f := (&models.A{}).Stage(stage)
+	__A__000002_g := (&models.A{}).Stage(stage)
+	__A__000003_q := (&models.A{}).Stage(stage)
+	__A__000004_q := (&models.A{}).Stage(stage)
+	__A__000005_q := (&models.A{}).Stage(stage)
+	__A__000006_q := (&models.A{}).Stage(stage)
 
 	// Setup of values
 
-	__A__000000_eee.Name = `eee`
+	__A__000000_f.Name = `f`
+
+	__A__000001_f.Name = `f`
+
+	__A__000002_g.Name = `g`
+
+	__A__000003_q.Name = `q`
+
+	__A__000004_q.Name = `q`
+
+	__A__000005_q.Name = `q`
+
+	__A__000006_q.Name = `q`
 
 	// Setup of pointers
 }
