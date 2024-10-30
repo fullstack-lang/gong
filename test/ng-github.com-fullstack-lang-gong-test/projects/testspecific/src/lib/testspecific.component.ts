e47f4ba6ef6dd7fc4c628bb879f8f76979d69448
peugeot <thomaspeugeot@yahoo.fr>
2024-10-30 21:35:40 +0100

    Problem with dbLite version: channel no longer active Fixes #519 with test for web socket

--- test/ng-github.com-fullstack-lang-gong-test/projects/testspecific/src/lib/testspecific.component.ts
+++ test/ng-github.com-fullstack-lang-gong-test/projects/testspecific/src/lib/testspecific.component.ts
@@ -1,16 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as test from '../../../test/src/public-api'
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'lib-testspecific',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTableModule
+  ],
   template: `
-    <p>
-      testspecific works!
-    </p>
+
+<table mat-table [dataSource]="frontRepo.array_Astructs" class="mat-elevation-z8">
+
+    <!-- ID Column -->
+    <ng-container matColumnDef="ID">
+      <th mat-header-cell *matHeaderCellDef> ID </th>
+      <td mat-cell *matCellDef="let astruct"> {{astruct.ID}} </td>
+    </ng-container>
+
+    <!-- Name Column -->
+    <ng-container matColumnDef="Name">
+      <th mat-header-cell *matHeaderCellDef> Name </th>
+      <td mat-cell *matCellDef="let astruct"> {{astruct.Name}} </td>
+    </ng-container>
+
+
+    <!-- Header Row -->
+    <tr mat-header-row *matHeaderRowDef="['ID', 'Name']"></tr>
+    <!-- Data Row -->
+    <tr mat-row *matRowDef="let row; columns: ['ID', 'Name'];"></tr>
+
+</table>
   `,
   styles: ``
 })
-export class TestspecificComponent {
+export class TestspecificComponent implements OnInit {
+
+  array_Astructs = new Array<test.Astruct>() // array of front instances
+
+  StacksNames = test.StacksNames
+  public frontRepo = new (test.FrontRepo)
+
+  constructor(
+    private frontRepoService: test.FrontRepoService,
+  ) { }
+
+  ngOnInit(): void {
+
+    this.frontRepoService.connectToWebSocket(this.StacksNames.Test).subscribe(
+      gongtablesFrontRepo => {
+        console.log("front repo updated")
+        this.frontRepo = gongtablesFrontRepo
+      }
+    )
+
+    console.log("ngOnInit")
+  }
 
 }
