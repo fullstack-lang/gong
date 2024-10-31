9b1d0e50001ff2df40dbc3dee4640963c9ac7204
peugeot <thomaspeugeot@yahoo.fr>
2024-10-31 13:45:52 +0100

    new gongc

--- go.mod
+++ go.mod
@@ -23,9 +23,9 @@ require (
 )
 
 require (
-	github.com/fullstack-lang/gongdoc v0.0.0-20241030112531-9c612480a741
-	github.com/fullstack-lang/gongtable v0.0.0-20241030105810-6f53febe9436
-	github.com/fullstack-lang/gongtree v0.0.0-20241030112033-344f45172d6b
+	github.com/fullstack-lang/gongdoc v0.0.0-20241031123431-4da42e4f85ca
+	github.com/fullstack-lang/gongtable v0.0.0-20241031123432-45fc0bc829a0
+	github.com/fullstack-lang/gongtree v0.0.0-20241031123432-de6d9e9f4df2
 	github.com/fullstack-lang/maticons v0.0.0-20230823061653-70c364fbdd00
 	github.com/glebarez/sqlite v1.11.0
 	github.com/gorilla/websocket v1.5.3
@@ -39,7 +39,7 @@ require (
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
-	github.com/fullstack-lang/gongsvg v0.0.0-20241029101741-c51a49dcf14a // indirect
+	github.com/fullstack-lang/gongsvg v0.0.0-20241031123432-d7bca6016816 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.5 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/glebarez/go-sqlite v1.22.0 // indirect
--- vendor/github.com/fullstack-lang/gongtable/go/orm/db.go
+++ vendor/github.com/fullstack-lang/gongtable/go/orm/db.go
@@ -767,7 +767,7 @@ func (db *DBLite) First(instanceDB any, conds ...any) (db.DBInterface, error) {
 		tmp, ok := db.cellDBs[uint(i)]
 
 		if !ok {
-			return nil, errors.New(fmt.Sprintf("db Cell Unkown entry %d", i))
+			return nil, errors.New(fmt.Sprintf("db.First Cell Unkown entry %d", i))
 		}
 
 		cellDB, _ := instanceDB.(*CellDB)
@@ -777,7 +777,7 @@ func (db *DBLite) First(instanceDB any, conds ...any) (db.DBInterface, error) {
 		tmp, ok := db.cellbooleanDBs[uint(i)]
 
 		if !ok {
-			return nil, errors.New(fmt.Sprintf("db CellBoolean Unkown entry %d", i))
+			return nil, errors.New(fmt.Sprintf("db.First CellBoolean Unkown entry %d", i))
 		}
 
 		cellbooleanDB, _ := instanceDB.(*CellBooleanDB)
@@ -787,7 +787,7 @@ func (db *DBLite) First(instanceDB any, conds ...any) (db.DBInterface, error) {
 		tmp, ok := db.cellfloat64DBs[uint(i)]
 
 		if !ok {
-			return nil, errors.New(fmt.Sprintf("db CellFloat64 Unkown entry %d", i))
+			return nil, errors.New(fmt.Sprintf("db.First CellFloat64 Unkown entry %d", i))
 		}
 
 		cellfloat64DB, _ := instanceDB.(*CellFloat64DB)
@@ -797,7 +797,7 @@ func (db *DBLite) First(instanceDB any, conds ...any) (db.DBInterface, error) {
 		tmp, ok := db.celliconDBs[uint(i)]
 
 		if !ok {
-			return nil, errors.New(fmt.Sprintf("db CellIcon Unkown entry %d", i))
+			return nil, errors.New(fmt.Sprintf("db.First CellIcon Unkown entry %d", i))
 		}
 
 		celliconDB, _ := instanceDB.(*CellIconDB)
@@ -807,7 +807,7 @@ func (db *DBLite) First(instanceDB any, conds ...any) (db.DBInterface, error) {
 		tmp, ok := db.cellintDBs[uint(i)]
 
 		if !ok {
-			return nil, errors.New(fmt.Sprintf("db CellInt Unkown entry %d", i))
+			return nil, errors.New(fmt.Sprintf("db.First CellInt Unkown entry %d", i))
 		}
 
 		cellintDB, _ := instanceDB.(*CellIntDB)
@@ -817,7 +817,7 @@ func (db *DBLite) First(instanceDB any, conds ...any) (db.DBInterface, error) {
 		tmp, ok := db.cellstringDBs[uint(i)]
 
 		if !ok {
-			return nil, errors.New(fmt.Sprintf("db CellString Unkown entry %d", i))
+			return nil, errors.New(fmt.Sprintf("db.First CellString Unkown entry %d", i))
 		}
 
 		cellstringDB, _ := instanceDB.(*CellStringDB)
@@ -827,7 +827,7 @@ func (db *DBLite) First(instanceDB any, conds ...any) (db.DBInterface, error) {
 		tmp, ok := db.checkboxDBs[uint(i)]
 
 		if !ok {
-			return nil, errors.New(fmt.Sprintf("db CheckBox Unkown entry %d", i))
+			return nil, errors.New(fmt.Sprintf("db.First CheckBox Unkown entry %d", i))
 		}
 
 		checkboxDB, _ := instanceDB.(*CheckBoxDB)
@@ -837,7 +837,7 @@ func (db *DBLite) First(instanceDB any, conds ...any) (db.DBInterface, error) {
 		tmp, ok := db.displayedcolumnDBs[uint(i)]
 
 		if !ok {
-			return nil, errors.New(fmt.Sprintf("db DisplayedColumn Unkown entry %d", i))
+			return nil, errors.New(fmt.Sprintf("db.First DisplayedColumn Unkown entry %d", i))
 		}
 
 		displayedcolumnDB, _ := instanceDB.(*DisplayedColumnDB)
@@ -847,7 +847,7 @@ func (db *DBLite) First(instanceDB any, conds ...any) (db.DBInterface, error) {
 		tmp, ok := db.formdivDBs[uint(i)]
 
 		if !ok {
-			return nil, errors.New(fmt.Sprintf("db FormDiv Unkown entry %d", i))
+			return nil, errors.New(fmt.Sprintf("db.First FormDiv Unkown entry %d", i))
 		}
 
 		formdivDB, _ := instanceDB.(*FormDivDB)
@@ -857,7 +857,7 @@ func (db *DBLite) First(instanceDB any, conds ...any) (db.DBInterface, error) {
 		tmp, ok := db.formeditassocbuttonDBs[uint(i)]
 
 		if !ok {
-			return nil, errors.New(fmt.Sprintf("db FormEditAssocButton Unkown entry %d", i))
+			return nil, errors.New(fmt.Sprintf("db.First FormEditAssocButton Unkown entry %d", i))
 		}
 
 		formeditassocbuttonDB, _ := instanceDB.(*FormEditAssocButtonDB)
@@ -867,7 +867,7 @@ func (db *DBLite) First(instanceDB any, conds ...any) (db.DBInterface, error) {
 		tmp, ok := db.formfieldDBs[uint(i)]
 
 		if !ok {
-			return nil, errors.New(fmt.Sprintf("db FormField Unkown entry %d", i))
+			return nil, errors.New(fmt.Sprintf("db.First FormField Unkown entry %d", i))
 		}
 
 		formfieldDB, _ := instanceDB.(*FormFieldDB)
@@ -877,7 +877,7 @@ func (db *DBLite) First(instanceDB any, conds ...any) (db.DBInterface, error) {
 		tmp, ok := db.formfielddateDBs[uint(i)]
 
 		if !ok {
-			return nil, errors.New(fmt.Sprintf("db FormFieldDate Unkown entry %d", i))
+			return nil, errors.New(fmt.Sprintf("db.First FormFieldDate Unkown entry %d", i))
 		}
 
 		formfielddateDB, _ := instanceDB.(*FormFieldDateDB)
@@ -887,7 +887,7 @@ func (db *DBLite) First(instanceDB any, conds ...any) (db.DBInterface, error) {
 		tmp, ok := db.formfielddatetimeDBs[uint(i)]
 
 		if !ok {
-			return nil, errors.New(fmt.Sprintf("db FormFieldDateTime Unkown entry %d", i))
+			return nil, errors.New(fmt.Sprintf("db.First FormFieldDateTime Unkown entry %d", i))
 		}
 
 		formfielddatetimeDB, _ := instanceDB.(*FormFieldDateTimeDB)
@@ -897,7 +897,7 @@ func (db *DBLite) First(instanceDB any, conds ...any) (db.DBInterface, error) {
 		tmp, ok := db.formfieldfloat64DBs[uint(i)]
 
 		if !ok {
-			return nil, errors.New(fmt.Sprintf("db FormFieldFloat64 Unkown entry %d", i))
+			return nil, errors.New(fmt.Sprintf("db.First FormFieldFloat64 Unkown entry %d", i))
 		}
 
 		formfieldfloat64DB, _ := instanceDB.(*FormFieldFloat64DB)
@@ -907,7 +907,7 @@ func (db *DBLite) First(instanceDB any, conds ...any) (db.DBInterface, error) {
 		tmp, ok := db.formfieldintDBs[uint(i)]
 
 		if !ok {
-			return nil, errors.New(fmt.Sprintf("db FormFieldInt Unkown entry %d", i))
+			return nil, errors.New(fmt.Sprintf("db.First FormFieldInt Unkown entry %d", i))
 		}
 
 		formfieldintDB, _ := instanceDB.(*FormFieldIntDB)
@@ -917,7 +917,7 @@ func (db *DBLite) First(instanceDB any, conds ...any) (db.DBInterface, error) {
 		tmp, ok := db.formfieldselectDBs[uint(i)]
 
 		if !ok {
-			return nil, errors.New(fmt.Sprintf("db FormFieldSelect Unkown entry %d", i))
+			return nil, errors.New(fmt.Sprintf("db.First FormFieldSelect Unkown entry %d", i))
 		}
 
 		formfieldselectDB, _ := instanceDB.(*FormFieldSelectDB)
@@ -927,7 +927,7 @@ func (db *DBLite) First(instanceDB any, conds ...any) (db.DBInterface, error) {
 		tmp, ok := db.formfieldstringDBs[uint(i)]
 
 		if !ok {
-			return nil, errors.New(fmt.Sprintf("db FormFieldString Unkown entry %d", i))
+			return nil, errors.New(fmt.Sprintf("db.First FormFieldString Unkown entry %d", i))
 		}
 
 		formfieldstringDB, _ := instanceDB.(*FormFieldStringDB)
@@ -937,7 +937,7 @@ func (db *DBLite) First(instanceDB any, conds ...any) (db.DBInterface, error) {
 		tmp, ok := db.formfieldtimeDBs[uint(i)]
 
 		if !ok {
-			return nil, errors.New(fmt.Sprintf("db FormFieldTime Unkown entry %d", i))
+			return nil, errors.New(fmt.Sprintf("db.First FormFieldTime Unkown entry %d", i))
 		}
 
 		formfieldtimeDB, _ := instanceDB.(*FormFieldTimeDB)
@@ -947,7 +947,7 @@ func (db *DBLite) First(instanceDB any, conds ...any) (db.DBInterface, error) {
 		tmp, ok := db.formgroupDBs[uint(i)]
 
 		if !ok {
-			return nil, errors.New(fmt.Sprintf("db FormGroup Unkown entry %d", i))
+			return nil, errors.New(fmt.Sprintf("db.First FormGroup Unkown entry %d", i))
 		}
 
 		formgroupDB, _ := instanceDB.(*FormGroupDB)
@@ -957,7 +957,7 @@ func (db *DBLite) First(instanceDB any, conds ...any) (db.DBInterface, error) {
 		tmp, ok := db.formsortassocbuttonDBs[uint(i)]
 
 		if !ok {
-			return nil, errors.New(fmt.Sprintf("db FormSortAssocButton Unkown entry %d", i))
+			return nil, errors.New(fmt.Sprintf("db.First FormSortAssocButton Unkown entry %d", i))
 		}
 
 		formsortassocbuttonDB, _ := instanceDB.(*FormSortAssocButtonDB)
@@ -967,7 +967,7 @@ func (db *DBLite) First(instanceDB any, conds ...any) (db.DBInterface, error) {
 		tmp, ok := db.optionDBs[uint(i)]
 
 		if !ok {
-			return nil, errors.New(fmt.Sprintf("db Option Unkown entry %d", i))
+			return nil, errors.New(fmt.Sprintf("db.First Option Unkown entry %d", i))
 		}
 
 		optionDB, _ := instanceDB.(*OptionDB)
@@ -977,7 +977,7 @@ func (db *DBLite) First(instanceDB any, conds ...any) (db.DBInterface, error) {
 		tmp, ok := db.rowDBs[uint(i)]
 
 		if !ok {
-			return nil, errors.New(fmt.Sprintf("db Row Unkown entry %d", i))
+			return nil, errors.New(fmt.Sprintf("db.First Row Unkown entry %d", i))
 		}
 
 		rowDB, _ := instanceDB.(*RowDB)
@@ -987,7 +987,7 @@ func (db *DBLite) First(instanceDB any, conds ...any) (db.DBInterface, error) {
 		tmp, ok := db.tableDBs[uint(i)]
 
 		if !ok {
-			return nil, errors.New(fmt.Sprintf("db Table Unkown entry %d", i))
+			return nil, errors.New(fmt.Sprintf("db.First Table Unkown entry %d", i))
 		}
 
 		tableDB, _ := instanceDB.(*TableDB)
