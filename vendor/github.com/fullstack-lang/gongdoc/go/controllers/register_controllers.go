ba606a7f7e497a3f64024fbf8373c4cf85c390d8
peugeot <thomaspeugeot@yahoo.fr>
2024-10-31 19:32:02 +0100

    Problem with dbLite version: channel no longer active Fixes #519 - new gongc + vendoring

--- vendor/github.com/fullstack-lang/gongdoc/go/controllers/register_controllers.go
+++ vendor/github.com/fullstack-lang/gongdoc/go/controllers/register_controllers.go
@@ -218,7 +218,7 @@ func (controller *Controller) onWebSocketRequestForBackRepoContent(c *gin.Contex
 			// ReadMessage is used to detect client disconnection
 			_, _, err := wsConnection.ReadMessage()
 			if err != nil {
-				log.Println("WebSocket read error (client disconnected):", err)
+				log.Println("github.com/fullstack-lang/gongdoc/go", stackPath, "WebSocket read error (client disconnected):", err)
 				cancel() // Cancel the context
 				return
 			}
