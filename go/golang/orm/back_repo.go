f1fd5f5eed516cac74717a483012b22172cfa33a
peugeot <thomaspeugeot@yahoo.fr>
2024-11-03 11:31:26 +0100

    Add a mutex on the backRepo to avoid commit write in // with a websocket payload packing #524 - generation

--- go/golang/orm/back_repo.go
+++ go/golang/orm/back_repo.go
@@ -100,6 +100,11 @@ func (backRepo *BackRepoStruct) IncrementPushFromFrontNb() uint {
 
 // Commit the BackRepoStruct inner variables and link to the database
 func (backRepo *BackRepoStruct) Commit(stage *models.StageStruct) {
+
+	// forbid read of back repo during commit
+	backRepo.rwMutex.Lock()
+	defer backRepo.rwMutex.Unlock()
+
 	// insertion point for per struct back repo phase one commit{{` + string(rune(BackRepoPerStructPhaseOneCommits)) + `}}
 
 	// insertion point for per struct back repo phase two commit{{` + string(rune(BackRepoPerStructPhaseTwoCommits)) + `}}
@@ -236,6 +241,7 @@ func (backRepoStruct *BackRepoStruct) broadcastNbCommitToBack() {
 		}
 	}
 }
+
 `
 
 type BackRepoSubTemplateInsertion int
