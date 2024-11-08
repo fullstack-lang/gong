cd10a91889a74e3fcc636d47df78ac34e834f8ad
peugeot <thomaspeugeot@yahoo.fr>
2024-11-08 08:34:42 +0100

    new gongc & go get -u ./...

--- vendor/github.com/tealeg/xlsx/v3/file.go
+++ vendor/github.com/tealeg/xlsx/v3/file.go
@@ -202,15 +202,15 @@ func (f *File) AddSheetWithCellStore(sheetName string, constructor CellStoreCons
 	if _, exists := f.Sheet[sheetName]; exists {
 		return nil, fmt.Errorf("duplicate sheet name '%s'.", sheetName)
 	}
-	
+
 	if err := IsSaneSheetName(sheetName); err != nil {
 		return nil, fmt.Errorf("sheet name is not valid: %w", err)
 	}
 	sheet := &Sheet{
-		Name:     sheetName,
-		File:     f,
-		Selected: len(f.Sheets) == 0,
-		Cols:     &ColStore{},
+		Name:          sheetName,
+		File:          f,
+		Selected:      len(f.Sheets) == 0,
+		Cols:          &ColStore{},
 		cellStoreName: sheetName,
 	}
 
@@ -335,7 +335,7 @@ func autoFilterDefinedName(sheet *Sheet, sheetIndex int) (*xlsxDefinedName, erro
 // representing the file in terms of the structure of an XLSX file.
 func (f *File) MakeStreamParts() (map[string]string, error) {
 	var parts map[string]string
-	var refTable *RefTable = NewSharedStringRefTable()
+	var refTable *RefTable = NewSharedStringRefTable(DEFAULT_REFTABLE_SIZE)
 	refTable.isWrite = true
 	var workbookRels WorkBookRels = make(WorkBookRels)
 	var err error
@@ -465,7 +465,7 @@ func (f *File) MakeStreamParts() (map[string]string, error) {
 // MarshallParts constructs a map of file name to XML content representing the file
 // in terms of the structure of an XLSX file.
 func (f *File) MarshallParts(zipWriter *zip.Writer) error {
-	var refTable *RefTable = NewSharedStringRefTable()
+	var refTable *RefTable = NewSharedStringRefTable(DEFAULT_REFTABLE_SIZE)
 	refTable.isWrite = true
 	var workbookRels WorkBookRels = make(WorkBookRels)
 	var err error
@@ -650,9 +650,10 @@ func (f *File) MarshallParts(zipWriter *zip.Writer) error {
 // Here, value would be set to the raw value of the cell A1 in the
 // first sheet in the XLSX file.
 func (f *File) ToSlice() (output [][][]string, err error) {
-	output = [][][]string{}
+	sheetCount := len(f.Sheets)
+	output = make([][][]string, 0, sheetCount)
 	for _, sheet := range f.Sheets {
-		s := [][]string{}
+		s := make([][]string, 0, sheet.MaxRow)
 		err := sheet.ForEachRow(func(row *Row) error {
 			r := []string{}
 			err := row.ForEachCell(func(cell *Cell) error {
