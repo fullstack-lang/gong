bf3c63523c40fe4583f339a5de263c6179ffdf57
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2024-12-20 16:59:10 +0100

    extension of API change

--- test/go/probe/assoc_sorting_button_impl.go
+++ test/go/probe/assoc_sorting_button_impl.go
@@ -28,9 +28,9 @@ func NewOnSortingEditon[InstanceType models.PointerToGongstruct, FieldType model
 }
 
 type OnSortingEditon[InstanceType models.PointerToGongstruct, FieldType models.PointerToGongstruct] struct {
-	instance   InstanceType
-	field      *[]FieldType
-	probe *Probe
+	instance InstanceType
+	field    *[]FieldType
+	probe    *Probe
 }
 
 func (onSortingEditon *OnSortingEditon[InstanceType, FieldType]) OnButtonPressed() {
@@ -68,7 +68,8 @@ func (onSortingEditon *OnSortingEditon[InstanceType, FieldType]) OnButtonPressed
 			cell.Name = fmt.Sprintf("Row %s - Column %s", instance.GetName(), fieldName)
 
 			cellString := new(gongtable_models.CellString).Stage(tableStageForSelection)
-			cellString.Name = models.GetFieldStringValueFromPointer(instance, fieldName)
+			value := models.GetFieldStringValueFromPointer(instance, fieldName)
+			cellString.Name = value.GetValueString()
 			cellString.Value = cellString.Name
 			cell.CellString = cellString
 
@@ -101,9 +102,9 @@ func NewTableSortSaver[InstanceType models.PointerToGongstruct, FieldType models
 }
 
 type TableSortSaver[InstanceType models.PointerToGongstruct, FieldType models.PointerToGongstruct] struct {
-	instance   InstanceType
-	field      *[]FieldType
-	probe *Probe
+	instance InstanceType
+	field    *[]FieldType
+	probe    *Probe
 
 	// map giving the relation between the row ID and the instance
 	map_RowID_instance *map[*gongtable_models.Row]FieldType
