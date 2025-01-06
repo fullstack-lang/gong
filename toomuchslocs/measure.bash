7f95a55bb1df2a2bbb21d1d09f11a59235c988bb
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-01-06 16:49:45 +0100

    added too much slocs stack

---
+++ toomuchslocs/measure.bash
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <n>"
+    exit 1
+fi
+
+n=$1
+output_file="size_measurements.txt"
+mkdir -p go/models
+
+for ((i=200; i<=$n; i=i+10)); do
+    # Generate Go file with i structs
+    cat > go/models/model.go << EOF
+package models
+
+$(for ((j=1; j<=$i; j++)); do
+echo "type Model$j struct {
+    Name string
+
+    Floatfield, Floatfield2 float64
+
+	Intfield int
+
+    Model$j *Model$j
+    Model$js []*Model$j
+ }"
+done)
+EOF
+
+    # Run gongc command
+    start_time=$(date +%s.%N)
+    gongc -skipGoModCommands -skipNg go/models
+    end_time=$(date +%s.%N)
+    execution_time=$(awk -v start="$start_time" -v end="$end_time" 'BEGIN{printf "%.6f\n", end-start}') 
+
+    # Measure file size
+    size=$(du -b ./go/cmd/toomuchslocs | cut -f1)
+    
+    # Log size
+    echo "$i $size $execution_time" >> $output_file
+done
