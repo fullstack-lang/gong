e0c2ea977295c3e6f9c9e6036585c20edb46a1e8
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-02-16 11:27:53 +0100

    Added index in the web socket message

--- go/golang/orm/back_repo_data.go
+++ go/golang/orm/back_repo_data.go
@@ -5,6 +5,9 @@ package orm
 
 type BackRepoData struct {
 	// insertion point for slices{{` + string(rune(BackRepoDataSlice)) + `}}
+
+	// index of the web socket for this stack type (unique among all stack instances)
+	GONG__Index int
 }
 
 func CopyBackRepoToBackRepoData(backRepo *BackRepoStruct, backRepoData *BackRepoData) {
