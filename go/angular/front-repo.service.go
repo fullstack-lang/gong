30c7616eefbb677738cf7a177407341c2379cd0b
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-02-26 00:18:35 +0100

    more robust initaliation of observableFrontRepo (ng18)

--- go/angular/front-repo.service.go
+++ go/angular/front-repo.service.go
@@ -138,20 +138,10 @@ export class FrontRepoService {
 	}
 
 	// typing of observable can be messy in typescript. Therefore, one force the type
-	observableFrontRepo: [
+	observableFrontRepo!: [
 		Observable<null>, // see below for the of(null) observable
 		// insertion point sub template {{` + string(rune(NgLibFrontRepoObservableArrayType)) + `}}
-	] = [
-			// Using "combineLatest" with a placeholder observable.
-			//
-			// This allows the typescript compiler to pass when no GongStruct is present in the front API
-			//
-			// The "of(null)" is a "meaningless" observable that emits a single value (null) and completes.
-			// This is used as a workaround to satisfy TypeScript requirements and the "combineLatest" 
-			// expectation for a non-empty array of observables.
-			of(null), // 
-			// insertion point sub template{{` + string(rune(NgLibFrontRepoObservableRefs)) + `}}
-		];
+	];
 
 	//
 	// pull performs a GET on all struct of the stack and redeem association pointers 
