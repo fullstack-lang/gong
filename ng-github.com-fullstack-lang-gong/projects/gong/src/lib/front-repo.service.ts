cb7eed4531b96534f2cec79b6f22953914bd4828
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-02-25 04:31:04 +0100

    new gongc

--- ng-github.com-fullstack-lang-gong/projects/gong/src/lib/front-repo.service.ts
+++ ng-github.com-fullstack-lang-gong/projects/gong/src/lib/front-repo.service.ts
@@ -96,6 +96,8 @@ export class FrontRepo { // insertion point sub template
 	map_ID_SliceOfPointerToGongStructField = new Map<number, SliceOfPointerToGongStructField>() // map of front instances
 
 
+	public GONG__Index = -1
+
 	// getFrontArray allows for a get function that is robust to refactoring of the named struct name
 	// for instance frontRepo.getArray<Astruct>( Astruct.GONGSTRUCT_NAME), is robust to a refactoring of Astruct identifier
 	// contrary to frontRepo.Astructs_array which is not refactored when Astruct identifier is modified
@@ -655,6 +657,7 @@ export class FrontRepoService {
 				const backRepoData = new BackRepoData(JSON.parse(event.data))
 
 				let frontRepo = new (FrontRepo)
+				frontRepo.GONG__Index = backRepoData.GONG__Index
 
 				// 
 				// First Step: init map of instances
