67db7a59879a6b4be3f86a8cabcedf7c7026b101
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-02-25 05:42:12 +0100

    new gongc & go get -u ./...

--- vendor/github.com/fullstack-lang/gongdoc/go/fullstack/new_stack_instance.go
+++ vendor/github.com/fullstack-lang/gongdoc/go/fullstack/new_stack_instance.go
@@ -11,7 +11,7 @@ import (
 	// this will import the angular front end source code directory (versionned with git) in the vendor directory
 	// this path will be included in the "tsconfig.json" front end compilation paths
 	// to include this stack front end code
-	_ "github.com/fullstack-lang/gongdoc/ng-github.com-fullstack-lang-gongdoc/projects"
+	_ "github.com/fullstack-lang/gongdoc/ng-github.com-fullstack-lang-gongdoc"
 )
 
 // NewStackInstance creates a new stack instance from the Stack Model
