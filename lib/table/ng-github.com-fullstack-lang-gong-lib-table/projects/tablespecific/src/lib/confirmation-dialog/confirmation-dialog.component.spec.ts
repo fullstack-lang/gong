5ead6bbf5147d1981846f14bcac1b1e873ab7f98
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-03-02 17:24:11 +0100

    table

---
+++ lib/table/ng-github.com-fullstack-lang-gong-lib-table/projects/tablespecific/src/lib/confirmation-dialog/confirmation-dialog.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ConfirmationDialogComponent } from './confirmation-dialog.component';
+
+describe('ConfirmationDialogComponent', () => {
+  let component: ConfirmationDialogComponent;
+  let fixture: ComponentFixture<ConfirmationDialogComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ConfirmationDialogComponent]
+    });
+    fixture = TestBed.createComponent(ConfirmationDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
