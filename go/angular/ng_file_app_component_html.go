81292a2ac5e0fcfa29dd10fbc681a6618f6e6e1b
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-03-09 05:43:34 +0100

    have probe use split lib with compile time safety #561

--- go/angular/ng_file_app_component_html.go
+++ go/angular/ng_file_app_component_html.go
@@ -2,8 +2,8 @@ package angular
 
 const NgFileAppComponentHtml = `<as-split unit="pixel" direction="vertical">
     <as-split-area [size]="400">
-        <lib-test-specific GONG__StackPath="{{pkgname}}">
-        </lib-test-specific>
+        <lib-{{pkgname}}-specific GONG__StackPath="{{pkgname}}">
+        </lib-{{pkgname}}-specific>
     </as-split-area>
     <as-split-area [size]="$any('*')">
         <lib-split-specific [GONG__StackPath]="StackName"></lib-split-specific>
