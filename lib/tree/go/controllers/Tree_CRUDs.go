19ebaebe34f2d7038d4d23f94c2972eddbe94613
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-03-09 13:52:27 +0100

    tree is now pure gong

--- lib/tree/go/controllers/Tree_CRUDs.go
+++ lib/tree/go/controllers/Tree_CRUDs.go
@@ -66,7 +66,8 @@ func (controller *Controller) GetTrees(c *gin.Context) {
 	}
 	backRepo := controller.Map_BackRepos[stackPath]
 	if backRepo == nil {
-		log.Panic("Stack github.com/fullstack-lang/gong/lib/tree/go/models, Unkown stack", stackPath)
+		message := "Stack github.com/fullstack-lang/gong/lib/tree/go, Unkown stack: \"" + stackPath + "\""
+		log.Panic(message)
 	}
 	db := backRepo.BackRepoTree.GetDB()
 
@@ -129,7 +130,8 @@ func (controller *Controller) PostTree(c *gin.Context) {
 	}
 	backRepo := controller.Map_BackRepos[stackPath]
 	if backRepo == nil {
-		log.Panic("Stack github.com/fullstack-lang/gong/lib/tree/go/models, Unkown stack", stackPath)
+		message := "Stack github.com/fullstack-lang/gong/lib/tree/go, Unkown stack: \"" + stackPath + "\""
+		log.Panic(message)
 	}
 	db := backRepo.BackRepoTree.GetDB()
 
@@ -199,7 +201,8 @@ func (controller *Controller) GetTree(c *gin.Context) {
 	}
 	backRepo := controller.Map_BackRepos[stackPath]
 	if backRepo == nil {
-		log.Panic("Stack github.com/fullstack-lang/gong/lib/tree/go/models, Unkown stack", stackPath)
+		message := "Stack github.com/fullstack-lang/gong/lib/tree/go, Unkown stack: \"" + stackPath + "\""
+		log.Panic(message)
 	}
 	db := backRepo.BackRepoTree.GetDB()
 
@@ -248,7 +251,8 @@ func (controller *Controller) UpdateTree(c *gin.Context) {
 	}
 	backRepo := controller.Map_BackRepos[stackPath]
 	if backRepo == nil {
-		log.Panic("Stack github.com/fullstack-lang/gong/lib/tree/go/models, Unkown stack", stackPath)
+		message := "Stack github.com/fullstack-lang/gong/lib/tree/go, Unkown stack: \"" + stackPath + "\""
+		log.Panic(message)
 	}
 	db := backRepo.BackRepoTree.GetDB()
 
@@ -338,7 +342,8 @@ func (controller *Controller) DeleteTree(c *gin.Context) {
 	}
 	backRepo := controller.Map_BackRepos[stackPath]
 	if backRepo == nil {
-		log.Panic("Stack github.com/fullstack-lang/gong/lib/tree/go/models, Unkown stack", stackPath)
+		message := "Stack github.com/fullstack-lang/gong/lib/tree/go, Unkown stack: \"" + stackPath + "\""
+		log.Panic(message)
 	}
 	db := backRepo.BackRepoTree.GetDB()
 
