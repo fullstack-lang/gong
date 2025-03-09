8aba643754f5a25e57c84d257b266696c184b106
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-03-09 14:23:11 +0100

    gong

---
+++ ng-github.com-fullstack-lang-gong/projects/gongspecific/src/lib/gong-specific/gong-specific.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-gong-specific',
+  imports: [],
+  templateUrl: './gong-specific.component.html',
+  styleUrl: './gong-specific.component.css'
+})
+export class GongSpecificComponent {
+
+}
