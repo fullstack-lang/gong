fc0e3c07d1d7bed09e26fee323551cb7f8c8057c
peugeot <thomaspeugeot@yahoo.fr>
2025-03-16 21:25:19 +0100

    solved 564

--- go/golang/orm/back_repo.go
+++ go/golang/orm/back_repo.go
@@ -105,17 +105,23 @@ func (backRepo *BackRepoStruct) Commit(stage *models.StageStruct) {
 
 	// forbid read of back repo during commit
 	backRepo.rwMutex.Lock()
-	defer backRepo.rwMutex.Unlock()
 
 	// insertion point for per struct back repo phase one commit{{` + string(rune(BackRepoPerStructPhaseOneCommits)) + `}}
 
 	// insertion point for per struct back repo phase two commit{{` + string(rune(BackRepoPerStructPhaseTwoCommits)) + `}}
+
+	// important to release the mutex before calls to IncrementCommitFromBackNb
+	// because it will block otherwise
+	backRepo.rwMutex.Unlock()
 
 	backRepo.IncrementCommitFromBackNb()
 }
 
 // Checkout the database into the stage
 func (backRepo *BackRepoStruct) Checkout(stage *models.StageStruct) {
+
+	backRepo.rwMutex.Lock()
+	defer backRepo.rwMutex.Unlock()
 	// insertion point for per struct back repo phase one commit{{` + string(rune(BackRepoPerStructPhaseOneCheckouts)) + `}}
 
 	// insertion point for per struct back repo phase two commit{{` + string(rune(BackRepoPerStructPhaseTwoCheckouts)) + `}}
