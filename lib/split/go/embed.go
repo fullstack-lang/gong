4e2b105194e6b1a8b33e9c4048d6274c0ab74e05
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-03-22 13:35:21 +0100

    added front code vendored for split

--- lib/split/go/embed.go
+++ lib/split/go/embed.go
@@ -1,6 +1,18 @@
 package split
 
-import "embed"
+import (
+	"embed"
+
+	// those are imported in order for client of split to have all
+	// front code vendored
+
+	_ "github.com/fullstack-lang/gong/lib/button/go/stack"
+	_ "github.com/fullstack-lang/gong/lib/cursor/go/stack"
+	_ "github.com/fullstack-lang/gong/lib/slider/go/stack"
+	_ "github.com/fullstack-lang/gong/lib/svg/go/stack"
+	_ "github.com/fullstack-lang/gong/lib/tone/go/stack"
+	_ "github.com/fullstack-lang/gong/lib/tree/go/stack"
+)
 
 //go:embed models
 var GoModelsDir embed.FS
