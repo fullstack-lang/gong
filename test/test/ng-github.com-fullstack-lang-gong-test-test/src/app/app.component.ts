e7bfe4e48241f25dede2c99ed4b93c5b3f63df71
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-03-22 12:38:21 +0100

    test front is now independant of models

--- test/test/ng-github.com-fullstack-lang-gong-test-test/src/app/app.component.ts
+++ test/test/ng-github.com-fullstack-lang-gong-test-test/src/app/app.component.ts
@@ -1,34 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
-import { AngularSplitModule } from 'angular-split';
-
-import * as test from '../../projects/test/src/public-api'
-
-import { TestSpecificComponent } from '../../projects/testspecific/src/lib/test-specific/test-specific.component'
-
 import { SplitSpecificComponent } from '@vendored_components/github.com/fullstack-lang/gong/lib/split/ng-github.com-fullstack-lang-gong-lib-split/projects/splitspecific/src/lib/split-specific/split-specific.component'
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    AngularSplitModule,
     SplitSpecificComponent,
-    TestSpecificComponent
   ],
 
   templateUrl: './app.component.html',
 })
-export class AppComponent implements OnInit {
-
-  StackName = "test"
-  StackNameProbe = "test-probe"
-
-  constructor(
-  ) {
-  }
-
-  ngOnInit(): void {
-  }
+export class AppComponent {
 }
