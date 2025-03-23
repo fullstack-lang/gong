6c232f58dba3db32ab2f2ad3674425f6ff04fe89
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-03-23 12:14:01 +0100

    added stager boilerplate code

---
+++ test/test/go/models/stager.go
@@ -0,0 +1,38 @@
+// generated boilerplate code
+// edit the file for adding other stages
+package models
+
+import (
+	"github.com/gin-gonic/gin"
+
+	split "github.com/fullstack-lang/gong/lib/split/go/models"
+	split_stack "github.com/fullstack-lang/gong/lib/split/go/stack"
+)
+
+type Stager struct {
+	stage      *StageStruct
+	splitStage *split.StageStruct
+}
+
+func NewStager(r *gin.Engine, stage *StageStruct) (stager *Stager) {
+
+	stager = new(Stager)
+
+	stager.stage = stage
+	stager.splitStage = split_stack.NewStack(r, "", "", "", "", false, false).Stage
+
+	(&split.View{
+		Name: "Probe",
+		RootAsSplitAreas: []*split.AsSplitArea{
+			(&split.AsSplitArea{
+				Split: (&split.Split{
+					StackName: stage.GetName() + ProbeSplitSuffix,
+				}).Stage(stager.splitStage),
+			}).Stage(stager.splitStage),
+		},
+	}).Stage(stager.splitStage)
+
+	stager.splitStage.Commit()
+
+	return
+}
