036a54f8b23d8208b4820dd9b190d8ce27249da7
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-03-26 05:48:38 +0100

    split: AsPlits is not a slice, it should be a pointer #575 : new gongc

--- go/golang/probe/probe.go
+++ go/golang/probe/probe.go
@@ -99,72 +99,56 @@ func updateSplitStage(probe *Probe) {
 
 	probe.splitStage.Reset()
 
-	mainView := (&split.View{
+	(&split.View{
 		Name: "Main view",
+		RootAsSplitAreas: []*split.AsSplitArea{
+			(&split.AsSplitArea{
+				Name: "Top",
+				Size: 50,
+				AsSplit: (&split.AsSplit{
+					Name:      "Top, sidebar, table & form",
+					Direction: split.Horizontal,
+					AsSplitAreas: []*split.AsSplitArea{
+						(&split.AsSplitArea{
+							Name: "sidebar tree",
+							Size: 20,
+							Tree: (&split.Tree{
+								Name:      "Sidebar",
+								StackName: probe.treeStage.GetName(),
+								TreeName:  SideBarTreeName,
+							}).Stage(probe.splitStage),
+						}).Stage(probe.splitStage),
+						(&split.AsSplitArea{
+							Name: "table",
+							Size: 50,
+							Table: (&split.Table{
+								Name:      "Table",
+								StackName: probe.tableStage.GetName(),
+								TableName: TableName,
+							}).Stage(probe.splitStage),
+						}).Stage(probe.splitStage),
+						(&split.AsSplitArea{
+							Name: "form",
+							Size: 30,
+							Form: (&split.Form{
+								Name:      "Form",
+								StackName: probe.formStage.GetName(),
+								FormName:  FormName,
+							}).Stage(probe.splitStage),
+						}).Stage(probe.splitStage),
+					},
+				}).Stage(probe.splitStage),
+			}).Stage(probe.splitStage),
+			(&split.AsSplitArea{
+				Name: "Bottom",
+				Size: 50,
+				Doc: (&split.Doc{
+					Name:      "Doc",
+					StackName: probe.splitStage.GetName(),
+				}).Stage(probe.splitStage),
+			}).Stage(probe.splitStage),
+		},
 	}).Stage(probe.splitStage)
-
-	topSplitArea := (&split.AsSplitArea{
-		Name: "Top",
-		Size: 50,
-	}).Stage(probe.splitStage)
-	mainView.RootAsSplitAreas = append(mainView.RootAsSplitAreas, topSplitArea)
-
-	horizontalSplit := (&split.AsSplit{
-		Name:      "Top, sidebar, table & form",
-		Direction: split.Horizontal,
-	}).Stage(probe.splitStage)
-	topSplitArea.AsSplits = append(topSplitArea.AsSplits, horizontalSplit)
-
-	sidebarArea := (&split.AsSplitArea{
-		Name: "sidebar tree",
-		Size: 20,
-	}).Stage(probe.splitStage)
-	horizontalSplit.AsSplitAreas = append(horizontalSplit.AsSplitAreas, sidebarArea)
-
-	tree := (&split.Tree{
-		Name:      "Sidebar",
-		StackName: probe.treeStage.GetName(),
-		TreeName:  SideBarTreeName,
-	}).Stage(probe.splitStage)
-	sidebarArea.Tree = tree
-
-	tableArea := (&split.AsSplitArea{
-		Name: "table",
-		Size: 50,
-	}).Stage(probe.splitStage)
-	horizontalSplit.AsSplitAreas = append(horizontalSplit.AsSplitAreas, tableArea)
-
-	table := (&split.Table{
-		Name:      "Table",
-		StackName: probe.tableStage.GetName(),
-		TableName: TableName,
-	}).Stage(probe.splitStage)
-	tableArea.Table = table
-
-	formArea := (&split.AsSplitArea{
-		Name: "form",
-		Size: 30,
-	}).Stage(probe.splitStage)
-	horizontalSplit.AsSplitAreas = append(horizontalSplit.AsSplitAreas, formArea)
-
-	form := (&split.Form{
-		Name:      "Form",
-		StackName: probe.formStage.GetName(),
-		FormName:  FormName,
-	}).Stage(probe.splitStage)
-	formArea.Form = form
-
-	bottomSplitArea := (&split.AsSplitArea{
-		Name: "Bottom",
-		Size: 50,
-	}).Stage(probe.splitStage)
-	mainView.RootAsSplitAreas = append(mainView.RootAsSplitAreas, bottomSplitArea)
-
-	doc := (&split.Doc{
-		Name:      "Doc",
-		StackName: probe.splitStage.GetName(),
-	}).Stage(probe.splitStage)
-	bottomSplitArea.Doc = doc
 
 	probe.splitStage.Commit()
 }
