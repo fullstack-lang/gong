272a31a0e65dea08d51743198daf5753aba4f49a
peugeot <thomaspeugeot@yahoo.fr>
2025-03-29 19:36:37 +0100

    slider has the uniform front code

--- lib/slider/ng-github.com-fullstack-lang-gong-lib-slider/src/app/app.component.ts
+++ lib/slider/ng-github.com-fullstack-lang-gong-lib-slider/src/app/app.component.ts
@@ -1,33 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-
-import { AngularSplitModule } from 'angular-split';
-
-import * as slider from '../../projects/slider/src/public-api'
-
-import { SliderSpecificComponent } from '../../projects/sliderspecific/src/lib/slider-specific/slider-specific.component'
+import { Component } from '@angular/core';
 
 import { SplitSpecificComponent } from '@vendored_components/github.com/fullstack-lang/gong/lib/split/ng-github.com-fullstack-lang-gong-lib-split/projects/splitspecific/src/lib/split-specific/split-specific.component'
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    AngularSplitModule,
     SplitSpecificComponent,
-    SliderSpecificComponent
   ],
 
   templateUrl: './app.component.html',
 })
-export class AppComponent implements OnInit {
-
-  StackName = "slider"
-
-  constructor(
-  ) {
-  }
-
-  ngOnInit(): void {
-  }
+export class AppComponent {
 }
