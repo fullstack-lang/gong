24b7e2b25a05d5de68a4c68e8202afb38604257f
peugeot <thomaspeugeot@yahoo.fr>
2025-03-29 19:36:37 +0100

    slider has the uniform front code

---
+++ lib/slider/go/cmd/slider/stager.go
@@ -0,0 +1,51 @@
+// generated boilerplate code
+// edit the file for adding other stages
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+
+	// insertion point for models import
+	slider_models "github.com/fullstack-lang/gong/lib/slider/go/models"
+
+	split "github.com/fullstack-lang/gong/lib/split/go/models"
+	split_stack "github.com/fullstack-lang/gong/lib/split/go/stack"
+)
+
+type Stager struct {
+	stage      *slider_models.Stage
+	splitStage *split.Stage
+}
+
+func NewStager(r *gin.Engine, stage *slider_models.Stage) (stager *Stager) {
+
+	stager = new(Stager)
+
+	stager.stage = stage
+
+	// the root split name is "" by convention. Is is the same for all gong applications
+	// that do not develop their specific angular component
+	stager.splitStage = split_stack.NewStack(r, "", "", "", "", false, false).Stage
+
+	(&split.View{
+		Name: "Probe",
+		RootAsSplitAreas: []*split.AsSplitArea{
+			(&split.AsSplitArea{
+				Size: 30,
+				Slider: (&split.Slider{
+					StackName: stage.GetName(),
+				}).Stage(stager.splitStage),
+			}).Stage(stager.splitStage),
+			(&split.AsSplitArea{
+				Size: 70,
+				Split: (&split.Split{
+					StackName: stage.GetProbeSplitStageName(),
+				}).Stage(stager.splitStage),
+			}).Stage(stager.splitStage),
+		},
+	}).Stage(stager.splitStage)
+
+	stager.splitStage.Commit()
+
+	return
+}
