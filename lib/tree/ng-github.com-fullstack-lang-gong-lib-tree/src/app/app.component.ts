146611bb5d1720f14a388065f66d8d6afe814e51
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-03-29 13:09:50 +0100

    tree environment

--- lib/tree/ng-github.com-fullstack-lang-gong-lib-tree/src/app/app.component.ts
+++ lib/tree/ng-github.com-fullstack-lang-gong-lib-tree/src/app/app.component.ts
@@ -1,33 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-
-import { AngularSplitModule } from 'angular-split';
-
-import * as tree from '../../projects/tree/src/public-api'
-
-import { TreeSpecificComponent } from '../../projects/treespecific/src/lib/tree-specific/tree-specific.component'
+import { Component } from '@angular/core';
 
 import { SplitSpecificComponent } from '@vendored_components/github.com/fullstack-lang/gong/lib/split/ng-github.com-fullstack-lang-gong-lib-split/projects/splitspecific/src/lib/split-specific/split-specific.component'
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    AngularSplitModule,
     SplitSpecificComponent,
-    TreeSpecificComponent
   ],
 
   templateUrl: './app.component.html',
 })
-export class AppComponent implements OnInit {
-
-  StackName = tree.TreeStackName.TreeStackDefaultName
-
-  constructor(
-  ) {
-  }
-
-  ngOnInit(): void {
-  }
+export class AppComponent {
 }
