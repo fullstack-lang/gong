909e7e0f67cd89b8f0409b70fdbaf91cd71f2b1f
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-03-29 12:58:13 +0100

    svg has now an anonymous front

--- lib/svg/ng-github.com-fullstack-lang-gong-lib-svg/src/app/app.component.ts
+++ lib/svg/ng-github.com-fullstack-lang-gong-lib-svg/src/app/app.component.ts
@@ -1,33 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-
-import { AngularSplitModule } from 'angular-split';
-
-import * as svg from '../../projects/svg/src/public-api'
-
-import { SvgSpecificComponent } from '../../projects/svgspecific/src/lib/svg-specific/svg-specific.component'
+import { Component } from '@angular/core';
 
 import { SplitSpecificComponent } from '@vendored_components/github.com/fullstack-lang/gong/lib/split/ng-github.com-fullstack-lang-gong-lib-split/projects/splitspecific/src/lib/split-specific/split-specific.component'
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    AngularSplitModule,
     SplitSpecificComponent,
-    SvgSpecificComponent
   ],
 
   templateUrl: './app.component.html',
 })
-export class AppComponent implements OnInit {
-
-  StackName = svg.StackName.StackNameDefault
-
-  constructor(
-  ) {
-  }
-
-  ngOnInit(): void {
-  }
+export class AppComponent {
 }
