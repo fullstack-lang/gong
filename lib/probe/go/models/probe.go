9da8e62380c6fea4cbc5275a5088aeca2715abf4
peugeot <thomaspeugeot@yahoo.fr>
2025-03-31 08:04:06 +0200

    Develop a frontless Probe stack #614

--- lib/probe/go/models/probe.go
+++ lib/probe/go/models/probe.go
@@ -54,7 +54,11 @@ type ProbebStage interface {
 type Probe2 struct {
 	Name       string // not even sure we need a named struct (parametrization ?)
 	stage      *Stage
-	splitStage *split.Stage // root
+	splitStage *split.Stage // split stage of the probe
+}
+
+func (probe *Probe2) GetSplitStage() (splitStage *split.Stage) {
+	return probe.splitStage
 }
 
 func NewProbe2(r *gin.Engine, stage *Stage) (probe *Probe2) {
@@ -63,16 +67,40 @@ func NewProbe2(r *gin.Engine, stage *Stage) (probe *Probe2) {
 
 	// the root split name is "" by convention. Is is the same for all gong applications
 	// that do not develop their specific angular component
-	probe.splitStage = split_stack.NewStack(r, stage.GetName(), "", "", "", true, false).Stage
+	probe.splitStage = split_stack.NewStack(r, stage.GetName(), "", "", "", true, true).Stage
 
 	(&split.View{
 		Name: "Probe 2",
 		RootAsSplitAreas: []*split.AsSplitArea{
 			(&split.AsSplitArea{
-				Size: 50,
+				Name:             "editor",
+				ShowNameInHeader: true,
+				Size:             50,
+				AsSplit: (&split.AsSplit{
+					Direction: split.Horizontal,
+					AsSplitAreas: []*split.AsSplitArea{
+						(&split.AsSplitArea{
+							Name:             "sidebar",
+							ShowNameInHeader: true,
+							Size:             20,
+						}).Stage(probe.splitStage),
+						(&split.AsSplitArea{
+							Name:             "table",
+							ShowNameInHeader: true,
+							Size:             50,
+						}).Stage(probe.splitStage),
+						(&split.AsSplitArea{
+							Name:             "form",
+							ShowNameInHeader: true,
+							Size:             30,
+						}).Stage(probe.splitStage),
+					},
+				}).Stage(probe.splitStage),
 			}).Stage(probe.splitStage),
 			(&split.AsSplitArea{
-				Size: 50,
+				Name:             "for doc stage",
+				ShowNameInHeader: true,
+				Size:             50,
 			}).Stage(probe.splitStage),
 		},
 	}).Stage(probe.splitStage)
