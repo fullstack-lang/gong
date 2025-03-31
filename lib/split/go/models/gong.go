06e3b57b74cc69c31efa4b9384b1d28fcba7aff1
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-04-01 01:07:29 +0200

    Develop a frontless Probe stack #614 - doc loaded

--- lib/split/go/models/gong.go
+++ lib/split/go/models/gong.go
@@ -3,11 +3,14 @@ package models
 
 import (
 	"cmp"
+	"embed"
 	"errors"
 	"fmt"
 	"math"
 	"slices"
 	"time"
+
+	split_go "github.com/fullstack-lang/gong/lib/split/go"
 )
 
 func __Gong__Abs(x int) int {
@@ -287,6 +290,14 @@ func (stage *Stage) GetType() string {
 
 func (stage *Stage) GetMap_GongStructName_InstancesNb() map[string]int {
 	return stage.Map_GongStructName_InstancesNb
+}
+
+func (stage *Stage) GetModelsEmbededDir() embed.FS {
+	return split_go.GoModelsDir
+}
+
+func (stage *Stage) GetDigramsEmbededDir() embed.FS {
+	return split_go.GoDiagramsDir
 }
 
 type GONG__Identifier struct {
