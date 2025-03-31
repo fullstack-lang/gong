760e733019ad1da9e2a6f885785b52709efa2b61
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-04-01 00:50:14 +0200

    Develop a frontless Probe stack #614 - new doc load signature

--- lib/load/go/models/gong.go
+++ lib/load/go/models/gong.go
@@ -113,6 +113,10 @@ func (stage *Stage) GetType() string {
 	return "github.com/fullstack-lang/gong/lib/load/go/models"
 }
 
+func (stage *Stage) GetMap_GongStructName_InstancesNb() map[string]int {
+	return stage.Map_GongStructName_InstancesNb
+}
+
 type GONG__Identifier struct {
 	Ident string
 	Type  GONG__ExpressionType
--- test/test3/go/models/gong.go
+++ test/test3/go/models/gong.go
@@ -125,6 +125,10 @@ func (stage *Stage) GetType() string {
 	return "github.com/fullstack-lang/gong/test/test3/go/models"
 }
 
+func (stage *Stage) GetMap_GongStructName_InstancesNb() map[string]int {
+	return stage.Map_GongStructName_InstancesNb
+}
+
 type GONG__Identifier struct {
 	Ident string
 	Type  GONG__ExpressionType
