a4d1fd5cf12bd1180552b6492ee39db4a30ebb83
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-04-26 19:24:39 +0200

    added proxy to svg text

--- lib/svg/go/models/svg_text.go
+++ lib/svg/go/models/svg_text.go
@@ -6,4 +6,19 @@ type SvgText struct {
 	//gong:text
 	//gong:width 600 gong:height 300
 	Text string
+
+	Proxy SvgTextProxyInterface
+}
+
+type SvgTextProxyInterface interface {
+	Updated()
+}
+
+func (svgtext *SvgText) OnAfterUpdate(
+	stage *Stage,
+	stageSvgText, frontSvgText *SvgText) {
+
+	if svgtext.Proxy != nil {
+		svgtext.Proxy.Updated()
+	}
 }
