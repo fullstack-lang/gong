3b59aa80bbbe2ccfe7f15e2b33e8af62807ac8ef
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-04-29 08:56:24 +0200

    WIP

--- lib/doc2/go/prepare/prepare.go
+++ lib/doc2/go/prepare/prepare.go
@@ -20,7 +20,7 @@ func Prepare(
 	doc2Stage := doc2_stack.NewStack(r, "", "", "", "", embeddedDiagrams, true).Stage
 
 	splitStage := split_stack.NewStack(r, "", "", "", "", false, false).Stage
-	treeStage := tree_stack.NewStack(r, "", "", "", "", false, false).Stage
+	treeStage := tree_stack.NewStack(r, doc2Stage.GetProbeTreeSidebarStageName(), "", "", "", false, true).Stage
 	svgStage := svg_stack.NewStack(r, "", "", "", "", false, false).Stage
 
 	doc2_models.NewStager(
