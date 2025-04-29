876238248390de78174a7bab0edcf350bb93ebde
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-04-29 08:34:38 +0200

    Doc 2 #619 - WIP

--- lib/doc2/go/models/stager.go
+++ lib/doc2/go/models/stager.go
@@ -6,34 +6,65 @@ import (
 	"github.com/gin-gonic/gin"
 
 	split "github.com/fullstack-lang/gong/lib/split/go/models"
-	split_stack "github.com/fullstack-lang/gong/lib/split/go/stack"
+
+	tree_models "github.com/fullstack-lang/gong/lib/tree/go/models"
+
+	svg_models "github.com/fullstack-lang/gong/lib/svg/go/models"
 )
 
 type Stager struct {
 	stage      *Stage
 	splitStage *split.Stage
+	treeStage  *tree_models.Stage
+	svgStage   *svg_models.Stage
 }
 
-func NewStager(r *gin.Engine, stage *Stage) (stager *Stager) {
+func NewStager(
+	r *gin.Engine,
+	stage *Stage,
+	splitStage *split.Stage,
+	treeStage *tree_models.Stage,
+	svgStage *svg_models.Stage,
+) (stager *Stager) {
 
 	stager = new(Stager)
 
 	stager.stage = stage
-
-	// the root split name is "" by convention. Is is the same for all gong applications
-	// that do not develop their specific angular component
-	stager.splitStage = split_stack.NewStack(r, "", "", "", "", false, false).Stage
+	stager.splitStage = splitStage
+	stager.treeStage = treeStage
+	stager.svgStage = svgStage
 
 	// StageBranch will stage on the the first argument
 	// all instances related to the second argument
 	split.StageBranch(stager.splitStage, &split.View{
 		Name: "Probe",
 		RootAsSplitAreas: []*split.AsSplitArea{
-			(&split.AsSplitArea{
+			{
+				Size: 50,
+				AsSplit: (&split.AsSplit{
+					Direction: split.Horizontal,
+					AsSplitAreas: []*split.AsSplitArea{
+						{
+							Size: 25,
+							Tree: &split.Tree{
+								StackName: stager.treeStage.GetName(),
+							},
+						},
+						{
+							Size: 75,
+							Svg: &split.Svg{
+								StackName: stager.svgStage.GetName(),
+							},
+						},
+					},
+				}),
+			},
+			{
+				Size: 50,
 				Split: (&split.Split{
 					StackName: stage.GetProbeSplitStageName(),
 				}),
-			}),
+			},
 		},
 	})
 
