04b0ad5c502f976d220e101c46b2875f4944ffbf
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-04-30 08:41:31 +0200

    Doc 2 Fixes #619 - une petite touche par la

--- lib/doc2/go/models/stager_update_and_commit_tree_stage.go
+++ lib/doc2/go/models/stager_update_and_commit_tree_stage.go
@@ -1,6 +1,10 @@
 package models
 
 import (
+	"fmt"
+	"log"
+
+	"github.com/fullstack-lang/gong/lib/tree/go/buttons"
 	tree "github.com/fullstack-lang/gong/lib/tree/go/models"
 )
 
@@ -15,6 +19,14 @@ func (stager *Stager) UpdateAndCommitTreeStage() {
 				{
 					Name:       "Class Diagrams",
 					IsExpanded: true,
+					Buttons: []*tree.Button{
+						tree.NewButton(
+							&ButtonNewClassdiagramProxy{
+								stager: stager,
+							},
+							"Class Diagramm Add Button",
+							string(buttons.BUTTON_add)),
+					},
 				},
 			},
 		},
@@ -23,6 +35,52 @@ func (stager *Stager) UpdateAndCommitTreeStage() {
 	stager.treeStage.Commit()
 }
 
-type NewClassdiagramButtonProxy struct {
+type ButtonNewClassdiagramProxy struct {
 	stager *Stager
+}
+
+// GetButtonsStage implements models.Target.
+func (b *ButtonNewClassdiagramProxy) GetButtonsStage() *tree.Stage {
+	return b.stager.treeStage
+}
+
+// OnAfterUpdateButton implements models.Target.
+func (b *ButtonNewClassdiagramProxy) OnAfterUpdateButton() {
+
+	stager := b.stager
+	stage := stager.stage
+
+	// check unicity of name, otherwise, add an index
+	var hasNameCollision bool
+	initialName := "Default"
+	newClassdiagramName := initialName
+	index := 0
+	// loop until the name of the new diagram is not in collision with an existing
+	// diagram name
+	for index == 0 || hasNameCollision {
+		index++
+		hasNameCollision = false
+		for classdiagram := range *GetGongstructInstancesSet[Classdiagram](stage) {
+			if classdiagram.Name == newClassdiagramName {
+				hasNameCollision = true
+			}
+		}
+		if hasNameCollision {
+			newClassdiagramName = initialName + fmt.Sprintf("_%d", index)
+		}
+	}
+
+	newClassdiagram := (&Classdiagram{Name: newClassdiagramName}).Stage(stage)
+
+	diagramPackages := *GetGongstructInstancesSet[DiagramPackage](stage)
+	var diagramPackage *DiagramPackage
+	for k, _ := range diagramPackages {
+		diagramPackage = k
+	}
+	if diagramPackage == nil {
+		log.Fatalln("There should be at least one diagram package on the stage")
+	}
+
+	diagramPackage.Classdiagrams = append(diagramPackage.Classdiagrams, newClassdiagram)
+	stage.Commit()
 }
