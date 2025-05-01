2c263c438bb6e879af6d24718fc0fc8faa089520
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-05-01 12:56:24 +0200

    Doc 2 #619 - before check per diagram

--- lib/doc2/go/cmd/doc2/data/zzz_diagrams.go
+++ lib/doc2/go/cmd/doc2/data/zzz_diagrams.go
@@ -25,23 +25,33 @@ func _(stage *models.Stage) {
 	// Declaration of instances to stage
 
 	__Classdiagram__000000_Default := (&models.Classdiagram{}).Stage(stage)
+	__Classdiagram__000001_Default_1 := (&models.Classdiagram{}).Stage(stage)
+	__Classdiagram__000002_Default_2 := (&models.Classdiagram{}).Stage(stage)
 
-	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_01T10_36_09Z := (&models.DiagramPackage{}).Stage(stage)
+	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_01T10_49_57Z := (&models.DiagramPackage{}).Stage(stage)
 
 	// Setup of values
 
 	__Classdiagram__000000_Default.Name = `Default`
 	__Classdiagram__000000_Default.IsInDrawMode = false
 
-	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_01T10_36_09Z.Name = `Diagram Package created the 2025-05-01T10:36:09Z`
-	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_01T10_36_09Z.Path = ``
-	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_01T10_36_09Z.GongModelPath = ``
-	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_01T10_36_09Z.IsEditable = false
-	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_01T10_36_09Z.IsReloaded = false
-	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_01T10_36_09Z.AbsolutePathToDiagramPackage = ``
+	__Classdiagram__000001_Default_1.Name = `Default_1`
+	__Classdiagram__000001_Default_1.IsInDrawMode = false
+
+	__Classdiagram__000002_Default_2.Name = `Default_2`
+	__Classdiagram__000002_Default_2.IsInDrawMode = false
+
+	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_01T10_49_57Z.Name = `Diagram Package created the 2025-05-01T10:49:57Z`
+	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_01T10_49_57Z.Path = ``
+	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_01T10_49_57Z.GongModelPath = ``
+	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_01T10_49_57Z.IsEditable = false
+	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_01T10_49_57Z.IsReloaded = false
+	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_01T10_49_57Z.AbsolutePathToDiagramPackage = ``
 
 	// Setup of pointers
 	// setup of Classdiagram instances pointers
 	// setup of DiagramPackage instances pointers
-	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_01T10_36_09Z.Classdiagrams = append(__DiagramPackage__000000_Diagram_Package_created_the_2025_05_01T10_36_09Z.Classdiagrams, __Classdiagram__000000_Default)
+	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_01T10_49_57Z.Classdiagrams = append(__DiagramPackage__000000_Diagram_Package_created_the_2025_05_01T10_49_57Z.Classdiagrams, __Classdiagram__000000_Default)
+	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_01T10_49_57Z.Classdiagrams = append(__DiagramPackage__000000_Diagram_Package_created_the_2025_05_01T10_49_57Z.Classdiagrams, __Classdiagram__000001_Default_1)
+	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_01T10_49_57Z.Classdiagrams = append(__DiagramPackage__000000_Diagram_Package_created_the_2025_05_01T10_49_57Z.Classdiagrams, __Classdiagram__000002_Default_2)
 }
