1a5d2fda22d5354f94fd8bf6ca5f5070bd0cd21d
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-05-01 12:37:34 +0200

    Doc 2 Fixes #619 - new gongc

--- lib/tone/go/controllers/register_controllers.go
+++ lib/tone/go/controllers/register_controllers.go
@@ -131,11 +131,13 @@ func (controller *Controller) onWebSocketRequestForBackRepoContent(c *gin.Contex
 		}
 	}
 
-	log.Printf("Stack github.com/fullstack-lang/gong/lib/tone/go: stack path: '%s', new ws index %d",
-		stackPath, controller.listenerIndex,
-	)
 	index := controller.listenerIndex
 	controller.listenerIndex++
+	log.Printf(
+		"%s github.com/fullstack-lang/gong/lib/tone/go: Con: '%s', index %d",
+		time.Now().Format("2006-01-02 15:04:05.000000"),
+		stackPath, index,
+	)
 
 	backRepo := controller.Map_BackRepos[stackPath]
 	if backRepo == nil {
@@ -166,7 +168,8 @@ func (controller *Controller) onWebSocketRequestForBackRepoContent(c *gin.Contex
 	backRepoData := new(orm.BackRepoData)
 	orm.CopyBackRepoToBackRepoData(backRepo, backRepoData)
 	backRepoData.GONG__Index = index
-
+	
+	refresh := 0
 	err = wsConnection.WriteJSON(backRepoData)
 	if err != nil {
 		log.Println("github.com/fullstack-lang/gong/lib/tone/go:\n",
@@ -174,7 +177,12 @@ func (controller *Controller) onWebSocketRequestForBackRepoContent(c *gin.Contex
 		fmt.Println(err)
 		return
 	} else {
-		log.Println(time.Now().Format("2006-01-02 15:04:05.000000"), "github.com/fullstack-lang/gong/lib/tone/go: 1st sent backRepoData of stack:", stackPath, "index", index)
+	log.Printf(
+		"%s github.com/fullstack-lang/gong/lib/tree/go: %03d: '%s', index %d",
+		time.Now().Format("2006-01-02 15:04:05.000000"),
+		refresh,
+		stackPath, index,
+	)
 	}
 	for {
 		select {
@@ -184,6 +192,7 @@ func (controller *Controller) onWebSocketRequestForBackRepoContent(c *gin.Contex
 		default:
 			for nbCommitBackRepo := range updateCommitBackRepoNbChannel {
 				_ = nbCommitBackRepo
+				refresh += 1
 
 				backRepoData := new(orm.BackRepoData)
 				orm.CopyBackRepoToBackRepoData(backRepo, backRepoData)
@@ -201,7 +210,12 @@ func (controller *Controller) onWebSocketRequestForBackRepoContent(c *gin.Contex
 					cancel() // Cancel the context
 					return
 				} else {
-					log.Println(time.Now().Format("2006-01-02 15:04:05.000000"), "github.com/fullstack-lang/gong/lib/tone/go: sent backRepoData of stack:", stackPath, "index", index)
+					log.Printf(
+						"%s github.com/fullstack-lang/gong/lib/tree/go: %03d: '%s', index %d",
+						time.Now().Format("2006-01-02 15:04:05.000000"),
+						refresh,
+						stackPath, index,
+					)
 				}
 			}
 		}
