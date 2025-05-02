24214c31679d73a1feb87849719f334d047afa3c
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-05-02 21:29:02 +0200

    Doc 2 #619 § display gong enums

--- lib/doc2/go/cmd/doc2/data/zzz_diagrams.go
+++ lib/doc2/go/cmd/doc2/data/zzz_diagrams.go
@@ -24,12 +24,8 @@ func _(stage *models.Stage) {
 	// Declaration of instances to stage
 
 	__Classdiagram__000000_Default := (&models.Classdiagram{}).Stage(stage)
-	__Classdiagram__000001_Default_1 := (&models.Classdiagram{}).Stage(stage)
 
 	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_02T09_21_37Z := (&models.DiagramPackage{}).Stage(stage)
-
-	__GongStructShape__000000_Default_AttributeShape := (&models.GongStructShape{}).Stage(stage)
-	__GongStructShape__000001_Default_Classdiagram := (&models.GongStructShape{}).Stage(stage)
 
 	// Setup of values
 
@@ -41,14 +37,6 @@ func _(stage *models.Stage) {
 	__Classdiagram__000000_Default.NodeGongEnumsIsExpanded = false
 	__Classdiagram__000000_Default.NodeGongNotesIsExpanded = false
 
-	__Classdiagram__000001_Default_1.Name = `Default_1`
-	__Classdiagram__000001_Default_1.IsInDrawMode = true
-	__Classdiagram__000001_Default_1.IsInRenameMode = false
-	__Classdiagram__000001_Default_1.IsExpanded = false
-	__Classdiagram__000001_Default_1.NodeNamedStructsIsExpanded = false
-	__Classdiagram__000001_Default_1.NodeGongEnumsIsExpanded = false
-	__Classdiagram__000001_Default_1.NodeGongNotesIsExpanded = false
-
 	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_02T09_21_37Z.Name = `Diagram Package created the 2025-05-02T09:21:37Z`
 	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_02T09_21_37Z.Path = ``
 	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_02T09_21_37Z.GongModelPath = ``
@@ -56,39 +44,9 @@ func _(stage *models.Stage) {
 	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_02T09_21_37Z.IsReloaded = false
 	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_02T09_21_37Z.AbsolutePathToDiagramPackage = ``
 
-	__GongStructShape__000000_Default_AttributeShape.Name = `Default-AttributeShape`
-	__GongStructShape__000000_Default_AttributeShape.X = 23.000000
-	__GongStructShape__000000_Default_AttributeShape.Y = 47.000000
-
-	//gong:ident [ref_models.AttributeShape] comment added to overcome the problem with the comment map association
-	__GongStructShape__000000_Default_AttributeShape.Identifier = `ref_models.AttributeShape`
-	__GongStructShape__000000_Default_AttributeShape.ShowNbInstances = false
-	__GongStructShape__000000_Default_AttributeShape.NbInstances = 0
-	__GongStructShape__000000_Default_AttributeShape.Width = 240.000000
-	__GongStructShape__000000_Default_AttributeShape.Height = 63.000000
-	__GongStructShape__000000_Default_AttributeShape.IsSelected = false
-	__GongStructShape__000000_Default_AttributeShape.IsExpanded = false
-
-	__GongStructShape__000001_Default_Classdiagram.Name = `Default-Classdiagram`
-	__GongStructShape__000001_Default_Classdiagram.X = 67.000000
-	__GongStructShape__000001_Default_Classdiagram.Y = 90.000000
-
-	//gong:ident [ref_models.Classdiagram] comment added to overcome the problem with the comment map association
-	__GongStructShape__000001_Default_Classdiagram.Identifier = `ref_models.Classdiagram`
-	__GongStructShape__000001_Default_Classdiagram.ShowNbInstances = false
-	__GongStructShape__000001_Default_Classdiagram.NbInstances = 0
-	__GongStructShape__000001_Default_Classdiagram.Width = 240.000000
-	__GongStructShape__000001_Default_Classdiagram.Height = 63.000000
-	__GongStructShape__000001_Default_Classdiagram.IsSelected = false
-	__GongStructShape__000001_Default_Classdiagram.IsExpanded = false
-
 	// Setup of pointers
 	// setup of Classdiagram instances pointers
-	__Classdiagram__000000_Default.GongStructShapes = append(__Classdiagram__000000_Default.GongStructShapes, __GongStructShape__000000_Default_AttributeShape)
-	__Classdiagram__000000_Default.GongStructShapes = append(__Classdiagram__000000_Default.GongStructShapes, __GongStructShape__000001_Default_Classdiagram)
 	// setup of DiagramPackage instances pointers
 	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_02T09_21_37Z.Classdiagrams = append(__DiagramPackage__000000_Diagram_Package_created_the_2025_05_02T09_21_37Z.Classdiagrams, __Classdiagram__000000_Default)
-	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_02T09_21_37Z.Classdiagrams = append(__DiagramPackage__000000_Diagram_Package_created_the_2025_05_02T09_21_37Z.Classdiagrams, __Classdiagram__000001_Default_1)
 	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_02T09_21_37Z.SelectedClassdiagram = __Classdiagram__000000_Default
-	// setup of GongStructShape instances pointers
 }
