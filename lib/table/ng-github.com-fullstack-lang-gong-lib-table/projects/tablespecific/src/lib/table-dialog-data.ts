70f5740ad9adb4ced10b493ee1342a07c9b48cdf
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-05-17 18:22:08 +0200

    probe : associations are missing when creating an object #651 Front save this configuration when "save is pressed"
    Front reuse this configuration when the button is pressed again.

--- lib/table/ng-github.com-fullstack-lang-gong-lib-table/projects/tablespecific/src/lib/table-dialog-data.ts
+++ lib/table/ng-github.com-fullstack-lang-gong-lib-table/projects/tablespecific/src/lib/table-dialog-data.ts
@@ -3,5 +3,6 @@ import * as table from '../../../table/src/public-api'
 export interface TableDialogData {
     Name: string
     TableName: string
+    AssociationStorage: string
     initialSelection?: table.Row[] // Add this optional property
 }
