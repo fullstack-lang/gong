5c41504a8cd4c3d2aa870e79c8f06075c8c56454
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-05-18 17:45:40 +0200

    use the new doc2 node exapnsion encoding

--- lib/svg/go/diagrams/diagrams.go
+++ lib/svg/go/diagrams/diagrams.go
@@ -40,11 +40,8 @@ func _(stage *models.Stage) {
 	__Classdiagram__000000_Default.IsInRenameMode = false
 	__Classdiagram__000000_Default.IsExpanded = true
 	__Classdiagram__000000_Default.NodeGongStructsIsExpanded = true
-	__Classdiagram__000000_Default.NodeGongStructNodeExpansionBinaryEncoding = 65544
 	__Classdiagram__000000_Default.NodeGongEnumsIsExpanded = false
-	__Classdiagram__000000_Default.NodeGongEnumNodeExpansionBinaryEncoding = 0
 	__Classdiagram__000000_Default.NodeGongNotesIsExpanded = false
-	__Classdiagram__000000_Default.NodeGongNoteNodeExpansionBinaryEncoding = 0
 
 	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_05T06_12_28Z.Name = `Diagram Package created the 2025-05-05T06:12:28Z`
 	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_05T06_12_28Z.Path = ``
