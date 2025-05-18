746fe572a0095481a8fa2d4f7508a93b27536998
peugeot <thomaspeugeot@yahoo.fr>
2025-05-18 21:01:41 +0200

    composable stagers Fixes #681 - before adjustment

--- test/test/go/models/stager.go
+++ test/test/go/models/stager.go
@@ -6,60 +6,48 @@ import (
 	"github.com/gin-gonic/gin"
 
 	split "github.com/fullstack-lang/gong/lib/split/go/models"
-	split_stack "github.com/fullstack-lang/gong/lib/split/go/stack"
 
 	svg "github.com/fullstack-lang/gong/lib/svg/go/models"
 	svg_stack "github.com/fullstack-lang/gong/lib/svg/go/stack"
 )
 
 type Stager struct {
-	stage      *Stage
-	splitStage *split.Stage
-	svgStage   *svg.Stage
+	stage        *Stage
+	splitStage   *split.Stage
+	svgStage     *svg.Stage
+	asSplitAreas []*split.AsSplitArea
 }
 
-func NewStager(r *gin.Engine, stage *Stage, name string) (stager *Stager) {
+func NewStager(r *gin.Engine, stage *Stage, splitStage *split.Stage) (stager *Stager) {
 
 	stager = new(Stager)
 
 	stager.stage = stage
+	stager.splitStage = splitStage
 
+	// creates a svg stack and display the front into the split
 	svgPersistanceFile := "svg.go"
-	// the root split name is "" by convention. Is is the same for all gong applications
-	// that do not develop their specific angular component
-	stager.splitStage = split_stack.NewStack(r, "", "", "", "", false, false).Stage
-	stager.svgStage = svg_stack.NewStack(r, name, svgPersistanceFile, svgPersistanceFile, "", true, true).Stage
+	stager.svgStage = svg_stack.NewStack(r, stage.GetName(), svgPersistanceFile, svgPersistanceFile, "", true, true).Stage
 
-	split.StageBranch(stager.splitStage, &split.View{
-		Name: "Probe",
-		RootAsSplitAreas: []*split.AsSplitArea{
-			(&split.AsSplitArea{
-				Size: 20,
-				Svg: (&split.Svg{
-					StackName: stager.svgStage.GetName(),
-				}),
+	stager.asSplitAreas = []*split.AsSplitArea{
+		(&split.AsSplitArea{
+			Size: 50,
+			Svg: (&split.Svg{
+				StackName: stager.svgStage.GetName(),
 			}),
-			(&split.AsSplitArea{
-				Size: 80,
-				Split: (&split.Split{
-					StackName: stage.GetProbeSplitStageName(),
-				}),
+		}),
+		(&split.AsSplitArea{
+			Size: 50,
+			Split: (&split.Split{
+				StackName: stager.svgStage.GetProbeSplitStageName(),
 			}),
-		},
-	})
+		}),
+	}
 
-	split.StageBranch(stager.splitStage, &split.View{
-		Name: "Probe",
-		RootAsSplitAreas: []*split.AsSplitArea{
-			(&split.AsSplitArea{
-				Split: (&split.Split{
-					StackName: stager.svgStage.GetProbeSplitStageName(),
-				}),
-			}),
-		},
-	})
+	return
+}
 
-	stager.splitStage.Commit()
-
+func (stager *Stager) GetAsSplitAreas() (asSplitAreas []*split.AsSplitArea) {
+	asSplitAreas = stager.asSplitAreas
 	return
 }
