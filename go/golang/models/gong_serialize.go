bc39a84ad7cd7fb0a0620e35859262e2e4908edf
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-05-18 15:02:33 +0200

    gong/ serialize / shorten sheet name to 31 characters #675

--- go/golang/models/gong_serialize.go
+++ go/golang/models/gong_serialize.go
@@ -130,7 +130,7 @@ func SerializeStage(stage *Stage, filename string) {
 			}
 		}
 	}
-	
+
 	var tab ExcelizeTabulator
 	tab.SetExcelizeFile(f)
 	{
@@ -139,7 +139,13 @@ func SerializeStage(stage *Stage, filename string) {
 			fmt.Println("cannot write xl file : ", err)
 		}
 	}
-		
+}
+
+func shortenString(s string) string {
+	if len(s) > 31 {
+		return s[:31]
+	}
+	return s
 }
 
 // Tabulator is an interface for writing to a table strings
@@ -151,6 +157,8 @@ type Tabulator interface {
 
 func Serialize[Type Gongstruct](stage *Stage, tab Tabulator) {
 	sheetName := GetGongstructName[Type]()
+
+	sheetName = shortenString(sheetName)
 
 	// Create a new sheet.
 	tab.AddSheet(sheetName)
@@ -195,6 +203,8 @@ func (tab *ExcelizeTabulator) AddCell(sheetName string, rowId, columnIndex int, 
 
 func SerializeExcelizePointerToGongstruct[Type PointerToGongstruct](stage *Stage, f *excelize.File) {
 	sheetName := GetPointerToGongstructName[Type]()
+
+	sheetName = shortenString(sheetName)
 
 	// Create a new sheet.
 	f.NewSheet(sheetName)
@@ -249,6 +259,8 @@ func SerializeExcelizePointerToGongstruct[Type PointerToGongstruct](stage *Stage
 
 func SerializeExcelize[Type Gongstruct](stage *Stage, f *excelize.File) {
 	sheetName := GetGongstructName[Type]()
+
+	sheetName = shortenString(sheetName)
 
 	// Create a new sheet.
 	f.NewSheet(sheetName)
