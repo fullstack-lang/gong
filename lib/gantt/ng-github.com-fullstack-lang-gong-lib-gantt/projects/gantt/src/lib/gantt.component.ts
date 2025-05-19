146e93c274327de9968507d5e2c8b84766b5f32b
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-05-19 08:11:08 +0200

    Clean up lib/gantt with new arch Fixes #654

---
+++ lib/gantt/ng-github.com-fullstack-lang-gong-lib-gantt/projects/gantt/src/lib/gantt.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-gantt',
+  imports: [],
+  template: `
+    <p>
+      gantt works!
+    </p>
+  `,
+  styles: ``
+})
+export class GanttComponent {
+
+}
