b41bfca91c2efbba0c4915bf601734914cd0313a
peugeot <thomaspeugeot@yahoo.fr>
2025-05-19 07:19:45 +0200

    simpler version

--- test/test/go/models/stager.go
+++ test/test/go/models/stager.go
@@ -6,15 +6,11 @@ import (
 	"github.com/gin-gonic/gin"
 
 	split "github.com/fullstack-lang/gong/lib/split/go/models"
-
-	svg "github.com/fullstack-lang/gong/lib/svg/go/models"
-	svg_stack "github.com/fullstack-lang/gong/lib/svg/go/stack"
 )
 
 type Stager struct {
 	stage       *Stage
 	splitStage  *split.Stage
-	svgStage    *svg.Stage
 	asSplitArea *split.AsSplitArea
 }
 
@@ -24,10 +20,6 @@ func NewStager(r *gin.Engine, stage *Stage, splitStage *split.Stage) (stager *St
 
 	stager.stage = stage
 	stager.splitStage = splitStage
-
-	// creates a svg stack and display the front into the split
-	svgPersistanceFile := "svg.go"
-	stager.svgStage = svg_stack.NewStack(r, stage.GetName(), svgPersistanceFile, svgPersistanceFile, "", true, true).Stage
 
 	stager.asSplitArea = &split.AsSplitArea{}
 
