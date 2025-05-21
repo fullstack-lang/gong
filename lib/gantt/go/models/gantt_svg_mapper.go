549fd47944c475753eece2c15eaeed4cf8ec702f
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-05-21 08:38:26 +0200

    simplification

--- lib/gantt/go/models/gantt_svg_mapper.go
+++ lib/gantt/go/models/gantt_svg_mapper.go
@@ -3,8 +3,4 @@ package models
 // type of the singloton for interception of gantt commit in order to generate
 // the svg
 type GanttSVGMapper struct {
-	ganttToRender *Gantt
-
-	// to overwrite the gantt definition file
-	GanttOuputFile string
 }
