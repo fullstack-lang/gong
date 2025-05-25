417a16a38a5c2787d9aaddfaa635431febfcf6b6
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-05-25 09:09:36 +0200

    Load implement upload Fixes #629 - preparation

---
+++ lib/load/go/models/stager.go
@@ -0,0 +1,36 @@
+// generated boilerplate code
+// edit the file for adding other stages
+package models
+
+import (
+	"github.com/gin-gonic/gin"
+
+	split "github.com/fullstack-lang/gong/lib/split/go/models"
+)
+
+type Stager struct {
+	stage       *Stage
+	splitStage  *split.Stage
+	asSplitArea *split.AsSplitArea
+}
+
+func NewStager(r *gin.Engine, stage *Stage, splitStage *split.Stage) (stager *Stager) {
+
+	stager = new(Stager)
+
+	stager.stage = stage
+	stager.splitStage = splitStage
+
+	stager.asSplitArea = &split.AsSplitArea{
+		Load: &split.Load{
+			StackName: stage.GetName(),
+		},
+	}
+
+	return
+}
+
+func (stager *Stager) GetAsSplitArea() (asSplitArea *split.AsSplitArea) {
+	asSplitArea = stager.asSplitArea
+	return
+}
