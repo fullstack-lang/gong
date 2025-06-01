f5dcb97b0ad5176745a44b88157af135bf9a3f04
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-06-01 12:46:58 +0200

    Unmarshall new(ref_models.AEnumType) Fixes #699 - preparare diagrams.go file

--- test/test/go/diagrams/diagrams.go
+++ test/test/go/diagrams/diagrams.go
@@ -23,49 +23,7 @@ var _ map[string]any = map[string]any{
 // function will stage objects
 func _(stage *models.Stage) {
 
-	// Declaration of instances to stage
-
-	__Classdiagram__000000_Default := (&models.Classdiagram{}).Stage(stage)
-
-	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_04T22_30_30Z := (&models.DiagramPackage{}).Stage(stage)
-
 	__GongEnumShape__000000_Default_AEnumType := (&models.GongEnumShape{}).Stage(stage)
 
-	// Setup of values
-
-	__Classdiagram__000000_Default.Name = `Default`
-	__Classdiagram__000000_Default.Description = ``
-	__Classdiagram__000000_Default.IsIncludedInStaticWebSite = false
-	__Classdiagram__000000_Default.IsInRenameMode = false
-	__Classdiagram__000000_Default.IsExpanded = true
-	__Classdiagram__000000_Default.NodeGongStructsIsExpanded = false
-	__Classdiagram__000000_Default.NodeGongStructNodeExpansion = ``
-	__Classdiagram__000000_Default.NodeGongEnumsIsExpanded = true
-	__Classdiagram__000000_Default.NodeGongEnumNodeExpansion = ``
-	__Classdiagram__000000_Default.NodeGongNotesIsExpanded = true
-	__Classdiagram__000000_Default.NodeGongNoteNodeExpansion = ``
-
-	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_04T22_30_30Z.Name = `Diagram Package created the 2025-05-04T22:30:30Z`
-	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_04T22_30_30Z.Path = ``
-	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_04T22_30_30Z.GongModelPath = ``
-	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_04T22_30_30Z.AbsolutePathToDiagramPackage = ``
-
-	__GongEnumShape__000000_Default_AEnumType.Name = `Default-AEnumType`
-	__GongEnumShape__000000_Default_AEnumType.X = 72.000000
-	__GongEnumShape__000000_Default_AEnumType.Y = 85.000000
-
-	//gong:ident [ref_models.AEnumType] comment added to overcome the problem with the comment map association
-	__GongEnumShape__000000_Default_AEnumType.Identifier = `ref_models.AEnumType`
 	__GongEnumShape__000000_Default_AEnumType.IdentifierMeta = new(ref_models.AEnumType)
-	__GongEnumShape__000000_Default_AEnumType.Width = 240.000000
-	__GongEnumShape__000000_Default_AEnumType.Height = 63.000000
-	__GongEnumShape__000000_Default_AEnumType.IsExpanded = false
-
-	// Setup of pointers
-	// setup of Classdiagram instances pointers
-	__Classdiagram__000000_Default.GongEnumShapes = append(__Classdiagram__000000_Default.GongEnumShapes, __GongEnumShape__000000_Default_AEnumType)
-	// setup of DiagramPackage instances pointers
-	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_04T22_30_30Z.Classdiagrams = append(__DiagramPackage__000000_Diagram_Package_created_the_2025_05_04T22_30_30Z.Classdiagrams, __Classdiagram__000000_Default)
-	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_04T22_30_30Z.SelectedClassdiagram = __Classdiagram__000000_Default
-	// setup of GongEnumShape instances pointers
 }
