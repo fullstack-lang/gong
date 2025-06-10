f47283a7254e85be4f838ea30df2efe1c70e0300
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-06-10 04:33:49 +0200

    recompiled gong

---
+++ go/models/stager.go
@@ -0,0 +1,32 @@
+// generated boilerplate code
+// edit the file for adding other stages
+package models
+
+import (
+	"github.com/gin-gonic/gin"
+
+	split "github.com/fullstack-lang/gong/lib/split/go/models"
+)
+
+type Stager struct {
+	stage       *Stage
+	splitStage  *split.Stage
+	asSplitArea *split.AsSplitArea
+}
+
+func NewStager(r *gin.Engine, stage *Stage, splitStage *split.Stage) (stager *Stager) {
+
+	stager = new(Stager)
+
+	stager.stage = stage
+	stager.splitStage = splitStage
+
+	stager.asSplitArea = &split.AsSplitArea{}
+
+	return
+}
+
+func (stager *Stager) GetAsSplitArea() (asSplitArea *split.AsSplitArea) {
+	asSplitArea = stager.asSplitArea
+	return
+}
