045ce3031e7bd14744a433f4098998132f1c3b3b
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-06-14 03:38:29 +0200

    probe: make the stage names more explicit #748

--- lib/sim/go/controllers/register_controllers.go
+++ lib/sim/go/controllers/register_controllers.go
@@ -163,7 +163,7 @@ func (controller *Controller) onWebSocketRequestForBackRepoContent(c *gin.Contex
 				return false
 			}
 
-			log.Printf("CheckOrigin: Accepted - Origin '%s' with port %d", origin, port)
+			log.Printf("%s CheckOrigin: Accepted - Origin '%s' with port %d", time.Now().Format("2006-01-02 15:04:05.000000"), origin, port)
 			return true
 		},
 	}
@@ -216,7 +216,7 @@ func (controller *Controller) onWebSocketRequestForBackRepoContent(c *gin.Contex
 			// ReadMessage is used to detect client disconnection
 			_, _, err := wsConnection.ReadMessage()
 			if err != nil {
-				log.Println("github.com/fullstack-lang/gong/lib/sim/go", stackPath, "WS client disconnected:", err)
+				log.Println(time.Now().Format("2006-01-02 15:04:05.000000"), "github.com/fullstack-lang/gong/lib/sim/go", stackPath, "WS client disconnected:", err)
 				cancel() // Cancel the context
 				return
 			}
