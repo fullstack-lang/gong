4d5217835120eab04a3be4f3c85d70063d63d9aa
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-06-20 10:10:24 +0200

    gong : GetStructInstancesByOrder, have an automated version Fixes #763 - manual

--- test/test/go/models/zzz_gong.go
+++ test/test/go/models/zzz_gong.go
@@ -264,6 +264,29 @@ func GetNamedStructInstances[T PointerToGongstruct](set map[T]any, order map[T]u
 	return
 }
 
+func GetStructInstancesByOrderAuto[T PointerToGongstruct](stage *Stage) (res []T) {
+
+	var t T
+	switch any(t).(type) {
+	case *Astruct:
+		tmp := GetStructInstancesByOrder(stage.Astructs, stage.AstructMap_Staged_Order)
+
+		// Create a new slice of the generic type T with the same capacity.
+		res = make([]T, 0, len(tmp))
+
+		// Iterate over the source slice and perform a type assertion on each element.
+		for _, v := range tmp {
+			// Assert that the element 'v' can be treated as type 'T'.
+			// Note: This relies on the constraint that PointerToGongstruct
+			// is an interface that *Astruct implements.
+			res = append(res, any(v).(T))
+		}
+		return res
+	}
+
+	return
+}
+
 func GetStructInstancesByOrder[T PointerToGongstruct](set map[T]any, order map[T]uint) (res []T) {
 
 	orderedSet := []T{}
