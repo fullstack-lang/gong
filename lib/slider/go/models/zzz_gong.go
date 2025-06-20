3cda01dce22701a90a5c748f6b66259f1a1bc2ed
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-06-20 10:22:00 +0200

    gong : GetStructInstancesByOrder, have an automated version Fixes #763 - new gongc

--- lib/slider/go/models/zzz_gong.go
+++ lib/slider/go/models/zzz_gong.go
@@ -218,6 +218,71 @@ func GetNamedStructInstances[T PointerToGongstruct](set map[T]any, order map[T]u
 	return
 }
 
+func GetStructInstancesByOrderAuto[T PointerToGongstruct](stage *Stage) (res []T) {
+	var t T
+	switch any(t).(type) {
+		// insertion point for case
+	case *Checkbox:
+		tmp := GetStructInstancesByOrder(stage.Checkboxs, stage.CheckboxMap_Staged_Order)
+
+		// Create a new slice of the generic type T with the same capacity.
+		res = make([]T, 0, len(tmp))
+
+		// Iterate over the source slice and perform a type assertion on each element.
+		for _, v := range tmp {
+			// Assert that the element 'v' can be treated as type 'T'.
+			// Note: This relies on the constraint that PointerToGongstruct
+			// is an interface that *Checkbox implements.
+			res = append(res, any(v).(T))
+		}
+		return res
+	case *Group:
+		tmp := GetStructInstancesByOrder(stage.Groups, stage.GroupMap_Staged_Order)
+
+		// Create a new slice of the generic type T with the same capacity.
+		res = make([]T, 0, len(tmp))
+
+		// Iterate over the source slice and perform a type assertion on each element.
+		for _, v := range tmp {
+			// Assert that the element 'v' can be treated as type 'T'.
+			// Note: This relies on the constraint that PointerToGongstruct
+			// is an interface that *Group implements.
+			res = append(res, any(v).(T))
+		}
+		return res
+	case *Layout:
+		tmp := GetStructInstancesByOrder(stage.Layouts, stage.LayoutMap_Staged_Order)
+
+		// Create a new slice of the generic type T with the same capacity.
+		res = make([]T, 0, len(tmp))
+
+		// Iterate over the source slice and perform a type assertion on each element.
+		for _, v := range tmp {
+			// Assert that the element 'v' can be treated as type 'T'.
+			// Note: This relies on the constraint that PointerToGongstruct
+			// is an interface that *Layout implements.
+			res = append(res, any(v).(T))
+		}
+		return res
+	case *Slider:
+		tmp := GetStructInstancesByOrder(stage.Sliders, stage.SliderMap_Staged_Order)
+
+		// Create a new slice of the generic type T with the same capacity.
+		res = make([]T, 0, len(tmp))
+
+		// Iterate over the source slice and perform a type assertion on each element.
+		for _, v := range tmp {
+			// Assert that the element 'v' can be treated as type 'T'.
+			// Note: This relies on the constraint that PointerToGongstruct
+			// is an interface that *Slider implements.
+			res = append(res, any(v).(T))
+		}
+		return res
+
+	}
+	return
+}
+
 func GetStructInstancesByOrder[T PointerToGongstruct](set map[T]any, order map[T]uint) (res []T) {
 
 	orderedSet := []T{}
