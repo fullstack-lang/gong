839d46cccc3cde582770cd6cf489fdaa7c162c54
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-06-21 11:08:39 +0200

    doc2 : display number of instance Fixes #774 get rid of field gongStructShape.ShowNbInstances - generated

--- test/test3/go/probe/probe.go
+++ test/test3/go/probe/probe.go
@@ -92,6 +92,7 @@ func NewProbe(
 		test3_go.GoModelsDir,
 		test3_go.GoDiagramsDir,
 		probe.diagramEditor,
+		stageOfInterest.Map_GongStructName_InstancesNb,
 	)
 
 	probe.dataEditor = &split.AsSplit{
