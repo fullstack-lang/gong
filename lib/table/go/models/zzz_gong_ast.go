0089714769aefead31dbe5987587281f90d9a42a
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-06-26 00:15:20 +0200

    ast parser : 16 ms par assignment when processing 5000 assignments Fixes #779

--- lib/table/go/models/zzz_gong_ast.go
+++ lib/table/go/models/zzz_gong_ast.go
@@ -190,7 +190,8 @@ func ParseAstFileFromAst(stage *Stage, inFile *ast.File, fset *token.FileSet) er
 						// Create an ast.CommentMap from the ast.File's comments.
 						// This helps keeping the association between comments
 						// and AST nodes.
-						cmap := ast.NewCommentMap(fset, inFile, inFile.Comments)
+						// cmap := ast.NewCommentMap(fset, inFile, inFile.Comments)
+						var cmap ast.CommentMap
 						astCoordinate := "\tAssignStmt: "
 						// log.Println(// astCoordinate)
 						assignStmt := stmt
