854251e6804b57d63e08c1536bba9beeb9fc3758
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-07-25 08:51:20 +0200

    comma corrected

--- lib/ssg/go/cmd/ssg/data/stage.go
+++ lib/ssg/go/cmd/ssg/data/stage.go
@@ -21,10 +21,10 @@ var _ map[string]any = map[string]any{
 // function will stage objects
 func _(stage *models.Stage) {
 
-	const __write__local_time = "2025-07-25 08:37:34.268305 CEST"
-	const __write__utc_time__ = "2025-07-25 06:37:34.268305 UTC"
+	const __write__local_time = "2025-07-25 08:50:35.388495 CEST"
+	const __write__utc_time__ = "2025-07-25 06:50:35.388495 UTC"
 
-	const __commitId__ = "0000000007"
+	const __commitId__ = "0000000008"
 
 	// Declaration of instances to stage
 
@@ -32,7 +32,7 @@ func _(stage *models.Stage) {
 	__Chapter__000001_Advanced_Topics := (&models.Chapter{}).Stage(stage)
 	__Chapter__000002_Third_Chapter_ := (&models.Chapter{}).Stage(stage)
 
-	__Content__000000_The_Vera_C_Rubin_Observatory_ := (&models.Content{}).Stage(stage)
+	__Content__000000_The_Vera_C_Rubin_Observatory := (&models.Content{}).Stage(stage)
 
 	__Page__000000_First_Steps := (&models.Page{}).Stage(stage)
 
@@ -50,8 +50,8 @@ This demonstrates how new lines are generated`
 	__Chapter__000002_Third_Chapter_.Name = `Third Chapter `
 	__Chapter__000002_Third_Chapter_.MardownContent = ``
 
-	__Content__000000_The_Vera_C_Rubin_Observatory_.Name = `The Vera C. Rubin Observatory,`
-	__Content__000000_The_Vera_C_Rubin_Observatory_.MardownContent = `The Vera C. Rubin Observatory, formerly known as the Large Synoptic Survey Telescope (LSST), is an astronomical observatory in Chile. Its main task is an astronomical survey of the entire available southern sky every few nights, creating a time-lapse record over ten years, the Legacy Survey of Space and Time (also abbreviated LSST).[2][3][4] The observatory is located on the El Peñón peak of Cerro Pachón, a 2,682-meter-high (8,799 ft) mountain in Coquimbo Region, in northern Chile, alongside the existing Gemini South and Southern Astrophysical Research Telescopes.[5] The Rubin Observatory base facility is located about 100 kilometres (62 miles) away from the observatory by road, in the city of La Serena. The observatory is named for Vera Rubin, an American astronomer who pioneered discoveries about galactic rotation rates.
+	__Content__000000_The_Vera_C_Rubin_Observatory.Name = `The Vera C. Rubin Observatory`
+	__Content__000000_The_Vera_C_Rubin_Observatory.MardownContent = `The Vera C. Rubin Observatory, formerly known as the Large Synoptic Survey Telescope (LSST), is an astronomical observatory in Chile. Its main task is an astronomical survey of the entire available southern sky every few nights, creating a time-lapse record over ten years, the Legacy Survey of Space and Time (also abbreviated LSST).[2][3][4] The observatory is located on the El Peñón peak of Cerro Pachón, a 2,682-meter-high (8,799 ft) mountain in Coquimbo Region, in northern Chile, alongside the existing Gemini South and Southern Astrophysical Research Telescopes.[5] The Rubin Observatory base facility is located about 100 kilometres (62 miles) away from the observatory by road, in the city of La Serena. The observatory is named for Vera Rubin, an American astronomer who pioneered discoveries about galactic rotation rates.
 
 Vera C. Rubin Observatory is a joint initiative of the U.S. National Science Foundation (NSF) and the U.S. Department of Energy's Office of Science and is operated jointly by NSF NOIRLab and SLAC National Accelerator Laboratory.[6]
 
@@ -60,11 +60,11 @@ The Rubin Observatory houses the Simonyi Survey Telescope, a wide-field reflecti
 The Rubin Observatory was proposed in 2001 as the LSST, and construction of the mirror began (with private funds) in 2007. The LSST then became the top-ranked large ground-based project in the 2010 Astrophysics Decadal Survey, and the project officially began construction on 1 August 2014, when the United States National Science Foundation (NSF) authorized the FY2014 portion ($27.5 million) of its construction budget.[9] Funding comes from the NSF, the United States Department of Energy, and private funding raised by the dedicated international non-profit organization, the LSST Discovery Alliance.[10] Operations are under the management of the Association of Universities for Research in Astronomy (AURA).[11] The total construction cost was expected to be about $680 million.[12]
 
 Site construction began on 14 April 2015 with the ceremonial laying of the first stone.[13][14] The first on-sky observations with the engineering camera occurred on 24 October 2024,[15] while system first light images were released 23 June 2025. Full survey operations are planned to begin later in 2025, due to COVID-related schedule delays.[16] Data is scheduled to become fully public after two years.[17]`
-	__Content__000000_The_Vera_C_Rubin_Observatory_.ContentPath = `content`
-	__Content__000000_The_Vera_C_Rubin_Observatory_.OutputPath = `public`
-	__Content__000000_The_Vera_C_Rubin_Observatory_.LayoutPath = `../../defaults/layouts`
-	__Content__000000_The_Vera_C_Rubin_Observatory_.StaticPath = `../../defaults/static`
-	__Content__000000_The_Vera_C_Rubin_Observatory_.Target = models.FILE
+	__Content__000000_The_Vera_C_Rubin_Observatory.ContentPath = `content`
+	__Content__000000_The_Vera_C_Rubin_Observatory.OutputPath = `public`
+	__Content__000000_The_Vera_C_Rubin_Observatory.LayoutPath = `../../defaults/layouts`
+	__Content__000000_The_Vera_C_Rubin_Observatory.StaticPath = `../../defaults/static`
+	__Content__000000_The_Vera_C_Rubin_Observatory.Target = models.FILE
 
 	__Page__000000_First_Steps.Name = `First Steps`
 	__Page__000000_First_Steps.MardownContent = `Here is the detailed content for the first page within Chapter 1.
@@ -95,9 +95,9 @@ Legend of the table`
 	// setup of Chapter instances pointers
 	__Chapter__000000_Getting_Started.Pages = append(__Chapter__000000_Getting_Started.Pages, __Page__000000_First_Steps)
 	// setup of Content instances pointers
-	__Content__000000_The_Vera_C_Rubin_Observatory_.Chapters = append(__Content__000000_The_Vera_C_Rubin_Observatory_.Chapters, __Chapter__000000_Getting_Started)
-	__Content__000000_The_Vera_C_Rubin_Observatory_.Chapters = append(__Content__000000_The_Vera_C_Rubin_Observatory_.Chapters, __Chapter__000001_Advanced_Topics)
-	__Content__000000_The_Vera_C_Rubin_Observatory_.Chapters = append(__Content__000000_The_Vera_C_Rubin_Observatory_.Chapters, __Chapter__000002_Third_Chapter_)
+	__Content__000000_The_Vera_C_Rubin_Observatory.Chapters = append(__Content__000000_The_Vera_C_Rubin_Observatory.Chapters, __Chapter__000000_Getting_Started)
+	__Content__000000_The_Vera_C_Rubin_Observatory.Chapters = append(__Content__000000_The_Vera_C_Rubin_Observatory.Chapters, __Chapter__000001_Advanced_Topics)
+	__Content__000000_The_Vera_C_Rubin_Observatory.Chapters = append(__Content__000000_The_Vera_C_Rubin_Observatory.Chapters, __Chapter__000002_Third_Chapter_)
 	// setup of Page instances pointers
 }
 
