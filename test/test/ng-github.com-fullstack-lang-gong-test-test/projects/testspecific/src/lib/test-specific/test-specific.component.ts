449ab560024d4f121d58bbe2080114fa85b2c0c8
peugeot <thomaspeugeot@yahoo.fr>
2025-08-30 09:35:08 +0200

    svg: add an extra REST API endpoint to inform the backend a CTRL pick was done Fixes #838

--- test/test/ng-github.com-fullstack-lang-gong-test-test/projects/testspecific/src/lib/test-specific/test-specific.component.ts
+++ test/test/ng-github.com-fullstack-lang-gong-test-test/projects/testspecific/src/lib/test-specific/test-specific.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import * as test from '../../../../test/src/public-api'
+
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'lib-test-specific',
-  imports: [],
+  imports: [
+    MatButtonModule,
+  ],
   templateUrl: './test-specific.component.html',
   styleUrl: './test-specific.component.css'
 })
-export class TestSpecificComponent {
+export class TestSpecificComponent implements OnInit {
+
+  @Input() Name: string = ""
+
+  public frontRepo?: test.FrontRepo;
+
+  constructor(
+    private frontRepoService: test.FrontRepoService,
+  ) { }
+
+  ngOnInit(): void {
+    console.log("ngOnInit");
+
+    this.frontRepoService.connectToWebSocket(this.Name).subscribe({
+      next: (frontRepo) => {
+        this.frontRepo = frontRepo;
+
+        for (let astruct of this.frontRepo.array_Astructs) {
+          console.log("astruct:", astruct.Name)
+        }
+      }
+    })
+
+  }
 
 }
