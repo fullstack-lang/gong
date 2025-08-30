0ac4f382ca764e802c50b36d69b9a170006113a8
peugeot <thomaspeugeot@yahoo.fr>
2025-08-30 09:35:08 +0200

    svg: add an extra REST API endpoint to inform the backend a CTRL pick was done Fixes #838

--- test/test/ng-github.com-fullstack-lang-gong-test-test/src/app/app.component.ts
+++ test/test/ng-github.com-fullstack-lang-gong-test-test/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 
 import { SplitSpecificComponent } from '@vendored_components/github.com/fullstack-lang/gong/lib/split/ng-github.com-fullstack-lang-gong-lib-split/projects/splitspecific/src/lib/split-specific/split-specific.component'
+import { AngularSplitModule } from 'angular-split';
+import { TestSpecificComponent } from "../../projects/testspecific/src/lib/test-specific/test-specific.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     SplitSpecificComponent,
+    AngularSplitModule,
+    TestSpecificComponent
   ],
 
   templateUrl: './app.component.html',
