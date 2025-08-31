fd27d4389ea3a00fc8c50efdd14389d3b4b55804
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-08-31 15:07:11 +0200

    svg: add an extra REST API endpoint to inform the backend a CTRL pick was done Fixes #838 - function signature OK

---
+++ test/test/go/models/zzz_gong_orchestrator_wme.go
@@ -0,0 +1,27 @@
+// generated code - do not edit
+package models
+
+// insertion point
+// AstructOrchestratorWithMouseEvent
+type AstructOrchestratorWithMouseEvent struct {
+}
+
+func (orchestrator *AstructOrchestratorWithMouseEvent) OnAfterUpdateWithMouseEvent(
+	gongsvgStage *Stage,
+	stagedAstruct, backRepoAstruct *Astruct, mouseEvent *MouseEvent) {
+
+	stagedAstruct.OnAfterUpdateWithMouseEvent(gongsvgStage, backRepoAstruct, mouseEvent)
+}
+
+func SetOrchestratorOnAfterUpdateWithMouseEvent[Type Gongstruct](stage *Stage) {
+
+	var ret Type
+
+	switch any(ret).(type) {
+	// insertion point
+	case Astruct:
+		stage.OnAfterAstructUpdateWithMouseEventCallback = new(AstructOrchestratorWithMouseEvent)
+
+	}
+}
+
