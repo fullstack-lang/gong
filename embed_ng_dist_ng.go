f808a522acfe7eb6a3f5b2e827daeba5daf9157b
peugeot <thomaspeugeot@yahoo.fr>
2025-09-29 00:52:35 +0200

    gong gong is now level1

--- embed_ng_dist_ng.go
+++
@@ -1,9 +0,0 @@
-package gong
-
-import "embed"
-
-// NgDistNg is the export of angular distribution. This allows
-// embedding of the pages in the web server
-//
-//go:embed ng-github.com-fullstack-lang-gong/dist/ng-github.com-fullstack-lang-gong
-var NgDistNg embed.FS
