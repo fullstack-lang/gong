a086ab861ad3a3b9dc15fe9f294a9177384a8698
peugeot <thomaspeugeot@yahoo.fr>
2025-10-28 15:44:07 +0100

    svg: robustness to missing values for RectAnchorType, TextAnchorType, ... Fixes #865

--- lib/svg/go/cmd/svg/data/simple.go
+++ lib/svg/go/cmd/svg/data/simple.go
@@ -499,7 +499,7 @@ func _(stage *models.Stage) {
 	__RectAnchoredRect__000002_TopRight.RX = 5.000000
 	__RectAnchoredRect__000002_TopRight.X_Offset = 0.000000
 	__RectAnchoredRect__000002_TopRight.Y_Offset = 0.000000
-	__RectAnchoredRect__000002_TopRight.RectAnchorType = models.RECT_TOP_RIGHT
+	// __RectAnchoredRect__000002_TopRight.RectAnchorType = models.RECT_TOP_RIGHT
 	__RectAnchoredRect__000002_TopRight.WidthFollowRect = false
 	__RectAnchoredRect__000002_TopRight.HeightFollowRect = false
 	__RectAnchoredRect__000002_TopRight.HasToolTip = false
@@ -1022,4 +1022,3 @@ Oriented line 3`
 	// setup of SVG instances pointers
 	__SVG__000000_simple.Layers = append(__SVG__000000_simple.Layers, __Layer__000000_Layer_1)
 }
-
