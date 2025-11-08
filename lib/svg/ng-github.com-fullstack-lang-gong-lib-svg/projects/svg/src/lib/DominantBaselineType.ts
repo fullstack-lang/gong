4fc6e50ff46183b6d5d20f398952261a2eb6ac8e
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-11-08 09:30:03 +0100

    Svg: RectAnchoredText add dominant baseline attribute Fixes #879

--- lib/svg/ng-github.com-fullstack-lang-gong-lib-svg/projects/svg/src/lib/DominantBaselineType.ts
+++ lib/svg/ng-github.com-fullstack-lang-gong-lib-svg/projects/svg/src/lib/DominantBaselineType.ts
@@ -2,10 +2,18 @@
 export enum DominantBaselineType {
 	// insertion point	
 	DominantBaselineAuto = "auto",
-	DominantBaselineMiddle = "middle",
 	DominantBaselineAlphabetic = "alphabetic",
-	DominantBaselineHanging = "hanging",
 	DominantBaselineIdeographic = "ideographic",
+	DominantBaselineHanging = "hanging",
+	DominantBaselineMathematical = "mathematical",
+	DominantBaselineCentral = "central",
+	DominantBaselineMiddle = "middle",
+	DominantBaselineTextAfterEdge = "text-after-edge",
+	DominantBaselineTextBeforeEdge = "text-before-edge",
+	DominantBaselineTextTop = "text-top",
+	DominantBaselineUseScript = "use-script",
+	DominantBaselineNoChange = "no-change",
+	DominantBaselineResetSize = "reset-size",
 }
 
 export interface DominantBaselineTypeSelect {
@@ -15,8 +23,16 @@ export interface DominantBaselineTypeSelect {
 
 export const DominantBaselineTypeList: DominantBaselineTypeSelect[] = [ // insertion point	
 	{ value: DominantBaselineType.DominantBaselineAuto, viewValue: "auto" },
-	{ value: DominantBaselineType.DominantBaselineMiddle, viewValue: "middle" },
 	{ value: DominantBaselineType.DominantBaselineAlphabetic, viewValue: "alphabetic" },
-	{ value: DominantBaselineType.DominantBaselineHanging, viewValue: "hanging" },
 	{ value: DominantBaselineType.DominantBaselineIdeographic, viewValue: "ideographic" },
+	{ value: DominantBaselineType.DominantBaselineHanging, viewValue: "hanging" },
+	{ value: DominantBaselineType.DominantBaselineMathematical, viewValue: "mathematical" },
+	{ value: DominantBaselineType.DominantBaselineCentral, viewValue: "central" },
+	{ value: DominantBaselineType.DominantBaselineMiddle, viewValue: "middle" },
+	{ value: DominantBaselineType.DominantBaselineTextAfterEdge, viewValue: "text-after-edge" },
+	{ value: DominantBaselineType.DominantBaselineTextBeforeEdge, viewValue: "text-before-edge" },
+	{ value: DominantBaselineType.DominantBaselineTextTop, viewValue: "text-top" },
+	{ value: DominantBaselineType.DominantBaselineUseScript, viewValue: "use-script" },
+	{ value: DominantBaselineType.DominantBaselineNoChange, viewValue: "no-change" },
+	{ value: DominantBaselineType.DominantBaselineResetSize, viewValue: "reset-size" },
 ];
