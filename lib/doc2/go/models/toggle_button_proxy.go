fda926f2fcde39fefde9de4d7c38b05d621b0452
peugeot <thomaspeugeot@yahoo.fr>
2025-11-13 04:26:23 +0100

    doc2 : persistance of display properties per diagram Fixes #887 - cleanup before operation

---
+++ lib/doc2/go/models/toggle_button_proxy.go
@@ -0,0 +1,19 @@
+package models
+
+import tree "github.com/fullstack-lang/gong/lib/tree/go/models"
+
+type toggleButtonProxy struct {
+	stager      *Stager
+	toggleValue *bool
+}
+
+func (p *toggleButtonProxy) ButtonUpdated(
+	treeStage *tree.Stage,
+	staged, front *tree.Button) {
+
+	*p.toggleValue = !*p.toggleValue
+
+	p.stager.UpdateAndCommitSVGStage()
+	p.stager.UpdateAndCommitTreeStage()
+	p.stager.stage.Commit()
+}
