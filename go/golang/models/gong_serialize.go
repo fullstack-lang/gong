1a2b9570fa3bfd295ed532ddc721bb64a8a9b188
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-11-22 07:13:11 +0100

    do not generate the ID when no addIDs Fixes #902- auto headers

--- go/golang/models/gong_serialize.go
+++ go/golang/models/gong_serialize.go
@@ -228,15 +228,18 @@ func SerializeExcelizePointerToGongstruct2[Type PointerToGongstruct](stage *Stag
 
 	line := 1
 
-	for index, fieldName := range GetFieldsFromPointer[Type]() {
+	for index, fieldHeader := range GetFieldsFromPointer[Type]() {
 		if !addIDs {
-			f.SetCellStr(sheetName, fmt.Sprintf("%s%d", IntToLetters(int32(index+1)), line), fieldName.Name)
+			f.SetCellStr(sheetName, fmt.Sprintf("%s%d", IntToLetters(int32(index+1)), line), fieldHeader.Name)
 		} else {
-			f.SetCellStr(sheetName, fmt.Sprintf("%s%d", IntToLetters(int32(2*index+1)), line), fieldName.Name)
-			if index == 0 {
-				f.SetCellStr(sheetName, fmt.Sprintf("%s%d", IntToLetters(int32(2*index+2)), line), "ID")
-			} else {
-				f.SetCellStr(sheetName, fmt.Sprintf("%s%d", IntToLetters(int32(2*index+2)), line), fieldName.Name+":ID")
+			f.SetCellStr(sheetName, fmt.Sprintf("%s%d", IntToLetters(int32(2*index+1)), line), fieldHeader.Name)
+			switch fieldHeader.GongFieldValueType {
+			case GongFieldValueTypePointer:
+				f.SetCellStr(sheetName, fmt.Sprintf("%s%d", IntToLetters(int32(2*index+2)), line), fieldHeader.Name+":ID")
+			case GongFieldValueTypeSliceOfPointers:
+				f.SetCellStr(sheetName, fmt.Sprintf("%s%d", IntToLetters(int32(2*index+2)), line), fieldHeader.Name+":IDs")
+			default:
+				f.SetCellStr(sheetName, fmt.Sprintf("%s%d", IntToLetters(int32(2*index+2)), line), fieldHeader.Name+":Basic")
 			}
 		}
 	}
