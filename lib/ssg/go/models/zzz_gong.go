c34e6afd3a4c6cfa01f53b6010f2f800d58de118
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-11-22 12:48:26 +0100

    rever #910

--- lib/ssg/go/models/zzz_gong.go
+++ lib/ssg/go/models/zzz_gong.go
@@ -449,13 +449,6 @@ func (stage *Stage) CommitWithSuspendedCallbacks() {
 }
 
 func (stage *Stage) Commit() {
-	if stage.OnInitCommitCallback != nil {
-		stage.OnInitCommitCallback.BeforeCommit(stage)
-	}
-	if stage.OnInitCommitFromBackCallback != nil {
-		stage.OnInitCommitFromBackCallback.BeforeCommit(stage)
-	}
-
 	stage.ComputeReverseMaps()
 	stage.commitId++
 	stage.commitTimeStamp = time.Now()
