83b1b23e0d770ed528a4534f9829fac347e71b50
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-11-25 00:51:24 +0100

    CUD at the root of the tree

--- test/test/go/cmd/test/data/stage.go
+++ test/test/go/cmd/test/data/stage.go
@@ -21,10 +21,10 @@ var _ map[string]any = map[string]any{
 // function will stage objects
 func _(stage *models.Stage) {
 
-	const __write__local_time = "2025-11-25 00:16:39.451213 CET"
-	const __write__utc_time__ = "2025-11-24 23:16:39.451213 UTC"
+	const __write__local_time = "2025-11-25 00:49:42.533905 CET"
+	const __write__utc_time__ = "2025-11-24 23:49:42.533905 UTC"
 
-	const __commitId__ = "0000000138"
+	const __commitId__ = "0000000139"
 
 	// Declaration of instances to stage
 
