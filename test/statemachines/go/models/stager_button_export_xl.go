9d8bd741963eb65a415b9f55db3bcf1a2f31413c
Thomas Peugeot <thomaspeugeot@users.noreply.github.com>
2025-12-08 18:40:03 +0100

    Add stop button to updateExportXLButtonStage
    Added a stop button to the export XL stage.

--- test/statemachines/go/models/stager_button_export_xl.go
+++ test/statemachines/go/models/stager_button_export_xl.go
@@ -2,6 +2,8 @@ package models
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"time"
 
 	button "github.com/fullstack-lang/gong/lib/button/go/models"
@@ -32,6 +34,17 @@ func (stager *Stager) updateExportXLButtonStage() {
 
 	button_.Proxy = proxy
 
+	buttonKill := button.NewButton(
+		&StopButtonProxy{
+			stager: stager,
+		},
+		"Stop for maintenance",
+		string(buttons.BUTTON_stop_circle),
+		"Stop for maintenance",
+	)
+
+	group.Buttons = append(group.Buttons, buttonKill)
+
 	button.StageBranch(stage, layout)
 
 	stage.Commit()
@@ -47,4 +60,18 @@ func (proxy *ExportXLButtonProxy) Updated() {
 	filename := fmt.Sprintf("./SC-DOC-7300-0016-MOSS-%s.xlsx", time.Now().Local().Format("2006-01-02-15-04-05"))
 
 	SerializeStageMessageTypeTransitionEtat(proxy.stager.stage, filename)
+}
+
+type StopButtonProxy struct {
+	stager *Stager
+}
+
+func (e *StopButtonProxy) GetButtonsStage() *button.Stage {
+	return e.stager.buttonExportXLStage
+}
+
+func (proxy *StopButtonProxy) OnAfterUpdateButton() {
+
+	log.Println("Arret demandé de l'application")
+	os.Exit(0)
 }
