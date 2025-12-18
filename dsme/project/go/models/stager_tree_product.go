863e001850808bb4f510fbea2f838c3db67c3fe1
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-12-18 19:23:48 +0100

    before enforcing all projects at the root

---
+++ dsme/project/go/models/stager_tree_product.go
@@ -0,0 +1,45 @@
+package models
+
+import (
+	tree "github.com/fullstack-lang/gong/lib/tree/go/models"
+
+	"github.com/fullstack-lang/gong/lib/tree/go/buttons"
+)
+
+func (stager *Stager) updateProductTreeStage() {
+
+	stager.treeProductsStage.Reset()
+
+	root := stager.root
+
+	treeInstance := new(tree.Tree).Stage(stager.treeProductsStage)
+
+	for _, project := range root.Projects {
+
+		nodeForAddButton := new(tree.Node).Stage(stager.treeProductsStage)
+		nodeForAddButton.Name = project.Name
+		treeInstance.RootNodes = append(treeInstance.RootNodes, nodeForAddButton)
+
+		addButton := (&tree.Button{
+			Name: "Product" + " " + string(buttons.BUTTON_add),
+			Icon: string(buttons.BUTTON_add)}).Stage(stager.treeProductsStage)
+		nodeForAddButton.Buttons = append(nodeForAddButton.Buttons, addButton)
+		addButton.Impl = &ProductAddButtonProxy{
+			stager:   stager,
+			products: project.RootProducts,
+		}
+
+		for _, product := range project.RootProducts {
+			nodeProduct := new(tree.Node).Stage(stager.treeProductsStage)
+			nodeProduct.Name = product.Name
+			nodeProduct.HasCheckboxButton = true
+			nodeProduct.IsExpanded = true
+
+			treeInstance.RootNodes = append(treeInstance.RootNodes, nodeProduct)
+		}
+	}
+
+	tree.StageBranch(stager.treeProductsStage, treeInstance)
+
+	stager.treeProductsStage.Commit()
+}
