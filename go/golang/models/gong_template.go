2d67b411b06b8c21bdb8845835001c8b8e96c41a
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-12-19 02:31:37 +0100

    new gong

--- go/golang/models/gong_template.go
+++ go/golang/models/gong_template.go
@@ -575,7 +575,7 @@ func GetSliceOfPointersReverseMap[Start, End Gongstruct](fieldname string, stage
 
 // GetPointerToGongstructName returns the name of the Gongstruct
 // this can be usefull if one want program robust to refactoring
-func GetPointerToGongstructName[Type PointerToGongstruct]() (res string) {
+func GetPointerToGongstructName[Type GongstructIF]() (res string) {
 
 	var ret Type
 
@@ -590,7 +590,7 @@ type ReverseField struct {
 	Fieldname      string
 }
 
-func GetReverseFields[Type PointerToGongstruct]() (res []ReverseField) {
+func GetReverseFields[Type GongstructIF]() (res []ReverseField) {
 
 	res = make([]ReverseField, 0)
 
