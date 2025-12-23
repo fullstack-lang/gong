38578e55c3ea5d92347817c0ab93852e943e57e3
peugeot <thomaspeugeot@yahoo.fr>
2025-12-23 19:04:52 +0100

    project : more genericity Fixes #1011 - helper

--- dsme/project/go/models/generic_helpers.go
+++ dsme/project/go/models/generic_helpers.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"log"
 	"slices"
 
 	tree "github.com/fullstack-lang/gong/lib/tree/go/models"
@@ -56,6 +57,69 @@ func OnRemoveCompositionShape[
 		compositionShape.UnstageVoid(stager.stage)
 		idx := slices.Index(*shapes, compositionShape)
 		*shapes = slices.Delete(*shapes, idx, idx+1)
+		stager.stage.Commit()
+	}
+}
+
+func OnUpdateElementInDiagram[
+	AT interface {
+		*AT_
+		AbstractType
+	},
+	AT_ Gongstruct,
+	CT interface {
+		*CT_
+		RectShapeInterface
+		ConcreteType
+	},
+	CT_ Gongstruct](
+	stager *Stager,
+	diagram *Diagram,
+	element AT,
+	elementsWhoseNodeIsExpanded *[]AT,
+	shapes *[]CT,
+	shapesMap *map[AT]CT,
+) func(stage *tree.Stage, stagedNode, frontNode *tree.Node) {
+	return func(stage *tree.Stage, stagedNode, frontNode *tree.Node) {
+		// find the shape (if any)
+		shape := (*shapesMap)[element]
+
+		if frontNode.IsChecked && !stagedNode.IsChecked {
+			stagedNode.IsChecked = frontNode.IsChecked
+			if shape != nil {
+				log.Panic("adding a shape to an already product shape")
+			}
+			shape = newShapeToDiagram(element, diagram, shapes, stager.stage)
+			stager.stage.Commit()
+			return
+		}
+		if !frontNode.IsChecked && stagedNode.IsChecked {
+			stagedNode.IsChecked = frontNode.IsChecked
+			if shape == nil {
+				log.Panic("remove a non existing shape to product")
+			}
+			shape.UnstageVoid(stager.stage)
+			idx := slices.Index(*shapes, shape)
+			*shapes = slices.Delete(*shapes, idx, idx+1)
+			stager.stage.Commit()
+			return
+		}
+
+		if frontNode.IsExpanded != stagedNode.IsExpanded {
+			stagedNode.IsExpanded = frontNode.IsExpanded
+			if frontNode.IsExpanded {
+				if slices.Index(*elementsWhoseNodeIsExpanded, element) == -1 {
+					*elementsWhoseNodeIsExpanded = append(*elementsWhoseNodeIsExpanded, element)
+				}
+			} else {
+				if idx := slices.Index(*elementsWhoseNodeIsExpanded, element); idx != -1 {
+					*elementsWhoseNodeIsExpanded = slices.Delete(*elementsWhoseNodeIsExpanded, idx, idx+1)
+				}
+			}
+			return
+		}
+
+		stager.probeForm.FillUpFormFromGongstruct(element, GetPointerToGongstructName[*Product]())
 		stager.stage.Commit()
 	}
 }
