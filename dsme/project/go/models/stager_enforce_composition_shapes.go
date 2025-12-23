1553635b8305303651f1f3aa7bbb2af3990a59af
peugeot <thomaspeugeot@yahoo.fr>
2025-12-23 04:04:46 +0100

    project : add products to the diagram Fixes #1006 enforcing

---
+++ dsme/project/go/models/stager_enforce_composition_shapes.go
@@ -0,0 +1,43 @@
+// new file
+package models
+
+import "slices"
+
+func (stager *Stager) enforceCompositionShapes() (needCommit bool) {
+	for _, diagram := range GetGongstrucsSorted[*Diagram](stager.stage) {
+
+		// map of products present in the diagram
+		set_Product := make(map[*Product]struct{})
+		for _, productShape := range diagram.Product_Shapes {
+			if productShape.Product != nil {
+				set_Product[productShape.Product] = struct{}{}
+			}
+		}
+
+		// check that the composition shape is valid
+		for i := len(diagram.Composition_Shapes) - 1; i >= 0; i-- {
+			compositionShape := diagram.Composition_Shapes[i]
+			product := compositionShape.Product
+
+			// check if the product is present in the diagram
+			// if not, remove the composition shape
+			if _, ok := set_Product[product]; !ok {
+				diagram.Composition_Shapes = slices.Delete(diagram.Composition_Shapes, i, i+1)
+				compositionShape.Unstage(stager.stage)
+				needCommit = true
+				continue
+			}
+
+			// check if the parent product is present in the diagram
+			// if not, remove the composition shape
+			parentProduct := product.parentProduct
+			if _, ok := set_Product[parentProduct]; !ok {
+				diagram.Composition_Shapes = slices.Delete(diagram.Composition_Shapes, i, i+1)
+				compositionShape.Unstage(stager.stage)
+				needCommit = true
+				continue
+			}
+		}
+	}
+	return
+}
