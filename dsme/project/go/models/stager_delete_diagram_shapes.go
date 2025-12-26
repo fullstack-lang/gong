9899cd9eda5df6dbc7041de5ea3aea49e897c7c2
peugeot <thomaspeugeot@yahoo.fr>
2025-12-26 12:56:18 +0100

    project : diagram, add a "show all" button Fixes #1016 - WIP

--- dsme/project/go/models/stager_delete_diagram_shapes.go
+++ dsme/project/go/models/stager_delete_diagram_shapes.go
@@ -1,21 +1,23 @@
 package models
 
-// removeAllShapes
-func (stager *Stager) removeAllShapes(diagram *Diagram) {
+// deletePBSShapes
+func (stager *Stager) deletePBSShapes(diagram *Diagram) {
 	for _, shape := range diagram.Product_Shapes {
 		shape.Unstage(stager.stage)
 	}
 	diagram.Product_Shapes = nil
 
-	for _, shape := range diagram.Task_Shapes {
-		shape.Unstage(stager.stage)
-	}
-	diagram.Task_Shapes = nil
-
 	for _, shape := range diagram.ProductComposition_Shapes {
 		shape.Unstage(stager.stage)
 	}
 	diagram.ProductComposition_Shapes = nil
+}
+
+func (stager *Stager) deleteWBSShapes(diagram *Diagram) {
+	for _, shape := range diagram.Task_Shapes {
+		shape.Unstage(stager.stage)
+	}
+	diagram.Task_Shapes = nil
 
 	for _, shape := range diagram.TaskComposition_Shapes {
 		shape.Unstage(stager.stage)
