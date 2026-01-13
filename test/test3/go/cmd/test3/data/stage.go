643b01a4e0689e764cb5db39a8057ca77f8862f8
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2026-01-13 01:38:14 +0100

    gong / ast : refactoring Fixes #1063 - manual

--- test/test3/go/cmd/test3/data/stage.go
+++ test/test3/go/cmd/test3/data/stage.go
@@ -24,129 +24,130 @@ func _(stage *models.Stage) {
 	// insertion point for declaration of instances to stage
 
 	__A__00000000_ := (&models.A{Name: `A09xxx`}).Stage(stage)
-	__A__00000006_ := (&models.A{Name: `A6`}).Stage(stage)
-	__A__00000007_ := (&models.A{Name: `fff`}).Stage(stage)
+	__A__00000001_ := (&models.A{Name: `A6`}).Stage(stage)
+	__A__00000002_ := (&models.A{Name: `fff`}).Stage(stage)
 
-	__B__00000001_ := (&models.B{Name: `1___0009`}).Stage(stage)
-	__B__00000002_ := (&models.B{Name: `2gfsdfggf`}).Stage(stage)
-	__B__00000006_ := (&models.B{Name: `6`}).Stage(stage)
-	__B__00000007_ := (&models.B{Name: `7`}).Stage(stage)
-	__B__00000008_ := (&models.B{Name: `8`}).Stage(stage)
-	__B__00000009_ := (&models.B{Name: `9`}).Stage(stage)
-	__B__00000010_ := (&models.B{Name: `10`}).Stage(stage)
-	__B__00000011_ := (&models.B{Name: `11`}).Stage(stage)
-	__B__00000012_ := (&models.B{Name: `12`}).Stage(stage)
-	__B__00000013_ := (&models.B{Name: `13`}).Stage(stage)
-	__B__00000014_ := (&models.B{Name: `14`}).Stage(stage)
-	__B__00000015_ := (&models.B{Name: `15`}).Stage(stage)
-	__B__00000016_ := (&models.B{Name: `16`}).Stage(stage)
-	__B__00000017_ := (&models.B{Name: `17`}).Stage(stage)
-	__B__00000020_ := (&models.B{Name: `20`}).Stage(stage)
-	__B__00000021_ := (&models.B{Name: `21`}).Stage(stage)
-	__B__00000022_ := (&models.B{Name: `22`}).Stage(stage)
-	__B__00000024_ := (&models.B{Name: `24`}).Stage(stage)
-	__B__00000025_ := (&models.B{Name: `25`}).Stage(stage)
-	__B__00000026_ := (&models.B{Name: `26`}).Stage(stage)
-	__B__00000027_ := (&models.B{Name: `27`}).Stage(stage)
-	__B__00000029_ := (&models.B{Name: `29`}).Stage(stage)
-	__B__00000030_ := (&models.B{Name: `30`}).Stage(stage)
-	__B__00000031_ := (&models.B{Name: `31`}).Stage(stage)
-	__B__00000032_ := (&models.B{Name: `32`}).Stage(stage)
-	__B__00000033_ := (&models.B{Name: `33`}).Stage(stage)
-	__B__00000034_ := (&models.B{Name: `34`}).Stage(stage)
-	__B__00000035_ := (&models.B{Name: `B35`}).Stage(stage)
+	__B__00000000_ := (&models.B{Name: `1___0009`}).Stage(stage)
+	__B__00000001_ := (&models.B{Name: `2gfsdfggf`}).Stage(stage)
+	__B__00000002_ := (&models.B{Name: `6`}).Stage(stage)
+	__B__00000003_ := (&models.B{Name: `7`}).Stage(stage)
+	__B__00000004_ := (&models.B{Name: `8`}).Stage(stage)
+	__B__00000005_ := (&models.B{Name: `9`}).Stage(stage)
+	__B__00000006_ := (&models.B{Name: `10`}).Stage(stage)
+	__B__00000007_ := (&models.B{Name: `11`}).Stage(stage)
+	__B__00000008_ := (&models.B{Name: `12`}).Stage(stage)
+	__B__00000009_ := (&models.B{Name: `13`}).Stage(stage)
+	__B__00000010_ := (&models.B{Name: `14`}).Stage(stage)
+	__B__00000011_ := (&models.B{Name: `15`}).Stage(stage)
+	__B__00000012_ := (&models.B{Name: `16`}).Stage(stage)
+	__B__00000013_ := (&models.B{Name: `17`}).Stage(stage)
+	__B__00000014_ := (&models.B{Name: `20`}).Stage(stage)
+	__B__00000015_ := (&models.B{Name: `21`}).Stage(stage)
+	__B__00000016_ := (&models.B{Name: `22`}).Stage(stage)
+	__B__00000017_ := (&models.B{Name: `24`}).Stage(stage)
+	__B__00000018_ := (&models.B{Name: `25`}).Stage(stage)
+	__B__00000019_ := (&models.B{Name: `26`}).Stage(stage)
+	__B__00000020_ := (&models.B{Name: `27`}).Stage(stage)
+	__B__00000021_ := (&models.B{Name: `29`}).Stage(stage)
+	__B__00000022_ := (&models.B{Name: `30`}).Stage(stage)
+	__B__00000023_ := (&models.B{Name: `31`}).Stage(stage)
+	__B__00000024_ := (&models.B{Name: `32`}).Stage(stage)
+	__B__00000025_ := (&models.B{Name: `33`}).Stage(stage)
+	__B__00000026_ := (&models.B{Name: `34`}).Stage(stage)
+	__B__00000027_ := (&models.B{Name: `B35`}).Stage(stage)
 
 	// insertion point for initialization of values
 
 	__A__00000000_.Name = `A09xxx`
-	__A__00000000_.Date, _ = time.Parse("2006-01-02 15:04:05.999999999 -0700 MST", "2040-01-01 00:00:00 +0000 UTC")
+	__A__00000000_.Date, _ = time.Parse("2006-01-02 15:04:05.999999999 -0700 MST", "0001-01-01 00:00:00 +0000 UTC")
 	__A__00000000_.FloatValue = 14.500000
 	__A__00000000_.IntValue = 9
 	__A__00000000_.Duration = -446400000000000
-	__A__00000000_.EnumString = models.EnumTypeString_Value1
+	__A__00000000_.EnumString = models.
 	__A__00000000_.EnumInt = models.EnumTypeInt_Value2
 
-	__A__00000006_.Name = `A6`
-	__A__00000006_.Date, _ = time.Parse("2006-01-02 15:04:05.999999999 -0700 MST", "0001-01-01 00:00:00 +0000 UTC")
-	__A__00000006_.FloatValue = 0.000000
-	__A__00000006_.IntValue = 0
-	__A__00000006_.Duration = 0
-	__A__00000006_.EnumString = models.EnumTypeString_Value1
-	__A__00000006_.EnumInt = models.EnumTypeInt_Value1
+	__A__00000001_.Name = `A6`
+	__A__00000001_.Date, _ = time.Parse("2006-01-02 15:04:05.999999999 -0700 MST", "0001-01-01 00:00:00 +0000 UTC")
+	__A__00000001_.FloatValue = 0.000000
+	__A__00000001_.IntValue = 0
+	__A__00000001_.Duration = 0
+	__A__00000001_.EnumString = models.
+	__A__00000001_.EnumInt = models.EnumTypeInt_Value2
 
-	__A__00000007_.Name = `fff`
-	__A__00000007_.Date, _ = time.Parse("2006-01-02 15:04:05.999999999 -0700 MST", "0001-01-01 00:00:00 +0000 UTC")
-	__A__00000007_.FloatValue = 0.000000
-	__A__00000007_.IntValue = 0
-	__A__00000007_.Duration = 0
-	__A__00000007_.EnumString = ""
-	__A__00000007_.EnumInt = 0
+	__A__00000002_.Name = `fff`
+	__A__00000002_.Date, _ = time.Parse("2006-01-02 15:04:05.999999999 -0700 MST", "0001-01-01 00:00:00 +0000 UTC")
+	__A__00000002_.FloatValue = 0.000000
+	__A__00000002_.IntValue = 0
+	__A__00000002_.Duration = 0
+	__A__00000002_.EnumString = ""
+	__A__00000002_.EnumInt = 0
 
-	__B__00000001_.Name = `1___0009`
+	__B__00000000_.Name = `1___0009`
 
-	__B__00000002_.Name = `2gfsdfggf`
+	__B__00000001_.Name = `2gfsdfggf`
 
-	__B__00000006_.Name = `6`
+	__B__00000002_.Name = `6`
 
-	__B__00000007_.Name = `7`
+	__B__00000003_.Name = `7`
 
-	__B__00000008_.Name = `8`
+	__B__00000004_.Name = `8`
 
-	__B__00000009_.Name = `9`
+	__B__00000005_.Name = `9`
 
-	__B__00000010_.Name = `10`
+	__B__00000006_.Name = `10`
 
-	__B__00000011_.Name = `11`
+	__B__00000007_.Name = `11`
 
-	__B__00000012_.Name = `12`
+	__B__00000008_.Name = `12`
 
-	__B__00000013_.Name = `13`
+	__B__00000009_.Name = `13`
 
-	__B__00000014_.Name = `14`
+	__B__00000010_.Name = `14`
 
-	__B__00000015_.Name = `15`
+	__B__00000011_.Name = `15`
 
-	__B__00000016_.Name = `16`
+	__B__00000012_.Name = `16`
 
-	__B__00000017_.Name = `17`
+	__B__00000013_.Name = `17`
 
-	__B__00000020_.Name = `20`
+	__B__00000014_.Name = `20`
 
-	__B__00000021_.Name = `21`
+	__B__00000015_.Name = `21`
 
-	__B__00000022_.Name = `22`
+	__B__00000016_.Name = `22`
 
-	__B__00000024_.Name = `24`
+	__B__00000017_.Name = `24`
 
-	__B__00000025_.Name = `25`
+	__B__00000018_.Name = `25`
 
-	__B__00000026_.Name = `26`
+	__B__00000019_.Name = `26`
 
-	__B__00000027_.Name = `27`
+	__B__00000020_.Name = `27`
 
-	__B__00000029_.Name = `29`
+	__B__00000021_.Name = `29`
 
-	__B__00000030_.Name = `30`
+	__B__00000022_.Name = `30`
 
-	__B__00000031_.Name = `31`
+	__B__00000023_.Name = `31`
 
-	__B__00000032_.Name = `32`
+	__B__00000024_.Name = `32`
 
-	__B__00000033_.Name = `33`
+	__B__00000025_.Name = `33`
 
-	__B__00000034_.Name = `34`
+	__B__00000026_.Name = `34`
 
-	__B__00000035_.Name = `B35`
+	__B__00000027_.Name = `B35`
 
 	// insertion point for setup of pointers
-	__A__00000000_.B = __B__00000010_
-	__A__00000006_.B = __B__00000001_
-	__A__00000006_.Bs = append(__A__00000006_.Bs, __B__00000007_)
-	__A__00000006_.Bs = append(__A__00000006_.Bs, __B__00000008_)
-	__A__00000006_.Bs = append(__A__00000006_.Bs, __B__00000006_)
-	__A__00000006_.Bs = append(__A__00000006_.Bs, __B__00000002_)
-	__A__00000006_.Bs = append(__A__00000006_.Bs, __B__00000001_)
-	__A__00000007_.B = __B__00000012_
-	__A__00000007_.Bs = append(__A__00000007_.Bs, __B__00000006_)
-	__A__00000007_.Bs = append(__A__00000007_.Bs, __B__00000008_)
+	__A__00000000_.B = __B__00000006_
+	__A__00000001_.B = __B__00000000_
+	__A__00000001_.Bs = append(__A__00000001_.Bs, __B__00000003_)
+	__A__00000001_.Bs = append(__A__00000001_.Bs, __B__00000004_)
+	__A__00000001_.Bs = append(__A__00000001_.Bs, __B__00000002_)
+	__A__00000001_.Bs = append(__A__00000001_.Bs, __B__00000001_)
+	__A__00000001_.Bs = append(__A__00000001_.Bs, __B__00000000_)
+	__A__00000001_.Bs = append(__A__00000001_.Bs, __B__00000005_)
+	__A__00000002_.B = __B__00000008_
+	__A__00000002_.Bs = append(__A__00000002_.Bs, __B__00000002_)
+	__A__00000002_.Bs = append(__A__00000002_.Bs, __B__00000004_)
 }
---
+++ test/test3/go/models/zzz_gong_ast2.go
@@ -0,0 +1,285 @@
+// package models
+package models
+
+import (
+	"embed"
+	"errors"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"log"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// ------------------------------------------------------------------------------------------------
+// STATIC AST PARSING LOGIC
+// ------------------------------------------------------------------------------------------------
+
+// ModelUnmarshaller abstracts the logic for setting fields on a staged instance
+type ModelUnmarshaller interface {
+	// Initialize creates the struct, stages it, and returns the pointer as 'any'
+	Initialize(stage *Stage, instanceName string, preserveOrder bool) (any, error)
+
+	// UnmarshallField sets a field's value based on the AST expression
+	UnmarshallField(stage *Stage, instance any, fieldName string, valueExpr ast.Expr, identifierMap map[string]any) error
+}
+
+// Unmarshallers is the registry of all model unmarshallers
+var Unmarshallers = make(map[string]ModelUnmarshaller)
+
+// ParseAstFile Parse pathToFile and stages all instances declared in the file
+func ParseAstFile2(stage *Stage, pathToFile string, preserveOrder bool) error {
+	fileOfInterest, err := filepath.Abs(pathToFile)
+	if err != nil {
+		return errors.New("Path does not exist %s ;" + fileOfInterest)
+	}
+
+	fset := token.NewFileSet()
+	inFile, errParser := parser.ParseFile(fset, fileOfInterest, nil, parser.ParseComments)
+	if errParser != nil {
+		return errors.New("Unable to parser " + errParser.Error())
+	}
+
+	return ParseAstFileFromAst(stage, inFile, fset, preserveOrder)
+}
+
+// ParseAstEmbeddedFile parses the Go source code from an embedded file
+func ParseAstEmbeddedFile2(stage *Stage, directory embed.FS, pathToFile string) error {
+	fileContentBytes, err := directory.ReadFile(pathToFile)
+	if err != nil {
+		return errors.New(stage.GetName() + "; Unable to read embedded file " + err.Error())
+	}
+
+	fset := token.NewFileSet()
+	inFile, errParser := parser.ParseFile(fset, pathToFile, fileContentBytes, parser.ParseComments)
+	if errParser != nil {
+		return errors.New("Unable to parse embedded file '" + pathToFile + "': " + errParser.Error())
+	}
+
+	return ParseAstFileFromAst(stage, inFile, fset, false)
+}
+
+// ParseAstFileFromAst traverses the AST and stages instances using the Unmarshaller registry
+func ParseAstFileFromAst(stage *Stage, inFile *ast.File, fset *token.FileSet, preserveOrder bool) error {
+
+	// 1. Remove Global Variables: Use a local map to track variable names to instances
+	identifierMap := make(map[string]any)
+
+	// 2. Visitor Pattern: Traverse the AST
+	ast.Inspect(inFile, func(n ast.Node) bool {
+		switch node := n.(type) {
+		case *ast.AssignStmt:
+			if len(node.Lhs) < 1 || len(node.Rhs) < 1 {
+				return true
+			}
+
+			// CASE 1: Initialization ( := )
+			if node.Tok == token.DEFINE {
+				if ident, ok := node.Lhs[0].(*ast.Ident); ok {
+					var typeName string
+					var instanceName string
+
+					// Inspect RHS to find the Struct Type and Name
+					ast.Inspect(node.Rhs[0], func(expr ast.Node) bool {
+						if compLit, ok := expr.(*ast.CompositeLit); ok {
+							if selExpr, ok := compLit.Type.(*ast.SelectorExpr); ok {
+								typeName = selExpr.Sel.Name
+								// Attempt to find Name field in literal
+								for _, elt := range compLit.Elts {
+									if kv, ok := elt.(*ast.KeyValueExpr); ok {
+										if k, ok := kv.Key.(*ast.Ident); ok && k.Name == "Name" {
+											if v, ok := kv.Value.(*ast.BasicLit); ok {
+												instanceName = strings.Trim(v.Value, "\"`")
+											}
+										}
+									}
+								}
+								return false
+							}
+						}
+						return true
+					})
+
+					// Dispatch to specific Unmarshaller
+					if typeName != "" {
+						if unmarshaller, exists := Unmarshallers[typeName]; exists {
+							instance, err := unmarshaller.Initialize(stage, instanceName, preserveOrder)
+							if err == nil {
+								identifierMap[ident.Name] = instance
+							}
+						}
+					}
+				}
+				return false
+			}
+
+			// CASE 2: Assignment ( = )
+			if node.Tok == token.ASSIGN {
+				if selExpr, ok := node.Lhs[0].(*ast.SelectorExpr); ok {
+					if ident, ok := selExpr.X.(*ast.Ident); ok {
+						if instance, exists := identifierMap[ident.Name]; exists {
+							// Extract TypeName from identifier convention (e.g. __A__...)
+							// This avoids needing to store type info explicitly in the map
+							parts := strings.Split(ident.Name, "__")
+							if len(parts) >= 2 {
+								typeName := parts[1]
+								if unmarshaller, exists := Unmarshallers[typeName]; exists {
+									// 3. Strategy Pattern: Delegate to Handler
+									unmarshaller.UnmarshallField(stage, instance, selExpr.Sel.Name, node.Rhs[0], identifierMap)
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		return true
+	})
+
+	return nil
+}
+
+// --- Generic Helpers for Unmarshallers ---
+
+func ExtractString(expr ast.Expr) string {
+	if bl, ok := expr.(*ast.BasicLit); ok {
+		return strings.Trim(bl.Value, "\"`")
+	}
+	return ""
+}
+
+func ExtractInt(expr ast.Expr) int {
+	if bl, ok := expr.(*ast.BasicLit); ok {
+		val, _ := strconv.Atoi(bl.Value)
+		return val
+	}
+	if ue, ok := expr.(*ast.UnaryExpr); ok && ue.Op == token.SUB {
+		if bl, ok := ue.X.(*ast.BasicLit); ok {
+			val, _ := strconv.Atoi(bl.Value)
+			return -val
+		}
+	}
+	return 0
+}
+
+func ExtractFloat(expr ast.Expr) float64 {
+	if bl, ok := expr.(*ast.BasicLit); ok {
+		val, _ := strconv.ParseFloat(bl.Value, 64)
+		return val
+	}
+	if ue, ok := expr.(*ast.UnaryExpr); ok && ue.Op == token.SUB {
+		if bl, ok := ue.X.(*ast.BasicLit); ok {
+			val, _ := strconv.ParseFloat(bl.Value, 64)
+			return -val
+		}
+	}
+	return 0.0
+}
+
+func ExtractBool(expr ast.Expr) bool {
+	if ident, ok := expr.(*ast.Ident); ok {
+		return ident.Name == "true"
+	}
+	return false
+}
+
+// ------------------------------------------------------------------------------------------------
+// GENERATED MODULAR HANDLERS
+// ------------------------------------------------------------------------------------------------
+
+func init() {
+	Unmarshallers["A"] = &AUnmarshaller{}
+	Unmarshallers["B"] = &BUnmarshaller{}
+}
+
+// --- A Unmarshaller ---
+type AUnmarshaller struct{}
+
+func (u *AUnmarshaller) Initialize(stage *Stage, instanceName string, preserveOrder bool) (any, error) {
+	instance := new(A)
+	instance.Name = instanceName
+	if !preserveOrder {
+		instance.Stage(stage)
+	} else {
+		instance.Stage(stage)
+	}
+	return instance, nil
+}
+
+func (u *AUnmarshaller) UnmarshallField(stage *Stage, i any, fieldName string, valueExpr ast.Expr, identifierMap map[string]any) error {
+	instance := i.(*A)
+	switch fieldName {
+	case "Name":
+		instance.Name = ExtractString(valueExpr)
+	case "Date":
+		if bl, ok := valueExpr.(*ast.BasicLit); ok {
+			instance.Date, _ = time.Parse("2006-01-02 15:04:05.999999999 -0700 MST", strings.Trim(bl.Value, "\"`"))
+		}
+	case "FloatValue":
+		instance.FloatValue = ExtractFloat(valueExpr)
+	case "IntValue":
+		instance.IntValue = ExtractInt(valueExpr)
+	case "Duration":
+		instance.Duration = time.Duration(ExtractInt(valueExpr))
+	case "EnumString":
+		// Handle Enum (Usually a SelectorExpr like models.Value1)
+		if sel, ok := valueExpr.(*ast.SelectorExpr); ok {
+			// In case the generated code requires FromCodeString (standard Gong pattern)
+			// We can simulate it or direct cast if the method isn't generated yet.
+			// Here we assume direct cast for simplicity or standard string extraction.
+			instance.EnumString = EnumTypeString(sel.Sel.Name)
+		} else {
+			// Fallback if it is a string literal
+			instance.EnumString = EnumTypeString(ExtractString(valueExpr))
+		}
+	case "EnumInt":
+		if sel, ok := valueExpr.(*ast.SelectorExpr); ok {
+			enumValue := sel.Sel.Name
+			var val EnumTypeInt
+			err := (&val).FromCodeString(enumValue)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			instance.EnumInt = EnumTypeInt(val)
+		}
+	case "B":
+		if ident, ok := valueExpr.(*ast.Ident); ok {
+			if val, ok := identifierMap[ident.Name]; ok {
+				instance.B = val.(*B)
+			}
+		}
+	case "Bs":
+		if call, ok := valueExpr.(*ast.CallExpr); ok {
+			if len(call.Args) >= 2 {
+				if ident, ok := call.Args[len(call.Args)-1].(*ast.Ident); ok {
+					if val, ok := identifierMap[ident.Name]; ok {
+						instance.Bs = append(instance.Bs, val.(*B))
+					}
+				}
+			}
+		}
+	}
+	return nil
+}
+
+// --- B Unmarshaller ---
+type BUnmarshaller struct{}
+
+func (u *BUnmarshaller) Initialize(stage *Stage, instanceName string, preserveOrder bool) (any, error) {
+	instance := new(B)
+	instance.Name = instanceName
+	instance.Stage(stage)
+	return instance, nil
+}
+
+func (u *BUnmarshaller) UnmarshallField(stage *Stage, i any, fieldName string, valueExpr ast.Expr, identifierMap map[string]any) error {
+	instance := i.(*B)
+	switch fieldName {
+	case "Name":
+		instance.Name = ExtractString(valueExpr)
+	}
+	return nil
+}
