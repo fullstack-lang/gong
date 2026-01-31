4fead41cd2a7a7d1a8ef98b7ff9d6feb858f4002
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2026-01-31 12:51:41 +0100

    #1075 at commit, reset hard behind commits recompute orders at each compute reference, auto

--- go/golang/models/gong_template.go
+++ go/golang/models/gong_template.go
@@ -181,7 +181,8 @@ func (stage *Stage) ApplyBackwardCommit() error {
 		return err
 	}
 
-	stage.ComputeReference()
+	stage.ComputeReferenceAndOrders()
+
 	stage.commitsBehind++
 
 	return nil
@@ -214,7 +215,8 @@ func (stage *Stage) ApplyForwardCommit() error {
 		log.Println("error during ApplyForwardCommit: ", err)
 		return err
 	}
-	stage.ComputeReference()
+	stage.ComputeReferenceAndOrders()
+
 	stage.commitsBehind--
 	return nil
 }
@@ -234,10 +236,13 @@ func (stage *Stage) ResetHard() {
 	stage.backwardCommits = stage.backwardCommits[:newCommitsLen]
 	stage.commitsBehind = 0
 	stage.navigationMode = GongNavigationModeNormal
+}
 
-	// recompute the next order for each struct
-	// this is necessary because the order might have been incremented
-	// during the commits that have been discarded
+// recomputeOrders recomputes the next order for each struct
+// this is necessary because the order might have been incremented
+// during the commits that have been discarded
+// insertion point for max order recomputation
+func (stage *Stage) recomputeOrders() {
 	// insertion point for max order recomputation {{` + string(rune(ModelGongStructResetHard)) + `}}
 }
 
@@ -254,9 +259,9 @@ func (stage *Stage) SetProbeIF(probeIF ProbeIF) {
 }
 
 func (stage *Stage) GetProbeIF() ProbeIF {
-    if stage.probeIF == nil {
-        return nil
-    }
+	if stage.probeIF == nil {
+		return nil
+	}
 
 	return stage.probeIF
 }
@@ -484,7 +489,7 @@ func (stage *Stage) Commit() {
 
 	if stage.IsInDeltaMode() {
 		stage.ComputeForwardAndBackwardCommits()
-		stage.ComputeReference()
+		stage.ComputeReferenceAndOrders()
 		if stage.GetProbeIF() != nil {
 			stage.GetProbeIF().Refresh()
 		}
@@ -545,7 +550,7 @@ func (stage *Stage) Reset() { // insertion point for array reset{{` + string(run
 		stage.GetProbeIF().ResetNotifications()
 	}
 	if stage.IsInDeltaMode() {
-		stage.ComputeReference()
+		stage.ComputeReferenceAndOrders()
 	}
 }
 
