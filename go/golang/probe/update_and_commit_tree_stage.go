1a786dbda1cf9df0c256ff25d95067c9de76e963
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2026-01-31 18:00:33 +0100

    #1075 at commit, reset hard behind commits Fixes #1075 - updated desme/project

--- go/golang/probe/update_and_commit_tree_stage.go
+++ go/golang/probe/update_and_commit_tree_stage.go
@@ -178,13 +178,17 @@ func (probe *Probe) AddCommitNavigationNode(appendChildrenNodeFunc func(models.G
 	stageOfInterest := probe.stageOfInterest
 
 	deltaNode := &tree.Node{}
-	deltaNode.Name += fmt.Sprintf("<--> %d < -%d ",
-		len(stageOfInterest.GetBackwardCommits()),
-		stageOfInterest.GetCommitsBehind())
+	deltaNode.Name += fmt.Sprintf("%d",
+		len(stageOfInterest.GetBackwardCommits()))
+	if stageOfInterest.GetCommitsBehind() > 0 {
+		deltaNode.Name += fmt.Sprintf(" (-%d)", stageOfInterest.GetCommitsBehind())
+	}
+	deltaNode.PreceedingIcon = string(gongtree_buttons.BUTTON_history)
+	deltaNode.IsWithPreceedingIcon = true
 
 	backwardButton := &tree.Button{
 		Name:            "BackwardButton",
-		Icon:            string(gongtree_buttons.BUTTON_arrow_back),
+		Icon:            string(gongtree_buttons.BUTTON_undo),
 		HasToolTip:      true,
 		ToolTipText:     "Go to previous commit",
 		ToolTipPosition: tree.Below,
@@ -203,13 +207,12 @@ func (probe *Probe) AddCommitNavigationNode(appendChildrenNodeFunc func(models.G
 
 	if stageOfInterest.GetCommitsBehind() == len(stageOfInterest.GetBackwardCommits()) {
 		backwardButton.IsDisabled = true
-		backwardButton.Icon = string(gongtree_buttons.BUTTON_do_not_disturb)
 		backwardButton.ToolTipText = "No more previous commits"
 	}
 
 	forwardButton := &tree.Button{
 		Name:            "ForwardButton",
-		Icon:            string(gongtree_buttons.BUTTON_arrow_forward),
+		Icon:            string(gongtree_buttons.BUTTON_redo),
 		HasToolTip:      true,
 		ToolTipText:     "Go to next commit",
 		ToolTipPosition: tree.Below,
@@ -228,7 +231,6 @@ func (probe *Probe) AddCommitNavigationNode(appendChildrenNodeFunc func(models.G
 
 	if stageOfInterest.GetCommitsBehind() == 0 {
 		forwardButton.IsDisabled = true
-		forwardButton.Icon = string(gongtree_buttons.BUTTON_do_not_disturb)
 		forwardButton.ToolTipText = "No more next commits"
 	}
 
