b2a4766ca3f6b9d21f6cf9d24cb6d91d59b8fe5d
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2026-02-11 08:23:37 +0100

    Basic Concurrency Access Control Fixes #1094 - new gong

--- lib/gantt/go/models/zzz_gong.go
+++ lib/gantt/go/models/zzz_gong.go
@@ -11,6 +11,7 @@ import (
 	"slices"
 	"sort"
 	"strings"
+	"sync"
 	"time"
 
 	gantt_go "github.com/fullstack-lang/gong/lib/gantt/go"
@@ -260,6 +261,8 @@ type Stage struct {
 	// navigationMode is set to Navigating
 	navigationMode gongStageNavigationMode
 	commitsBehind  int // the number of commits the stage is behind the front of the history
+
+	lock sync.RWMutex
 }
 
 type gongStageNavigationMode string
@@ -347,6 +350,22 @@ func (stage *Stage) ApplyForwardCommit() error {
 
 func (stage *Stage) GetCommitsBehind() int {
 	return stage.commitsBehind
+}
+
+func (stage *Stage) Lock() {
+	stage.lock.Lock()
+}
+
+func (stage *Stage) Unlock() {
+	stage.lock.Unlock()
+}
+
+func (stage *Stage) RLock() {
+	stage.lock.RLock()
+}
+
+func (stage *Stage) RUnlock() {
+	stage.lock.RUnlock()
 }
 
 // ResetHard removes the more recent
