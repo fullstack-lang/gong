723e7ce959b5f8912e8dccd6ef16ea62af833b0c
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2026-02-12 02:07:07 +0100

    provides a squeash to rebase the whole log. Fixes #1101 - new gong

--- test/test3/go/probe/update_and_commit_tree.go
+++ test/test3/go/probe/update_and_commit_tree.go
@@ -282,21 +282,23 @@ func (probe *Probe) AddCommitNavigationNode(appendChildrenNodeFunc func(models.G
 		}
 	}
 
-	orphansButton := &tree.Button{
-		Name:            "OrphansButton",
-		Icon:            string(gongtree_buttons.BUTTON_delete),
-		HasToolTip:      true,
-		ToolTipText:     "Discard all commits history (git orphan)",
-		ToolTipPosition: tree.Below,
-	}
-	deltaNode.Buttons = append(deltaNode.Buttons, orphansButton)
-	orphansButton.Impl = &tree.FunctionalButtonProxy{
-		OnUpdated: func(stage *tree.Stage,
-			stagedButton, frontButton *tree.Button,
-		) {
-			stageOfInterest.Orphans()
-			probe.Refresh()
-		},
+	if len(stageOfInterest.GetBackwardCommits()) > 0 {
+		orphansButton := &tree.Button{
+			Name:            "OrphansButton",
+			Icon:            string(gongtree_buttons.BUTTON_delete),
+			HasToolTip:      true,
+			ToolTipText:     "Discard all commits history (git orphan)",
+			ToolTipPosition: tree.Below,
+		}
+		deltaNode.Buttons = append(deltaNode.Buttons, orphansButton)
+		orphansButton.Impl = &tree.FunctionalButtonProxy{
+			OnUpdated: func(stage *tree.Stage,
+				stagedButton, frontButton *tree.Button,
+			) {
+				stageOfInterest.Orphans()
+				probe.Refresh()
+			},
+		}
 	}
 
 	logCommitsButton := &tree.Button{
