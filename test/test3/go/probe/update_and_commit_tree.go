1040d8e4f3375e8a7699156ccd36ed301d28d7c7
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2026-02-12 02:03:59 +0100

    provides a squeash to rebase the whole log. Fixes #1101 - manual

--- test/test3/go/probe/update_and_commit_tree.go
+++ test/test3/go/probe/update_and_commit_tree.go
@@ -282,6 +282,23 @@ func (probe *Probe) AddCommitNavigationNode(appendChildrenNodeFunc func(models.G
 		}
 	}
 
+	orphansButton := &tree.Button{
+		Name:            "OrphansButton",
+		Icon:            string(gongtree_buttons.BUTTON_delete),
+		HasToolTip:      true,
+		ToolTipText:     "Discard all commits history (git orphan)",
+		ToolTipPosition: tree.Below,
+	}
+	deltaNode.Buttons = append(deltaNode.Buttons, orphansButton)
+	orphansButton.Impl = &tree.FunctionalButtonProxy{
+		OnUpdated: func(stage *tree.Stage,
+			stagedButton, frontButton *tree.Button,
+		) {
+			stageOfInterest.Orphans()
+			probe.Refresh()
+		},
+	}
+
 	logCommitsButton := &tree.Button{
 		Name:            "LogCommitsButton",
 		Icon:            string(gongtree_buttons.BUTTON_playlist_add),
