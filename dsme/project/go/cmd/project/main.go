2a5d4d7598f4bef3f1238db5f4839ccf966d4132
Thomas PEUGEOT <tpeugeot@pil.moss>
2026-02-23 18:32:34 +0100

    project: default embedded diagrams

--- dsme/project/go/cmd/project/main.go
+++ dsme/project/go/cmd/project/main.go
@@ -13,7 +13,7 @@ var (
 	unmarshallFromCode = flag.String("unmarshallFromCode", "", "unmarshall data from go file and '.go' (must be lowercased without spaces), If unmarshallFromCode arg is '', no unmarshalling")
 	marshallOnCommit   = flag.String("marshallOnCommit", "", "on all commits, marshall staged data to a go file with the marshall name and '.go' (must be lowercased without spaces). If marshall arg is '', no marshalling")
 
-	embeddedDiagrams = flag.Bool("embeddedDiagrams", false, "parse/analysis go/models and go/embeddedDiagrams")
+	embeddedDiagrams = flag.Bool("embeddedDiagrams", true, "parse/analysis go/models and go/embeddedDiagrams")
 
 	port = flag.Int("port", 8080, "port server")
 )
