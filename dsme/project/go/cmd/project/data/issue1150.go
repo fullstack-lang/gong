836d519645b61b0e91cf49d05316c8a3ffce74ea
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2026-02-28 00:28:08 +0100

    project : undo/redo does not work (part 4) Fixes #1150 - prepare test

--- dsme/project/go/cmd/project/data/issue1150.go
+++ dsme/project/go/cmd/project/data/issue1150.go
@@ -24,58 +24,58 @@ func _(stage *models.Stage) {
 
 	// insertion point for declaration of instances to stage
 
-	__Diagram__00000000_ := (&models.Diagram{Name: `D1`}).Stage(stage)
+	__Diagram__00000005_ := (&models.Diagram{Name: `D5`}).Stage(stage)
 
-	__Project__00000000_ := (&models.Project{Name: `NewProject`}).Stage(stage)
+	__Project__00000001_ := (&models.Project{Name: `P1`}).Stage(stage)
 
-	__Resource__00000003_ := (&models.Resource{Name: `R1`}).Stage(stage)
+	__Resource__00000003_ := (&models.Resource{Name: `R3`}).Stage(stage)
 
-	__ResourceShape__00000003_ := (&models.ResourceShape{Name: `R1-D1`}).Stage(stage)
+	__ResourceShape__00000003_ := (&models.ResourceShape{Name: `R3-D1`}).Stage(stage)
 
-	__Root__00000000_ := (&models.Root{Name: `Root`}).Stage(stage)
+	__Root__00000004_ := (&models.Root{Name: `Root 4`}).Stage(stage)
 
 	// insertion point for initialization of values
 
-	__Diagram__00000000_.Name = `D1`
-	__Diagram__00000000_.IsChecked = true
-	__Diagram__00000000_.IsEditable_ = true
-	__Diagram__00000000_.ShowPrefix = true
-	__Diagram__00000000_.DefaultBoxWidth = 250.000000
-	__Diagram__00000000_.DefaultBoxHeigth = 100.000000
-	__Diagram__00000000_.Width = 2108.862056
-	__Diagram__00000000_.Height = 2476.845003
-	__Diagram__00000000_.IsExpanded = true
-	__Diagram__00000000_.ComputedPrefix = ``
-	__Diagram__00000000_.IsInRenameMode = false
-	__Diagram__00000000_.IsPBSNodeExpanded = false
-	__Diagram__00000000_.IsWBSNodeExpanded = false
-	__Diagram__00000000_.IsNotesNodeExpanded = false
-	__Diagram__00000000_.IsResourcesNodeExpanded = true
+	__Diagram__00000005_.Name = `D5`
+	__Diagram__00000005_.IsChecked = true
+	__Diagram__00000005_.IsEditable_ = true
+	__Diagram__00000005_.ShowPrefix = true
+	__Diagram__00000005_.DefaultBoxWidth = 250.000000
+	__Diagram__00000005_.DefaultBoxHeigth = 100.000000
+	__Diagram__00000005_.Width = 2108.862056
+	__Diagram__00000005_.Height = 2476.845003
+	__Diagram__00000005_.IsExpanded = true
+	__Diagram__00000005_.ComputedPrefix = ``
+	__Diagram__00000005_.IsInRenameMode = false
+	__Diagram__00000005_.IsPBSNodeExpanded = false
+	__Diagram__00000005_.IsWBSNodeExpanded = false
+	__Diagram__00000005_.IsNotesNodeExpanded = false
+	__Diagram__00000005_.IsResourcesNodeExpanded = true
 
-	__Project__00000000_.Name = `NewProject`
-	__Project__00000000_.IsExpanded = true
-	__Project__00000000_.ComputedPrefix = ``
-	__Project__00000000_.IsInRenameMode = false
+	__Project__00000001_.Name = `P1`
+	__Project__00000001_.IsExpanded = true
+	__Project__00000001_.ComputedPrefix = ``
+	__Project__00000001_.IsInRenameMode = false
 
-	__Resource__00000003_.Name = `R1`
+	__Resource__00000003_.Name = `R3`
 	__Resource__00000003_.Description = ``
 	__Resource__00000003_.IsExpanded = false
 	__Resource__00000003_.ComputedPrefix = `1`
 	__Resource__00000003_.IsInRenameMode = false
 
-	__ResourceShape__00000003_.Name = `-D1`
+	__ResourceShape__00000003_.Name = `R3-D1`
 	__ResourceShape__00000003_.IsExpanded = false
 	__ResourceShape__00000003_.X = 152.659628
 	__ResourceShape__00000003_.Y = 184.647694
 	__ResourceShape__00000003_.Width = 250.000000
 	__ResourceShape__00000003_.Height = 100.000000
 
-	__Root__00000000_.Name = `Root`
-	__Root__00000000_.NbPixPerCharacter = 8.000000
-	__Diagram__00000000_.Resource_Shapes = slices.Insert(__Diagram__00000000_.Resource_Shapes, 0, __ResourceShape__00000003_)
+	__Root__00000004_.Name = `Root 4`
+	__Root__00000004_.NbPixPerCharacter = 8.000000
+	__Diagram__00000005_.Resource_Shapes = slices.Insert(__Diagram__00000005_.Resource_Shapes, 0, __ResourceShape__00000003_)
 	stage.Commit()
 
-	__Diagram__00000000_.Resource_Shapes = slices.Delete(__Diagram__00000000_.Resource_Shapes, 0, 1)
+	__Diagram__00000005_.Resource_Shapes = slices.Delete(__Diagram__00000005_.Resource_Shapes, 0, 1)
 	__ResourceShape__00000003_.Unstage(stage)
 	stage.Commit()
 }
