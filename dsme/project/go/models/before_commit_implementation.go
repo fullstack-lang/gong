33ad4ffd9923ac578f5a4d730698c23d016b0fe0
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2026-02-27 03:40:00 +0100

    simplification

--- dsme/project/go/models/before_commit_implementation.go
+++ dsme/project/go/models/before_commit_implementation.go
@@ -6,19 +6,6 @@ type BeforeCommitImplementation struct {
 
 func (c *BeforeCommitImplementation) BeforeCommit(stage *Stage) {
 	c.stager.enforceSemantic()
-
-	var selectedDiagram *Diagram
-	for _, diagram := range GetGongstrucsSorted[*Diagram](stage) {
-		if diagram.IsEditable() {
-			selectedDiagram = diagram
-			break
-		}
-	}
-	if selectedDiagram != nil {
-		TaskOutputShapes := selectedDiagram.TaskOutputShapes
-		_ = TaskOutputShapes
-	}
-
 	c.stager.tree()
 	c.stager.svg()
 	c.stager.button()
