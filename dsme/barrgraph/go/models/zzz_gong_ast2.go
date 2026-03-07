20d36f792dc3fa3d758fd8cf54c38be7a12c3fb8
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2026-03-07 11:08:49 +0100

    barrgraph : WIP

--- dsme/barrgraph/go/models/zzz_gong_ast2.go
+++ dsme/barrgraph/go/models/zzz_gong_ast2.go
@@ -719,8 +719,8 @@ func (u *DiagramUnmarshaller) UnmarshallField(stage *Stage, i GongstructIF, fiel
 				}
 			}
 		}
-	case "BetweenDatesInterval":
-		instance.BetweenDatesInterval = time.Duration(GongExtractInt(valueExpr))
+	case "NbYearsForIntervals":
+		instance.NbYearsForIntervals = GongExtractInt(valueExpr)
 	case "XMargin":
 		instance.XMargin = GongExtractFloat(valueExpr)
 	case "YMargin":
