94739f0461f83d178b595b908f66345a96d1f03e
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2026-03-19 18:36:13 +0100

    project : organize objects in hierarchical libraries (instead of non hierarchical projects) Fixes #1133 - on the way to rigourous insert/delete and hide / show

--- dsme/project/go/models/stager_ux_tree_pbs.go
+++ dsme/project/go/models/stager_ux_tree_pbs.go
@@ -10,23 +10,15 @@ func (stager *Stager) treePBSRecusriveInDiagram(diagram *Diagram, product *Produ
 		diagram,
 		parentNode,
 		product,
+		product.parentProduct,
 		&diagram.ProductsWhoseNodeIsExpanded,
 		&diagram.Product_Shapes,
 		&diagram.map_Product_ProductShape,
-	)
-
-	addAddItemButton(stager, &diagram.ProductsWhoseNodeIsExpanded, product, nil, productNode, &product.SubProducts, diagram, &diagram.Product_Shapes, &diagram.ProductComposition_Shapes)
-
-	// if product has a parent product, add a button to show/hide the link to the parent
-	addShowHideCompositionButton(
-		stager,
-		product,
-		product.parentProduct,
-		productNode,
-		diagram.map_Product_ProductShape,
 		diagram.map_Product_ProductCompositionShape,
 		&diagram.ProductComposition_Shapes,
 	)
+
+	addAddItemButton(stager, &diagram.ProductsWhoseNodeIsExpanded, product, nil, productNode, &product.SubProducts, diagram, &diagram.Product_Shapes, &diagram.ProductComposition_Shapes)
 
 	for _, product := range product.SubProducts {
 		stager.treePBSRecusriveInDiagram(diagram, product, productNode)
