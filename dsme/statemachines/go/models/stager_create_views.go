889cad0d01c12da4092cbe19fa0ed4ae81924079
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2026-03-21 03:25:15 +0100

    statemachines : allow to hide a state in a diagram Fixes #1168 - WIP

---
+++ dsme/statemachines/go/models/stager_create_views.go
@@ -0,0 +1,316 @@
+package models
+
+import (
+	split "github.com/fullstack-lang/gong/lib/split/go/models"
+)
+
+func (stager *Stager) create_views() {
+	split.StageBranch(stager.splitStage, &split.View{
+		Name:           "Edit view",
+		IsSelectedView: true,
+		RootAsSplitAreas: []*split.AsSplitArea{
+			{
+				Name:             "Main view area",
+				Size:             100,
+				ShowNameInHeader: false,
+				AsSplit: &split.AsSplit{
+					Direction: split.Vertical,
+					AsSplitAreas: []*split.AsSplitArea{
+						{
+							Name:             "Main view area",
+							Size:             100,
+							ShowNameInHeader: false,
+							AsSplit: &split.AsSplit{
+								Direction: split.Horizontal,
+								AsSplitAreas: []*split.AsSplitArea{
+									// Sidebar
+									{
+										Name:             "Sidebar with both trees",
+										ShowNameInHeader: false,
+										Size:             25,
+										AsSplit: &split.AsSplit{
+											Name:      "as split",
+											Direction: split.Vertical,
+											AsSplitAreas: []*split.AsSplitArea{
+												{
+													Name:             "Top",
+													Size:             100,
+													ShowNameInHeader: false,
+
+													AsSplit: &split.AsSplit{
+														Direction: split.Vertical,
+														AsSplitAreas: []*split.AsSplitArea{
+															{
+																Name:             "Model tree",
+																Size:             90,
+																ShowNameInHeader: false,
+																Tree: &split.Tree{
+																	StackName: stager.treeDiagramStage.GetName(),
+																},
+															},
+
+															//
+															{
+																Name:             "Export XL button",
+																Size:             10,
+																ShowNameInHeader: false,
+																Button: &split.Button{
+																	StackName: stager.buttonExportXLStage.GetName(),
+																},
+															},
+															//
+														}},
+												},
+											},
+										},
+									},
+
+									// SVG area
+									{
+										Name:             "svg diagram",
+										ShowNameInHeader: false,
+										Size:             55,
+										Svg: &split.Svg{
+											StackName: stager.svgStage.GetName(),
+										},
+									},
+									{
+										Name:             "form",
+										ShowNameInHeader: false,
+										Size:             20,
+										Form: &split.Form{
+											StackName: stager.probeForm.GetFormStage().GetName(),
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	})
+
+	split.StageBranch(stager.splitStage, &split.View{
+		Name: "Model view",
+		RootAsSplitAreas: []*split.AsSplitArea{
+			{
+				Name:             "Main view area",
+				Size:             100,
+				ShowNameInHeader: false,
+				AsSplit: &split.AsSplit{
+					Direction: split.Vertical,
+					AsSplitAreas: []*split.AsSplitArea{
+						{
+							Name:             "Main view area",
+							Size:             50,
+							ShowNameInHeader: false,
+							AsSplit: &split.AsSplit{
+								Direction: split.Horizontal,
+								AsSplitAreas: []*split.AsSplitArea{
+									// Sidebar
+									{
+										Name:             "Sidebar with both trees",
+										ShowNameInHeader: false,
+										Size:             25,
+										AsSplit: &split.AsSplit{
+											Name:      "as split",
+											Direction: split.Vertical,
+											AsSplitAreas: []*split.AsSplitArea{
+												{
+													Name:             "Top",
+													Size:             100,
+													ShowNameInHeader: false,
+
+													AsSplit: &split.AsSplit{
+														Direction: split.Vertical,
+														AsSplitAreas: []*split.AsSplitArea{
+															{
+																Name:             "Model tree",
+																Size:             90,
+																ShowNameInHeader: false,
+																Tree: &split.Tree{
+																	StackName: stager.treeDiagramStage.GetName(),
+																},
+															},
+
+															//
+															{
+																Name:             "Export XL button",
+																Size:             10,
+																ShowNameInHeader: false,
+																Button: &split.Button{
+																	StackName: stager.buttonExportXLStage.GetName(),
+																},
+															},
+															//
+														}},
+												},
+											},
+										},
+									},
+
+									// SVG area
+									{
+										Name:             "svg diagram",
+										ShowNameInHeader: false,
+										Size:             75,
+										Svg: &split.Svg{
+											StackName: stager.svgStage.GetName(),
+										},
+									},
+								},
+							},
+						},
+						{
+							Size:    50,
+							AsSplit: stager.probeForm.GetDataEditor(),
+						},
+					},
+				},
+			},
+		},
+	})
+
+	split.StageBranch(stager.splitStage, &split.View{
+		Name: "Main view",
+		RootAsSplitAreas: []*split.AsSplitArea{
+			{
+				Name:             "Main view area",
+				Size:             100,
+				ShowNameInHeader: false,
+				AsSplit: &split.AsSplit{
+					Direction: split.Horizontal,
+					AsSplitAreas: []*split.AsSplitArea{
+						{
+							Name:             "Sidebar with both trees",
+							ShowNameInHeader: false,
+							Size:             25,
+							AsSplit: &split.AsSplit{
+								Name:      "as split",
+								Direction: split.Vertical,
+								AsSplitAreas: []*split.AsSplitArea{
+									{
+										Name:             "Top",
+										Size:             50,
+										ShowNameInHeader: false,
+										Tree: &split.Tree{
+											StackName: stager.treeDiagramStage.GetName(),
+										},
+									},
+									{
+										Name:             "Bottom",
+										Size:             50,
+										ShowNameInHeader: false,
+										Tree: &split.Tree{
+											StackName: stager.treeObjectsSimulationStage.GetName(),
+										},
+									},
+								},
+							},
+						},
+						{
+							Name:             "Actions",
+							ShowNameInHeader: false,
+							Size:             20,
+							Button: &split.Button{
+								StackName: stager.buttonTransitionsStage.GetName(),
+							},
+						},
+						{
+							Name:             "svg diagram",
+							ShowNameInHeader: false,
+							Size:             55,
+							Svg: &split.Svg{
+								StackName: stager.svgStage.GetName(),
+							},
+						},
+					},
+				},
+			},
+		},
+	})
+
+	split.StageBranch(stager.splitStage, &split.View{
+		Name: "Sim. view",
+		RootAsSplitAreas: []*split.AsSplitArea{
+			{
+				Name:             "Main view area",
+				Size:             100,
+				ShowNameInHeader: false,
+				AsSplit: &split.AsSplit{
+					Direction: split.Horizontal,
+					AsSplitAreas: []*split.AsSplitArea{
+						{
+							Name:             "Actions",
+							ShowNameInHeader: false,
+							Size:             20,
+							Button: &split.Button{
+								StackName: stager.buttonTransitionsStage.GetName(),
+							},
+						},
+						{
+							Name:             "svg diagram",
+							ShowNameInHeader: false,
+							Size:             80,
+							Svg: &split.Svg{
+								StackName: stager.svgStage.GetName(),
+							},
+						},
+					},
+				},
+			},
+		},
+	})
+
+	split.StageBranch(stager.splitStage, &split.View{
+		Name: "Data Probe & Data Model",
+		RootAsSplitAreas: []*split.AsSplitArea{
+			{
+				Split: &split.Split{
+					StackName: stager.stage.GetProbeSplitStageName(),
+				},
+			},
+		},
+	})
+
+	split.StageBranch(stager.splitStage, &split.View{
+		Name: "Data Probe",
+		RootAsSplitAreas: []*split.AsSplitArea{
+			{
+				AsSplit: stager.probeForm.GetDataEditor(),
+			},
+		},
+	})
+
+	split.StageBranch(stager.splitStage, &split.View{
+		Name: "Data Model",
+		RootAsSplitAreas: []*split.AsSplitArea{
+			stager.probeForm.GetDiagramEditor(),
+		},
+	})
+
+	split.StageBranch(stager.splitStage, &split.View{
+		Name: "svg Probe",
+		RootAsSplitAreas: []*split.AsSplitArea{
+			{
+				Split: &split.Split{
+					StackName: stager.svgStage.GetProbeSplitStageName(),
+				},
+			},
+		},
+	})
+
+	split.StageBranch(stager.splitStage, &split.View{
+		Name: "Diagram Tree Probe",
+		RootAsSplitAreas: []*split.AsSplitArea{
+			{
+				Split: &split.Split{
+					StackName: stager.treeDiagramStage.GetProbeSplitStageName(),
+				},
+			},
+		},
+	})
+
+	stager.splitStage.Commit()
+}
