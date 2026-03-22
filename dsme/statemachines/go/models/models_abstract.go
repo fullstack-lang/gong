21f5befc73437af104452323e15fdaa59f42400e
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2026-03-22 16:34:11 +0100

    state machines

---
+++ dsme/statemachines/go/models/models_abstract.go
@@ -0,0 +1,190 @@
+package models
+
+import (
+	"time"
+)
+
+type Architecture struct {
+	Name string
+
+	StateMachines []*StateMachine
+	Roles         []*Role
+
+	NbPixPerCharacter float64
+}
+
+type Diagram struct {
+	Name           string
+	IsChecked      bool
+	IsExpanded     bool
+	IsEditable_    bool
+	IsInRenameMode bool
+
+	State_Shapes      []*StateShape
+	Transition_Shapes []*Transition_Shape
+}
+
+func (d *Diagram) IsEditable() bool {
+	return d.IsEditable_
+}
+
+type Message struct {
+	Name       string
+	IsSelected bool
+
+	MessageType *MessageType
+
+	OriginTransition *Transition
+}
+
+type MessageType struct {
+	Name string
+
+	//gong:text
+	//gong:width 600 gong:height 300
+	Description string
+}
+
+type Object struct {
+	Name       string
+	State      *State
+	IsSelected bool
+	Rank       int
+
+	DOF time.Time
+
+	Proxy *Object_Tree_Proxy
+
+	Messages []*Message
+}
+
+type Role struct {
+	Name string
+
+	// "U" for Unit, "S" for supervisor
+	Acronym string
+
+	// Role that inherit the access right of this Role
+	RolesWithSamePermissions []*Role
+}
+
+type State struct {
+	//gong:width 600
+	Name string
+
+	Parent *State
+
+	IsDecisionNode bool
+
+	IsFictif bool
+
+	IsEndState bool
+
+	// When there are SubStates, the State is composite
+	SubStates []*State
+
+	// Diagrams where a state is present is exported
+	// in the XL file
+	Diagrams []*Diagram
+
+	Entry      *Action
+	Activities []*Activities
+	Exit       *Action
+}
+
+func (state *State) IsComposite() bool {
+	return len(state.SubStates) > 0
+}
+
+// Activities action
+type Activities struct {
+	Name        string
+	Criticality Criticality
+}
+
+type Criticality string
+
+const (
+	CriticalityCritical Criticality = "Critical"
+	CriticalityDefault  Criticality = "Default"
+)
+
+type Action struct {
+	Name        string
+	Criticality Criticality
+}
+
+type StateMachine struct {
+	Name string
+
+	IsNodeExpanded bool
+
+	States   []*State
+	Diagrams []*Diagram
+
+	InitialState *State
+}
+
+// Transition decribes authorized between states
+type Transition struct {
+	//gong:width 600
+	Name string
+
+	//gong:width 600
+	Start *State
+
+	//gong:width 600
+	End *State
+
+	// For Role Base Access Control
+	RolesWithPermissions []*Role
+
+	GeneratedMessages []*MessageType
+
+	Guard *Guard
+
+	// Diagram dans lequel la transition est présente
+	Diagrams []*Diagram
+}
+
+type Guard struct {
+	Name string
+}
+
+func (transition *Transition) OnAfterUpdate(stage *Stage, stagedTransition, frontTransition *Transition) {
+
+	transition.performTransition(stage)
+}
+
+func (transition *Transition) performTransition(stage *Stage) {
+
+	objectSet := *GetGongstructInstancesSet[Object](stage)
+	for object := range objectSet {
+		if object.IsSelected {
+
+			object.State = transition.End
+
+			var messageTypeGenere *MessageType
+			for _, messageTypeGenere_ := range transition.GeneratedMessages {
+				messageTypeGenere = messageTypeGenere_
+			}
+
+			if messageTypeGenere != nil {
+				message := new(Message).Stage(stage)
+
+				message.Name = time.Now().Format("15:04:05") + " " + transition.Name + " -> " + messageTypeGenere.Name
+				message.MessageType = messageTypeGenere
+				message.OriginTransition = transition
+				object.Messages = append(object.Messages, message)
+
+				// set new message to selected
+				for message_ := range *GetGongstructInstancesSet[Message](stage) {
+					message_.IsSelected = false
+				}
+				message.IsSelected = true
+			}
+
+			object.Proxy.stager.stage.Commit()
+		}
+	}
+}
