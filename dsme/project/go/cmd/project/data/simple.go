728a56038f447d09f99b0d4f5f5926ce6f8d54e6
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2026-03-22 16:16:09 +0100

    Libraries as hierarchical object Fixes #1177 - toward DSME

--- dsme/project/go/cmd/project/data/simple.go
+++ dsme/project/go/cmd/project/data/simple.go
@@ -27,26 +27,34 @@ func _(stage *models.Stage) {
 
 	__Library__00000000_ := (&models.Library{Name: ``}).Stage(stage)
 
+	__Note__00000000_ := (&models.Note{Name: ``}).Stage(stage)
+
+	__NoteShape__00000000_ := (&models.NoteShape{Name: `-Default Diagram`}).Stage(stage)
+
 	__Product__00000000_ := (&models.Product{Name: `G3`}).Stage(stage)
+	__Product__00000001_ := (&models.Product{Name: `G3.1`}).Stage(stage)
+
+	__ProductCompositionShape__00000000_ := (&models.ProductCompositionShape{Name: `G3 to `}).Stage(stage)
 
 	__ProductShape__00000000_ := (&models.ProductShape{Name: `-Default Diagram`}).Stage(stage)
+	__ProductShape__00000001_ := (&models.ProductShape{Name: `-Default Diagram`}).Stage(stage)
 
 	// insertion point for initialization of values
 
 	__Diagram__00000000_.Name = `Default Diagram`
-	__Diagram__00000000_.ComputedPrefix = ``
+	__Diagram__00000000_.ComputedPrefix = `1`
 	__Diagram__00000000_.IsInRenameMode = false
 	__Diagram__00000000_.IsExpanded = true
-	__Diagram__00000000_.IsChecked = false
+	__Diagram__00000000_.IsChecked = true
 	__Diagram__00000000_.IsEditable_ = true
 	__Diagram__00000000_.ShowPrefix = false
 	__Diagram__00000000_.DefaultBoxWidth = 250.000000
 	__Diagram__00000000_.DefaultBoxHeigth = 70.000000
-	__Diagram__00000000_.Width = 4600.000000
-	__Diagram__00000000_.Height = 4600.000000
+	__Diagram__00000000_.Width = 6100.000000
+	__Diagram__00000000_.Height = 6100.000000
 	__Diagram__00000000_.IsPBSNodeExpanded = true
 	__Diagram__00000000_.IsWBSNodeExpanded = false
-	__Diagram__00000000_.IsNotesNodeExpanded = false
+	__Diagram__00000000_.IsNotesNodeExpanded = true
 	__Diagram__00000000_.IsResourcesNodeExpanded = false
 
 	__Library__00000000_.Name = ``
@@ -55,13 +63,42 @@ func _(stage *models.Stage) {
 	__Library__00000000_.IsExpanded = true
 	__Library__00000000_.NbPixPerCharacter = 8.000000
 
+	__Note__00000000_.Name = ``
+	__Note__00000000_.ComputedPrefix = `1`
+	__Note__00000000_.IsInRenameMode = false
+	__Note__00000000_.IsExpanded = false
+
+	__NoteShape__00000000_.Name = `-Default Diagram`
+	__NoteShape__00000000_.IsExpanded = false
+	__NoteShape__00000000_.X = 555.078303
+	__NoteShape__00000000_.Y = 266.001756
+	__NoteShape__00000000_.Width = 250.000000
+	__NoteShape__00000000_.Height = 70.000000
+	__NoteShape__00000000_.IsHidden = false
+
 	__Product__00000000_.Name = `G3`
-	__Product__00000000_.ComputedPrefix = ``
+	__Product__00000000_.ComputedPrefix = `1`
 	__Product__00000000_.IsInRenameMode = false
 	__Product__00000000_.IsExpanded = false
 	__Product__00000000_.Description = ``
 	__Product__00000000_.IsProducersNodeExpanded = false
 	__Product__00000000_.IsConsumersNodeExpanded = false
+
+	__Product__00000001_.Name = `G3.1`
+	__Product__00000001_.ComputedPrefix = `1.1`
+	__Product__00000001_.IsInRenameMode = false
+	__Product__00000001_.IsExpanded = false
+	__Product__00000001_.Description = ``
+	__Product__00000001_.IsProducersNodeExpanded = false
+	__Product__00000001_.IsConsumersNodeExpanded = false
+
+	__ProductCompositionShape__00000000_.Name = `G3 to `
+	__ProductCompositionShape__00000000_.StartRatio = 0.500000
+	__ProductCompositionShape__00000000_.EndRatio = 0.500000
+	__ProductCompositionShape__00000000_.StartOrientation = models.ORIENTATION_VERTICAL
+	__ProductCompositionShape__00000000_.EndOrientation = models.ORIENTATION_VERTICAL
+	__ProductCompositionShape__00000000_.CornerOffsetRatio = 1.680000
+	__ProductCompositionShape__00000000_.IsHidden = false
 
 	__ProductShape__00000000_.Name = `-Default Diagram`
 	__ProductShape__00000000_.IsExpanded = false
@@ -71,9 +108,26 @@ func _(stage *models.Stage) {
 	__ProductShape__00000000_.Height = 70.000000
 	__ProductShape__00000000_.IsHidden = false
 
+	__ProductShape__00000001_.Name = `-Default Diagram`
+	__ProductShape__00000001_.IsExpanded = false
+	__ProductShape__00000001_.X = 159.729721
+	__ProductShape__00000001_.Y = 252.660089
+	__ProductShape__00000001_.Width = 250.000000
+	__ProductShape__00000001_.Height = 70.000000
+	__ProductShape__00000001_.IsHidden = false
+
 	// insertion point for setup of pointers
 	__Diagram__00000000_.Product_Shapes = append(__Diagram__00000000_.Product_Shapes, __ProductShape__00000000_)
+	__Diagram__00000000_.Product_Shapes = append(__Diagram__00000000_.Product_Shapes, __ProductShape__00000001_)
+	__Diagram__00000000_.ProductsWhoseNodeIsExpanded = append(__Diagram__00000000_.ProductsWhoseNodeIsExpanded, __Product__00000000_)
+	__Diagram__00000000_.ProductComposition_Shapes = append(__Diagram__00000000_.ProductComposition_Shapes, __ProductCompositionShape__00000000_)
+	__Diagram__00000000_.Note_Shapes = append(__Diagram__00000000_.Note_Shapes, __NoteShape__00000000_)
 	__Library__00000000_.RootProducts = append(__Library__00000000_.RootProducts, __Product__00000000_)
+	__Library__00000000_.Notes = append(__Library__00000000_.Notes, __Note__00000000_)
 	__Library__00000000_.Diagrams = append(__Library__00000000_.Diagrams, __Diagram__00000000_)
+	__NoteShape__00000000_.Note = __Note__00000000_
+	__Product__00000000_.SubProducts = append(__Product__00000000_.SubProducts, __Product__00000001_)
+	__ProductCompositionShape__00000000_.Product = __Product__00000001_
 	__ProductShape__00000000_.Product = __Product__00000000_
+	__ProductShape__00000001_.Product = __Product__00000001_
 }
