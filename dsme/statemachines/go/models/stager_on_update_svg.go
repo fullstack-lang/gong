724c7191d66899228c928ed9a1ba0ddda9b9bac4
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2026-03-24 04:13:16 +0100

    alignment of statemachines on DSME

--- dsme/statemachines/go/models/stager_on_update_svg.go
+++ dsme/statemachines/go/models/stager_on_update_svg.go
@@ -6,44 +6,52 @@ import (
 	svg "github.com/fullstack-lang/gong/lib/svg/go/models"
 )
 
-func (stager *Stager) onUpdateSVG(updatedSVG *svg.SVG) {
+func (stager *Stager) onUpdateSVG(frontSVG *svg.SVG) {
 	// log.Println("SVG updated", updatedSVG.GetName())
+	diagram := stager.diagram
+	svgObject := stager.svgObject
 
-	// if the user is link two actor state shapes, get them
-	if updatedSVG.DrawingState == svg.DRAWING_LINK {
-		startStateShape, ok := stager.map_SvgRect_StateShape[updatedSVG.StartRect]
-		if !ok {
-			log.Panic(updatedSVG.StartRect.Name + "unknow actor state shape")
-		}
-
-		endStateShape, ok := stager.map_SvgRect_StateShape[updatedSVG.EndRect]
-		if !ok {
-			log.Panic(updatedSVG.StartRect.Name + "unknow actor state shape")
-		}
-
-		transition := new(Transition).Stage(stager.stage)
-		transition.Name = startStateShape.State.Name + " to " + endStateShape.State.Name
-		transition.Start = startStateShape.State
-		transition.End = endStateShape.State
-
-		transitionShape := new(Transition_Shape).Stage(stager.stage)
-		transitionShape.Name = startStateShape.State.Name + " to " + endStateShape.State.Name
-		transitionShape.Transition = transition
-		transitionShape.StartOrientation = ORIENTATION_HORIZONTAL
-		transitionShape.EndOrientation = ORIENTATION_HORIZONTAL
-		transitionShape.CornerOffsetRatio = 1.2
-		transitionShape.StartRatio = 0.5
-		transitionShape.EndRatio = 0.5
-
-		stager.currentDiagram.Transition_Shapes =
-			append(stager.currentDiagram.Transition_Shapes, transitionShape)
-
-		stager.probeForm.FillUpFormFromGongstruct(transition, "Transition")
-
-		// commit to encode the result, this will generate a new SVG generation
-		stager.stage.Commit()
-	} else {
+	if svgObject.DrawingState == frontSVG.DrawingState {
 		// in any cases, have the form editor set up with the instance
-		stager.probeForm.FillUpFormFromGongstruct(stager.currentDiagram, "Diagram")
+		stager.probeForm.FillUpFormFromGongstruct(diagram, "Diagram")
+		return
 	}
+
+	// IMPORTANT : we are only interested when the updateSVG has finished drawing the connexion
+	if frontSVG.DrawingState == svg.DRAWING_LINK {
+		return
+	}
+
+	startStateShape, ok := stager.map_SvgRect_StateShape[frontSVG.StartRect]
+	if !ok {
+		log.Panic(frontSVG.StartRect.Name + "unknow actor state shape")
+	}
+
+	endStateShape, ok := stager.map_SvgRect_StateShape[frontSVG.EndRect]
+	if !ok {
+		log.Panic(frontSVG.StartRect.Name + "unknow actor state shape")
+	}
+
+	transition := new(Transition).Stage(stager.stage)
+	transition.Name = startStateShape.State.Name + " to " + endStateShape.State.Name
+	transition.Start = startStateShape.State
+	transition.End = endStateShape.State
+
+	transitionShape := new(Transition_Shape).Stage(stager.stage)
+	transitionShape.Name = startStateShape.State.Name + " to " + endStateShape.State.Name
+	transitionShape.Transition = transition
+	transitionShape.StartOrientation = ORIENTATION_HORIZONTAL
+	transitionShape.EndOrientation = ORIENTATION_HORIZONTAL
+	transitionShape.CornerOffsetRatio = 1.2
+	transitionShape.StartRatio = 0.5
+	transitionShape.EndRatio = 0.5
+
+	stager.diagram.Transition_Shapes =
+		append(stager.diagram.Transition_Shapes, transitionShape)
+
+	stager.probeForm.FillUpFormFromGongstruct(transition, "Transition")
+
+	// commit to encode the result, this will generate a new SVG generation
+	stager.stage.Commit()
+
 }
