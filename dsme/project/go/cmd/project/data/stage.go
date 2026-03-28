946ab869182fd0f95a19120bcf3e7a79e67b700c
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2026-03-28 19:17:39 +0100

    dancing with diff

---
+++ dsme/project/go/cmd/project/data/stage.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"slices"
+	"time"
+
+	"github.com/fullstack-lang/gong/dsme/project/go/models"
+	// injection point for ident package import declaration{{ImportPackageDeclaration}}
+)
+
+// generated in order to avoid error in the package import
+// if there are no elements in the stage to marshall
+var _ time.Time
+var _ = slices.Index[[]int, int]
+
+// _ point for meta package dummy declaration{{ImportPackageDummyDeclaration}}
+
+// When parsed, those maps will help with the renaming process
+var _ map[string]any = map[string]any{
+	// injection point for docLink to identifiers{{EntriesDocLinkStringDocLinkIdentifier}}
+}
+
+// function will stage objects
+func _(stage *models.Stage) {
+
+	// insertion point for declaration of instances to stage
+
+	__Diagram__00000000_ := (&models.Diagram{Name: `Default Diagram`}).Stage(stage)
+
+	__Library__00000000_ := (&models.Library{Name: ``}).Stage(stage)
+
+	// insertion point for initialization of values
+
+	__Diagram__00000000_.Name = `Default Diagram`
+	__Diagram__00000000_.ComputedPrefix = `1`
+	__Diagram__00000000_.IsInRenameMode = false
+	__Diagram__00000000_.IsExpanded = true
+	__Diagram__00000000_.IsChecked = true
+	__Diagram__00000000_.IsEditable_ = true
+	__Diagram__00000000_.ShowPrefix = false
+	__Diagram__00000000_.DefaultBoxWidth = 250.000000
+	__Diagram__00000000_.DefaultBoxHeigth = 70.000000
+	__Diagram__00000000_.Width = 300.000000
+	__Diagram__00000000_.Height = 300.000000
+	__Diagram__00000000_.IsPBSNodeExpanded = false
+	__Diagram__00000000_.IsWBSNodeExpanded = false
+	__Diagram__00000000_.IsNotesNodeExpanded = false
+	__Diagram__00000000_.IsResourcesNodeExpanded = false
+
+	__Library__00000000_.Name = ``
+	__Library__00000000_.ComputedPrefix = ``
+	__Library__00000000_.IsInRenameMode = false
+	__Library__00000000_.IsExpanded = false
+	__Library__00000000_.NbPixPerCharacter = 8.000000
+
+	// insertion point for setup of pointers
+	__Library__00000000_.Diagrams = append(__Library__00000000_.Diagrams, __Diagram__00000000_)
+
+	stage.Commit()
+
+	// 
+	__Library__00000000_.IsExpanded = true
+	stage.Commit()
+}
