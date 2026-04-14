b9fb183cc5454b86f427cd10fcb7a4546f4579af
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2026-04-14 07:32:33 +0200

    Update Form2 with Form features Fixes #1237

--- lib/form/go/cmd/form/main.go
+++ lib/form/go/cmd/form/main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"strconv"
 
 	// insertion point for models import{{modelsImportDirective}}
 	form_stack "github.com/fullstack-lang/gong/lib/form/go/stack"
 	form_static "github.com/fullstack-lang/gong/lib/form/go/static"
+	"github.com/gin-gonic/gin"
 
 	// form_models "github.com/fullstack-lang/gong/lib/form/go/models"
 
 	split "github.com/fullstack-lang/gong/lib/split/go/models"
 	split_stack "github.com/fullstack-lang/gong/lib/split/go/stack"
+
+	table_fullstack "github.com/fullstack-lang/gong/lib/table/go/fullstack"
+	table "github.com/fullstack-lang/gong/lib/table/go/models"
+	table_models "github.com/fullstack-lang/gong/lib/table/go/models"
 )
 
 var (
@@ -79,5 +85,152 @@ func main() {
 	err := r.Run(":" + strconv.Itoa(*port))
 	if err != nil {
 		log.Fatalln(err.Error())
+	}
+}
+
+type OnAssocEditon struct {
+	r           *gin.Engine
+	sourceStack *table.Stage
+}
+
+// OnSave implements models.FormEditAssocButtonInterface.
+func (onAssocEditon *OnAssocEditon) OnSave() {
+	panic("unimplemented")
+}
+
+func (onAssocEditon *OnAssocEditon) OnButtonPressed() {
+	// create a new with the name append "-table"
+	newStage, _ := table_fullstack.NewStackInstance(
+		onAssocEditon.r,
+		onAssocEditon.sourceStack.GetName()+
+			string(table.StackNamePostFixForTableForAssociation))
+
+	fillUpSelectTableDummyStuff(newStage, string(table.TableSelectExtraName))
+	newStage.Commit()
+}
+
+type OnSortEditon struct {
+	r           *gin.Engine
+	sourceStack *table.Stage
+}
+
+func (onSortEditon *OnSortEditon) OnButtonPressed() {
+
+	newStage, _ := table_fullstack.NewStackInstance(
+		onSortEditon.r,
+		onSortEditon.sourceStack.GetName()+
+			string(table.StackNamePostFixForTableForAssociationSorting))
+
+	fillUpSortTableDummyStuff(newStage, string(table.TableSortExtraName))
+	newStage.Commit()
+}
+
+func fillUpSortTableDummyStuff(stage *table_models.Stage, tableName string) {
+	nbRows := 10
+	nbColumns := 1
+	table := new(table_models.Table).Stage(stage)
+	table.Name = tableName
+	table.CanDragDropRows = true
+	table.HasCloseButton = true
+
+	for j := 0; j < nbColumns; j++ {
+		column := new(table_models.DisplayedColumn).Stage(stage)
+		column.Name = fmt.Sprintf("Column %d", j)
+
+		table.DisplayedColumns = append(table.DisplayedColumns, column)
+	}
+
+	for i := 0; i < nbRows; i++ {
+		row := new(table_models.Row).Stage(stage)
+		row.Name = fmt.Sprintf("Row %d", i)
+		table.Rows = append(table.Rows, row)
+
+		for j := 0; j < nbColumns; j++ {
+			cell := new(table_models.Cell).Stage(stage)
+			cell.Name = fmt.Sprintf("Row %d - Column %d", i, j)
+
+			cellString := new(table_models.CellString).Stage(stage)
+			cellString.Name = cell.Name
+			cellString.Value = cell.Name
+			cell.CellString = cellString
+
+			row.Cells = append(row.Cells, cell)
+		}
+	}
+}
+
+func fillUpSelectTableDummyStuff(stage *table_models.Stage, tableName string) {
+	nbRows := 10
+	nbColumns := 1
+	table := new(table_models.Table).Stage(stage)
+	table.Name = tableName
+	table.HasColumnSorting = true
+	table.HasFiltering = true
+	table.HasPaginator = false
+	table.HasCheckableRows = true
+	table.HasSaveButton = true
+
+	for j := 0; j < nbColumns; j++ {
+		column := new(table_models.DisplayedColumn).Stage(stage)
+		column.Name = fmt.Sprintf("Column %d", j)
+		table.DisplayedColumns = append(table.DisplayedColumns, column)
+	}
+
+	for i := 0; i < nbRows; i++ {
+		row := new(table_models.Row).Stage(stage)
+		row.Name = fmt.Sprintf("Row %d", i)
+		table.Rows = append(table.Rows, row)
+
+		if i%2 == 0 {
+			row.IsChecked = true
+		}
+
+		for j := 0; j < nbColumns; j++ {
+			cell := new(table_models.Cell).Stage(stage)
+			cell.Name = fmt.Sprintf("Row %d - Column %d", i, j)
+
+			cellString := new(table_models.CellString).Stage(stage)
+			cellString.Name = cell.Name
+			cellString.Value = cell.Name
+			cell.CellString = cellString
+
+			row.Cells = append(row.Cells, cell)
+		}
+	}
+}
+
+func fillUpTableWithDummyStuff(stage *table_models.Stage, tableName string) {
+	nbRows := 200
+	nbColumns := 10
+	table := new(table_models.Table).Stage(stage)
+	table.Name = tableName
+	table.HasColumnSorting = true
+	table.HasFiltering = true
+	table.HasPaginator = true
+
+	table.NbOfStickyColumns = 2
+
+	for j := 0; j < nbColumns; j++ {
+		column := new(table_models.DisplayedColumn).Stage(stage)
+		column.Name = fmt.Sprintf("Column %d", j)
+		table.DisplayedColumns = append(table.DisplayedColumns, column)
+	}
+
+	for i := 0; i < nbRows; i++ {
+		row := new(table_models.Row).Stage(stage)
+		row.Name = fmt.Sprintf("Row %d", i)
+		table.Rows = append(table.Rows, row)
+
+		for j := 0; j < nbColumns; j++ {
+			cell := new(table_models.Cell).Stage(stage)
+			cell.Name = fmt.Sprintf("Row %d - Column %d", i, j)
+
+			cellString := new(table_models.CellString).Stage(stage)
+			cellString.Name = cell.Name
+			cellString.Value = cell.Name
+			cell.CellString = cellString
+
+			row.Cells = append(row.Cells, cell)
+		}
 	}
 }
