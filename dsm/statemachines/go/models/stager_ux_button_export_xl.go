2291d9110d8fe9905909924336fc6ec1cbac9172
Thomas PEUGEOT <tpeugeot@pil.moss>
2026-04-24 13:21:42 +0200

    statemachines: XL file download

--- dsm/statemachines/go/models/stager_ux_button_export_xl.go
+++ dsm/statemachines/go/models/stager_ux_button_export_xl.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"encoding/base64"
 	"fmt"
 	"log"
 	"os"
@@ -8,6 +9,8 @@ import (
 
 	button "github.com/fullstack-lang/gong/lib/button/go/models"
 	buttons "github.com/fullstack-lang/gong/lib/tree/go/buttons"
+
+	load "github.com/fullstack-lang/gong/lib/load/go/models"
 )
 
 func (stager *Stager) buttonsExportXL() {
@@ -57,9 +60,48 @@ type ExportXLButtonProxy struct {
 // Updated implements models.ButtonProxyInterface.
 func (proxy *ExportXLButtonProxy) Updated() {
 
-	filename := fmt.Sprintf("./SC-DOC-7300-0016-MOSS-%s.xlsx", time.Now().Local().Format("2006-01-02-15-04-05"))
+	stager := proxy.stager
 
-	SerializeStageMessageTypeTransitionEtat(proxy.stager.stage, filename)
+	loadStage := stager.loadStage
+	loadStage.Reset()
+	fileToDownload := new(load.FileToDownload).Stage(loadStage)
+
+	stager.filename = fmt.Sprintf("%s.xlsx", time.Now().Local().Format("2006-01-02-15-04-05"))
+
+	fileToDownload.Name = stager.filename
+
+	tempFile, err := os.CreateTemp("", stager.filename)
+	if err != nil {
+		log.Fatalf("Failed to create temporary file: %v", err)
+	}
+
+	// 2. Defer the removal of the file to ensure it's cleaned up.
+	defer os.Remove(tempFile.Name())
+
+	// 3. Marshall the data into the temporary file.
+	SerializeStageMessageTypeTransitionEtat(proxy.stager.stage, tempFile)
+
+	// 4. Read the content back from the file.
+	// os.ReadFile needs a file path, so we use the name.
+	// First, ensure the file is closed so all data is flushed to disk.
+	if err := tempFile.Close(); err != nil {
+		log.Fatalf("Failed to close temp file: %v", err)
+	}
+
+	content, err := os.ReadFile(tempFile.Name())
+	if err != nil {
+		log.Fatalf("Failed to read back temp file: %v", err)
+	}
+
+	// 5. The content is now a string in memory, and the file will be deleted.
+	fileToDownload.Base64EncodedContent = base64.StdEncoding.EncodeToString(content)
+
+	stager.loadStage.Commit()
+
+	log.Println("Finished exporting file", tempFile.Name())
+
+	time.Sleep(1 * time.Second) // Sleep to ensure the client has time to start the download before we delete the file.
+	stager.load()
 }
 
 type StopButtonProxy struct {
