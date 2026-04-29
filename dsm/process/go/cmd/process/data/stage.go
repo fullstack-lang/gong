8da72ba86b971f58dbd3d8b53a9a6004a5ac1ef4
Thomas PEUGEOT <tpeugeot@pil.moss>
2026-04-29 14:19:55 +0200

    process : end task representation

--- dsm/process/go/cmd/process/data/stage.go
+++ dsm/process/go/cmd/process/data/stage.go
@@ -47,10 +47,13 @@ func _(stage *models.Stage) {
 	__Task__00000002_ := (&models.Task{Name: `T2`}).Stage(stage)
 	__Task__00000003_ := (&models.Task{Name: `PP2 T1`}).Stage(stage)
 	__Task__00000004_ := (&models.Task{Name: `Start`}).Stage(stage)
+	__Task__00000005_ := (&models.Task{Name: `End`}).Stage(stage)
 
 	__TaskShape__00000000_ := (&models.TaskShape{Name: `T1-DiagramProcess`}).Stage(stage)
 	__TaskShape__00000002_ := (&models.TaskShape{Name: `T2-DiagramProcess`}).Stage(stage)
 	__TaskShape__00000004_ := (&models.TaskShape{Name: `Start-DiagramProcess`}).Stage(stage)
+	__TaskShape__00000005_ := (&models.TaskShape{Name: `PP2 T1-DiagramProcess`}).Stage(stage)
+	__TaskShape__00000006_ := (&models.TaskShape{Name: `End-DiagramProcess`}).Stage(stage)
 
 	// insertion point for initialization of values
 
@@ -61,7 +64,7 @@ func _(stage *models.Stage) {
 	__DiagramProcess__00000005_.IsShowPrefix = false
 	__DiagramProcess__00000005_.DefaultBoxWidth = 250.000000
 	__DiagramProcess__00000005_.DefaultBoxHeigth = 70.000000
-	__DiagramProcess__00000005_.Width = 1179.000000
+	__DiagramProcess__00000005_.Width = 1521.000000
 	__DiagramProcess__00000005_.Height = 1318.000000
 	__DiagramProcess__00000005_.IsProcesssNodeExpanded = false
 	__DiagramProcess__00000005_.IsParticipantsNodeExpanded = true
@@ -83,9 +86,9 @@ func _(stage *models.Stage) {
 
 	__ParticipantShape__00000006_.Name = `PP2-DiagramProcess`
 	__ParticipantShape__00000006_.IsExpanded = false
-	__ParticipantShape__00000006_.X = 347.000000
+	__ParticipantShape__00000006_.X = 461.000000
 	__ParticipantShape__00000006_.Y = 68.000000
-	__ParticipantShape__00000006_.Width = 261.000000
+	__ParticipantShape__00000006_.Width = 375.000000
 	__ParticipantShape__00000006_.Height = 940.000000
 	__ParticipantShape__00000006_.IsHidden = false
 
@@ -113,7 +116,7 @@ func _(stage *models.Stage) {
 	__ProcessShape__00000005_.IsExpanded = false
 	__ProcessShape__00000005_.X = 76.000000
 	__ProcessShape__00000005_.Y = 18.000000
-	__ProcessShape__00000005_.Width = 803.000000
+	__ProcessShape__00000005_.Width = 1145.000000
 	__ProcessShape__00000005_.Height = 1000.000000
 	__ProcessShape__00000005_.IsHidden = false
 
@@ -129,7 +132,7 @@ func _(stage *models.Stage) {
 
 	__Task__00000003_.Name = `PP2 T1`
 	__Task__00000003_.ComputedPrefix = ``
-	__Task__00000003_.IsStartTask = false
+	__Task__00000003_.IsStartTask = true
 	__Task__00000003_.IsEndTask = false
 
 	__Task__00000004_.Name = `Start`
@@ -137,10 +140,15 @@ func _(stage *models.Stage) {
 	__Task__00000004_.IsStartTask = true
 	__Task__00000004_.IsEndTask = false
 
+	__Task__00000005_.Name = `End`
+	__Task__00000005_.ComputedPrefix = ``
+	__Task__00000005_.IsStartTask = false
+	__Task__00000005_.IsEndTask = true
+
 	__TaskShape__00000000_.Name = `T1-DiagramProcess`
 	__TaskShape__00000000_.IsExpanded = false
-	__TaskShape__00000000_.X = 112.000000
-	__TaskShape__00000000_.Y = 266.141353
+	__TaskShape__00000000_.X = 142.000000
+	__TaskShape__00000000_.Y = 258.141353
 	__TaskShape__00000000_.Width = 163.000000
 	__TaskShape__00000000_.Height = 70.000000
 	__TaskShape__00000000_.IsHidden = false
@@ -155,11 +163,27 @@ func _(stage *models.Stage) {
 
 	__TaskShape__00000004_.Name = `Start-DiagramProcess`
 	__TaskShape__00000004_.IsExpanded = false
-	__TaskShape__00000004_.X = 147.000000
-	__TaskShape__00000004_.Y = 154.683844
+	__TaskShape__00000004_.X = 169.000000
+	__TaskShape__00000004_.Y = 142.683844
 	__TaskShape__00000004_.Width = 58.000000
 	__TaskShape__00000004_.Height = 20.000000
 	__TaskShape__00000004_.IsHidden = false
+
+	__TaskShape__00000005_.Name = `PP2 T1-DiagramProcess`
+	__TaskShape__00000005_.IsExpanded = false
+	__TaskShape__00000005_.X = 607.000000
+	__TaskShape__00000005_.Y = 146.449476
+	__TaskShape__00000005_.Width = 75.000000
+	__TaskShape__00000005_.Height = 20.000000
+	__TaskShape__00000005_.IsHidden = false
+
+	__TaskShape__00000006_.Name = `End-DiagramProcess`
+	__TaskShape__00000006_.IsExpanded = false
+	__TaskShape__00000006_.X = 195.000000
+	__TaskShape__00000006_.Y = 578.101302
+	__TaskShape__00000006_.Width = 68.000000
+	__TaskShape__00000006_.Height = 36.000000
+	__TaskShape__00000006_.IsHidden = false
 
 	// insertion point for setup of pointers
 	__DiagramProcess__00000005_.Process_Shapes = append(__DiagramProcess__00000005_.Process_Shapes, __ProcessShape__00000005_)
@@ -167,14 +191,18 @@ func _(stage *models.Stage) {
 	__DiagramProcess__00000005_.Participant_Shapes = append(__DiagramProcess__00000005_.Participant_Shapes, __ParticipantShape__00000006_)
 	__DiagramProcess__00000005_.Participant_Shapes = append(__DiagramProcess__00000005_.Participant_Shapes, __ParticipantShape__00000007_)
 	__DiagramProcess__00000005_.ParticipantWhoseNodeIsExpanded = append(__DiagramProcess__00000005_.ParticipantWhoseNodeIsExpanded, __Participant__00000003_)
+	__DiagramProcess__00000005_.ParticipantWhoseNodeIsExpanded = append(__DiagramProcess__00000005_.ParticipantWhoseNodeIsExpanded, __Participant__00000004_)
 	__DiagramProcess__00000005_.TaskShapes = append(__DiagramProcess__00000005_.TaskShapes, __TaskShape__00000000_)
 	__DiagramProcess__00000005_.TaskShapes = append(__DiagramProcess__00000005_.TaskShapes, __TaskShape__00000002_)
 	__DiagramProcess__00000005_.TaskShapes = append(__DiagramProcess__00000005_.TaskShapes, __TaskShape__00000004_)
+	__DiagramProcess__00000005_.TaskShapes = append(__DiagramProcess__00000005_.TaskShapes, __TaskShape__00000005_)
+	__DiagramProcess__00000005_.TaskShapes = append(__DiagramProcess__00000005_.TaskShapes, __TaskShape__00000006_)
 	__Library__00000000_.RootProcesses = append(__Library__00000000_.RootProcesses, __Process__00000005_)
 	__Library__00000000_.ProcesssWhoseNodeIsExpanded = append(__Library__00000000_.ProcesssWhoseNodeIsExpanded, __Process__00000005_)
 	__Participant__00000003_.Tasks = append(__Participant__00000003_.Tasks, __Task__00000001_)
 	__Participant__00000003_.Tasks = append(__Participant__00000003_.Tasks, __Task__00000002_)
 	__Participant__00000003_.Tasks = append(__Participant__00000003_.Tasks, __Task__00000004_)
+	__Participant__00000003_.Tasks = append(__Participant__00000003_.Tasks, __Task__00000005_)
 	__Participant__00000004_.Tasks = append(__Participant__00000004_.Tasks, __Task__00000003_)
 	__ParticipantShape__00000006_.Participant = __Participant__00000004_
 	__ParticipantShape__00000007_.Participant = __Participant__00000005_
@@ -189,4 +217,6 @@ func _(stage *models.Stage) {
 	__TaskShape__00000000_.Task = __Task__00000001_
 	__TaskShape__00000002_.Task = __Task__00000002_
 	__TaskShape__00000004_.Task = __Task__00000004_
+	__TaskShape__00000005_.Task = __Task__00000003_
+	__TaskShape__00000006_.Task = __Task__00000005_
 }
