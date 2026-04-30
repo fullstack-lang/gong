61480601ce3388a6747c1078736451f914966848
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2026-04-30 08:55:54 +0200

    semantic rule

--- dsm/process/go/models/stager_enforce_control_flow_rules.go
+++ dsm/process/go/models/stager_enforce_control_flow_rules.go
@@ -13,12 +13,23 @@ func (stager *Stager) enforceControlFlowRules() (needCommit bool) {
 
 			stager.probeForm.AddNotification(time.Now(), fmt.Sprintf("Control \"%s\" has no start, unstaging",
 				controlFlow.GetName()))
+			needCommit = true
+			continue
 		}
 		if controlFlow.End == nil {
 			controlFlow.Unstage(stager.stage)
 
 			stager.probeForm.AddNotification(time.Now(), fmt.Sprintf("Control \"%s\" has no end, unstaging",
 				controlFlow.GetName()))
+			needCommit = true
+			continue
+		}
+		if controlFlow.Start.owningParticipant != controlFlow.End.owningParticipant {
+			controlFlow.Unstage(stager.stage)
+
+			stager.probeForm.AddNotification(time.Now(), fmt.Sprintf("Control flow \"%s\" connects tasks from different participants, unstaging", controlFlow.GetName()))
+			needCommit = true
+			continue
 		}
 	}
 
