c8d2a0b05fbdfaa3a0523c0dce959cc59aeb90b7
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2026-05-01 05:03:50 +0200

    dsm : new elemnt node

--- dsm/project/go/models/yyy_add_node_to_tree.go
+++ dsm/project/go/models/yyy_add_node_to_tree.go
@@ -333,3 +333,223 @@ func addNodeToTreeWithConf[
 		conf.compositionShapes,
 	)
 }
+
+// ---------------------------------------------------------
+// 4. CONFIGURATION WITHOUT LINK
+// ---------------------------------------------------------
+
+type TreeNodeAndShapeConfigurationWithoutLink[
+	AT interface {
+		*AT_
+		AbstractType
+	},
+	AT_ Gongstruct,
+	ParentAT interface {
+		*ParentAT_
+		AbstractType
+	},
+	ParentAT_ Gongstruct,
+	CT interface {
+		*CT_
+		RectShapeInterface
+		ConcreteType
+	},
+	CT_ Gongstruct,
+	DiagramType interface {
+		DiagramIF
+		AbstractType
+		comparable
+	},
+] struct {
+	diagram                     DiagramType
+	parentNode                  *tree.Node
+	element                     AT
+	parentElement               ParentAT
+	elementsWhoseNodeIsExpanded *[]AT
+	shapes                      *[]CT
+	shapesMap                   map[AT]CT
+}
+
+func addNodeToTreeWithoutLink[
+	AT interface {
+		*AT_
+		AbstractType
+	},
+	AT_ Gongstruct,
+	ParentAT interface {
+		*ParentAT_
+		AbstractType
+	},
+	ParentAT_ Gongstruct,
+	CT interface {
+		*CT_
+		RectShapeInterface
+		ConcreteType
+	},
+	CT_ Gongstruct,
+	DiagramType interface {
+		DiagramIF
+		AbstractType
+		comparable
+	}](
+	stager *Stager,
+	diagram DiagramType,
+	parentNode *tree.Node,
+	element AT,
+	parentElement ParentAT,
+	elementsWhoseNodeIsExpanded *[]AT,
+	shapes *[]CT,
+	shapesMap map[AT]CT,
+) *tree.Node {
+	stage := stager.stage
+	node := &tree.Node{
+		Name: element.GetName(),
+
+		IsExpanded: slices.Index(*elementsWhoseNodeIsExpanded, element) != -1,
+
+		HasCheckboxButton:  true,
+		IsCheckboxDisabled: !diagram.GetIsChecked(),
+
+		HasToolTip:      true,
+		ToolTipPosition: tree.Above,
+		ToolTipText:     "Add " + GetGongstructNameFromPointer(element) + " to diagram",
+
+		IsNodeClickable: true,
+
+		IsInEditMode: element.GetIsInRenameMode(),
+	}
+	if diagram.GetIsShowPrefix() {
+		node.IsWithPrefix = true
+		node.Prefix = element.GetComputedPrefix()
+	}
+	parentNode.Children = append(parentNode.Children, node)
+
+	// if the element is not in rename mode, then we can add a button to enter rename mode
+	addRenameButton(element, node, stager)
+
+	// if the element is in the diagram, then we can add a button to remove it from the diagram
+	if shape, ok := shapesMap[element]; ok {
+		node.IsChecked = true
+		visibilityButton := &tree.Button{
+			Name:            diagram.GetName(),
+			Icon:            string(buttons.BUTTON_visibility_off),
+			ToolTipText:     "Hide from diagram",
+			HasToolTip:      true,
+			ToolTipPosition: tree.Right,
+			OnClick: func() {
+				shape.SetIsHidden(!shape.GetIsHidden())
+				stage.Commit()
+			},
+		}
+		if shape.GetIsHidden() {
+			visibilityButton.Icon = string(buttons.BUTTON_visibility)
+			visibilityButton.ToolTipText = "Show on diagram"
+		}
+		node.Buttons = append(node.Buttons, visibilityButton)
+	}
+
+	// add a button to have the list of other diagrams where the element is present
+	diagrams := stager.map_Element_Diagrams[any(element).(AbstractType)]
+	if len(diagrams) > 1 {
+		if diagram.GetElementWhoseDiagramListIsDisplayed() == any(element).(AbstractType) {
+			node.IsExpanded = true
+			diagramsButton := &tree.Button{
+				Name:            diagram.GetName(),
+				Icon:            string(buttons.BUTTON_list),
+				ToolTipText:     "List of other " + fmt.Sprint(len(diagrams)-1) + " diagrams where element is present",
+				HasToolTip:      true,
+				ToolTipPosition: tree.Right,
+				OnClick: func() {
+					diagram.SetElementWhoseDiagramListIsDisplayed(nil)
+					stage.Commit()
+				},
+			}
+			node.Buttons = append(node.Buttons, diagramsButton)
+
+			for _, diag := range diagrams {
+				if any(diag) == any(diagram) {
+					continue
+				}
+				diagramNode := &tree.Node{
+					Name:            diag.GetName(),
+					ToolTipText:     "Go to diagram \"" + diag.GetName() + "\"",
+					HasToolTip:      true,
+					ToolTipPosition: tree.Right,
+					IsNodeClickable: true,
+					OnUpdate: func(_ *tree.Stage, _, _ *tree.Node) {
+						for diagram_ := range *GetGongstructInstancesSetFromPointerType[DiagramType](stager.stage) {
+							diagram_.SetIsChecked(false)
+						}
+						diag.SetIsChecked(true)
+						diagram.SetElementWhoseDiagramListIsDisplayed(nil)
+						stage.Commit()
+					},
+				}
+				node.Children = append(node.Children, diagramNode)
+			}
+		} else {
+			node.Buttons = append(node.Buttons,
+				&tree.Button{
+					Name:            diagram.GetName(),
+					Icon:            string(buttons.BUTTON_list),
+					ToolTipText:     "Show list of other diagrams where \"" + element.GetName() + "\" is present",
+					HasToolTip:      true,
+					ToolTipPosition: tree.Right,
+					OnClick: func() {
+						diagram.SetElementWhoseDiagramListIsDisplayed(any(element).(AbstractType))
+						stage.Commit()
+					},
+				})
+		}
+	}
+
+	// what to do when the node is clicked
+	node.OnUpdate = onUpdateElementInDiagramWithoutLink(
+		stager,
+		diagram,
+		element,
+		parentElement,
+		elementsWhoseNodeIsExpanded,
+		shapes,
+		shapesMap,
+	)
+
+	return node
+}
+
+func addNodeToTreeWithoutLinkWithConf[
+	AT interface {
+		*AT_
+		AbstractType
+	},
+	AT_ Gongstruct,
+	ParentAT interface {
+		*ParentAT_
+		AbstractType
+	},
+	ParentAT_ Gongstruct,
+	CT interface {
+		*CT_
+		RectShapeInterface
+		ConcreteType
+	},
+	CT_ Gongstruct,
+	DiagramType interface {
+		DiagramIF
+		AbstractType
+		comparable
+	}](
+	stager *Stager,
+	conf TreeNodeAndShapeConfigurationWithoutLink[AT, AT_, ParentAT, ParentAT_, CT, CT_, DiagramType],
+) *tree.Node {
+	return addNodeToTreeWithoutLink(
+		stager,
+		conf.diagram,
+		conf.parentNode,
+		conf.element,
+		conf.parentElement,
+		conf.elementsWhoseNodeIsExpanded,
+		conf.shapes,
+		conf.shapesMap,
+	)
+}
