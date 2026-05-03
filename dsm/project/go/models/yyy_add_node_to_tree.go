265e23f7063849886808158dd47d3fd6e8210e73
peugeot <thomaspeugeot@yahoo.fr>
2026-05-03 10:26:12 +0200

    project : some refactoring

--- dsm/project/go/models/yyy_add_node_to_tree.go
+++ dsm/project/go/models/yyy_add_node_to_tree.go
@@ -33,47 +33,39 @@ func addNodeToTree[
 		comparable
 	}](
 	stager *Stager,
-	diagram DiagramType,
-	parentNode *tree.Node,
-	element AT,
-	parentElement AT,
-	elementsWhoseNodeIsExpanded *[]AT,
-	shapes *[]CT,
-	shapesMap map[AT]CT,
-	map_Element_CompositionShape map[AT]ACT,
-	compositionShapes *[]ACT,
+	conf TreeNodeShapeAndLinkConfiguration[AT, AT_, CT, CT_, ACT, ACT_, DiagramType],
 ) *tree.Node {
 	stage := stager.stage
 	node := &tree.Node{
-		Name: element.GetName(),
+		Name: conf.element.GetName(),
 
-		IsExpanded: slices.Index(*elementsWhoseNodeIsExpanded, element) != -1,
+		IsExpanded: slices.Index(*conf.elementsWhoseNodeIsExpanded, conf.element) != -1,
 
 		HasCheckboxButton:  true,
-		IsCheckboxDisabled: !diagram.GetIsChecked(),
+		IsCheckboxDisabled: !conf.diagram.GetIsChecked(),
 
 		HasToolTip:      true,
 		ToolTipPosition: tree.Above,
-		ToolTipText:     "Add " + GetGongstructNameFromPointer(element) + " to diagram",
+		ToolTipText:     "Add " + GetGongstructNameFromPointer(conf.element) + " to diagram",
 
 		IsNodeClickable: true,
 
-		IsInEditMode: element.GetIsInRenameMode(),
+		IsInEditMode: conf.element.GetIsInRenameMode(),
 	}
-	if diagram.GetIsShowPrefix() {
+	if conf.diagram.GetIsShowPrefix() {
 		node.IsWithPrefix = true
-		node.Prefix = element.GetComputedPrefix()
+		node.Prefix = conf.element.GetComputedPrefix()
 	}
-	parentNode.Children = append(parentNode.Children, node)
+	conf.parentNode.Children = append(conf.parentNode.Children, node)
 
 	// if the element is not in rename mode, then we can add a button to enter rename mode
-	addRenameButton(element, node, stager)
+	addRenameButton(conf.element, node, stager)
 
 	// if the element is in the diagram, then we can add a button to remove it from the diagram
-	if shape, ok := shapesMap[element]; ok {
+	if shape, ok := conf.shapesMap[conf.element]; ok {
 		node.IsChecked = true
 		visibilityButton := &tree.Button{
-			Name:            diagram.GetName(),
+			Name:            conf.diagram.GetName(),
 			Icon:            string(buttons.BUTTON_visibility_off),
 			ToolTipText:     "Hide from diagram",
 			HasToolTip:      true,
@@ -90,31 +82,31 @@ func addNodeToTree[
 		node.Buttons = append(node.Buttons, visibilityButton)
 	}
 
-	_, isParentInDiagram := shapesMap[parentElement]
-	_, isChildInDiagram := shapesMap[element]
+	_, isParentInDiagram := conf.shapesMap[conf.parentElement]
+	_, isChildInDiagram := conf.shapesMap[conf.element]
 	var compositionShape ACT
-	compositionShape, _ = map_Element_CompositionShape[element]
+	compositionShape, _ = conf.map_Element_CompositionShape[conf.element]
 
 	// add a button to have the list of other diagrams where the element is present
-	diagrams := stager.map_Element_Diagrams[element]
+	diagrams := stager.map_Element_Diagrams[conf.element]
 	if len(diagrams) > 1 {
-		if diagram.GetElementWhoseDiagramListIsDisplayed() == element {
+		if conf.diagram.GetElementWhoseDiagramListIsDisplayed() == conf.element {
 			node.IsExpanded = true
 			diagramsButton := &tree.Button{
-				Name:            diagram.GetName(),
+				Name:            conf.diagram.GetName(),
 				Icon:            string(buttons.BUTTON_list),
 				ToolTipText:     "List of other " + fmt.Sprint(len(diagrams)-1) + " diagrams where element is present",
 				HasToolTip:      true,
 				ToolTipPosition: tree.Right,
 				OnClick: func() {
-					diagram.SetElementWhoseDiagramListIsDisplayed(nil)
+					conf.diagram.SetElementWhoseDiagramListIsDisplayed(nil)
 					stage.Commit()
 				},
 			}
 			node.Buttons = append(node.Buttons, diagramsButton)
 
 			for _, diag := range diagrams {
-				if any(diag) == any(diagram) {
+				if any(diag) == any(conf.diagram) {
 					continue
 				}
 				diagramNode := &tree.Node{
@@ -128,7 +120,7 @@ func addNodeToTree[
 							diagram_.SetIsChecked(false)
 						}
 						diag.SetIsChecked(true)
-						diagram.SetElementWhoseDiagramListIsDisplayed(nil)
+						conf.diagram.SetElementWhoseDiagramListIsDisplayed(nil)
 						stage.Commit()
 					},
 				}
@@ -137,40 +129,38 @@ func addNodeToTree[
 		} else {
 			node.Buttons = append(node.Buttons,
 				&tree.Button{
-					Name:            diagram.GetName(),
+					Name:            conf.diagram.GetName(),
 					Icon:            string(buttons.BUTTON_list),
-					ToolTipText:     "Show list of other diagrams where \"" + element.GetName() + "\" is present",
+					ToolTipText:     "Show list of other diagrams where \"" + conf.element.GetName() + "\" is present",
 					HasToolTip:      true,
 					ToolTipPosition: tree.Right,
 					OnClick: func() {
-						diagram.SetElementWhoseDiagramListIsDisplayed(element)
+						conf.diagram.SetElementWhoseDiagramListIsDisplayed(conf.element)
 						stage.Commit()
 					},
 				})
 		}
 	}
 
-	//
-
 	// if the parent element is in the diagram and the child element is in the diagram,
 	// then we can add a checkbox to add/remove the link between the parent and child element to/from the diagram
-	if parentElement != nil && isParentInDiagram && isChildInDiagram {
+	if conf.parentElement != nil && isParentInDiagram && isChildInDiagram {
 		node.HasSecondCheckboxButton = true
 		node.SecondCheckboxHasToolTip = true
 		node.SecondCheckboxToolTipPosition = tree.Right
 
-		if _, ok := map_Element_CompositionShape[element]; ok {
-			node.CheckboxToolTipText = "Remove link shape \"" + parentElement.GetName() +
-				"\" to \"" + element.GetName() + "\" to diagram"
+		if _, ok := conf.map_Element_CompositionShape[conf.element]; ok {
+			node.CheckboxToolTipText = "Remove link shape \"" + conf.parentElement.GetName() +
+				"\" to \"" + conf.element.GetName() + "\" to diagram"
 			node.IsSecondCheckboxChecked = true
 		} else {
-			node.CheckboxToolTipText = "Add link shape \"" + parentElement.GetName() +
-				"\" to \"" + element.GetName() + "\" to diagram"
+			node.CheckboxToolTipText = "Add link shape \"" + conf.parentElement.GetName() +
+				"\" to \"" + conf.element.GetName() + "\" to diagram"
 		}
 
 		if compositionShape != nil {
 			showHideCompositionButton := &tree.Button{
-				Name:            GetGongstructNameFromPointer(element) + " " + string(buttons.BUTTON_add),
+				Name:            GetGongstructNameFromPointer(conf.element) + " " + string(buttons.BUTTON_add),
 				HasToolTip:      true,
 				ToolTipPosition: tree.Right,
 				OnClick: func() {
@@ -182,12 +172,12 @@ func addNodeToTree[
 			if compositionShape.GetIsHidden() {
 				_ = compositionShape
 				showHideCompositionButton.Icon = string(buttons.BUTTON_visibility)
-				showHideCompositionButton.ToolTipText = "Show link from \"" + parentElement.GetName() +
-					"\" to \"" + element.GetName() + "\""
+				showHideCompositionButton.ToolTipText = "Show link from \"" + conf.parentElement.GetName() +
+					"\" to \"" + conf.element.GetName() + "\""
 			} else {
 				showHideCompositionButton.Icon = string(buttons.BUTTON_visibility_off)
-				showHideCompositionButton.ToolTipText = "Hide link from \"" + parentElement.GetName() +
-					"\" to \"" + element.GetName() + "\""
+				showHideCompositionButton.ToolTipText = "Hide link from \"" + conf.parentElement.GetName() +
+					"\" to \"" + conf.element.GetName() + "\""
 			}
 			node.Buttons = append(node.Buttons, showHideCompositionButton)
 		}
@@ -198,14 +188,14 @@ func addNodeToTree[
 	// what to do when the node is clicked
 	node.OnUpdate = onUpdateElementInDiagram(
 		stager,
-		diagram,
-		element,
-		parentElement,
-		elementsWhoseNodeIsExpanded,
-		shapes,
-		shapesMap,
+		conf.diagram,
+		conf.element,
+		conf.parentElement,
+		conf.elementsWhoseNodeIsExpanded,
+		conf.shapes,
+		conf.shapesMap,
 		compositionShape,
-		compositionShapes,
+		conf.compositionShapes,
 	)
 
 	return node
@@ -323,15 +313,7 @@ func addNodeToTreeWithConf[
 ) *tree.Node {
 	return addNodeToTree(
 		stager,
-		conf.diagram,
-		conf.parentNode,
-		conf.element,
-		conf.parentElement,
-		conf.elementsWhoseNodeIsExpanded,
-		conf.shapes,
-		conf.shapesMap,
-		conf.map_Element_CompositionShape,
-		conf.compositionShapes,
+		conf,
 	)
 }
 
