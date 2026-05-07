f06a9d98eba622d3b4c869de638649e45260b5fb
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2026-05-07 08:55:56 +0200

    process : draw data flows

--- dsm/process/go/models/models_concrete_data_flow.go
+++ dsm/process/go/models/models_concrete_data_flow.go
@@ -23,25 +23,51 @@ func (s *DataFlowShape) SetAbstractElement(a AbstractType) {
 }
 
 func (s *DataFlowShape) GetAbstractEndElement() AbstractType {
-	if s.DataFlow == nil || s.DataFlow.EndTask == nil {
+	if s.DataFlow == nil || (s.DataFlow.EndTask == nil && s.DataFlow.EndExternalParticipant == nil) {
 		return nil
 	}
-	return s.DataFlow.EndTask
+	if s.DataFlow.EndTask != nil {
+		return s.DataFlow.EndTask
+	}
+	if s.DataFlow.EndExternalParticipant != nil {
+		return s.DataFlow.EndExternalParticipant
+	}
+	return nil // can not happen
 }
 
 func (s *DataFlowShape) SetAbstractEndElement(abstractElement AbstractType) {
-	s.DataFlow.EndTask = abstractElement.(*Task)
+	if endTask, ok := abstractElement.(*Task); ok {
+		s.DataFlow.EndTask = endTask
+		return
+	}
+	if endExternalParticipant, ok := abstractElement.(*Participant); ok {
+		s.DataFlow.EndExternalParticipant = endExternalParticipant
+		return
+	}
 }
 
 func (s *DataFlowShape) GetAbstractStartElement() AbstractType {
-	if s.DataFlow == nil || s.DataFlow.StartTask == nil {
+	if s.DataFlow == nil || (s.DataFlow.StartTask == nil && s.DataFlow.StartExternalParticipant == nil) {
 		return nil
 	}
-	return s.DataFlow.StartTask
+	if s.DataFlow.StartTask != nil {
+		return s.DataFlow.StartTask
+	}
+	if s.DataFlow.StartExternalParticipant != nil {
+		return s.DataFlow.StartExternalParticipant
+	}
+	return nil // can not happen
 }
 
 func (s *DataFlowShape) SetAbstractStartElement(abstractElement AbstractType) {
-	s.DataFlow.StartTask = abstractElement.(*Task)
+	if startTask, ok := abstractElement.(*Task); ok {
+		s.DataFlow.StartTask = startTask
+		return
+	}
+	if startExternalParticipant, ok := abstractElement.(*Participant); ok {
+		s.DataFlow.StartExternalParticipant = startExternalParticipant
+		return
+	}
 }
 
 var _ AssociationConcreteType = (*DataFlowShape)(nil)
