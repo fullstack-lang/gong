73614ef1012c6961ff54ba632ae1be96e7d176bd
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2026-05-10 22:37:22 +0200

    project : refactored web site generation

--- dsm/process/go/models/stager_export_website.go
+++ dsm/process/go/models/stager_export_website.go
@@ -1,7 +1,9 @@
 package models
 
 import (
+	"fmt"
 	"log"
+	"strings"
 
 	load "github.com/fullstack-lang/gong/lib/load/go/models"
 	ssg "github.com/fullstack-lang/gong/lib/ssg/go/models"
@@ -11,10 +13,119 @@ func (stager *Stager) exportWebsite() {
 	stager.ssgStage.Reset()
 
 	content := ssg.Content{
-		Name:           "Root to project the website",
-		ContentPath:    "/tmp/project",
-		MardownContent: "## Project website",
+		Name:           "Root to process the website",
+		ContentPath:    "/tmp/process",
+		MardownContent: "## Process website",
 		OutputPath:     "./generated static web site",
+	}
+
+	for _, process := range GetGongstrucsSorted[*Process](stager.stage) {
+		chapter := &ssg.Chapter{
+			Name:           process.Name,
+			MardownContent: "### " + process.Name,
+		}
+		content.Chapters = append(content.Chapters, chapter)
+
+		if len(process.Participants) > 0 {
+			page := &ssg.Page{
+				Name:           "Participants",
+				MardownContent: "### Participants\n\n| Name |\n|---|\n",
+			}
+			for _, p := range process.Participants {
+				page.MardownContent += fmt.Sprintf("| %s |\n", p.Name)
+			}
+			chapter.Pages = append(chapter.Pages, page)
+		}
+
+		if len(process.ExternalParticipants) > 0 {
+			page := &ssg.Page{
+				Name:           "External Participants",
+				MardownContent: "### External Participants\n\n| Name |\n|---|\n",
+			}
+			for _, p := range process.ExternalParticipants {
+				page.MardownContent += fmt.Sprintf("| %s |\n", p.Name)
+			}
+			chapter.Pages = append(chapter.Pages, page)
+		}
+
+		var allTasks []*Task
+		var allControlFlows []*ControlFlow
+		for _, p := range process.Participants {
+			allTasks = append(allTasks, p.Tasks...)
+			allControlFlows = append(allControlFlows, p.ControlFlows...)
+		}
+
+		if len(allTasks) > 0 {
+			page := &ssg.Page{
+				Name:           "Tasks",
+				MardownContent: "### Tasks\n\n| Name | Participant |\n|---|---|\n",
+			}
+			for _, p := range process.Participants {
+				for _, t := range p.Tasks {
+					page.MardownContent += fmt.Sprintf("| %s | %s |\n", t.Name, p.Name)
+				}
+			}
+			chapter.Pages = append(chapter.Pages, page)
+		}
+
+		if len(allControlFlows) > 0 {
+			page := &ssg.Page{
+				Name:           "Control Flows",
+				MardownContent: "### Control Flows\n\n| Name | Start | End |\n|---|---|---|\n",
+			}
+			for _, c := range allControlFlows {
+				start := ""
+				if c.Start != nil {
+					start = c.Start.Name
+				}
+				end := ""
+				if c.End != nil {
+					end = c.End.Name
+				}
+				page.MardownContent += fmt.Sprintf("| %s | %s | %s |\n", c.Name, start, end)
+			}
+			chapter.Pages = append(chapter.Pages, page)
+		}
+
+		if len(process.DataFlows) > 0 {
+			page := &ssg.Page{
+				Name:           "Data Flows",
+				MardownContent: "### Data Flows\n\n| Name |\n|---|\n",
+			}
+			for _, d := range process.DataFlows {
+				page.MardownContent += fmt.Sprintf("| %s |\n", d.Name)
+			}
+			chapter.Pages = append(chapter.Pages, page)
+		}
+
+		if len(process.DiagramProcesss) > 0 {
+			page := &ssg.Page{
+				Name:           "Diagrams",
+				MardownContent: "### Diagrams\n",
+			}
+			for _, diagram := range process.DiagramProcesss {
+				page.MardownContent += "\n- " + diagram.Name
+
+				svgObject := stager.generateSvgObject(diagram)
+				_ = svgObject
+				svgString, maxX, maxY := svgObject.GenerateString()
+
+				// Replace 100% width/height with exact pixel values to prevent the
+				// SVG from stretching and looking "too big" in the browser.
+				svgString = strings.Replace(svgString, `width="100%"`, fmt.Sprintf(`width="%f"`, maxX), 1)
+				svgString = strings.Replace(svgString, `height="100%"`, fmt.Sprintf(`height="%f"`, maxY), 1)
+
+				section := &ssg.Section{
+					Name:    diagram.Name,
+					IsImage: true,
+					SvgImage: &ssg.SvgImage{
+						Content: svgString,
+					},
+				}
+				page.Sections = append(page.Sections, section)
+			}
+			chapter.Pages = append(chapter.Pages, page)
+		}
 	}
 
 	content.LogoSVGFile = stager.GetRootLibrary().LogoSVGFile
