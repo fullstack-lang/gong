90697cf5af88f2eb922d291821f19c152b4f0868
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2026-05-23 17:59:18 +0200

    gong : convert Gong Test App to Single-File Offline Portable App (WASM) Fixes #1299 - step 2 & 3

---
+++ test/test4/go/cmd/test4/main_os.go
@@ -0,0 +1,24 @@
+//go:build !wasm
+
+package main
+
+import (
+	"flag"
+	"log"
+	"strconv"
+)
+
+func main() {
+	// 1. Parse arguments for the OS environment
+	flag.Parse()
+
+	// 2. Initialize the Gong app and get the Gin router
+	r := setupApp()
+
+	// 3. Start the standard TCP server
+	log.Println("Server ready serve on localhost:" + strconv.Itoa(*port))
+	err := r.Run(":" + strconv.Itoa(*port))
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+}
