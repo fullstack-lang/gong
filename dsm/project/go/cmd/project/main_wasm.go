3f617f5356be4969c038e6734025329f8d8fad5a
peugeot <thomaspeugeot@yahoo.fr>
2026-05-25 10:16:59 +0200

    gong : convert Gong Test App to Single-File Offline Portable App (WASM) Fixes #1299 - try for project

---
+++ dsm/project/go/cmd/project/main_wasm.go
@@ -0,0 +1,41 @@
+//go:build js && wasm
+
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/fullstack-lang/gong/dsm/project/go/level1stack"
+	"github.com/fullstack-lang/gong/dsm/project/go/models"
+)
+
+func main() {
+	fmt.Println("Initializing DSM Project WASM Backend...")
+
+	log.SetPrefix("project: ")
+	log.SetFlags(log.Lmicroseconds)
+
+	unmarshallFromCode := ""
+	marshallOnCommit := ""
+	embeddedDiagrams := true
+
+	// setup
+	// - model level1 stack with its probe
+	// - unmarshall/marshall go file with stage data
+	stack := level1stack.NewLevel1StackDelta("project", unmarshallFromCode, marshallOnCommit, true, embeddedDiagrams, true)
+	stack.Stage.SetGongMarshallingMode(models.GongMarshallingAppendCommit)
+	stack.Stage.SetIsWithGenesisCommit(true) // the genesis commit is the first commit of the stage, it is the one that contains the initial data. It cannot be rollbacked.
+
+	// refresh the probe, therefore we can see what has been unmarshalled
+	stack.Probe.Refresh()
+
+	// initiates the UX loop
+	models.NewStager(
+		stack.R,
+		stack.Stage,
+		stack.Probe,
+	)
+
+	select {} // Keep the WASM instance running indefinitely
+}
