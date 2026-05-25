f5a8c518abb65382db9000e57d351689a5706fed
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2026-05-25 12:58:59 +0200

    gong : convert Gong Test App to Single-File Offline Portable App (WASM) Fixes #1299 - test4. give up on project

--- test/test4/go/diagrams/diagrams.go
+++ test/test4/go/diagrams/diagrams.go
@@ -29,15 +29,53 @@ func _(stage *models.Stage) {
 
 	// insertion point for declaration of instances to stage
 
+	__AttributeShape__00000000_ := (&models.AttributeShape{Name: `Name`}).Stage(stage)
+
+	__Classdiagram__00000000_ := (&models.Classdiagram{Name: `Default`}).Stage(stage)
+
 	__DiagramPackage__00000000_ := (&models.DiagramPackage{Name: `Diagram Package created the 2026-05-23T10:02:02Z`}).Stage(stage)
 
+	__GongStructShape__00000000_ := (&models.GongStructShape{Name: `Default-Astruct`}).Stage(stage)
+
 	// insertion point for initialization of values
+
+	__AttributeShape__00000000_.Name = `Name`
+	__AttributeShape__00000000_.IdentifierMeta = ref_models.Astruct{}.Name
+	__AttributeShape__00000000_.FieldTypeAsString = ``
+	__AttributeShape__00000000_.Structname = `Astruct`
+	__AttributeShape__00000000_.Fieldtypename = `string`
+
+	__Classdiagram__00000000_.Name = `Default`
+	__Classdiagram__00000000_.Description = ``
+	__Classdiagram__00000000_.IsIncludedInStaticWebSite = false
+	__Classdiagram__00000000_.ShowNbInstances = false
+	__Classdiagram__00000000_.ShowMultiplicity = false
+	__Classdiagram__00000000_.ShowLinkNames = false
+	__Classdiagram__00000000_.IsInRenameMode = false
+	__Classdiagram__00000000_.IsExpanded = true
+	__Classdiagram__00000000_.NodeGongStructsIsExpanded = true
+	__Classdiagram__00000000_.NodeGongStructNodeExpansion = `[true]`
+	__Classdiagram__00000000_.NodeGongEnumsIsExpanded = false
+	__Classdiagram__00000000_.NodeGongEnumNodeExpansion = ``
+	__Classdiagram__00000000_.NodeGongNotesIsExpanded = false
+	__Classdiagram__00000000_.NodeGongNoteNodeExpansion = ``
 
 	__DiagramPackage__00000000_.Name = `Diagram Package created the 2026-05-23T10:02:02Z`
 	__DiagramPackage__00000000_.Path = ``
 	__DiagramPackage__00000000_.GongModelPath = ``
 	__DiagramPackage__00000000_.AbsolutePathToDiagramPackage = ``
 
+	__GongStructShape__00000000_.Name = `Default-Astruct`
+	__GongStructShape__00000000_.X = 68.000000
+	__GongStructShape__00000000_.Y = 61.000000
+	__GongStructShape__00000000_.IdentifierMeta = ref_models.Astruct{}
+	__GongStructShape__00000000_.Width = 240.000000
+	__GongStructShape__00000000_.Height = 83.000000
+	__GongStructShape__00000000_.IsSelected = false
+
 	// insertion point for setup of pointers
-	__DiagramPackage__00000000_.SelectedClassdiagram = nil
+	__Classdiagram__00000000_.GongStructShapes = append(__Classdiagram__00000000_.GongStructShapes, __GongStructShape__00000000_)
+	__DiagramPackage__00000000_.Classdiagrams = append(__DiagramPackage__00000000_.Classdiagrams, __Classdiagram__00000000_)
+	__DiagramPackage__00000000_.SelectedClassdiagram = __Classdiagram__00000000_
+	__GongStructShape__00000000_.AttributeShapes = append(__GongStructShape__00000000_.AttributeShapes, __AttributeShape__00000000_)
 }
