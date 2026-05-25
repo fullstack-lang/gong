500659f9df25b55bcccbeded9a277682b5b96dd2
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2026-05-25 05:37:07 +0200

    gong : convert Gong Test App to Single-File Offline Portable App (WASM) Fixes #1299 - rm test4 binary also

--- test/test4/cleanup_build_portable.sh
+++ test/test4/cleanup_build_portable.sh
@@ -9,6 +9,7 @@ DIST_BROWSER_DIR="$NG_DIR/dist/ng-github.com-fullstack-lang-gong-test-test4/brow
 
 # 0. CLEANUP (Crucial for Library updates!)
 echo "🧹 0/5: Cleaning up previous build artifacts..."
+rm -rf "$GO_DIR/cmd/test4/test4"
 rm -rf "$NG_DIR/dist/"
 rm -rf "$NG_DIR/.angular/cache/"
 rm -f "test4-portable-app.html"
