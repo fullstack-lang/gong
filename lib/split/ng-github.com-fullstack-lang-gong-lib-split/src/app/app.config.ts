1efb093548de1e83c2384efa017c2044a7c9a936
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2026-05-25 17:57:35 +0200

    portable app : updated split app

--- lib/split/ng-github.com-fullstack-lang-gong-lib-split/src/app/app.config.ts
+++ lib/split/ng-github.com-fullstack-lang-gong-lib-split/src/app/app.config.ts
@@ -1,14 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, withHashLocation } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { APP_BASE_HREF } from '@angular/common'; // 1. Import this
 
 import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { wasmInterceptor } from './wasm.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
-    provideAnimationsAsync(),
-    provideHttpClient(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withHashLocation()), 
+    
+    // 2. Set the base href dynamically to the exact current file path
+    { provide: APP_BASE_HREF, useValue: window.location.pathname }, 
+    
+    provideHttpClient(withInterceptors([wasmInterceptor]))
   ]
 };
