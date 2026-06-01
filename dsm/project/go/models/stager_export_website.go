4d9d753a81bc320199aa53f087384d56157ee122
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2026-06-01 02:34:26 +0200

    portable app : file import / export fails Fixes #1306 : export webstie project

--- dsm/project/go/models/stager_export_website.go
+++ dsm/project/go/models/stager_export_website.go
@@ -11,6 +11,7 @@ import (
 )
 
 func (stager *Stager) exportWebsite() {
+	fmt.Println("exportWebsite: Starting website export process...")
 	stager.ssgStage.Reset()
 
 	content := ssg.Content{
@@ -19,6 +20,7 @@ func (stager *Stager) exportWebsite() {
 	}
 
 	for _, lib := range GetGongstrucsSorted[*Library](stager.stage) {
+		fmt.Println("exportWebsite: Processing library:", lib.Name)
 		chapter := &ssg.Chapter{
 			Name:           lib.Name,
 			MardownContent: "### " + lib.Name,
@@ -111,6 +113,7 @@ func (stager *Stager) exportWebsite() {
 
 		if len(lib.Diagrams) > 0 {
 			for _, diagram := range lib.Diagrams {
+				fmt.Println("exportWebsite: Generating SVG for diagram:", diagram.Name)
 				svgObject := stager.generateSvgObject(diagram)
 				_ = svgObject
 				svgString, maxX, maxY := svgObject.GenerateString()
@@ -147,10 +150,13 @@ func (stager *Stager) exportWebsite() {
 			MardownContent: "### XL files\n",
 		}
 
+		fmt.Println("exportWebsite: Generating Excel files...")
 		excelBytes, err := SerializeStageAsBytes(stager.stage, false)
 		if err != nil {
 			log.Println("Error creating Excel export in memory:", err)
+			fmt.Println("exportWebsite: Error creating Excel in memory:", err)
 		} else {
+			fmt.Printf("exportWebsite: Excel generated successfully. Bytes: %d\n", len(excelBytes))
 			encodedXL := base64.StdEncoding.EncodeToString(excelBytes)
 			dataURI := "data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64," + encodedXL
 			page.MardownContent += fmt.Sprintf("\n[Download %s.xlsx](%s)\n", lib.Name, dataURI)
@@ -166,15 +172,21 @@ func (stager *Stager) exportWebsite() {
 
 	content.LogoSVGFile = stager.GetRootLibrary().LogoSVGFile
 
+	fmt.Println("exportWebsite: Staging SSG branch...")
 	ssg.StageBranch(stager.ssgStage, &content)
 
+	fmt.Println("exportWebsite: Committing SSG stage...")
 	stager.ssgStage.Commit()
 
+	fmt.Println("exportWebsite: Calling ssgStage.Generation(true)...")
 	zipData, err := stager.ssgStage.Generation(true)
 	if err != nil {
 		log.Println(err)
+		fmt.Println("exportWebsite: ERROR during SSG Generation:", err)
 	}
+	fmt.Printf("exportWebsite: Generation completed. zipData length: %d\n", len(zipData))
 
+	fmt.Println("exportWebsite: Preparing download payload...")
 	stager.loadStage.Reset()
 
 	fileToDownload := new(load.FileToDownload).Stage(stager.loadStage)
@@ -182,7 +194,9 @@ func (stager *Stager) exportWebsite() {
 
 	fileToDownload.Name = "site.zip"
 
+	fmt.Println("exportWebsite: Committing loadStage...")
 	stager.loadStage.Commit()
+	fmt.Println("exportWebsite: Website export process finished.")
 }
 
 func productToMD(p *Product, depth int) string {
