d18325c16166664f0c66b489933f09e987f4afad
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2026-06-21 04:53:20 +0200

    refactor: migrate command-line applications to use Cobra-based modular CLI architecture

---
+++ test/test/go/cmd/test/stage.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"slices"
+	"time"
+
+	"github.com/fullstack-lang/gong/test/test/go/models"
+	// injection point for ident package import declaration{{ImportPackageDeclaration}}
+)
+
+// generated in order to avoid error in the package import
+// if there are no elements in the stage to marshall
+var (
+	_ time.Time
+	_ = slices.Index[[]int, int]
+)
+
+// _ point for meta package dummy declaration{{ImportPackageDummyDeclaration}}
+
+// When parsed, those maps will help with the renaming process
+var _ map[string]any = map[string]any{
+	// injection point for docLink to identifiers{{EntriesDocLinkStringDocLinkIdentifier}}
+}
+
+// function will stage objects
+func _(stage *models.Stage) {
+
+	// insertion point for declaration of instances to stage
+
+	// insertion point for initialization of values
+
+	// insertion point for setup of pointers
+}
---
+++ test/test2/go/cmd/test2/commands/root.go
@@ -0,0 +1,34 @@
+package commands
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var logGINFlag bool
+
+var rootCmd = &cobra.Command{
+	Use:   "test2",
+	Short: "test2 is a pseudo CLI for test2 project",
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.PersistentFlags().BoolVar(&logGINFlag, "logGIN", false, "log mode for gin")
+
+	cobra.OnInitialize(initConfig)
+}
+
+func initConfig() {
+	log.SetPrefix("test2: ")
+	log.SetFlags(0)
+}
