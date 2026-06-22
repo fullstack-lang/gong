ff23cb0511352d65b5d6d9147dd2dd71728ed234
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2026-06-23 00:31:35 +0200

    feat: implement embedded data loading and example file selection in the tree UI

--- dsm/structure/go/models/stager_ux_tree.go
+++ dsm/structure/go/models/stager_ux_tree.go
@@ -1,8 +1,16 @@
 package models
 
 import (
+	"embed"
+	"go/parser"
+	"go/token"
+	"strings"
+
+	"github.com/fullstack-lang/gong/lib/tree/go/buttons"
 	tree "github.com/fullstack-lang/gong/lib/tree/go/models"
 )
+
+var DataFS *embed.FS
 
 func (stager *Stager) ux_tree() {
 	stager.treeStage.Reset()
@@ -14,6 +22,56 @@ func (stager *Stager) ux_tree() {
 	})
 
 	stager.treeLibrary(stager.getRootLibrary(), &treeInstance.RootNodes)
+
+	examplesNode := &tree.Node{
+		Name:            "Examples",
+		IsNodeClickable: true,
+		IsExpanded:      true,
+	}
+
+	examplesNode.Menu = &tree.Menu{
+		Name: "Examples Menu",
+	}
+
+	if DataFS != nil {
+		entries, err := DataFS.ReadDir("data")
+		if err == nil {
+			for _, entry := range entries {
+				if entry.IsDir() {
+					continue
+				}
+				entryName := entry.Name()
+				if !strings.HasSuffix(entryName, ".go") {
+					continue
+				}
+				examplesNode.Menu.Buttons = append(examplesNode.Menu.Buttons, &tree.Button{
+					Name: entryName,
+					Icon: string(buttons.BUTTON_file_open),
+					OnClick: func() {
+						content, err := DataFS.ReadFile("data/" + entryName)
+						if err == nil {
+							// if the user loads an example file, we don't want the file to be automatically overwritten
+							stager.stage.OnInitCommitCallback = nil
+
+							stager.stage.Reset()
+
+							fset := token.NewFileSet()
+							file, err := parser.ParseFile(fset, "", string(content), parser.ParseComments)
+							if err == nil {
+								ParseAstFileFromAst(stager.stage, file, fset, true)
+								stager.stage.ComputeReverseMaps()
+								stager.stage.ComputeInstancesNb()
+								stager.stage.ComputeReferenceAndOrders()
+								stager.stage.Commit()
+								stager.probeForm.Refresh()
+							}
+						}
+					},
+				})
+			}
+		}
+	}
+	treeInstance.RootNodes = append(treeInstance.RootNodes, examplesNode)
 
 	tree.StageBranch(stager.treeStage, treeInstance)
 
