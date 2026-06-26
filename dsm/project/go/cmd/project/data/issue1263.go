5ecaa7cbe1182abea010cfbce15ac1624fe05447
peugeot <thomaspeugeot@yahoo.fr>
2026-06-26 23:33:10 +0200

    feat: add Goals library and product to data initialization and update diagram node expansion logic

--- dsm/project/go/cmd/project/data/issue1263.go
+++ dsm/project/go/cmd/project/data/issue1263.go
@@ -28,8 +28,10 @@ func _(stage *models.Stage) {
 	// insertion point for declaration of instances to stage
 
 	__Diagram__00000000_ := (&models.Diagram{Name: `Default Diagram`}).Stage(stage)
+	__Diagram__00000001_ := (&models.Diagram{Name: `Default Diagram`}).Stage(stage)
 
-	__Library__00000000_ := (&models.Library{Name: ``}).Stage(stage)
+	__Library__00000000_ := (&models.Library{Name: `Pain points`}).Stage(stage)
+	__Library__00000001_ := (&models.Library{Name: `Goals`}).Stage(stage)
 
 	__Note__00000000_ := (&models.Note{Name: `Invariant : generated code never import a gong package `}).Stage(stage)
 
@@ -52,6 +54,7 @@ func _(stage *models.Stage) {
 	__Product__00000012_ := (&models.Product{Name: `Graph Data Integrity & Traversal`}).Stage(stage)
 	__Product__00000013_ := (&models.Product{Name: `Rely on heavily typed Stage`}).Stage(stage)
 	__Product__00000014_ := (&models.Product{Name: `lots of boilerplate code`}).Stage(stage)
+	__Product__00000015_ := (&models.Product{Name: `Goals`}).Stage(stage)
 
 	__ProductCompositionShape__00000000_ := (&models.ProductCompositionShape{Name: `Default Diagram-Paint points-lot of yyy files and no clear mental model `}).Stage(stage)
 	__ProductCompositionShape__00000001_ := (&models.ProductCompositionShape{Name: `Default Diagram-Paint points-lot of stager is a hybrid concept`}).Stage(stage)
@@ -83,6 +86,7 @@ func _(stage *models.Stage) {
 	__ProductShape__00000012_ := (&models.ProductShape{Name: `Default Diagram-Graph Data Integrity & Traversal`}).Stage(stage)
 	__ProductShape__00000013_ := (&models.ProductShape{Name: `Default Diagram-Rely on heavily typed Stage`}).Stage(stage)
 	__ProductShape__00000014_ := (&models.ProductShape{Name: `Default Diagram-lots of boilerplate code`}).Stage(stage)
+	__ProductShape__00000015_ := (&models.ProductShape{Name: `Default Diagram-Goals`}).Stage(stage)
 
 	// insertion point for initialization of values
 
@@ -123,7 +127,7 @@ func _(stage *models.Stage) {
 	__Diagram__00000000_.ComputedPrefix = `1`
 	__Diagram__00000000_.IsExpanded = true
 	__Diagram__00000000_.LayoutDirection = models.Vertical
-	__Diagram__00000000_.IsChecked = true
+	__Diagram__00000000_.IsChecked = false
 	__Diagram__00000000_.IsEditable_ = true
 	__Diagram__00000000_.IsShowPrefix = false
 	__Diagram__00000000_.IsPBSNodeExpanded = true
@@ -132,13 +136,67 @@ func _(stage *models.Stage) {
 	__Diagram__00000000_.IsNotesNodeExpanded = true
 	__Diagram__00000000_.IsResourcesNodeExpanded = false
 
-	__Library__00000000_.Name = ``
+	__Diagram__00000001_.Name = `Default Diagram`
+	__Diagram__00000001_.DefaultBoxWidth = 250.000000
+	__Diagram__00000001_.DefaultBoxHeigth = 70.000000
+	__Diagram__00000001_.DateFormat = ``
+	__Diagram__00000001_.Width = 509.678271
+	__Diagram__00000001_.Height = 334.099915
+	__Diagram__00000001_.IsTimeDiagram = false
+	__Diagram__00000001_.ComputedStart, _ = time.Parse("2006-01-02 15:04:05.999999999 -0700 MST", "0001-01-01 00:00:00 +0000 UTC")
+	__Diagram__00000001_.ComputedEnd, _ = time.Parse("2006-01-02 15:04:05.999999999 -0700 MST", "0001-01-01 00:00:00 +0000 UTC")
+	__Diagram__00000001_.ComputedDuration = 0
+	__Diagram__00000001_.UseManualStartAndEndDates = false
+	__Diagram__00000001_.ManualStart, _ = time.Parse("2006-01-02 15:04:05.999999999 -0700 MST", "0001-01-01 00:00:00 +0000 UTC")
+	__Diagram__00000001_.ManualEnd, _ = time.Parse("2006-01-02 15:04:05.999999999 -0700 MST", "0001-01-01 00:00:00 +0000 UTC")
+	__Diagram__00000001_.TimeStep = 0
+	__Diagram__00000001_.TimeStepScale = ""
+	__Diagram__00000001_.LaneHeight = 0.000000
+	__Diagram__00000001_.RatioBarToLaneHeight = 0.000000
+	__Diagram__00000001_.YTopMargin = 0.000000
+	__Diagram__00000001_.XLeftText = 0.000000
+	__Diagram__00000001_.TextHeight = 0.000000
+	__Diagram__00000001_.XLeftLanes = 0.000000
+	__Diagram__00000001_.XRightMargin = 0.000000
+	__Diagram__00000001_.ArrowLengthToTheRightOfStartBar = 0.000000
+	__Diagram__00000001_.ArrowTipLenght = 0.000000
+	__Diagram__00000001_.TimeLine_Color = ``
+	__Diagram__00000001_.TimeLine_FillOpacity = 0.000000
+	__Diagram__00000001_.TimeLine_Stroke = ``
+	__Diagram__00000001_.TimeLine_StrokeWidth = 0.000000
+	__Diagram__00000001_.DrawVerticalTimeLines = false
+	__Diagram__00000001_.Group_Stroke = ``
+	__Diagram__00000001_.Group_StrokeWidth = 0.000000
+	__Diagram__00000001_.Group_StrokeDashArray = ``
+	__Diagram__00000001_.DateYOffset = 0.000000
+	__Diagram__00000001_.AlignOnStartEndOnYearStart = false
+	__Diagram__00000001_.ComputedPrefix = `1`
+	__Diagram__00000001_.IsExpanded = true
+	__Diagram__00000001_.LayoutDirection = models.Vertical
+	__Diagram__00000001_.IsChecked = true
+	__Diagram__00000001_.IsEditable_ = true
+	__Diagram__00000001_.IsShowPrefix = false
+	__Diagram__00000001_.IsPBSNodeExpanded = true
+	__Diagram__00000001_.IsWBSNodeExpanded = true
+	__Diagram__00000001_.IsTaskGroupsNodeExpanded = false
+	__Diagram__00000001_.IsNotesNodeExpanded = false
+	__Diagram__00000001_.IsResourcesNodeExpanded = false
+
+	__Library__00000000_.Name = `Pain points`
 	__Library__00000000_.NbPixPerCharacter = 8.000000
 	__Library__00000000_.LogoSVGFile = ``
 	__Library__00000000_.ComputedPrefix = ``
 	__Library__00000000_.IsExpanded = true
 	__Library__00000000_.LayoutDirection = models.Vertical
 	__Library__00000000_.IsRootLibrary = true
+
+	__Library__00000001_.Name = `Goals`
+	__Library__00000001_.NbPixPerCharacter = 0.000000
+	__Library__00000001_.LogoSVGFile = ``
+	__Library__00000001_.ComputedPrefix = ``
+	__Library__00000001_.IsExpanded = true
+	__Library__00000001_.LayoutDirection = models.Vertical
+	__Library__00000001_.IsRootLibrary = false
 
 	__Note__00000000_.Name = `Invariant : generated code never import a gong package `
 	__Note__00000000_.ComputedPrefix = `1`
@@ -296,6 +354,15 @@ func _(stage *models.Stage) {
 	__Product__00000014_.ComputedPrefix = `1.2.2`
 	__Product__00000014_.IsExpanded = false
 	__Product__00000014_.LayoutDirection = models.Vertical
+
+	__Product__00000015_.Name = `Goals`
+	__Product__00000015_.Description = ``
+	__Product__00000015_.IsProducersNodeExpanded = false
+	__Product__00000015_.IsConsumersNodeExpanded = false
+	__Product__00000015_.IsImport = false
+	__Product__00000015_.ComputedPrefix = `1`
+	__Product__00000015_.IsExpanded = false
+	__Product__00000015_.LayoutDirection = models.Vertical
 
 	__ProductCompositionShape__00000000_.Name = `Default Diagram-Paint points-lot of yyy files and no clear mental model `
 	__ProductCompositionShape__00000000_.StartRatio = 0.500000
@@ -544,6 +611,15 @@ func _(stage *models.Stage) {
 	__ProductShape__00000014_.Height = 70.000000
 	__ProductShape__00000014_.IsHidden = false
 
+	__ProductShape__00000015_.Name = `Default Diagram-Goals`
+	__ProductShape__00000015_.OverideLayoutDirection = false
+	__ProductShape__00000015_.LayoutDirection = models.Vertical
+	__ProductShape__00000015_.X = 159.678271
+	__ProductShape__00000015_.Y = 164.099915
+	__ProductShape__00000015_.Width = 250.000000
+	__ProductShape__00000015_.Height = 70.000000
+	__ProductShape__00000015_.IsHidden = false
+
 	// insertion point for setup of pointers
 	__Diagram__00000000_.Product_Shapes = append(__Diagram__00000000_.Product_Shapes, __ProductShape__00000000_)
 	__Diagram__00000000_.Product_Shapes = append(__Diagram__00000000_.Product_Shapes, __ProductShape__00000001_)
@@ -560,7 +636,6 @@ func _(stage *models.Stage) {
 	__Diagram__00000000_.Product_Shapes = append(__Diagram__00000000_.Product_Shapes, __ProductShape__00000012_)
 	__Diagram__00000000_.Product_Shapes = append(__Diagram__00000000_.Product_Shapes, __ProductShape__00000013_)
 	__Diagram__00000000_.Product_Shapes = append(__Diagram__00000000_.Product_Shapes, __ProductShape__00000014_)
-	__Diagram__00000000_.ProductsWhoseNodeIsExpanded = append(__Diagram__00000000_.ProductsWhoseNodeIsExpanded, __Product__00000000_)
 	__Diagram__00000000_.ProductsWhoseNodeIsExpanded = append(__Diagram__00000000_.ProductsWhoseNodeIsExpanded, __Product__00000003_)
 	__Diagram__00000000_.ProductsWhoseNodeIsExpanded = append(__Diagram__00000000_.ProductsWhoseNodeIsExpanded, __Product__00000001_)
 	__Diagram__00000000_.ProductsWhoseNodeIsExpanded = append(__Diagram__00000000_.ProductsWhoseNodeIsExpanded, __Product__00000007_)
@@ -582,9 +657,13 @@ func _(stage *models.Stage) {
 	__Diagram__00000000_.ProductComposition_Shapes = append(__Diagram__00000000_.ProductComposition_Shapes, __ProductCompositionShape__00000013_)
 	__Diagram__00000000_.Note_Shapes = append(__Diagram__00000000_.Note_Shapes, __NoteShape__00000000_)
 	__Diagram__00000000_.NoteProductShapes = append(__Diagram__00000000_.NoteProductShapes, __NoteProductShape__00000000_)
+	__Diagram__00000001_.Product_Shapes = append(__Diagram__00000001_.Product_Shapes, __ProductShape__00000015_)
+	__Library__00000000_.SubLibraries = append(__Library__00000000_.SubLibraries, __Library__00000001_)
 	__Library__00000000_.RootProducts = append(__Library__00000000_.RootProducts, __Product__00000000_)
 	__Library__00000000_.Notes = append(__Library__00000000_.Notes, __Note__00000000_)
 	__Library__00000000_.Diagrams = append(__Library__00000000_.Diagrams, __Diagram__00000000_)
+	__Library__00000001_.RootProducts = append(__Library__00000001_.RootProducts, __Product__00000015_)
+	__Library__00000001_.Diagrams = append(__Library__00000001_.Diagrams, __Diagram__00000001_)
 	__Note__00000000_.Products = append(__Note__00000000_.Products, __Product__00000004_)
 	__NoteProductShape__00000000_.Note = __Note__00000000_
 	__NoteProductShape__00000000_.Product = __Product__00000004_
@@ -618,6 +697,7 @@ func _(stage *models.Stage) {
 	__Product__00000012_.ReferencedProduct = nil
 	__Product__00000013_.ReferencedProduct = nil
 	__Product__00000014_.ReferencedProduct = nil
+	__Product__00000015_.ReferencedProduct = nil
 	__ProductCompositionShape__00000000_.Product = __Product__00000001_
 	__ProductCompositionShape__00000001_.Product = __Product__00000002_
 	__ProductCompositionShape__00000002_.Product = __Product__00000003_
@@ -647,4 +727,5 @@ func _(stage *models.Stage) {
 	__ProductShape__00000012_.Product = __Product__00000012_
 	__ProductShape__00000013_.Product = __Product__00000013_
 	__ProductShape__00000014_.Product = __Product__00000014_
+	__ProductShape__00000015_.Product = __Product__00000015_
 }
