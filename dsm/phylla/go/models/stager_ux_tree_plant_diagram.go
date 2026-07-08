3ce7cfd6bd55367af4ce3deea57dfb7618f7518b
peugeot <thomaspeugeot@yahoo.fr>
2026-07-08 11:44:12 +0200

    feat: add visibility toggle button to AxesShape tree node and implement visibility getter/setter methods

--- dsm/phylla/go/models/stager_ux_tree_plant_diagram.go
+++ dsm/phylla/go/models/stager_ux_tree_plant_diagram.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	buttons "github.com/fullstack-lang/gong/lib/tree/go/buttons"
 	tree "github.com/fullstack-lang/gong/lib/tree/go/models"
 )
 
@@ -40,15 +41,32 @@ func (stager *Stager) treePlantDiagram(
 		stager.stage.Commit()
 	}
 
-	if plantDiagram.AxesShape != nil {
-		axesNode := &tree.Node{
-			Name:            plantDiagram.AxesShape.Name,
-			IsNodeClickable: true,
-		}
-		axesNode.OnClick = func(frontNode *tree.Node) {
-			stager.probeForm.FillUpFormFromGongstruct(plantDiagram.AxesShape, GetPointerToGongstructName[*AxesShape]())
-			stager.stage.Commit()
-		}
-		plantDiagramNode.Children = append(plantDiagramNode.Children, axesNode)
+	axesShape := plantDiagram.AxesShape
+	axesShapeNode := &tree.Node{
+		Name:            axesShape.Name,
+		IsNodeClickable: true,
 	}
+	axesShapeNode.OnClick = func(frontNode *tree.Node) {
+		stager.probeForm.FillUpFormFromGongstruct(axesShape, GetPointerToGongstructName[*AxesShape]())
+		stager.stage.Commit()
+	}
+	visibilityButton := &tree.Button{
+		Name:            "Hide",
+		Icon:            string(buttons.BUTTON_visibility_off),
+		ToolTipText:     "Hide from diagram",
+		HasToolTip:      true,
+		ToolTipPosition: tree.Right,
+		OnClick: func() {
+			axesShape.SetIsHidden(!axesShape.GetIsHidden())
+			stager.stage.Commit()
+		},
+	}
+	if axesShape.GetIsHidden() {
+		visibilityButton.Icon = string(buttons.BUTTON_visibility)
+		visibilityButton.Name = "Show"
+		visibilityButton.ToolTipText = "Show on diagram"
+	}
+	axesShapeNode.Buttons = append(axesShapeNode.Buttons, visibilityButton)
+	plantDiagramNode.Children = append(plantDiagramNode.Children, axesShapeNode)
+
 }
