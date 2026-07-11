745ceed0eb077d452fd1e3d811b1734d5c4eee4c
peugeot <thomaspeugeot@yahoo.fr>
2026-07-11 16:40:13 +0200

    fix: update component tracks to use ID and implement manual change detection in threejs-specific component

--- lib/split/ng-github.com-fullstack-lang-gong-lib-split/src/app/app.config.ts
+++ lib/split/ng-github.com-fullstack-lang-gong-lib-split/src/app/app.config.ts
@@ -1,4 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideNgtRenderer } from 'angular-three/dom';
 import { provideRouter, withHashLocation } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { APP_BASE_HREF } from '@angular/common'; // 1. Import this
@@ -14,6 +15,7 @@ export const appConfig: ApplicationConfig = {
     // 2. Set the base href dynamically to the exact current file path
     { provide: APP_BASE_HREF, useValue: window.location.pathname }, 
     
-    provideHttpClient(withInterceptors([wasmInterceptor]))
+    provideHttpClient(withInterceptors([wasmInterceptor])),
+    provideNgtRenderer()
   ]
 };
