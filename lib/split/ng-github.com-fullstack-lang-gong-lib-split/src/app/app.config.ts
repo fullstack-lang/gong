1312b9bef4db0ee7607d7e77f30058d091a53d1e
peugeot <thomaspeugeot@yahoo.fr>
2026-07-15 17:35:07 +0200

    refactor: update angular configuration with async animations and zone change detection providers

--- lib/split/ng-github.com-fullstack-lang-gong-lib-split/src/app/app.config.ts
+++ lib/split/ng-github.com-fullstack-lang-gong-lib-split/src/app/app.config.ts
@@ -3,22 +3,21 @@ import { provideRouter, withHashLocation } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { APP_BASE_HREF } from '@angular/common'; // 1. Import this
 import { provideNgtRenderer } from 'angular-three/dom';
-import { OverlayContainer } from '@angular/cdk/overlay';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
 import { wasmInterceptor } from './wasm.interceptor';
-import { CustomOverlayContainer } from './custom-overlay-container';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideAnimationsAsync(),
     provideRouter(routes, withHashLocation()), 
     
     // 2. Set the base href dynamically to the exact current file path
     { provide: APP_BASE_HREF, useValue: window.location.pathname }, 
     
     provideHttpClient(withInterceptors([wasmInterceptor])),
-    provideNgtRenderer(),
-    { provide: OverlayContainer, useClass: CustomOverlayContainer }
+    provideNgtRenderer()
   ]
 };
