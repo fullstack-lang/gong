213e44e83833f5dd2bff0d94c0e0ff3e63c266d9
peugeot <thomaspeugeot@yahoo.fr>
2026-07-16 08:41:31 +0200

    chore: regenerate stage data and update custom overlay container selectors

--- lib/threejs/ng-github.com-fullstack-lang-gong-lib-threejs/src/app/custom-overlay-container.ts
+++ lib/threejs/ng-github.com-fullstack-lang-gong-lib-threejs/src/app/custom-overlay-container.ts
@@ -4,9 +4,13 @@ import { OverlayContainer } from '@angular/cdk/overlay';
 @Injectable({ providedIn: 'root' })
 export class CustomOverlayContainer extends OverlayContainer {
   protected override _createContainer(): void {
+    console.log("CustomOverlayContainer (threejs): _createContainer called");
+    console.log("CustomOverlayContainer (threejs): document.body before append:", document.body);
     const container = document.createElement('div');
     container.classList.add('cdk-overlay-container');
     document.body.appendChild(container);
     this._containerElement = container;
+    console.log("CustomOverlayContainer (threejs): appended", container, "to body");
+    console.log("CustomOverlayContainer (threejs): stack trace:", new Error().stack);
   }
 }
